8c966dab0d77b776eae27c4e88bbb524af88074e
hanliyang <hanliyang@hygon.cn>
2021-04-08 08:07:08 -0400

    HYGON: KVM: SVM: Add KVM_SEV_SEND_UPDATE_VMSA command
    Mainline: KYLIN-only
    Severity: Important
    
    task: 216398
    
    The command is used for encrypting the VCPU register states of CSV2
    guest using the encryption context created with KVM_SEV_SEND_START.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: shitao <shitao@kylinos.cn>
    Change-Id: I6dbae4d0fae6c1b3c335338157284477df646f06
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/113189
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Reviewed-by: 李唯实 <liweishi@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -2228,6 +2228,14 @@ struct kvm_csv_command_batch {
 	__u64 csv_batch_list_uaddr;
 };
 
+struct kvm_csv_send_update_vmsa {
+	__u32 vcpu_id;
+	__u64 hdr_uaddr;
+	__u32 hdr_len;
+	__u64 trans_uaddr;
+	__u32 trans_len;
+};
+
 /* Available with KVM_CAP_MEMORY_ATTRIBUTES */
 #define KVM_SET_MEMORY_ATTRIBUTES              _IOW(KVMIO,  0xd2, struct kvm_memory_attributes)
 
