f9d82ecaa44ad67914d833573802b9bb5b6ed13d
Yabin Li <liyabin@hygon.cn>
2022-05-07 18:13:46 +0800

    HYGON: crypto: ccp: Support SM2 algorithm for hygon ccp.
    Mainline: KYLIN-only
    Severity: Important
    
    task: 216398
    
    In order to add SM2 driver for hygon ccp, relating to
    SM2_sign, SM2_verify, SM2_encrypt and SM2_decrypt.
    
    Signed-off-by: Yabin Li <liyabin@hygon.cn>
    Signed-off-by: yangdepei <yangdepei@hygon.cn>
    Signed-off-by: shitao <shitao@kylinos.cn>
    Change-Id: I6bea7df8bfa59b5b13c26b8324e897a002ec1ec4
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/112014
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Reviewed-by: 孟德慧 <mengdehui@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/crypto/ccp/ccp-crypto-main.c
+++ drivers/crypto/ccp/ccp-crypto-main.c
@@ -15,6 +15,7 @@
 #include <linux/scatterlist.h>
 #include <crypto/internal/hash.h>
 #include <crypto/internal/akcipher.h>
+#include <linux/machine_t.h>
 
 #include "ccp-crypto.h"
 
@@ -38,6 +39,10 @@ MODULE_PARM_DESC(des3_disable, "Disable use of 3DES - any non-zero value");
 static unsigned int rsa_disable;
 module_param(rsa_disable, uint, 0444);
 MODULE_PARM_DESC(rsa_disable, "Disable use of RSA - any non-zero value");
+
+static unsigned int sm_disable;
+module_param(sm_disable, uint, 0444);
+MODULE_PARM_DESC(sm_disable, "Disable use of SM2/SM3/SM4 - any non-zero value");
 
 /* List heads for the supported algorithms */
 static LIST_HEAD(hash_algs);
@@ -321,6 +326,17 @@ struct scatterlist *ccp_crypto_sg_table_add(struct sg_table *table,
 static int ccp_register_algs(void)
 {
 	int ret;
+
+#ifdef CONFIG_HYGON_GM
+	if (!sm_disable && is_vendor_hygon()) {
+		ret = ccp_register_sm2_hygon_algs(&akcipher_algs);
+		if (ret)
+			return ret;
+
+		/* Return on hygon platform */
+		return 0;
+	}
+#endif
 
 	if (!aes_disable) {
 		ret = ccp_register_aes_algs(&skcipher_algs);
