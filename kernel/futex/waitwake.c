a288e23298258826a3752b7e94e79560b4965cc1
Jens Axboe <axboe@kernel.dk>
2023-06-08 11:56:06 -0600

    futex: factor out the futex wake handling
    Mainline: 12a4be50aff30ee8f2c6a64020c82a4e997e8d6c
    From: v6.7-rc1
    Severity: Moderate
    
    task: 526909
    
    In preparation for having another waker that isn't futex_wake_mark(),
    add a wake handler in futex_q. No extra data is associated with the
    handler outside of struct futex_q itself. futex_wake_mark() is defined as
    the standard wakeup helper, now set through futex_q_init like other
    defaults.
    
    Normal sync futex waiting relies on wake_q holding tasks that should
    be woken up. This is what futex_wake_mark() does, it'll unqueue the
    futex and add the associated task to the wake queue. For async usage of
    futex waiting, rather than having tasks sleeping on the futex, we'll
    need to deal with a futex wake differently. For the planned io_uring
    case, that means posting a completion event for the task in question.
    Having a definable wake handler can help support that use case.
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit 12a4be50aff30ee8f2c6a64020c82a4e997e8d6c)
    
    K2CI-Arch: All
    Change-Id: Id38d9b524ec691bd4bb01e05bb7adf6020efec46
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151200
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/futex/waitwake.c
+++ kernel/futex/waitwake.c
@@ -178,7 +178,7 @@ int futex_wake(u32 __user *uaddr, unsigned int flags, int nr_wake, u32 bitset)
 			if (!(this->bitset & bitset))
 				continue;
 
-			futex_wake_mark(&wake_q, this);
+			this->wake(&wake_q, this);
 			if (++ret >= nr_wake)
 				break;
 		}
@@ -293,7 +293,7 @@ retry_private:
 				ret = -EINVAL;
 				goto out_unlock;
 			}
-			futex_wake_mark(&wake_q, this);
+			this->wake(&wake_q, this);
 			if (++ret >= nr_wake)
 				break;
 		}
@@ -307,7 +307,7 @@ retry_private:
 					ret = -EINVAL;
 					goto out_unlock;
 				}
-				futex_wake_mark(&wake_q, this);
+				this->wake(&wake_q, this);
 				if (++op_ret >= nr_wake2)
 					break;
 			}
