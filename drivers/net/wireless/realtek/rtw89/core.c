e4e465106f0620dd16a762adb07df0f3cd8ce08e
Zong-Zhe Yang <kevin_yang@realtek.com>
2023-09-08 11:11:42 +0800

    wifi: rtw89: mcc: track beacon offset and update when needed
    In MCC STA+GC mode, the offset between TBTTs of remote AP and remote GO
    might change. If the change is larger than tolerance, we should update
    MCC after re-calculating parameters for new things. So, we track that in
    rtw89_track_work() now. And, we add MCC update flow to tell FW either to
    change durations of roles or to replace entire pattern according to how
    MCC plans BT slot.
    
    Change-Id: I2342628df61e59cfb0b17e2e8047bda90f2bb80e
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230908031145.20931-6-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/core.c
+++ drivers/net/wireless/realtek/rtw89/core.c
@@ -2731,6 +2731,7 @@ static void rtw89_track_work(struct work_struct *work)
 	rtw89_phy_ul_tb_ctrl_track(rtwdev);
 	rtw89_tas_track(rtwdev);
 	rtw89_core_rfkill_poll(rtwdev, false);
+	rtw89_chanctx_track(rtwdev);
 
 	if (rtwdev->lps_enabled && !rtwdev->btc.lps)
 		rtw89_enter_lps_track(rtwdev);
