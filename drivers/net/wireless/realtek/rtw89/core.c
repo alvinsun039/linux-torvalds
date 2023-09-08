404054856bcdf1fd36ea47ce5252db69f2dc8d9a
Zong-Zhe Yang <kevin_yang@realtek.com>
2023-09-08 11:11:41 +0800

    wifi: rtw89: mcc: update role bitmap when changed
    Each MCC (multi-channel concurrency) role maintains a bitmap of mac IDs.
    The bitmap is supposed to contain the two points below.
    * mac ID of itself
    * mac ID(s) of STA(s) connecting to it
    Under STA+GC mode, the bitmaps of both roles should not change. However,
    under STA+GO mode, the bitmap of GO may change due to P2P clients which
    connect/disconnect to/from it.
    
    FW controls (TDMA-based) MCC things via mac IDs in bitmap of each role.
    For example, mac IDs are required by FW when it wants to pause role1's
    TX in role0 slot.
    
    So, to sync between driver and FW, we update the new mac ID bitmap of GO
    to FW once it's changed.
    
    Change-Id: Ie5ce14973af719b8ccabb7b73c7060475e1866a8
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20230908031145.20931-5-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/core.c
+++ drivers/net/wireless/realtek/rtw89/core.c
@@ -2943,6 +2943,8 @@ int rtw89_core_sta_add(struct rtw89_dev *rtwdev,
 			rtw89_warn(rtwdev, "failed to send h2c role info\n");
 			return ret;
 		}
+
+		rtw89_queue_chanctx_change(rtwdev, RTW89_CHANCTX_REMOTE_STA_CHANGE);
 	}
 
 	return 0;
@@ -3108,6 +3110,8 @@ int rtw89_core_sta_remove(struct rtw89_dev *rtwdev,
 			rtw89_warn(rtwdev, "failed to send h2c role info\n");
 			return ret;
 		}
+
+		rtw89_queue_chanctx_change(rtwdev, RTW89_CHANCTX_REMOTE_STA_CHANGE);
 	}
 
 	return 0;
