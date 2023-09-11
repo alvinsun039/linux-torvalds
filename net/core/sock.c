60673c29cc66edf87c01c73d5ce3ce650580b0bc
Eric Dumazet <edumazet@google.com>
2023-09-11 17:05:28 +0000

    tcp: no longer release socket ownership in tcp_release_cb()
    Mainline: b49d252216e4f9e3030865b79d5ca16f050e4a19
    From: v6.7-rc1
    Severity: Low
    
    This partially reverts c3f9b01849ef ("tcp: tcp_release_cb()
    should release socket ownership").
    
    prequeue has been removed by Florian in commit e7942d0633c4
    ("tcp: remove prequeue support")
    
    __tcp_checksum_complete_user() being gone, we no longer
    have to release socket ownership in tcp_release_cb().
    
    This is a prereq for third patch in the series
    ("net: call prot->release_cb() when processing backlog").
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    (backported from commit b49d252216e4f9e3030865b79d5ca16f050e4a19)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: If2b273e761dcf76a29e119b2c4d66362c7050c95
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/126127
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- net/core/sock.c
+++ net/core/sock.c
@@ -3529,9 +3529,6 @@ void release_sock(struct sock *sk)
 	if (sk->sk_backlog.tail)
 		__release_sock(sk);
 
-	/* Warning : release_cb() might need to release sk ownership,
-	 * ie call sock_release_ownership(sk) before us.
-	 */
 	if (sk->sk_prot->release_cb)
 		sk->sk_prot->release_cb(sk);
 
