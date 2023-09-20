cec3ec40dd4e76af056d9c9d0ec2e7eca11eec77
Eric Dumazet <edumazet@google.com>
2023-09-20 17:29:42 +0000

    tcp: constify tcp_rto_min() and tcp_rto_min_us() argument
    Mainline: f68a181fcd3b6a22236794813d5c9e5a62efd2b4
    From: v6.7-rc1
    Severity: Low
    
    Make clear these functions do not change any field from TCP socket.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (backported from commit f68a181fcd3b6a22236794813d5c9e5a62efd2b4)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ic56ccfee19e0b7f65c25222d45a1159601861d88
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/129391
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -729,7 +729,7 @@ static inline void tcp_fast_path_check(struct sock *sk)
 u32 tcp_delack_max(const struct sock *sk);
 
 /* Compute the actual rto_min value */
-static inline u32 tcp_rto_min(struct sock *sk)
+static inline u32 tcp_rto_min(const struct sock *sk)
 {
 	const struct dst_entry *dst = __sk_dst_get(sk);
 	u32 rto_min = inet_csk(sk)->icsk_rto_min;
@@ -739,7 +739,7 @@ static inline u32 tcp_rto_min(struct sock *sk)
 	return rto_min;
 }
 
-static inline u32 tcp_rto_min_us(struct sock *sk)
+static inline u32 tcp_rto_min_us(const struct sock *sk)
 {
 	return jiffies_to_usecs(tcp_rto_min(sk));
 }
