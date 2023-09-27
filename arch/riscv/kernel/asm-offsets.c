8f182e67d73a0683e44e4f72059ff2da744bc364
Sami Tolvanen <samitolvanen@google.com>
2023-09-27 22:48:00 +0000

    riscv: Deduplicate IRQ stack switching
    Mainline: 82982fdd5133fa7e0b2dfaf746d18d6f29922b82
    From: v6.7-rc1
    Severity: Moderate
    
    task: 535069
    
    With CONFIG_IRQ_STACKS, we switch to a separate per-CPU IRQ stack
    before calling handle_riscv_irq or __do_softirq. We currently
    have duplicate inline assembly snippets for stack switching in
    both code paths. Now that we can access per-CPU variables in
    assembly, implement call_on_irq_stack in assembly, and use that
    instead of redundant inline assembly.
    
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Link: https://lore.kernel.org/r/20230927224757.1154247-10-samitolvanen@google.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    (backported from commit 82982fdd5133fa7e0b2dfaf746d18d6f29922b82)
    
    Conflicts:
    	arch/riscv/kernel/asm-offsets.c
    	arch/riscv/kernel/irq.c
    
    Change-Id: I9c074a25d3392e0b88eb36925d2f1b3a75019e5f
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/131256
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- arch/riscv/kernel/asm-offsets.c
+++ arch/riscv/kernel/asm-offsets.c
@@ -13,6 +13,7 @@
 #include <asm/thread_info.h>
 #include <asm/ptrace.h>
 #include <asm/cpu_ops_sbi.h>
+#include <asm/stacktrace.h>
 #include <asm/suspend.h>
 
 void asm_offsets(void);
@@ -479,6 +480,10 @@ void asm_offsets(void)
 	OFFSET(KERNEL_MAP_VIRT_ADDR, kernel_mapping, virt_addr);
 	OFFSET(SBI_HART_BOOT_TASK_PTR_OFFSET, sbi_hart_boot_data, task_ptr);
 	OFFSET(SBI_HART_BOOT_STACK_PTR_OFFSET, sbi_hart_boot_data, stack_ptr);
+
+	DEFINE(STACKFRAME_SIZE_ON_STACK, ALIGN(sizeof(struct stackframe), STACK_ALIGN));
+	OFFSET(STACKFRAME_FP, stackframe, fp);
+	OFFSET(STACKFRAME_RA, stackframe, ra);
 
 #ifdef CONFIG_DYNAMIC_FTRACE_WITH_ARGS
 	DEFINE(FREGS_SIZE_ON_STACK, ALIGN(sizeof(struct __arch_ftrace_regs), STACK_ALIGN));
