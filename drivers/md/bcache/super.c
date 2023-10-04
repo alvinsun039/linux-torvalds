b24e9f8123e95b54ea5bca326543b37967e12646
Jan Kara <jack@suse.cz>
2023-10-04 11:37:57 +0200

    bcache: Fixup error handling in register_cache()
    Mainline: b3856da7906257a80a764d3dfc6b25e876a4403c
    From: v6.7-rc1
    Severity: Low
    
    task: 522091
    
    Coverity has noticed that the printing of error message in
    register_cache() uses already freed bdev_handle to get to bdev. In fact
    the problem has been there even before commit "bcache: Convert to
    bdev_open_by_path()" just a bit more subtle one - cache object itself
    could have been freed by the time we looked at ca->bdev and we don't
    hold any reference to bdev either so even that could in principle go
    away (due to device unplug or similar). Fix all these problems by
    printing the error message before closing the bdev.
    
    Fixes: dc893f51d24a ("bcache: Convert to bdev_open_by_path()")
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20231004093757.11560-1-jack@suse.cz
    Asked-by: Coly Li <colyli@suse.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit b3856da7906257a80a764d3dfc6b25e876a4403c)
    
    Change-Id: Icf7869722c4ff5c3c6b9e07a51bfe35ced25b015
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/112202
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -2362,6 +2362,13 @@ static int register_cache(struct cache_sb *sb, struct cache_sb_disk *sb_disk,
 
 	ret = cache_alloc(ca);
 	if (ret != 0) {
+		if (ret == -ENOMEM)
+			err = "cache_alloc(): -ENOMEM";
+		else if (ret == -EPERM)
+			err = "cache_alloc(): cache device is too small";
+		else
+			err = "cache_alloc(): unknown error";
+		pr_notice("error %pg: %s\n", bdev_handle->bdev, err);
 		/*
 		 * If we failed here, it means ca->kobj is not initialized yet,
 		 * kobject_put() won't be called and there is no chance to
@@ -2369,17 +2376,12 @@ static int register_cache(struct cache_sb *sb, struct cache_sb_disk *sb_disk,
 		 * we explicitly call bdev_release() here.
 		 */
 		bdev_release(bdev_handle);
-		if (ret == -ENOMEM)
-			err = "cache_alloc(): -ENOMEM";
-		else if (ret == -EPERM)
-			err = "cache_alloc(): cache device is too small";
-		else
-			err = "cache_alloc(): unknown error";
-		goto err;
+		return ret;
 	}
 
 	if (kobject_add(&ca->kobj, bdev_kobj(bdev_handle->bdev), "bcache")) {
-		err = "error calling kobject_add";
+		pr_notice("error %pg: error calling kobject_add\n",
+			  bdev_handle->bdev);
 		ret = -ENOMEM;
 		goto out;
 	}
@@ -2397,11 +2399,6 @@ static int register_cache(struct cache_sb *sb, struct cache_sb_disk *sb_disk,
 
 out:
 	kobject_put(&ca->kobj);
-
-err:
-	if (err)
-		pr_notice("error %pg: %s\n", ca->bdev_handle->bdev, err);
-
 	return ret;
 }
 
