096b65fe83a6291c07d95c1cc897cdaa2e93e935
Hans de Goede <hdegoede@redhat.com>
2023-10-14 22:53:11 +0200

    spi: Export acpi_spi_find_controller_by_adev()
    Mainline: a8ecbc54165fca767e75a82372a7be3810c667cf
    From: v6.7-rc1
    Severity: Low
    
    task: 345777
    
    Export acpi_spi_find_controller_by_adev() so that ACPI glue code which
    wants to dynamically create a spi_device using acpi_spi_device_alloc() or
    spi_new_device() on a controller, to which the code does not already have
    a reference, can find the controller.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20231014205314.59333-2-hdegoede@redhat.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (backported from commit a8ecbc54165fca767e75a82372a7be3810c667cf)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I2a950ddc53e2fc32264acf8a26e154248287f1c1
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/130350
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -2535,8 +2535,6 @@ static void acpi_spi_parse_apple_properties(struct acpi_device *dev,
 		lookup->mode |= SPI_CPHA;
 }
 
-static struct spi_controller *acpi_spi_find_controller_by_adev(struct acpi_device *adev);
-
 static int acpi_spi_add_resource(struct acpi_resource *ares, void *data)
 {
 	struct acpi_spi_lookup *lookup = data;
@@ -4556,7 +4554,7 @@ static int spi_acpi_controller_match(struct device *dev, const void *data)
 	return ACPI_COMPANION(dev->parent) == data;
 }
 
-static struct spi_controller *acpi_spi_find_controller_by_adev(struct acpi_device *adev)
+struct spi_controller *acpi_spi_find_controller_by_adev(struct acpi_device *adev)
 {
 	struct device *dev;
 
@@ -4570,6 +4568,7 @@ static struct spi_controller *acpi_spi_find_controller_by_adev(struct acpi_devic
 
 	return container_of(dev, struct spi_controller, dev);
 }
+EXPORT_SYMBOL_GPL(acpi_spi_find_controller_by_adev);
 
 static struct spi_device *acpi_spi_find_device_by_adev(struct acpi_device *adev)
 {
