219eab8ea07933a129deb3094a91b5fefaf614a5
Eric Dumazet <edumazet@google.com>
2023-10-20 12:57:41 +0000

    tcp: rename tcp_skb_timestamp()
    Mainline: d1a02ed66fe62aa2edd77bd54e270ebc33bd12ff
    From: v6.7-rc1
    Severity: Low
    
    This helper returns a 32bit TCP TSval from skb->tstamp.
    
    As we are going to support usec or ms units soon, rename it
    to tcp_skb_timestamp_ts() and add a boolean to select the unit.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (backported from commit d1a02ed66fe62aa2edd77bd54e270ebc33bd12ff)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Id53dc92c31997adf0d1876354953ada0c7ad9c85
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/69511
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -848,7 +848,7 @@ static unsigned int tcp_syn_options(struct sock *sk, struct sk_buff *skb,
 
 	if (likely(timestamps)) {
 		opts->options |= OPTION_TS;
-		opts->tsval = tcp_skb_timestamp(skb) + tp->tsoffset;
+		opts->tsval = tcp_skb_timestamp_ts(false, skb) + tp->tsoffset;
 		opts->tsecr = tp->rx_opt.ts_recent;
 		remaining -= TCPOLEN_TSTAMP_ALIGNED;
 	}
@@ -937,7 +937,7 @@ static unsigned int tcp_synack_options(const struct sock *sk,
 	}
 	if (likely(ireq->tstamp_ok)) {
 		opts->options |= OPTION_TS;
-		opts->tsval = tcp_skb_timestamp(skb) + tcp_rsk(req)->ts_off;
+		opts->tsval = tcp_skb_timestamp_ts(false, skb) + tcp_rsk(req)->ts_off;
 		opts->tsecr = READ_ONCE(req->ts_recent);
 		remaining -= TCPOLEN_TSTAMP_ALIGNED;
 	}
@@ -993,7 +993,7 @@ static unsigned int tcp_established_options(struct sock *sk, struct sk_buff *skb
 
 	if (likely(tp->rx_opt.tstamp_ok)) {
 		opts->options |= OPTION_TS;
-		opts->tsval = skb ? tcp_skb_timestamp(skb) + tp->tsoffset : 0;
+		opts->tsval = skb ? tcp_skb_timestamp_ts(false, skb) + tp->tsoffset : 0;
 		opts->tsecr = tp->rx_opt.ts_recent;
 		size += TCPOLEN_TSTAMP_ALIGNED;
 	}
@@ -3441,7 +3441,7 @@ int tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb, int segs)
 
 	/* Save stamp of the first (attempted) retransmit. */
 	if (!tp->retrans_stamp)
-		tp->retrans_stamp = tcp_skb_timestamp(skb);
+		tp->retrans_stamp = tcp_skb_timestamp_ts(false, skb);
 
 	if (tp->undo_retrans < 0)
 		tp->undo_retrans = 0;
--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -475,7 +475,7 @@ static bool tcp_rtx_probe0_timed_out(const struct sock *sk,
 	s32 rcv_delta;
 
 	rtx_delta = (u32)msecs_to_jiffies(tcp_time_stamp(tp) -
-			(tp->retrans_stamp ?: tcp_skb_timestamp(skb)));
+			(tp->retrans_stamp ?: tcp_skb_timestamp_ts(false, skb)));
 
 	if (user_timeout) {
 		/* If user application specified a TCP_USER_TIMEOUT,
@@ -546,7 +546,7 @@ void tcp_retransmit_timer(struct sock *sk)
 		struct inet_sock *inet = inet_sk(sk);
 		u32 rtx_delta;
 
-		rtx_delta = tcp_time_stamp(tp) - (tp->retrans_stamp ?: tcp_skb_timestamp(skb));
+		rtx_delta = tcp_time_stamp(tp) - (tp->retrans_stamp ?: tcp_skb_timestamp_ts(false, skb));
 		if (sk->sk_family == AF_INET) {
 			net_dbg_ratelimited("Probing zero-window on %pI4:%u/%u, seq=%u:%u, recv %ums ago, lasting %ums\n",
 				&inet->inet_daddr, ntohs(inet->inet_dport),
