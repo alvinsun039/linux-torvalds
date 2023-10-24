8c93d99dfea087c5fb292492056b9e7a9ea55c46
Daniel Borkmann <daniel@iogearbox.net>
2023-10-24 23:48:58 +0200

    netkit, bpf: Add bpf programmable net device
    Mainline: 35dfaad7188cdc043fde31709c796f5a692ba2bd
    From: v6.7-rc1
    Severity: Moderate
    
    Task: #205697
    
    This work adds a new, minimal BPF-programmable device called "netkit"
    (former PoC code-name "meta") we recently presented at LSF/MM/BPF. The
    core idea is that BPF programs are executed within the drivers xmit routine
    and therefore e.g. in case of containers/Pods moving BPF processing closer
    to the source.
    
    One of the goals was that in case of Pod egress traffic, this allows to
    move BPF programs from hostns tcx ingress into the device itself, providing
    earlier drop or forward mechanisms, for example, if the BPF program
    determines that the skb must be sent out of the node, then a redirect to
    the physical device can take place directly without going through per-CPU
    backlog queue. This helps to shift processing for such traffic from softirq
    to process context, leading to better scheduling decisions/performance (see
    measurements in the slides).
    
    In this initial version, the netkit device ships as a pair, but we plan to
    extend this further so it can also operate in single device mode. The pair
    comes with a primary and a peer device. Only the primary device, typically
    residing in hostns, can manage BPF programs for itself and its peer. The
    peer device is designated for containers/Pods and cannot attach/detach
    BPF programs. Upon the device creation, the user can set the default policy
    to 'pass' or 'drop' for the case when no BPF program is attached.
    
    Additionally, the device can be operated in L3 (default) or L2 mode. The
    management of BPF programs is done via bpf_mprog, so that multi-attach is
    supported right from the beginning with similar API and dependency controls
    as tcx. For details on the latter see commit 053c8e1f235d ("bpf: Add generic
    attach/detach/query API for multi-progs"). tc BPF compatibility is provided,
    so that existing programs can be easily migrated.
    
    Going forward, we plan to use netkit devices in Cilium as the main device
    type for connecting Pods. They will be operated in L3 mode in order to
    simplify a Pod's neighbor management and the peer will operate in default
    drop mode, so that no traffic is leaving between the time when a Pod is
    brought up by the CNI plugin and programs attached by the agent.
    Additionally, the programs we attach via tcx on the physical devices are
    using bpf_redirect_peer() for inbound traffic into netkit device, hence the
    latter is also supporting the ndo_get_peer_dev callback. Similarly, we use
    bpf_redirect_neigh() for the way out, pushing from netkit peer to phys device
    directly. Also, BIG TCP is supported on netkit device. For the follow-up
    work in single device mode, we plan to convert Cilium's cilium_host/_net
    devices into a single one.
    
    An extensive test suite for checking device operations and the BPF program
    and link management API comes as BPF selftests in this series.
    
    Co-developed-by: Nikolay Aleksandrov <razor@blackwall.org>
    Signed-off-by: Nikolay Aleksandrov <razor@blackwall.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Stanislav Fomichev <sdf@google.com>
    Acked-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://github.com/borkmann/iproute2/tree/pr/netkit
    Link: http://vger.kernel.org/bpfconf2023_material/tcx_meta_netdev_borkmann.pdf (24ff.)
    Link: https://lore.kernel.org/r/20231024214904.29825-2-daniel@iogearbox.net
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    
    K2CI-Arch: All
    Signed-off-by: Yun Lu <luyun@kylinos.cn>
    Change-Id: I7e49b37666b018a07f60a69e713999f1e679b03c
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/108553
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -1040,6 +1040,8 @@ enum bpf_attach_type {
 	BPF_TCX_INGRESS,
 	BPF_TCX_EGRESS,
 	BPF_TRACE_UPROBE_MULTI,
+	BPF_NETKIT_PRIMARY,
+	BPF_NETKIT_PEER,
 	__MAX_BPF_ATTACH_TYPE
 };
 
@@ -1059,6 +1061,7 @@ enum bpf_link_type {
 	BPF_LINK_TYPE_NETFILTER = 10,
 	BPF_LINK_TYPE_TCX = 11,
 	BPF_LINK_TYPE_UPROBE_MULTI = 12,
+	BPF_LINK_TYPE_NETKIT = 13,
 	MAX_BPF_LINK_TYPE,
 };
 
@@ -1644,6 +1647,13 @@ union bpf_attr {
 				__u32		flags;
 				__u32		pid;
 			} uprobe_multi;
+			struct {
+				union {
+					__u32	relative_fd;
+					__u32	relative_id;
+				};
+				__u64		expected_revision;
+			} netkit;
 		};
 	} link_create;
 
@@ -6563,6 +6573,10 @@ struct bpf_link_info {
 			__u32 ifindex;
 			__u32 attach_type;
 		} tcx;
+		struct {
+			__u32 ifindex;
+			__u32 attach_type;
+		} netkit;
 	};
 } __attribute__((aligned(8)));
 
