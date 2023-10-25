8b299f0958ff8955ff430d3de6c410091d280454
Geliang Tang <geliang.tang@suse.com>
2023-10-25 16:37:08 -0700

    mptcp: move sk assignment statement ahead
    Mainline: a6c85fc61c088c7ef43ba81e80b48c263a80602a
    From: v6.7-rc1
    Severity: Moderate
    
    task: 201243
    
    If we move the sk assignment statement ahead in mptcp_nl_cmd_sf_create()
    or mptcp_nl_cmd_sf_destroy(), right after the msk null-check statements,
    sk can be used after the create_err or destroy_err labels instead of
    open-coding it again.
    
    Reviewed-by: Matthieu Baerts <matttbe@kernel.org>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Signed-off-by: Mat Martineau <martineau@kernel.org>
    Link: https://lore.kernel.org/r/20231025-send-net-next-20231025-v1-7-db8f25f798eb@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    (backported from commit a6c85fc61c088c7ef43ba81e80b48c263a80602a)
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Change-Id: I9ef1cf9266f5f17b606e1a9f1cb22e1156366b84
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/98507
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- net/mptcp/pm_userspace.c
+++ net/mptcp/pm_userspace.c
@@ -346,6 +346,8 @@ int mptcp_pm_nl_subflow_create_doit(struct sk_buff *skb, struct genl_info *info)
 		return err;
 	}
 
+	sk = (struct sock *)msk;
+
 	if (!mptcp_pm_is_userspace(msk)) {
 		GENL_SET_ERR_MSG(info, "invalid request; userspace PM not selected");
 		goto create_err;
@@ -362,8 +364,6 @@ int mptcp_pm_nl_subflow_create_doit(struct sk_buff *skb, struct genl_info *info)
 		NL_SET_ERR_MSG_ATTR(info->extack, raddr, "error parsing remote addr");
 		goto create_err;
 	}
-
-	sk = (struct sock *)msk;
 
 	if (!mptcp_pm_addr_families_match(sk, &addr_l, &addr_r)) {
 		GENL_SET_ERR_MSG(info, "families mismatch");
@@ -392,7 +392,7 @@ int mptcp_pm_nl_subflow_create_doit(struct sk_buff *skb, struct genl_info *info)
 	spin_unlock_bh(&msk->pm.lock);
 
  create_err:
-	sock_put((struct sock *)msk);
+	sock_put(sk);
 	return err;
 }
 
@@ -469,6 +469,8 @@ int mptcp_pm_nl_subflow_destroy_doit(struct sk_buff *skb, struct genl_info *info
 		return err;
 	}
 
+	sk = (struct sock *)msk;
+
 	if (!mptcp_pm_is_userspace(msk)) {
 		GENL_SET_ERR_MSG(info, "invalid request; userspace PM not selected");
 		goto destroy_err;
@@ -498,7 +500,6 @@ int mptcp_pm_nl_subflow_destroy_doit(struct sk_buff *skb, struct genl_info *info
 		goto destroy_err;
 	}
 
-	sk = (struct sock *)msk;
 	lock_sock(sk);
 	ssk = mptcp_nl_find_ssk(msk, &addr_l, &addr_r);
 	if (ssk) {
@@ -518,7 +519,7 @@ int mptcp_pm_nl_subflow_destroy_doit(struct sk_buff *skb, struct genl_info *info
 	release_sock(sk);
 
 destroy_err:
-	sock_put((struct sock *)msk);
+	sock_put(sk);
 	return err;
 }
 
