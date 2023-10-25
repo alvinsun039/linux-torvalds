70bc73f403b5cc48158d2e94f6314b73223a10f0
Xin Jiang <jiangxin@hygon.cn>
2023-10-25 10:01:57 +0800

    HYGON: KVM: SVM: CSV: Add KVM_CSV3_SEND_ENCRYPT_DATA command
    Mainline: KYLIN-only
    Severity: Important
    
    task: 216398
    
    The command is used for encrypting the guest memory page using the
    encryption context.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: shitao <shitao@kylinos.cn>
    Change-Id: I2281072eb6c9f47833a3677c91106d22b28fdc64
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/118868
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -2262,6 +2262,7 @@ enum csv3_cmd_id {
 	KVM_CSV3_INIT = KVM_CSV3_NR_MIN,
 	KVM_CSV3_LAUNCH_ENCRYPT_DATA,
 	KVM_CSV3_LAUNCH_ENCRYPT_VMCB,
+	KVM_CSV3_SEND_ENCRYPT_DATA,
 
 	KVM_CSV3_NR_MAX,
 };
@@ -2294,6 +2295,15 @@ struct kvm_create_guest_memfd {
 	__u64 size;
 	__u64 flags;
 	__u64 reserved[6];
+};
+
+struct kvm_csv3_send_encrypt_data {
+	__u64 hdr_uaddr;
+	__u32 hdr_len;
+	__u64 guest_addr_data;
+	__u32 guest_addr_len;
+	__u64 trans_uaddr;
+	__u32 trans_len;
 };
 
 #endif /* __LINUX_KVM_H */
