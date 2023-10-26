1ed479d235da3f3428820c25a955fa2ff48c2ca5
Ping-Ke Shih <pkshih@realtek.com>
2023-10-26 20:00:47 +0800

    wifi: rtw89: pci: define PCI ring address for WiFi 7 chips
    PCI rings are used to DMA TX/RX packets. The address of WiFi 7 chips are
    different from previous ones, so add them according to hardware design.
    Another difference is that driver doesn't need to configure BD (buffer
    descriptor) RAM table, which is used by hardware to fetch BD ahead before
    fetching whole TX data.
    
    A TX ring contains numbers of TX BD (e.g. 512):
    
       TX BD (buffer descriptor; continual memory)
        +---+---+---+---+     +---+
        |   |   |   |   | ... |   |
        +-|-+---+---+---+     +---+
          |
          | point to TX WD (WiFi descriptor; metadata of a skb data)
          v
        +------+
        |      |
        |      |
        +-|----+
          |
          | point to a skb data
          v
        +------+
        |      |
        |      |
        +------+
    
    Change-Id: Ic3870d8289a4870509efa9291ca597de0ac16771
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20231026120049.9187-3-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/rtw8922ae.c
+++ drivers/net/wireless/realtek/rtw89/rtw8922ae.c
@@ -44,6 +44,7 @@ static const struct rtw89_pci_info rtw8922a_pci_info = {
 	.cpwm_addr		= R_BE_PCIE_CRPWM,
 	.tx_dma_ch_mask		= 0,
 	.bd_idx_addr_low_power	= NULL,
+	.dma_addr_set		= &rtw89_pci_ch_dma_addr_set_be,
 	.bd_ram_table		= NULL,
 
 	.ltr_set		= rtw89_pci_ltr_set_v2,
