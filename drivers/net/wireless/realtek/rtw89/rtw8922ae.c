a210bd9ca845a6e65378dffea03a2bce48aa1b6e
Ping-Ke Shih <pkshih@realtek.com>
2023-11-01 15:21:48 +0800

    wifi: rtw89: pci: add LTR v2 for WiFi 7 chip
    Mainline: e24ae0f07625d51934e9367e155510c71b088a2b
    From: v6.8-rc1
    Severity: Moderate
    
    task: 496179
    
    PCI LTR (Latency Tolerance Reporting) is a capability to yield expected
    power consumption, and we configure the parameters according to design.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20231101072149.21997-5-pkshih@realtek.com
    
    (backported from commit e24ae0f07625d51934e9367e155510c71b088a2b)
    
    K2CI-Arch: All
    Change-Id: I2e6e7a335cb8c8eec9a7ed9fa44a4c8a2b418e48
    Signed-off-by: Yi Cong <yicong@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/91947
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 王琦 <wangqi@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/net/wireless/realtek/rtw89/rtw8922ae.c
+++ drivers/net/wireless/realtek/rtw89/rtw8922ae.c
@@ -45,6 +45,7 @@ static const struct rtw89_pci_info rtw8922a_pci_info = {
 	.bd_idx_addr_low_power	= NULL,
 	.bd_ram_table		= NULL,
 
+	.ltr_set		= rtw89_pci_ltr_set_v2,
 	.fill_txaddr_info	= rtw89_pci_fill_txaddr_info_v1,
 };
 
