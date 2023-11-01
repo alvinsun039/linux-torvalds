0c54258f967ff1810f275c5a54ae445b510f7f2f
Liam R. Howlett <Liam.Howlett@oracle.com>
2023-11-01 13:16:25 -0400

    maple_tree: separate ma_state node from status
    Mainline: 067311d33e650adfe7ae23765959ddcc1ba18510
    From: v6.8-rc1
    Severity: Moderate
    
    task: 219076
    
    The maple tree node is overloaded to keep status as well as the active
    node.  This, unfortunately, results in a re-walk on underflow or overflow.
    Since the maple state has room, the status can be placed in its own enum
    in the structure.  Once an underflow/overflow is detected, certain modes
    can restore the status to active and others may need to re-walk just that
    one node to see the entry.
    
    The status being an enum has the benefit of detecting unhandled status in
    switch statements.
    
    [Liam.Howlett@oracle.com: fix comments about MAS_*]
      Link: https://lkml.kernel.org/r/20231106154124.614247-1-Liam.Howlett@oracle.com
    [Liam.Howlett@oracle.com: update forking to separate maple state and node]
      Link: https://lkml.kernel.org/r/20231106154551.615042-1-Liam.Howlett@oracle.com
    [Liam.Howlett@oracle.com: fix mas_prev() state separation code]
      Link: https://lkml.kernel.org/r/20231207193319.4025462-1-Liam.Howlett@oracle.com
    Link: https://lkml.kernel.org/r/20231101171629.3612299-9-Liam.Howlett@oracle.com
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Cc: Peng Zhang <zhangpeng.00@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (backported from commit 067311d33e650adfe7ae23765959ddcc1ba18510)
    
    K2CI-Arch: All
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Change-Id: Ib1b715cc83dcd704bb6f1e595325c1f1eb1bdcc5
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/112823
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1040,7 +1040,8 @@ struct vma_iterator {
 		.mas = {						\
 			.tree = &(__mm)->mm_mt,				\
 			.index = __addr,				\
-			.node = MAS_START,				\
+			.node = NULL,					\
+			.status = ma_start,				\
 		},							\
 	}
 
