92fea82d07ff056bb005f3f7df17ab7180deb33a
Breno Leitao <leitao@debian.org>
2023-11-21 08:07:37 -0800

    x86/bugs: Rename CONFIG_RETHUNK              => CONFIG_MITIGATION_RETHUNK
    Mainline: 0911b8c52c4d68c57d02f172daa55a42bce703f0
    From: v6.9-rc1
    Severity: Moderate
    
    task: 205742
    
    Step 10/10 of the namespace unification of CPU mitigations related Kconfig options.
    
    [ mingo: Added one more case. ]
    
    Suggested-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/r/20231121160740.1249350-11-leitao@debian.org
    
    (backported from commit 0911b8c52c4d68c57d02f172daa55a42bce703f0)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I95ad8e71085ffb04433b8eac5eb5ac21de8866cd
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/115094
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -298,7 +298,7 @@
  * where we have a stack but before any RET instruction.
  */
 .macro __UNTRAIN_RET ibpb_feature, call_depth_insns
-#if defined(CONFIG_RETHUNK) || defined(CONFIG_MITIGATION_IBPB_ENTRY)
+#if defined(CONFIG_MITIGATION_RETHUNK) || defined(CONFIG_MITIGATION_IBPB_ENTRY)
 	VALIDATE_UNRET_END
 	CALL_UNTRAIN_RET
 	ALTERNATIVE_2 "",						\
@@ -361,7 +361,7 @@ extern retpoline_thunk_t __x86_indirect_thunk_array[];
 extern retpoline_thunk_t __x86_indirect_call_thunk_array[];
 extern retpoline_thunk_t __x86_indirect_jump_thunk_array[];
 
-#ifdef CONFIG_RETHUNK
+#ifdef CONFIG_MITIGATION_RETHUNK
 extern void __x86_return_thunk(void);
 #else
 static inline void __x86_return_thunk(void) {}
