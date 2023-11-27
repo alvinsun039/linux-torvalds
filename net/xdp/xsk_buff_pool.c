6c259bc1a001aa68810157cc4749611575581b42
Stanislav Fomichev <sdf@google.com>
2023-11-27 11:03:07 -0800

    xsk: Support tx_metadata_len
    Mainline: 341ac980eab90ac1f6c22ee9f9da83ed9604d899
    From: v6.8-rc1
    Severity: Moderate
    
    task: 425042
    
    For zerocopy mode, tx_desc->addr can point to an arbitrary offset
    and carry some TX metadata in the headroom. For copy mode, there
    is no way currently to populate skb metadata.
    
    Introduce new tx_metadata_len umem config option that indicates how many
    bytes to treat as metadata. Metadata bytes come prior to tx_desc address
    (same as in RX case).
    
    The size of the metadata has mostly the same constraints as XDP:
    - less than 256 bytes
    - 8-byte aligned (compared to 4-byte alignment on xdp, due to 8-byte
      timestamp in the completion)
    - non-zero
    
    This data is not interpreted in any way right now.
    
    Reviewed-by: Song Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    Link: https://lore.kernel.org/r/20231127190319.1190813-2-sdf@google.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    (backported from commit 341ac980eab90ac1f6c22ee9f9da83ed9604d899)
    
    Change-Id: I2ae09f423aec5215ad3df32adee89c0782f7782f
    Signed-off-by: zhaochenguang <zhaochenguang@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/76031
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- net/xdp/xsk_buff_pool.c
+++ net/xdp/xsk_buff_pool.c
@@ -85,6 +85,7 @@ struct xsk_buff_pool *xp_create_and_assign_umem(struct xdp_sock *xs,
 		XDP_PACKET_HEADROOM;
 	pool->umem = umem;
 	pool->addrs = umem->addrs;
+	pool->tx_metadata_len = umem->tx_metadata_len;
 	spin_lock_init(&pool->rx_lock);
 	INIT_LIST_HEAD(&pool->free_list);
 	INIT_LIST_HEAD(&pool->xskb_list);
