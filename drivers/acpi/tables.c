98ac792459a6af6f5ecf8dcde774ae900631960c
Jing Li <jingli@wxiat.com>
2023-12-06 17:20:08 +0800

    SUNWAY: acpi: support MADT entry print for SW CINTC
    Mainline: KYLIN-only
    Severity: Low
    
    Task: #382309
    
    K2CI-Arch: Sunway
    Signed-off-by: Jing Li <jingli@wxiat.com>
    Signed-off-by: liuyunchen <liuyunchen@kylinos.cn>
    Change-Id: Ifec6fc2aa7595576b48f7d1d10add3ad4ce8fbc8
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/53810
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/acpi/tables.c
+++ drivers/acpi/tables.c
@@ -230,6 +230,15 @@ void acpi_table_print_madt_entry(struct acpi_subtable_header *header)
 		}
 		break;
 
+	case ACPI_MADT_TYPE_SW_CINTC:
+		{
+			struct acpi_madt_sw_cintc *p =
+				(struct acpi_madt_sw_cintc *)header;
+			pr_debug("SW CINTC (version[%u] flags[0x%x] hardware_id[0x%x])\n",
+				p->version, p->flags, p->hardware_id);
+		}
+		break;
+
 	default:
 		pr_warn("Found unsupported MADT entry (type = 0x%x)\n",
 			header->type);
