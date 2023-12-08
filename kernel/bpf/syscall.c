6822e4c604ac4e5f1b413af795c223f153c742b0
Hou Tao <houtao1@huawei.com>
2023-12-08 18:23:52 +0800

    bpf: Only call maybe_wait_bpf_programs() when map operation succeeds
    Mainline: 67ad2c73ff29b32bd09135ec07c26e59490dbb3b
    From: v6.8-rc1
    Severity: Low
    
    task: 522091
    
    There is no need to call maybe_wait_bpf_programs() if update or deletion
    operation fails. So only call maybe_wait_bpf_programs() if update or
    deletion operation succeeds.
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Link: https://lore.kernel.org/r/20231208102355.2628918-5-houtao@huaweicloud.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    (backported from commit 67ad2c73ff29b32bd09135ec07c26e59490dbb3b)
    
    Change-Id: I30613d1d2d04e970cedfe169a65f7e89bfe4ba86
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/112216
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1740,7 +1740,8 @@ static int map_update_elem(union bpf_attr *attr, bpfptr_t uattr)
 	}
 
 	err = bpf_map_update_value(map, fd_file(f), key, value, attr->flags);
-	maybe_wait_bpf_programs(map);
+	if (!err)
+		maybe_wait_bpf_programs(map);
 
 	kvfree(value);
 free_key:
@@ -1793,7 +1794,8 @@ static int map_delete_elem(union bpf_attr *attr, bpfptr_t uattr)
 	err = map->ops->map_delete_elem(map, key);
 	rcu_read_unlock();
 	bpf_enable_instrumentation();
-	maybe_wait_bpf_programs(map);
+	if (!err)
+		maybe_wait_bpf_programs(map);
 out:
 	kvfree(key);
 err_put:
