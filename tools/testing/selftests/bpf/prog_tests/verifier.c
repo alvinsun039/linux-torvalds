f622f1b084caf3ce526b964345e1003c0fb10ca1
Daniel Xu <dxu@dxuuu.xyz>
2023-12-11 13:20:09 -0700

    bpf: selftests: Add verifier tests for CO-RE bitfield writes
    Mainline: f04f2ce6018f3cb33ac96270b9153c2920ead190
    From: v6.8-rc1
    Severity: Low
    
    task: 501509
    
    Add some tests that exercise BPF_CORE_WRITE_BITFIELD() macro. Since some
    non-trivial bit fiddling is going on, make sure various edge cases (such
    as adjacent bitfields and bitfields at the edge of structs) are
    exercised.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>
    Link: https://lore.kernel.org/r/72698a1080fa565f541d5654705255984ea2a029.1702325874.git.dxu@dxuuu.xyz
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    
    Change-Id: If0e6d8fe4d2c572bb6fce051a49fe679630ea304
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/90386
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- tools/testing/selftests/bpf/prog_tests/verifier.c
+++ tools/testing/selftests/bpf/prog_tests/verifier.c
@@ -7,6 +7,7 @@
 #include "verifier_arena.skel.h"
 #include "verifier_array_access.skel.h"
 #include "verifier_basic_stack.skel.h"
+#include "verifier_bitfield_write.skel.h"
 #include "verifier_bounds.skel.h"
 #include "verifier_bounds_deduction.skel.h"
 #include "verifier_bounds_deduction_non_const.skel.h"
@@ -123,6 +124,7 @@ static void run_tests_aux(const char *skel_name,
 void test_verifier_and(void)                  { RUN(verifier_and); }
 void test_verifier_arena(void)                { RUN(verifier_arena); }
 void test_verifier_basic_stack(void)          { RUN(verifier_basic_stack); }
+void test_verifier_bitfield_write(void)       { RUN(verifier_bitfield_write); }
 void test_verifier_bounds(void)               { RUN(verifier_bounds); }
 void test_verifier_bounds_deduction(void)     { RUN(verifier_bounds_deduction); }
 void test_verifier_bounds_deduction_non_const(void)     { RUN(verifier_bounds_deduction_non_const); }
