0fab29b7b1599b39d7c84cff6ac3e28dfe8e5d72
Jens Axboe <axboe@kernel.dk>
2023-12-21 09:02:57 -0700

    io_uring/kbuf: add method for returning provided buffer ring head
    Mainline: d293b1a89694fc4918d9a4330a71ba2458f9d581
    From: v6.8-rc1
    Severity: Moderate
    
    task: 526909
    
    The tail of the provided ring buffer is shared between the kernel and
    the application, but the head is private to the kernel as the
    application doesn't need to see it. However, this also prevents the
    application from knowing how many buffers the kernel has consumed.
    Usually this is fine, as the information is inherently racy in that
    the kernel could be consuming buffers continually, but for cleanup
    purposes it may be relevant to know how many buffers are still left
    in the ring.
    
    Add IORING_REGISTER_PBUF_STATUS which will return status for a given
    provided buffer ring. Right now it just returns the head, but space
    is reserved for more information later in, if needed.
    
    Link: https://github.com/axboe/liburing/discussions/1020
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit d293b1a89694fc4918d9a4330a71ba2458f9d581)
    
    K2CI-Arch: All
    Change-Id: Ic3e64ca16d89d57317455e6ed179c6037f24fa54
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151221
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- io_uring/kbuf.h
+++ io_uring/kbuf.h
@@ -53,6 +53,7 @@ int io_provide_buffers(struct io_kiocb *req, unsigned int issue_flags);
 
 int io_register_pbuf_ring(struct io_ring_ctx *ctx, void __user *arg);
 int io_unregister_pbuf_ring(struct io_ring_ctx *ctx, void __user *arg);
+int io_register_pbuf_status(struct io_ring_ctx *ctx, void __user *arg);
 
 unsigned int __io_put_kbuf(struct io_kiocb *req, unsigned issue_flags);
 
