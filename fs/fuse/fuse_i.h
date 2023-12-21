3b4f4751c609fbf32dbcd2a001b0bd7c244da6cc
lifangxu <lifangxu@kylinos.cn>
2023-12-21 19:23:19 +0800

    KYLIN: kabi: fuse: reserve space for future expansion
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 211268
    
    Reserve space for future fuse kabi expansion
    
    K2CI-Arch: All
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Change-Id: Ib8b669b00a481256e6265b98da15b9847cea4033
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/106677
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -31,6 +31,7 @@
 #include <linux/pid_namespace.h>
 #include <linux/refcount.h>
 #include <linux/user_namespace.h>
+#include <linux/ky_kabi.h>
 
 /** Default max number of pages that can be used in a single read request */
 #define FUSE_DEFAULT_MAX_PAGES_PER_REQ 32
@@ -173,6 +174,8 @@ struct fuse_inode {
 #endif
 	/** Submount specific lookup tracking */
 	struct fuse_submount_lookup *submount_lookup;
+
+	KY_KABI_RESERVE(1)
 };
 
 /** FUSE inode state bits */
@@ -501,6 +504,8 @@ struct fuse_dev {
 
 	/** list entry on fc->devices */
 	struct list_head entry;
+
+	KY_KABI_RESERVE(1)
 };
 
 enum fuse_dax_mode {
@@ -867,6 +872,11 @@ struct fuse_conn {
 
 	/* New writepages go into this bucket */
 	struct fuse_sync_bucket __rcu *curr_bucket;
+
+	KY_KABI_RESERVE(1)
+	KY_KABI_RESERVE(2)
+	KY_KABI_RESERVE(3)
+	KY_KABI_RESERVE(4)
 };
 
 /*
