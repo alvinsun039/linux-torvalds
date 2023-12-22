0f4bf7a5b0455cd08af57baeec69e652d3dc9c54
Yonghong Song <yonghong.song@linux.dev>
2023-12-21 19:17:39 -0800

    bpf: Add objcg to bpf_mem_alloc
    Mainline: 9fc8e802048ad150e8032c4f3dbf40112160cfe9
    From: v6.8-rc1
    Severity: Moderate
    
    task: 361735
    
    The objcg is a bpf_mem_alloc level property since all bpf_mem_cache's
    are with the same objcg. This patch made such a property explicit.
    The next patch will use this property to save and restore objcg
    for percpu unit allocator.
    
    Acked-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>
    Link: https://lore.kernel.org/r/20231222031739.1288590-1-yonghong.song@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    (backported from commit 9fc8e802048ad150e8032c4f3dbf40112160cfe9)
    
    Cc: kernel-stable-team #SP0+
    Signed-off-by: Yun Lu <luyun@kylinos.cn>
    Change-Id: I78d5ffc53ec20631f960bdb4d1d7d088655ba1d0
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/138877
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- kernel/bpf/memalloc.c
+++ kernel/bpf/memalloc.c
@@ -519,6 +519,7 @@ int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu)
 		if (memcg_bpf_enabled())
 			objcg = get_obj_cgroup_from_current();
 #endif
+		ma->objcg = objcg;
 		for_each_possible_cpu(cpu) {
 			c = per_cpu_ptr(pc, cpu);
 			c->unit_size = unit_size;
@@ -538,6 +539,7 @@ int bpf_mem_alloc_init(struct bpf_mem_alloc *ma, int size, bool percpu)
 #ifdef CONFIG_MEMCG_KMEM
 	objcg = get_obj_cgroup_from_current();
 #endif
+	ma->objcg = objcg;
 	for_each_possible_cpu(cpu) {
 		cc = per_cpu_ptr(pcc, cpu);
 		for (i = 0; i < NUM_CACHES; i++) {
@@ -687,9 +689,8 @@ void bpf_mem_alloc_destroy(struct bpf_mem_alloc *ma)
 			rcu_in_progress += atomic_read(&c->call_rcu_ttrace_in_progress);
 			rcu_in_progress += atomic_read(&c->call_rcu_in_progress);
 		}
-		/* objcg is the same across cpus */
-		if (c->objcg)
-			obj_cgroup_put(c->objcg);
+		if (ma->objcg)
+			obj_cgroup_put(ma->objcg);
 		destroy_mem_alloc(ma, rcu_in_progress);
 	}
 	if (ma->caches) {
@@ -705,8 +706,8 @@ void bpf_mem_alloc_destroy(struct bpf_mem_alloc *ma)
 				rcu_in_progress += atomic_read(&c->call_rcu_in_progress);
 			}
 		}
-		if (c->objcg)
-			obj_cgroup_put(c->objcg);
+		if (ma->objcg)
+			obj_cgroup_put(ma->objcg);
 		destroy_mem_alloc(ma, rcu_in_progress);
 	}
 }
