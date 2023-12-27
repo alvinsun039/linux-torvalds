98ad37df28a9cd4784f88e9ee0ab8a27cc1f47ce
Kinsey Ho <kinseyho@google.com>
2023-12-27 14:12:03 +0000

    mm/mglru: remove CONFIG_MEMCG
    Mainline: 745b13e647cd119e70d16b57698e12b7c86ca264
    From: v6.8-rc1
    Severity: Moderate
    
    task: 447145
    
    Remove CONFIG_MEMCG in a refactoring to improve code readability at
    the cost of a few bytes in struct lru_gen_folio per node when
    CONFIG_MEMCG=n.
    
    Link: https://lkml.kernel.org/r/20231227141205.2200125-4-kinseyho@google.com
    Change-Id: I2eaf2bbdf7712463feb994bc3c33e26b6bc2ca79
    Signed-off-by: Kinsey Ho <kinseyho@google.com>
    Co-developed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Tested-by: Donet Tom <donettom@linux.vnet.ibm.com>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Cc: kernel test robot <lkp@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: liqiang01 <liqiang01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56329
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 彭灿 <pengcan@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1115,9 +1115,7 @@ struct lru_gen_mm_list {
 
 void lru_gen_add_mm(struct mm_struct *mm);
 void lru_gen_del_mm(struct mm_struct *mm);
-#ifdef CONFIG_MEMCG
 void lru_gen_migrate_mm(struct mm_struct *mm);
-#endif
 
 static inline void lru_gen_init_mm(struct mm_struct *mm)
 {
@@ -1148,11 +1146,9 @@ static inline void lru_gen_del_mm(struct mm_struct *mm)
 {
 }
 
-#ifdef CONFIG_MEMCG
 static inline void lru_gen_migrate_mm(struct mm_struct *mm)
 {
 }
-#endif
 
 static inline void lru_gen_init_mm(struct mm_struct *mm)
 {
--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -445,14 +445,12 @@ struct lru_gen_folio {
 	atomic_long_t refaulted[NR_HIST_GENS][ANON_AND_FILE][MAX_NR_TIERS];
 	/* whether the multi-gen LRU is enabled */
 	bool enabled;
-#ifdef CONFIG_MEMCG
 	/* the memcg generation this lru_gen_folio belongs to */
 	u8 gen;
 	/* the list segment this lru_gen_folio belongs to */
 	u8 seg;
 	/* per-node lru_gen_folio list for global reclaim */
 	struct hlist_nulls_node list;
-#endif
 };
 
 enum {
@@ -497,11 +495,6 @@ struct lru_gen_mm_walk {
 	bool can_swap;
 	bool force_scan;
 };
-
-void lru_gen_init_lruvec(struct lruvec *lruvec);
-void lru_gen_look_around(struct page_vma_mapped_walk *pvmw);
-
-#ifdef CONFIG_MEMCG
 
 /*
  * For each node, memcgs are divided into two generations: the old and the
@@ -560,6 +553,8 @@ struct lru_gen_memcg {
 };
 
 void lru_gen_init_pgdat(struct pglist_data *pgdat);
+void lru_gen_init_lruvec(struct lruvec *lruvec);
+void lru_gen_look_around(struct page_vma_mapped_walk *pvmw);
 
 void lru_gen_init_memcg(struct mem_cgroup *memcg);
 void lru_gen_exit_memcg(struct mem_cgroup *memcg);
@@ -567,19 +562,6 @@ void lru_gen_online_memcg(struct mem_cgroup *memcg);
 void lru_gen_offline_memcg(struct mem_cgroup *memcg);
 void lru_gen_release_memcg(struct mem_cgroup *memcg);
 void lru_gen_soft_reclaim(struct mem_cgroup *memcg, int nid);
-
-#else /* !CONFIG_MEMCG */
-
-#define MEMCG_NR_GENS	1
-
-struct lru_gen_memcg {
-};
-
-static inline void lru_gen_init_pgdat(struct pglist_data *pgdat)
-{
-}
-
-#endif /* CONFIG_MEMCG */
 
 #else /* !CONFIG_LRU_GEN */
 
@@ -594,8 +576,6 @@ static inline void lru_gen_init_lruvec(struct lruvec *lruvec)
 static inline void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 {
 }
-
-#ifdef CONFIG_MEMCG
 
 static inline void lru_gen_init_memcg(struct mem_cgroup *memcg)
 {
@@ -620,8 +600,6 @@ static inline void lru_gen_release_memcg(struct mem_cgroup *memcg)
 static inline void lru_gen_soft_reclaim(struct mem_cgroup *memcg, int nid)
 {
 }
-
-#endif /* CONFIG_MEMCG */
 
 #endif /* CONFIG_LRU_GEN */
 
