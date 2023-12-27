900422e45c73c9f5b7623d82402c6a55345c8915
Eric Biggers <ebiggers@google.com>
2023-12-27 11:14:28 -0600

    f2fs: move release of block devices to after kill_block_super()
    Mainline: 275dca4630c165edea9abe27113766bc1173f878
    From: v6.8-rc1
    Severity: Moderate
    
    task: 458576
    
    Call destroy_device_list() and free the f2fs_sb_info from
    kill_f2fs_super(), after the call to kill_block_super().  This is
    necessary to order it after the call to fscrypt_destroy_keyring() once
    generic_shutdown_super() starts calling fscrypt_destroy_keyring() just
    after calling ->put_super.  This is because fscrypt_destroy_keyring()
    may call into f2fs_get_devices() via the fscrypt_operations.
    
    Reviewed-by: Chao Yu <chao@kernel.org>
    Link: https://lore.kernel.org/r/20231227171429.9223-2-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    
    (backported from commit 275dca4630c165edea9abe27113766bc1173f878)
    
    K2CI-Arch: All
    Change-Id: I5575c7cb3f25726ede0c37b5cdb683369ceae3a9
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/57029
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1681,12 +1681,10 @@ static void f2fs_put_super(struct super_block *sb)
 
 	kvfree(sbi->ckpt);
 
-	sb->s_fs_info = NULL;
 	if (sbi->s_chksum_driver)
 		crypto_free_shash(sbi->s_chksum_driver);
 	kfree(sbi->raw_super);
 
-	destroy_device_list(sbi);
 	f2fs_destroy_page_array_cache(sbi);
 	f2fs_destroy_xattr_caches(sbi);
 #ifdef CONFIG_QUOTA
@@ -1701,7 +1699,6 @@ static void f2fs_put_super(struct super_block *sb)
 #if IS_ENABLED(CONFIG_UNICODE)
 	utf8_unload(sb->s_encoding);
 #endif
-	kfree(sbi);
 }
 
 int f2fs_sync_fs(struct super_block *sb, int sync)
@@ -4849,9 +4846,9 @@ static struct dentry *f2fs_mount(struct file_system_type *fs_type, int flags,
 
 static void kill_f2fs_super(struct super_block *sb)
 {
-	if (sb->s_root) {
-		struct f2fs_sb_info *sbi = F2FS_SB(sb);
+	struct f2fs_sb_info *sbi = F2FS_SB(sb);
 
+	if (sb->s_root) {
 		set_sbi_flag(sbi, SBI_IS_CLOSE);
 		f2fs_stop_gc_thread(sbi);
 		f2fs_stop_discard_thread(sbi);
@@ -4878,6 +4875,12 @@ static void kill_f2fs_super(struct super_block *sb)
 			sb->s_flags &= ~SB_RDONLY;
 	}
 	kill_block_super(sb);
+	/* Release block devices last, after fscrypt_destroy_keyring(). */
+	if (sbi) {
+		destroy_device_list(sbi);
+		kfree(sbi);
+		sb->s_fs_info = NULL;
+	}
 }
 
 static struct file_system_type f2fs_fs_type = {
