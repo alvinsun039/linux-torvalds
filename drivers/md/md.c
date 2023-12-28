3df6ddf7ccb55af8fba5ec85184aab69e0b14723
Yu Kuai <yukuai3@huawei.com>
2023-12-28 20:55:51 +0800

    md: remove redundant check of 'mddev->sync_thread'
    Mainline: 61c90765e131e63ead773b9b99167415e246a945
    From: v6.9-rc1
    Severity: Moderate
    
    task: 211761
    
    The lifetime of sync_thread:
    
    1) Set MD_RECOVERY_NEEDED and wake up daemon thread (by ioctl/sysfs or
       other events);
    2) Daemon thread woke up, md_check_recovery() found that
       MD_RECOVERY_NEEDED is set:
       a) try to grab reconfig_mutex;
       b) set MD_RECOVERY_RUNNING;
       c) clear MD_RECOVERY_NEEDED, and then queue sync_work;
    3) md_start_sync() choose sync_action, then register sync_thread;
    4) md_do_sync() is done, set MD_RECOVERY_DONE and wake up daemon thread;
    5) Daemon thread woke up, md_check_recovery() found that
       MD_RECOVERY_DONE is set:
       a) try to grab reconfig_mutex;
       b) unregister sync_thread;
       c) clear MD_RECOVERY_RUNNING and MD_RECOVERY_DONE;
    
    Hence there is no such case that MD_RECOVERY_RUNNING is not set, while
    sync_thread is registered.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Signed-off-by: Song Liu <song@kernel.org>
    Link: https://lore.kernel.org/r/20231228125553.2697765-2-yukuai1@huaweicloud.com
    (backported from commit 61c90765e131e63ead773b9b99167415e246a945)
    
    K2CI-Arch: All
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Change-Id: I9cbb9a3d4c1f5a5c41ed5e10c13a3f58f501e7a1
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/107203
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -3365,8 +3365,7 @@ static ssize_t new_offset_store(struct md_rdev *rdev,
 	if (kstrtoull(buf, 10, &new_offset) < 0)
 		return -EINVAL;
 
-	if (mddev->sync_thread ||
-	    test_bit(MD_RECOVERY_RUNNING,&mddev->recovery))
+	if (test_bit(MD_RECOVERY_RUNNING, &mddev->recovery))
 		return -EBUSY;
 	if (new_offset == rdev->data_offset)
 		/* reset is always permitted */
@@ -4050,8 +4049,7 @@ level_store(struct mddev *mddev, const char *buf, size_t len)
 	 */
 
 	rv = -EBUSY;
-	if (mddev->sync_thread ||
-	    test_bit(MD_RECOVERY_RUNNING, &mddev->recovery) ||
+	if (test_bit(MD_RECOVERY_RUNNING, &mddev->recovery) ||
 	    mddev->reshape_position != MaxSector ||
 	    mddev->sysfs_active)
 		goto out_unlock;
@@ -6453,7 +6451,6 @@ static int md_set_readonly(struct mddev *mddev, struct block_device *bdev)
 
 	mutex_lock(&mddev->open_mutex);
 	if ((mddev->pers && atomic_read(&mddev->openers) > !!bdev) ||
-	    mddev->sync_thread ||
 	    test_bit(MD_RECOVERY_RUNNING, &mddev->recovery)) {
 		pr_warn("md: %s still in use.\n",mdname(mddev));
 		err = -EBUSY;
@@ -6506,7 +6503,6 @@ static int do_md_stop(struct mddev *mddev, int mode,
 	mutex_lock(&mddev->open_mutex);
 	if ((mddev->pers && atomic_read(&mddev->openers) > !!bdev) ||
 	    mddev->sysfs_active ||
-	    mddev->sync_thread ||
 	    test_bit(MD_RECOVERY_RUNNING, &mddev->recovery)) {
 		pr_warn("md: %s still in use.\n",mdname(mddev));
 		mutex_unlock(&mddev->open_mutex);
@@ -7352,8 +7348,7 @@ static int update_size(struct mddev *mddev, sector_t num_sectors)
 	 * of each device.  If num_sectors is zero, we find the largest size
 	 * that fits.
 	 */
-	if (test_bit(MD_RECOVERY_RUNNING, &mddev->recovery) ||
-	    mddev->sync_thread)
+	if (test_bit(MD_RECOVERY_RUNNING, &mddev->recovery))
 		return -EBUSY;
 	if (!md_is_rdwr(mddev))
 		return -EROFS;
@@ -7390,8 +7385,7 @@ static int update_raid_disks(struct mddev *mddev, int raid_disks)
 	if (raid_disks <= 0 ||
 	    (mddev->max_disks && raid_disks >= mddev->max_disks))
 		return -EINVAL;
-	if (mddev->sync_thread ||
-	    test_bit(MD_RECOVERY_RUNNING, &mddev->recovery) ||
+	if (test_bit(MD_RECOVERY_RUNNING, &mddev->recovery) ||
 	    test_bit(MD_RESYNCING_REMOTE, &mddev->recovery) ||
 	    mddev->reshape_position != MaxSector)
 		return -EBUSY;
