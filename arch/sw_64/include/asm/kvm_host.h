d76c90cf634035f1bd29925c5b5fa717c7424d4d
Min Fanlei <minfanlei@wxiat.com>
2024-01-02 10:51:13 +0800

    SUNWAY: kvm: fix clock sync of CORE4 live migration
    Mainline: KYLIN-only
    Severity: Low
    
    Task: #382309
    
    Move "migration_mark" and "shtclock" from struct kvm_vcpu_arch to
    struct vcpucb, so that they can be synced between the src and dst
    guest.
    
    K2CI-Arch: Sunway
    Signed-off-by: Min Fanlei <minfanlei@wxiat.com>
    Signed-off-by: liuyunchen <liuyunchen@kylinos.cn>
    Change-Id: If21a27137e39c2ec8419c6be2da030adedc75781
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/66208
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- arch/sw_64/include/asm/kvm_host.h
+++ arch/sw_64/include/asm/kvm_host.h
@@ -124,10 +124,6 @@ struct kvm_vcpu_arch {
 
 	/* Cache some mmu pages needed inside spinlock regions */
 	struct kvm_mmu_memory_cache mmu_page_cache;
-
-	/* guest live migration */
-	unsigned long migration_mark;
-	unsigned long shtclock;
 };
 
 struct vmem_info {
