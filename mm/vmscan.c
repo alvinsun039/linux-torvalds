4cc7508471cb2b275c45a1467d91421a8b4d86bb
Yuchen Tang <tangyuchen5@huawei.com>
2024-01-18 14:40:31 +0800

    OPENEULER: mm: Export symbol reclaim_pages
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 211762
    
    Export reclaim_pages so that etmem_swap can exploit
    this for memory reclamation.
    
    In etmem swap module, it will receive a list of pages
    that need to be reclaimed, sent from the user mode
    program. etmem swap module will do some checks and add
    appropriate candidates to folio_list, which will later
    be reclaimed by reclaim_pages.
    
    K2CI-Arch: All
    Link: https://gitee.com/openeuler/kernel/issues/I8T1MB
    Signed-off-by: Yuchen Tang <tangyuchen5@huawei.com>
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Change-Id: Ie2eaf3999fa4f0400dc6ca3afe5557a555aa4c81
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/107117
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2157,6 +2157,7 @@ unsigned long reclaim_pages(struct list_head *folio_list, bool ignore_references
 
 	return nr_reclaimed;
 }
+EXPORT_SYMBOL_GPL(reclaim_pages);
 
 static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 				 struct lruvec *lruvec, struct scan_control *sc)
