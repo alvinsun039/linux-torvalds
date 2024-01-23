86b2bc858d958824933266f3b7792ab8c93bd346
Christian Brauner <brauner@kernel.org>
2024-01-23 14:26:46 +0100

    bdev: make struct bdev_handle private to the block layer
    Mainline: a56aefca8d386181415a1fb7cfec2f72b0404797
    From: v6.9-rc1
    Severity: Low
    
    task: 522091
    
    Link: https://lore.kernel.org/r/20240123-vfs-bdev-file-v2-29-adbd023e19cc@kernel.org
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit a56aefca8d386181415a1fb7cfec2f72b0404797)
    
    Change-Id: If997a531e5405e9ded85226e3824770363006e24
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/112287
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- block/blk.h
+++ block/blk.h
@@ -26,6 +26,12 @@ struct blk_flush_queue {
 	struct request		*flush_rq;
 };
 
+struct bdev_handle {
+	struct block_device *bdev;
+	void *holder;
+	blk_mode_t mode;
+};
+
 bool is_flush_rq(struct request *req);
 
 struct blk_flush_queue *blk_alloc_flush_queue(int node, int cmd_size,
@@ -574,7 +580,7 @@ static inline void bio_issue_init(struct bio_issue *issue,
 }
 
 void bdev_release(struct bdev_handle *handle);
-struct bdev_handle *bdev_open_by_dev(dev_t dev, blk_mode_t mode, void *holder,
-		const struct blk_holder_ops *hops);
-
+int bdev_open(struct block_device *bdev, blk_mode_t mode, void *holder,
+	      const struct blk_holder_ops *hops, struct file *bdev_file);
+int bdev_permission(dev_t dev, blk_mode_t mode, void *holder);
 #endif /* BLK_INTERNAL_H */
