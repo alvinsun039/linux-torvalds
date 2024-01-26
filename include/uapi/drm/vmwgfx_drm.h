b75662644e1d40242f8774da2163f4ecdeeafde9
Maaz Mombasawala <maaz.mombasawala@broadcom.com>
2024-01-26 15:08:02 -0500

    drm/vmwgfx: Add SPDX header to vmwgfx_drm.h
    Mainline: 0c10a15d21222ef22b41d11e423d7b17ed3d4a5d
    From: v6.9-rc1
    Severity: Low
    
    task: 454528
    
    Update vmwgfx_drm.h with SPDX-License-Identifier:
    (GPL-2.0 WITH Linux-syscall-note) OR MIT
    
    Signed-off-by: Maaz Mombasawala <maaz.mombasawala@broadcom.com>
    Reviewed-by: Martin Krastev <martin.krastev@broadcom.com>
    Signed-off-by: Zack Rusin <zack.rusin@broadcom.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240126200804.732454-4-zack.rusin@broadcom.com
    
    (backported from commit 0c10a15d21222ef22b41d11e423d7b17ed3d4a5d)
    
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>
    Change-Id: I2bd0e230163ef707c7dc86093487d38754aab183
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/53452
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 唐龙军 <tanglongjun@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 赵国汗 <zhaoguohan@kylinos.cn>

--- include/uapi/drm/vmwgfx_drm.h
+++ include/uapi/drm/vmwgfx_drm.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: (GPL-2.0 WITH Linux-syscall-note) OR MIT */
 /**************************************************************************
  *
  * Copyright © 2009-2022 VMware, Inc., Palo Alto, CA., USA
