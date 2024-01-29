026b866d9a1aee817a24927b7546a3ced77fcbbe
Johannes Berg <johannes.berg@intel.com>
2024-01-29 21:22:03 +0200

    wifi: iwlwifi: mvm: refactor duplicate chanctx condition
    Mainline: ce1fa3adc007ce3fd6beb8f4d733e00daa64c6c0
    From: v6.9-rc1
    Severity: Moderate
    
    task: 496179
    
    Refactor the check for using a chanctx's def vs. min_def,
    to have the same in both places and reuse it later.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240129211905.6fcde4051adf.I343934874612d21727ed167accaa967958b2c25b@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    K2CI-Arch: All
    Change-Id: I8090eb6d26de749be9ace9bf5a142c9fffc89480
    Signed-off-by: Yi Cong <yicong@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/91912
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -2726,4 +2726,17 @@ bool iwl_mvm_enable_fils(struct iwl_mvm *mvm,
 			 struct ieee80211_chanctx_conf *ctx);
 void iwl_mvm_mld_select_links(struct iwl_mvm *mvm, struct ieee80211_vif *vif,
 			      bool valid_links_changed);
+
+bool iwl_mvm_is_ftm_responder_chanctx(struct iwl_mvm *mvm,
+				      struct ieee80211_chanctx_conf *ctx);
+
+static inline struct cfg80211_chan_def *
+iwl_mvm_chanctx_def(struct iwl_mvm *mvm, struct ieee80211_chanctx_conf *ctx)
+{
+	bool use_def = iwl_mvm_is_ftm_responder_chanctx(mvm, ctx) ||
+		iwl_mvm_enable_fils(mvm, ctx);
+
+	return use_def ? &ctx->def : &ctx->min_def;
+}
+
 #endif /* __IWL_MVM_H__ */
