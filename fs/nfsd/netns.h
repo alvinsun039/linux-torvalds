c67d810066029a471b1ef1aea4320cd8e0a984a5
Jeff Layton <jlayton@kernel.org>
2024-01-31 18:02:23 -0500

    nfsd: adapt to breakup of struct file_lock
    Mainline: 05580bbfc6bcac93eae2c8a1bf9dc245b2934b28
    From: v6.9-rc1
    Severity: Moderate
    
    task: 444220
    
    Most of the existing APIs have remained the same, but subsystems that
    access file_lock fields directly need to reach into struct
    file_lock_core now.
    
    K2CI-Arch: All
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Link: https://lore.kernel.org/r/20240131-flsplit-v3-42-c6129007ee8d@kernel.org
    Reviewed-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit 05580bbfc6bcac93eae2c8a1bf9dc245b2934b28)
    
    Change-Id: I92e76595fbf2994fd7d02bfe285287e0580cf738
    Signed-off-by: Can Peng <pengcan@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/55678
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘宝林 <liubaolin@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/nfsd/netns.h
+++ fs/nfsd/netns.h
@@ -10,7 +10,6 @@
 
 #include <net/net_namespace.h>
 #include <net/netns/generic.h>
-#define _NEED_FILE_LOCK_FIELD_MACROS
 #include <linux/filelock.h>
 #include <linux/nfs4.h>
 #include <linux/percpu_counter.h>
