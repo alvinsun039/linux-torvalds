45563b0efffe080f46684ced86048104ed96d395
Jeff Layton <jlayton@kernel.org>
2024-01-31 18:02:22 -0500

    nfs: adapt to breakup of struct file_lock
    Mainline: dd1fac6ae648cac4e92ccc829e94750ddfed5e52
    From: v6.9-rc1
    Severity: Moderate
    
    task: 444220
    
    Most of the existing APIs have remained the same, but subsystems that
    access file_lock fields directly need to reach into struct
    file_lock_core now.
    
    K2CI-Arch: All
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Link: https://lore.kernel.org/r/20240131-flsplit-v3-41-c6129007ee8d@kernel.org
    Reviewed-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit dd1fac6ae648cac4e92ccc829e94750ddfed5e52)
    
    Change-Id: I5bf2023a8c4dd0904465bc6dbbd712d7875ed086
    Signed-off-by: Can Peng <pengcan@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/55677
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 刘宝林 <liubaolin@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -32,7 +32,6 @@
 #include <linux/compaction.h>
 
 #include <linux/uaccess.h>
-#define _NEED_FILE_LOCK_FIELD_MACROS
 #include <linux/filelock.h>
 
 #include "delegation.h"
@@ -724,15 +723,15 @@ do_getlk(struct file *filp, int cmd, struct file_lock *fl, int is_local)
 {
 	struct inode *inode = filp->f_mapping->host;
 	int status = 0;
-	unsigned int saved_type = fl->fl_type;
+	unsigned int saved_type = fl->c.flc_type;
 
 	/* Try local locking first */
 	posix_test_lock(filp, fl);
-	if (fl->fl_type != F_UNLCK) {
+	if (fl->c.flc_type != F_UNLCK) {
 		/* found a conflict */
 		goto out;
 	}
-	fl->fl_type = saved_type;
+	fl->c.flc_type = saved_type;
 
 	if (NFS_PROTO(inode)->have_delegation(inode, FMODE_READ))
 		goto out_noconflict;
@@ -744,7 +743,7 @@ do_getlk(struct file *filp, int cmd, struct file_lock *fl, int is_local)
 out:
 	return status;
 out_noconflict:
-	fl->fl_type = F_UNLCK;
+	fl->c.flc_type = F_UNLCK;
 	goto out;
 }
 
@@ -769,7 +768,7 @@ do_unlk(struct file *filp, int cmd, struct file_lock *fl, int is_local)
 		 * 	If we're signalled while cleaning up locks on process exit, we
 		 * 	still need to complete the unlock.
 		 */
-		if (status < 0 && !(fl->fl_flags & FL_CLOSE))
+		if (status < 0 && !(fl->c.flc_flags & FL_CLOSE))
 			return status;
 	}
 
@@ -836,12 +835,12 @@ int nfs_lock(struct file *filp, int cmd, struct file_lock *fl)
 	int is_local = 0;
 
 	dprintk("NFS: lock(%pD2, t=%x, fl=%x, r=%lld:%lld)\n",
-			filp, fl->fl_type, fl->fl_flags,
+			filp, fl->c.flc_type, fl->c.flc_flags,
 			(long long)fl->fl_start, (long long)fl->fl_end);
 
 	nfs_inc_stats(inode, NFSIOS_VFSLOCK);
 
-	if (fl->fl_flags & FL_RECLAIM)
+	if (fl->c.flc_flags & FL_RECLAIM)
 		return -ENOGRACE;
 
 	if (NFS_SERVER(inode)->flags & NFS_MOUNT_LOCAL_FCNTL)
@@ -873,9 +872,9 @@ int nfs_flock(struct file *filp, int cmd, struct file_lock *fl)
 	int is_local = 0;
 
 	dprintk("NFS: flock(%pD2, t=%x, fl=%x)\n",
-			filp, fl->fl_type, fl->fl_flags);
+			filp, fl->c.flc_type, fl->c.flc_flags);
 
-	if (!(fl->fl_flags & FL_FLOCK))
+	if (!(fl->c.flc_flags & FL_FLOCK))
 		return -ENOLCK;
 
 	if (NFS_SERVER(inode)->flags & NFS_MOUNT_LOCAL_FLOCK)
