52f672b73bc062bcc87a660b10d5720c49c4ba62
Tony Solomonik <tony.solomonik@gmail.com>
2024-02-02 14:17:23 +0200

    Add do_ftruncate that truncates a struct file
    Mainline: 5f0d594c602f870e3a3872f7ea42bf846a1d26cf
    From: v6.9-rc1
    Severity: Important
    
    task: 530755
    
    do_sys_ftruncate receives a file descriptor, fgets the struct file, and
    finally actually truncates the file.
    
    do_ftruncate allows for passing in a file directly, with the caller
    already holding a reference to it.
    
    Signed-off-by: Tony Solomonik <tony.solomonik@gmail.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Link: https://lore.kernel.org/r/20240202121724.17461-2-tony.solomonik@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit 5f0d594c602f870e3a3872f7ea42bf846a1d26cf)
    
    Change-Id: I11094b49980772268547b3403ff93aa3dfb4ae81
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117836
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/open.c
+++ fs/open.c
@@ -158,49 +158,52 @@ COMPAT_SYSCALL_DEFINE2(truncate, const char __user *, path, compat_off_t, length
 }
 #endif
 
-long do_sys_ftruncate(unsigned int fd, loff_t length, int small)
+long do_ftruncate(struct file *file, loff_t length, int small)
 {
 	struct inode *inode;
 	struct dentry *dentry;
+	int error;
+
+	/* explicitly opened as large or we are on 64-bit box */
+	if (file->f_flags & O_LARGEFILE)
+		small = 0;
+
+	dentry = file->f_path.dentry;
+	inode = dentry->d_inode;
+	if (!S_ISREG(inode->i_mode) || !(file->f_mode & FMODE_WRITE))
+		return -EINVAL;
+
+	/* Cannot ftruncate over 2^31 bytes without large file support */
+	if (small && length > MAX_NON_LFS)
+		return -EINVAL;
+
+	/* Check IS_APPEND on real upper inode */
+	if (IS_APPEND(file_inode(file)))
+		return -EPERM;
+	sb_start_write(inode->i_sb);
+	error = security_file_truncate(file);
+	if (!error)
+		error = do_truncate(file_mnt_idmap(file), dentry, length,
+				    ATTR_MTIME | ATTR_CTIME, file);
+	sb_end_write(inode->i_sb);
+
+	return error;
+}
+
+long do_sys_ftruncate(unsigned int fd, loff_t length, int small)
+{
 	struct fd f;
 	int error;
 
-	error = -EINVAL;
 	if (length < 0)
-		goto out;
-	error = -EBADF;
+		return -EINVAL;
 	f = fdget(fd);
 	if (!fd_file(f))
-		goto out;
+		return -EBADF;
 
-	/* explicitly opened as large or we are on 64-bit box */
-	if (fd_file(f)->f_flags & O_LARGEFILE)
-		small = 0;
+	error = do_ftruncate(fd_file(f), length, small);
 
-	dentry = fd_file(f)->f_path.dentry;
-	inode = dentry->d_inode;
-	error = -EINVAL;
-	if (!S_ISREG(inode->i_mode) || !(fd_file(f)->f_mode & FMODE_WRITE))
-		goto out_putf;
-
-	error = -EINVAL;
-	/* Cannot ftruncate over 2^31 bytes without large file support */
-	if (small && length > MAX_NON_LFS)
-		goto out_putf;
-
-	error = -EPERM;
-	/* Check IS_APPEND on real upper inode */
-	if (IS_APPEND(file_inode(fd_file(f))))
-		goto out_putf;
-	sb_start_write(inode->i_sb);
-	error = security_file_truncate(fd_file(f));
-	if (!error)
-		error = do_truncate(file_mnt_idmap(fd_file(f)), dentry, length,
-				    ATTR_MTIME | ATTR_CTIME, fd_file(f));
-	sb_end_write(inode->i_sb);
-out_putf:
 	fdput(f);
-out:
 	return error;
 }
 
