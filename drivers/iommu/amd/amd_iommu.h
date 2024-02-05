2e4bc330fe8e309748193880b24d8be4c3cfcc14
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-02-05 11:56:01 +0000

    iommu/amd: Introduce get_amd_iommu_from_dev()
    Mainline: 6f35fe5d8a0ad0125c52fb20f5d67000e369eb3a
    From: v6.9-rc1
    Severity: Important
    
    task: 551017
    
    Introduce get_amd_iommu_from_dev() and get_amd_iommu_from_dev_data().
    And replace rlookup_amd_iommu() with the new helper function where
    applicable to avoid unnecessary loop to look up struct amd_iommu from
    struct device.
    
    Suggested-by: Jason Gunthorpe <jgg@ziepe.ca>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20240205115615.6053-4-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Change-Id: I24c55d9b9b01bc4c3daa83dc1b7d75e0c3d44537
    Signed-off-by: leixiang <leixiang@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/145638
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -133,6 +133,21 @@ static inline void *alloc_pgtable_page_noprof(int nid, gfp_t gfp)
 }
 #define alloc_pgtable_page(...)	alloc_hooks(alloc_pgtable_page_noprof(__VA_ARGS__))
 
+/*
+ * This must be called after device probe completes. During probe
+ * use rlookup_amd_iommu() get the iommu.
+ */
+static inline struct amd_iommu *get_amd_iommu_from_dev(struct device *dev)
+{
+	return iommu_get_iommu_dev(dev, struct amd_iommu, iommu);
+}
+
+/* This must be called after device probe completes. */
+static inline struct amd_iommu *get_amd_iommu_from_dev_data(struct iommu_dev_data *dev_data)
+{
+	return iommu_get_iommu_dev(dev_data->dev, struct amd_iommu, iommu);
+}
+
 bool translation_pre_enabled(struct amd_iommu *iommu);
 bool amd_iommu_is_attach_deferred(struct device *dev);
 int __init add_special_device(u8 type, u8 id, u32 *devid, bool cmd_line);
