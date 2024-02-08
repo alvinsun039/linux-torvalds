845508a5b9b23493d6e029abba8514b7ba7cc40b
Yicong Yang <yangyicong@hisilicon.com>
2024-02-08 10:40:26 +0800

    perf stat: Support per-cluster aggregation
    Mainline: cbc917a1b03bce85f385c1e640c9dcb02ffb9ab0
    From: v6.9-rc1
    Severity: Low
    
    Some platforms have 'cluster' topology and CPUs in the cluster will
    share resources like L3 Cache Tag (for HiSilicon Kunpeng SoC) or L2
    cache (for Intel Jacobsville). Currently parsing and building cluster
    topology have been supported since [1].
    
    perf stat has already supported aggregation for other topologies like
    die or socket, etc. It'll be useful to aggregate per-cluster to find
    problems like L3T bandwidth contention.
    
    This patch add support for "--per-cluster" option for per-cluster
    aggregation. Also update the docs and related test. The output will
    be like:
    
    [root@localhost tmp]# perf stat -a -e LLC-load --per-cluster -- sleep 5
    
     Performance counter stats for 'system wide':
    
    S56-D0-CLS158    4      1,321,521,570      LLC-load
    S56-D0-CLS594    4        794,211,453      LLC-load
    S56-D0-CLS1030    4             41,623      LLC-load
    S56-D0-CLS1466    4             41,646      LLC-load
    S56-D0-CLS1902    4             16,863      LLC-load
    S56-D0-CLS2338    4             15,721      LLC-load
    S56-D0-CLS2774    4             22,671      LLC-load
    [...]
    
    On a legacy system without cluster or cluster support, the output will
    be look like:
    [root@localhost perf]# perf stat -a -e cycles --per-cluster -- sleep 1
    
     Performance counter stats for 'system wide':
    
    S56-D0-CLS0   64         18,011,485      cycles
    S7182-D0-CLS0   64         16,548,835      cycles
    
    Note that this patch doesn't mix the cluster information in the outputs
    of --per-core to avoid breaking any tools/scripts using it.
    
    Note that perf recently supports "--per-cache" aggregation, but it's not
    the same with the cluster although cluster CPUs may share some cache
    resources. For example on my machine all clusters within a die share the
    same L3 cache:
    $ cat /sys/devices/system/cpu/cpu0/cache/index3/shared_cpu_list
    0-31
    $ cat /sys/devices/system/cpu/cpu0/topology/cluster_cpus_list
    0-3
    
    [1] commit c5e22feffdd7 ("topology: Represent clusters of CPUs within a die")
    
    Tested-by: Jie Zhan <zhanjie9@hisilicon.com>
    Reviewed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Reviewed-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Cc: james.clark@arm.com
    Cc: 21cnbao@gmail.com
    Cc: prime.zeng@hisilicon.com
    Cc: Jonathan.Cameron@huawei.com
    Cc: fanghao11@huawei.com
    Cc: linuxarm@huawei.com
    Cc: tim.c.chen@intel.com
    Cc: linux-arm-kernel@lists.infradead.org
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Link: https://lore.kernel.org/r/20240208024026.2691-1-yangyicong@huawei.com
    
    (backported from commit cbc917a1b03bce85f385c1e640c9dcb02ffb9ab0)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I398b271c94dc69f2d5754d3d22824736d4de99c1
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/116406
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- tools/perf/tests/shell/stat+csv_output.sh
+++ tools/perf/tests/shell/stat+csv_output.sh
@@ -41,6 +41,7 @@ function commachecker()
 	;; "--per-socket")	exp=8
 	;; "--per-node")	exp=8
 	;; "--per-die")		exp=8
+	;; "--per-cluster")	exp=8
 	;; "--per-cache")	exp=8
 	esac
 
@@ -78,6 +79,7 @@ then
 	check_system_wide_no_aggr "CSV" "$perf_cmd"
 	check_per_core "CSV" "$perf_cmd"
 	check_per_cache_instance "CSV" "$perf_cmd"
+	check_per_cluster "CSV" "$perf_cmd"
 	check_per_die "CSV" "$perf_cmd"
 	check_per_socket "CSV" "$perf_cmd"
 else
