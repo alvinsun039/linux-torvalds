f3752361d3f150f6b26cb885e23ef74a74b23078
Eric Dumazet <edumazet@google.com>
2024-02-08 14:43:23 +0000

    net-device: move lstats in net_device_read_txrx
    Mainline: c353c7b7ffb7ae6ed8f3339906fe33c8be6cf344
    From: v6.8-rc5
    Severity: Low
    
    task: 526171
    
    dev->lstats is notably used from loopback ndo_start_xmit()
    and other virtual drivers.
    
    Per cpu stats updates are dirtying per-cpu data,
    but the pointer itself is read-only.
    
    Fixes: 43a71cd66b9c ("net-device: reorganize net_device fast path variables")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Coco Li <lixiaoyan@google.com>
    Cc: Simon Horman <horms@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (backported from commit c353c7b7ffb7ae6ed8f3339906fe33c8be6cf344)
    
    Signed-off-by: zhulei <zhulei@kylinos.cn>
    Change-Id: Iaaa30feb21a436d0f1c7be37a4ec98e194ea4e4b
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/115579
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 危荣广 <weirongguang@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2137,6 +2137,11 @@ struct net_device {
 
 	/* TXRX read-mostly hotpath */
 	__cacheline_group_begin(net_device_read_txrx);
+	union {
+		struct pcpu_lstats __percpu		*lstats;
+		struct pcpu_sw_netstats __percpu	*tstats;
+		struct pcpu_dstats __percpu		*dstats;
+	};
 	unsigned int		flags;
 	unsigned short		hard_header_len;
 	netdev_features_t	features;
@@ -2392,11 +2397,6 @@ struct net_device {
 	enum netdev_ml_priv_type	ml_priv_type;
 
 	enum netdev_stat_type		pcpu_stat_type:8;
-	union {
-		struct pcpu_lstats __percpu		*lstats;
-		struct pcpu_sw_netstats __percpu	*tstats;
-		struct pcpu_dstats __percpu		*dstats;
-	};
 
 #if IS_ENABLED(CONFIG_GARP)
 	struct garp_port __rcu	*garp_port;
