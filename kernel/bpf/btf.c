6a9fd089d1fc6853efac2926b02d7c9ad0279399
Kui-Feng Lee <thinker.li@gmail.com>
2024-02-08 18:37:48 -0800

    bpf: Move __kfunc_param_match_suffix() to btf.c.
    Mainline: 6115a0aeef01aef152ad7738393aad11422bfb82
    From: v6.9-rc1
    Severity: Moderate
    
    task: 225067
    
    Move __kfunc_param_match_suffix() to btf.c and rename it as
    btf_param_match_suffix(). It can be reused by bpf_struct_ops later.
    
    Signed-off-by: Kui-Feng Lee <thinker.li@gmail.com>
    Link: https://lore.kernel.org/r/20240209023750.1153905-3-thinker.li@gmail.com
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    
    (backported from commit 6115a0aeef01aef152ad7738393aad11422bfb82)
    
    Conflicts:
            kernel/bpf/btf.c
            kernel/bpf/verifier.c
    
    Signed-off-by: chenyuan <chenyuan@kylinos.cn>
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ib4ff428c352dcc7e8e83a63133f10e474228a089
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/125656
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -8652,3 +8652,21 @@ btf_add_struct_ops(struct btf *btf, struct bpf_struct_ops *st_ops)
 
 	return 0;
 }
+
+bool btf_param_match_suffix(const struct btf *btf,
+			    const struct btf_param *arg,
+			    const char *suffix)
+{
+	int suffix_len = strlen(suffix), len;
+	const char *param_name;
+
+	/* In the future, this can be ported to use BTF tagging */
+	param_name = btf_name_by_offset(btf, arg->name_off);
+	if (str_is_empty(param_name))
+		return false;
+	len = strlen(param_name);
+	if (len <= suffix_len)
+		return false;
+	param_name += len - suffix_len;
+	return !strncmp(param_name, suffix, suffix_len);
+}
