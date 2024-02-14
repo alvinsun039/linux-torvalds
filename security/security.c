662344aaff1f4e531bd3e5d76fcd5e21ac1ec03b
Jann Horn <jannh@google.com>
2024-02-14 17:05:38 +0100

    lsm: fix integer overflow in lsm_set_self_attr() syscall
    Mainline: d8bdd795d383a23e38ac48a40d3d223caf47b290
    From: v6.8-rc5
    Severity: Moderate
    
    task: 369031
    
    security_setselfattr() has an integer overflow bug that leads to
    out-of-bounds access when userspace provides bogus input:
    `lctx->ctx_len + sizeof(*lctx)` is checked against `lctx->len` (and,
    redundantly, also against `size`), but there are no checks on
    `lctx->ctx_len`.
    Therefore, userspace can provide an `lsm_ctx` with `->ctx_len` set to a
    value between `-sizeof(struct lsm_ctx)` and -1, and this bogus `->ctx_len`
    will then be passed to an LSM module as a buffer length, causing LSM
    modules to perform out-of-bounds accesses.
    
    The following reproducer will demonstrate this under ASAN (if AppArmor is
    loaded as an LSM):
    
    ```
    
    struct lsm_ctx {
      uint64_t id;
      uint64_t flags;
      uint64_t len;
      uint64_t ctx_len;
      char ctx[];
    };
    
    int main(void) {
      size_t size = sizeof(struct lsm_ctx);
      struct lsm_ctx *ctx = malloc(size);
      ctx->id = 104/*LSM_ID_APPARMOR*/;
      ctx->flags = 0;
      ctx->len = size;
      ctx->ctx_len = -sizeof(struct lsm_ctx);
      syscall(
        460/*__NR_lsm_set_self_attr*/,
        /*attr=*/  100/*LSM_ATTR_CURRENT*/,
        /*ctx=*/   ctx,
        /*size=*/  size,
        /*flags=*/ 0
      );
    }
    ```
    
    Fixes: a04a1198088a ("LSM: syscalls for current process attributes")
    Signed-off-by: Jann Horn <jannh@google.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    [PM: subj tweak, removed ref to ASAN splat that isn't included]
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    (backported from commit d8bdd795d383a23e38ac48a40d3d223caf47b290)
    
    Signed-off-by: chenyuan <chenyuan@kylinos.cn>
    Signed-off-by: WeiLixiang <weilixiang@kylinos.cn>
    Change-Id: I966bdbe8f7b3be8c7baa1c013730c6cdc1fcd9df
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/139817
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- security/security.c
+++ security/security.c
@@ -29,6 +29,7 @@
 #include <linux/backing-dev.h>
 #include <linux/string.h>
 #include <linux/msg.h>
+#include <linux/overflow.h>
 #include <net/flow.h>
 #ifdef CONFIG_CREDP
 #include <asm/iee-cred.h>
@@ -4036,6 +4037,7 @@ int security_setselfattr(unsigned int attr, struct lsm_ctx __user *uctx,
 	struct security_hook_list *hp;
 	struct lsm_ctx *lctx;
 	int rc = LSM_RET_DEFAULT(setselfattr);
+	u64 required_len;
 
 	if (flags)
 		return -EINVAL;
@@ -4048,8 +4050,9 @@ int security_setselfattr(unsigned int attr, struct lsm_ctx __user *uctx,
 	if (IS_ERR(lctx))
 		return PTR_ERR(lctx);
 
-	if (size < lctx->len || size < lctx->ctx_len + sizeof(*lctx) ||
-	    lctx->len < lctx->ctx_len + sizeof(*lctx)) {
+	if (size < lctx->len ||
+	    check_add_overflow(sizeof(*lctx), lctx->ctx_len, &required_len) ||
+	    lctx->len < required_len) {
 		rc = -EINVAL;
 		goto free_out;
 	}
