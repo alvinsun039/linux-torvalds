d6679d1b6763112fe8bdddc3dafc41893196b853
Marc Zyngier <maz@kernel.org>
2024-02-14 13:18:26 +0000

    KVM: arm64: Snapshot all non-zero RES0/RES1 sysreg fields for later checking
    Mainline: b80b701d5a67d07f4df4a21e09cb31f6bc1feeca
    From: v6.9-rc1
    Severity: Low
    
    As KVM now strongly relies on accurately handling the RES0/RES1 bits
    on a number of paths, add a compile-time checker that will blow in
    the face of the innocent bystander, should they try to sneak in an
    update that changes any of these RES0/RES1 fields.
    
    It is expected that such an update will come with the relevant
    KVM update if needed.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20240214131827.2856277-26-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    
    (backported from commit b80b701d5a67d07f4df4a21e09cb31f6bc1feeca)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I9e3a06d60e212c5e1d164d4a925cd13beb4a2ba2
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/166016
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -31,6 +31,7 @@
 
 #include <trace/events/kvm.h>
 
+#include "check-res-bits.h"
 #include "sys_regs.h"
 #include "vgic/vgic.h"
 
@@ -4074,6 +4075,8 @@ int __init kvm_sys_reg_table_init(void)
 	bool valid = true;
 	unsigned int i;
 	int ret = 0;
+
+	check_res_bits();
 
 	/* Make sure tables are unique and in order. */
 	valid &= check_sysreg_table(sys_reg_descs, ARRAY_SIZE(sys_reg_descs), false);
