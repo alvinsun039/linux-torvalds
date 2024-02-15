da3909a57d85828b94e74d1e188d623b5309d71a
Roberto Sassu <roberto.sassu@huawei.com>
2024-02-15 11:31:10 +0100

    evm: Move to LSM infrastructure
    Mainline: 9238311176115aac1b1a86e8e968c04ebec747a1
    From: v6.9-rc1
    Severity: Low
    
    task: 534723
    
    As for IMA, move hardcoded EVM function calls from various places in the
    kernel to the LSM infrastructure, by introducing a new LSM named 'evm'
    (last and always enabled like 'ima'). The order in the Makefile ensures
    that 'evm' hooks are executed after 'ima' ones.
    
    Make EVM functions as static (except for evm_inode_init_security(), which
    is exported), and register them as hook implementations in init_evm_lsm().
    Also move the inline functions evm_inode_remove_acl(),
    evm_inode_post_remove_acl(), and evm_inode_post_set_acl() from the public
    evm.h header to evm_main.c.
    
    Unlike before (see commit to move IMA to the LSM infrastructure),
    evm_inode_post_setattr(), evm_inode_post_set_acl(),
    evm_inode_post_remove_acl(), and evm_inode_post_removexattr() are not
    executed for private inodes.
    
    Finally, add the LSM_ID_EVM case in lsm_list_modules_test.c
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    Acked-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    (backported from commit 9238311176115aac1b1a86e8e968c04ebec747a1)
    
    Change-Id: I209fc927e929c6e32a8dc206363b642153be6dd9
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/120286
    Reviewed-by: 孟德慧 <mengdehui@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/attr.c
+++ fs/attr.c
@@ -16,7 +16,6 @@
 #include <linux/fcntl.h>
 #include <linux/filelock.h>
 #include <linux/security.h>
-#include <linux/evm.h>
 
 /**
  * setattr_should_drop_sgid - determine whether the setgid bit needs to be
@@ -500,7 +499,6 @@ int notify_change(struct mnt_idmap *idmap, struct dentry *dentry,
 	if (!error) {
 		fsnotify_change(dentry, ia_valid);
 		security_inode_post_setattr(idmap, dentry, ia_valid);
-		evm_inode_post_setattr(idmap, dentry, ia_valid);
 	}
 
 	return error;
