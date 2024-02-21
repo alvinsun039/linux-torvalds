fc5564921aaff62e00ab6a2cd17432da96cacb09
Lukasz Luba <lukasz.luba@arm.com>
2024-02-21 14:25:50 +0000

    PM: EM: Fix nr_states warnings in static checks
    Mainline: 3a561ea2413ea5a740f3b1d6b5355d46f88a7456
    From: v6.9-rc1
    Severity: Moderate
    
    task: 594019
    
    During the static checks nr_states has been mentioned by the kernel test
    robot. Fix the warning in those 2 places.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    K2CI-Arch: All
    Change-Id: If5c9b12e59ed509b73228e22932cb331071f19cf
    Signed-off-by: Yaxiong Tian <tianyaxiong@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/175631
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/power/energy_model.c
+++ kernel/power/energy_model.c
@@ -284,6 +284,7 @@ static int em_compute_costs(struct device *dev, struct em_perf_state *table,
  * em_dev_compute_costs() - Calculate cost values for new runtime EM table
  * @dev		: Device for which the EM table is to be updated
  * @table	: The new EM table that is going to get the costs calculated
+ * @nr_states	: Number of performance states
  *
  * Calculate the em_perf_state::cost values for new runtime EM table. The
  * values are used for EAS during task placement. It also calculates and sets
@@ -732,7 +733,6 @@ static void em_check_capacity_update(void)
 		struct cpufreq_policy *policy;
 		unsigned long em_max_perf;
 		struct device *dev;
-		int nr_states;
 
 		if (cpumask_test_cpu(cpu, cpu_done_mask))
 			continue;
@@ -753,7 +753,6 @@ static void em_check_capacity_update(void)
 		cpumask_or(cpu_done_mask, cpu_done_mask,
 			   em_span_cpus(pd));
 
-		nr_states = pd->nr_perf_states;
 		cpu_capacity = arch_scale_cpu_capacity(cpu);
 
 		rcu_read_lock();
