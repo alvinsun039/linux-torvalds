e457945b45ebd465f0a8785c2c42f055ed1559b2
Eric Dumazet <edumazet@google.com>
2024-02-28 13:54:28 +0000

    ipv6: annotate data-races around cnf.mtu6
    Mainline: e7135f484994494a38071e3653a83d21d305f50c
    From: v6.9-rc1
    Severity: Moderate
    
    task: 554291
    
    idev->cnf.mtu6 might be read locklessly, add appropriate READ_ONCE()
    and WRITE_ONCE() annotations.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (backported from commit e7135f484994494a38071e3653a83d21d305f50c)
    
    Change-Id: I192b01a671a396474f9db63746fcb1d55f0ef3ee
    Signed-off-by: Yun Lu <luyun@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/159354
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -3649,7 +3649,7 @@ static int addrconf_notify(struct notifier_block *this, unsigned long event,
 
 		if (idev) {
 			rt6_mtu_change(dev, dev->mtu);
-			idev->cnf.mtu6 = dev->mtu;
+			WRITE_ONCE(idev->cnf.mtu6, dev->mtu);
 			break;
 		}
 
@@ -3741,7 +3741,7 @@ static int addrconf_notify(struct notifier_block *this, unsigned long event,
 			if (idev->cnf.mtu6 != dev->mtu &&
 			    dev->mtu >= IPV6_MIN_MTU) {
 				rt6_mtu_change(dev, dev->mtu);
-				idev->cnf.mtu6 = dev->mtu;
+				WRITE_ONCE(idev->cnf.mtu6, dev->mtu);
 			}
 			idev->tstamp = jiffies;
 			inet6_ifinfo_notify(RTM_NEWLINK, idev);
