dbac483d8f561ff2b2971215f60ddc7eb01a2870
Chengchang Tang <tangchengchang@huawei.com>
2024-03-01 16:36:55 +0800

    OPENEULER: RDMA/hns: Support congestion control algorithm parameter configuration
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 218886
    
    hns roce support 4 congestion control algorithms. Each algorihm
    involves multiple parameters. This patch add port sysfs directory
    for each algorithm, which allows users to modify the parameters
    of these algorithms.
    
    K2CI-Arch: Arm64
    Link: https://gitee.com/openeuler/kernel/issues/I95PFR
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Juan Zhou <zhoujuan51@h-partners.com>
    Signed-off-by: taowei <taowei@kylinos.cn>
    Change-Id: I2c020e395fc7c51375fa0db8fe2dbab057adb76f
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/117325
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/infiniband/hw/hns/hns_roce_device.h
+++ drivers/infiniband/hw/hns/hns_roce_device.h
@@ -801,6 +801,14 @@ struct hns_roce_eq_table {
 	struct hns_roce_eq	*eq;
 };
 
+enum hns_roce_scc_algo {
+	HNS_ROCE_SCC_ALGO_DCQCN = 0,
+	HNS_ROCE_SCC_ALGO_LDCP,
+	HNS_ROCE_SCC_ALGO_HC3,
+	HNS_ROCE_SCC_ALGO_DIP,
+	HNS_ROCE_SCC_ALGO_TOTAL,
+};
+
 struct hns_roce_caps {
 	u64		fw_ver;
 	u8		num_ports;
@@ -1043,6 +1051,20 @@ struct hns_roce_hw {
 				u64 *stats, u32 port, int *hw_counters);
 	const struct ib_device_ops *hns_roce_dev_ops;
 	const struct ib_device_ops *hns_roce_dev_srq_ops;
+	int (*config_scc_param)(struct hns_roce_dev *hr_dev,
+				enum hns_roce_scc_algo algo);
+	int (*query_scc_param)(struct hns_roce_dev *hr_dev,
+			       enum hns_roce_scc_algo alog);
+};
+
+#define HNS_ROCE_SCC_PARAM_SIZE 4
+struct hns_roce_scc_param {
+	__le32 param[HNS_ROCE_SCC_PARAM_SIZE];
+	u32 lifespan;
+	unsigned long timestamp;
+	enum hns_roce_scc_algo algo_type;
+	struct delayed_work scc_cfg_dwork;
+	struct hns_roce_dev *hr_dev;
 };
 
 struct hns_roce_dev {
@@ -1258,6 +1280,8 @@ static inline u8 get_tclass(const struct ib_global_route *grh)
 	       grh->traffic_class >> DSCP_SHIFT : grh->traffic_class;
 }
 
+extern const struct attribute_group *hns_attr_port_groups[];
+
 void hns_roce_init_uar_table(struct hns_roce_dev *dev);
 int hns_roce_uar_alloc(struct hns_roce_dev *dev, struct hns_roce_uar *uar);
 
@@ -1399,6 +1423,8 @@ struct hns_user_mmap_entry *
 hns_roce_user_mmap_entry_insert(struct ib_ucontext *ucontext, u64 address,
 				size_t length,
 				enum hns_roce_mmap_type mmap_type);
+void hns_roce_register_sysfs(struct hns_roce_dev *hr_dev);
+void hns_roce_unregister_sysfs(struct hns_roce_dev *hr_dev);
 void hns_roce_add_unfree_umem(struct hns_roce_user_db_page *user_page,
 			     struct hns_roce_dev *hr_dev);
 void hns_roce_free_unfree_umem(struct hns_roce_dev *hr_dev);
