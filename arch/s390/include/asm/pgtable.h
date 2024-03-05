ca80b4a35e391817664a085ce7b0cda17f58c761
Peter Xu <peterx@redhat.com>
2024-03-05 12:37:50 +0800

    mm/treewide: align up pXd_leaf() retval across archs
    Mainline: c05995b7ec2a73bf813a8944978e175f8e4ec3ac
    From: v6.9-rc1
    Severity: Moderate
    
    task: 216398
    
    Even if pXd_leaf() API is defined globally, it's not clear on the retval,
    and there are three types used (bool, int, unsigned log).
    
    Always return a boolean for pXd_leaf() APIs.
    
    Link: https://lkml.kernel.org/r/20240305043750.93762-11-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Mike Rapoport (IBM) <rppt@kernel.org>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Kirill A. Shutemov <kirill@shutemov.name>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: "Naveen N. Rao" <naveen.n.rao@linux.ibm.com>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit c05995b7ec2a73bf813a8944978e175f8e4ec3ac)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I50525ff7ce7ea1f4d09a00bcedc6922198f129d1
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/129158
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>

--- arch/s390/include/asm/pgtable.h
+++ arch/s390/include/asm/pgtable.h
@@ -715,7 +715,7 @@ static inline int pud_none(pud_t pud)
 }
 
 #define pud_leaf pud_leaf
-static inline int pud_leaf(pud_t pud)
+static inline bool pud_leaf(pud_t pud)
 {
 	if ((pud_val(pud) & _REGION_ENTRY_TYPE_MASK) != _REGION_ENTRY_TYPE_R3)
 		return 0;
@@ -723,7 +723,7 @@ static inline int pud_leaf(pud_t pud)
 }
 
 #define pmd_leaf pmd_leaf
-static inline int pmd_leaf(pmd_t pmd)
+static inline bool pmd_leaf(pmd_t pmd)
 {
 	return (pmd_val(pmd) & _SEGMENT_ENTRY_LARGE) != 0;
 }
