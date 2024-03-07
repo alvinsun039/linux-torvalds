c8d5e1c586fb30bf1f3b69c166c52424995342f3
Zi Yan <ziy@nvidia.com>
2024-03-07 13:18:54 -0500

    mm/huge_memory: skip invalid debugfs new_order input for folio split
    Mainline: 2394aef616cf38fbf2e797c6845ccd35d76ce256
    From: v6.9-rc1
    Severity: Moderate
    
    task: 205742
    
    User can put arbitrary new_order via debugfs for folio split test.
    Although new_order check is added to split_huge_page_to_list_order() in
    the prior commit, these two additional checks can avoid unnecessary folio
    locking and split_folio_to_order() calls.
    
    Link: https://lkml.kernel.org/r/20240307181854.138928-2-zi.yan@sent.com
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Closes: https://lore.kernel.org/linux-mm/7dda9283-b437-4cf8-ab0d-83c330deb9c0@moroto.mountain/
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 2394aef616cf38fbf2e797c6845ccd35d76ce256)
    
    K2CI-Arch: All
    Signed-off-by: liujinlong <liujinlong@kylinos.cn>
    Change-Id: I3b46eaab54ef99acc006177054ab481a7c291e47
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/116729
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -3345,6 +3345,9 @@ static int split_huge_pages_pid(int pid, unsigned long vaddr_start,
 		if (!is_transparent_hugepage(folio))
 			goto next;
 
+		if (new_order >= folio_order(folio))
+			goto next;
+
 		total++;
 		/*
 		 * For folios with private, split_huge_page_to_list_to_order()
@@ -3411,6 +3414,9 @@ static int split_huge_pages_in_file(const char *file_path, pgoff_t off_start,
 
 		total++;
 		nr_pages = folio_nr_pages(folio);
+
+		if (new_order >= folio_order(folio))
+			goto next;
 
 		if (!folio_trylock(folio))
 			goto next;
