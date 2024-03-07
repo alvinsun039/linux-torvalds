418e3e24ed6dcb736f76092c8767d0954d50913d
Mickaël Salaün <mic@digikod.net>
2024-03-07 10:39:23 +0100

    landlock: Rename "ptrace" files to "task"
    Mainline: e3e37fe022a486d83c71eacb59fb5b6b0ebdbf78
    From: v6.9-rc1
    Severity: Moderate
    
    task: 503429
    
    ptrace.[ch] are currently only used for the ptrace LSM hooks but their
    scope will expand with IPCs and audit support.  Rename ptrace.[ch] to
    task.[ch], which better reflect their content.  Similarly, rename
    landlock_add_ptrace_hooks() to landlock_add_task_hooks().  Keep header
    files for now.
    
    Cc: Günther Noack <gnoack@google.com>
    Cc: Paul Moore <paul@paul-moore.com>
    Link: https://lore.kernel.org/r/20240307093923.1466071-2-mic@digikod.net
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    
    (backported from commit e3e37fe022a486d83c71eacb59fb5b6b0ebdbf78)
    
    Change-Id: If026427229970b9d7383d96548cd61890f43d660
    Signed-off-by: chenyuan <chenyuan@kylinos.cn>

--- security/landlock/setup.c
+++ security/landlock/setup.c
@@ -14,8 +14,8 @@
 #include "cred.h"
 #include "fs.h"
 #include "net.h"
-#include "ptrace.h"
 #include "setup.h"
+#include "task.h"
 
 bool landlock_initialized __ro_after_init = false;
 
@@ -34,7 +34,7 @@ struct lsm_blob_sizes landlock_blob_sizes __ro_after_init = {
 static int __init landlock_init(void)
 {
 	landlock_add_cred_hooks();
-	landlock_add_ptrace_hooks();
+	landlock_add_task_hooks();
 	landlock_add_fs_hooks();
 	landlock_add_net_hooks();
 	landlock_initialized = true;
