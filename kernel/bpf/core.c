03d69fa180c0915271889dbde295713038d4bfa4
Alexei Starovoitov <ast@kernel.org>
2024-03-07 17:08:02 -0800

    bpf: Add x86-64 JIT support for bpf_addr_space_cast instruction.
    Mainline: 142fd4d2dcf58b1720a6af644f31de1a5551f219
    From: v6.9-rc1
    Severity: Low
    
    task: 225067
    
    LLVM generates bpf_addr_space_cast instruction while translating
    pointers between native (zero) address space and
    __attribute__((address_space(N))).
    The addr_space=1 is reserved as bpf_arena address space.
    
    rY = addr_space_cast(rX, 0, 1) is processed by the verifier and
    converted to normal 32-bit move: wX = wY
    
    rY = addr_space_cast(rX, 1, 0) has to be converted by JIT:
    
    aux_reg = upper_32_bits of arena->user_vm_start
    aux_reg <<= 32
    wX = wY // clear upper 32 bits of dst register
    if (wX) // if not zero add upper bits of user_vm_start
      wX |= aux_reg
    
    JIT can do it more efficiently:
    
    mov dst_reg32, src_reg32  // 32-bit move
    shl dst_reg, 32
    or dst_reg, user_vm_start
    rol dst_reg, 32
    xor r11, r11
    test dst_reg32, dst_reg32 // check if lower 32-bit are zero
    cmove r11, dst_reg	  // if so, set dst_reg to zero
    			  // Intel swapped src/dst register encoding in CMOVcc
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/bpf/20240308010812.89848-5-alexei.starovoitov@gmail.com
    
    Conflicts:
    	arch/x86/net/bpf_jit_comp.c
    	include/linux/filter.h
    	kernel/bpf/core.c
    
    Signed-off-by: chenyuan <chenyuan@kylinos.cn>
    Change-Id: I7872b8c483e1a28acc9205622b2640488f868c3e
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/125669
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -2915,6 +2915,11 @@ bool __weak bpf_jit_supports_far_kfunc_call(void)
 	return false;
 }
 
+bool __weak bpf_jit_supports_arena(void)
+{
+	return false;
+}
+
 /* To execute LD_ABS/LD_IND instructions __bpf_prog_run() may call
  * skb_copy_bits(), so provide a weak definition of it for NET-less config.
  */
