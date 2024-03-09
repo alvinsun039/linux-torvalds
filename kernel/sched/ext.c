2d275b74dbb3c720a02e91fa41dd078fda77e870
Andrii Nakryiko <andrii@kernel.org>
2024-03-08 16:47:39 -0800

    bpf: move sleepable flag from bpf_prog_aux to bpf_prog
    Mainline: 66c8473135c62f478301a0e5b3012f203562dfa6
    From: v6.9-rc1
    Severity: Low
    
    task: 501509
    
    prog->aux->sleepable is checked very frequently as part of (some) BPF
    program run hot paths. So this extra aux indirection seems wasteful and
    on busy systems might cause unnecessary memory cache misses.
    
    Let's move sleepable flag into prog itself to eliminate unnecessary
    pointer dereference.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Message-ID: <20240309004739.2961431-1-andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Change-Id: I9bffe7fcf9041e9bcbb491933b775c72cae8a856
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/90847
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -5454,7 +5454,7 @@ static int bpf_scx_check_member(const struct btf_type *t,
 	case offsetof(struct sched_ext_ops, exit):
 		break;
 	default:
-		if (prog->aux->sleepable)
+		if (prog->sleepable)
 			return -EINVAL;
 	}
 
