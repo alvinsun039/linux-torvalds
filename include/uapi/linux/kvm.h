d103138be72e336c7fa22a79c14e5b637d79e82c
Xin Jiang <jiangxin@hygon.cn>
2024-03-11 12:06:54 +0800

    HYGON: KVM: Define CSV3 key management command id
    Mainline: KYLIN-only
    Severity: Important
    
    task: 216398
    
    Define Hygon CSV3 key management command id and structure. The
    command definition is available in Hygon CSV3 spec.
    
    Signed-off-by: Xin Jiang <jiangxin@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: shitao <shitao@kylinos.cn>
    Change-Id: I0604900961a1f2c1e85beba0780af1d622eb66b8
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/114326
    Reviewed-by: 张浩 <zhanghao1@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -2255,6 +2255,27 @@ struct kvm_csv_init {
 #define KVM_CONTROL_PRE_SYSTEM_RESET    _IO(KVMIO, 0xe8)
 #define KVM_CONTROL_POST_SYSTEM_RESET   _IO(KVMIO, 0xe9)
 
+/* CSV3 command */
+enum csv3_cmd_id {
+	KVM_CSV3_NR_MIN = 0xc0,
+
+	KVM_CSV3_INIT = KVM_CSV3_NR_MIN,
+	KVM_CSV3_LAUNCH_ENCRYPT_DATA,
+	KVM_CSV3_LAUNCH_ENCRYPT_VMCB,
+
+	KVM_CSV3_NR_MAX,
+};
+
+struct kvm_csv3_init_data {
+	__u64 nodemask;
+};
+
+struct kvm_csv3_launch_encrypt_data {
+	__u64 gpa;
+	__u64 uaddr;
+	__u32 len;
+};
+
 /* Available with KVM_CAP_MEMORY_ATTRIBUTES */
 #define KVM_SET_MEMORY_ATTRIBUTES              _IOW(KVMIO,  0xd2, struct kvm_memory_attributes)
 
