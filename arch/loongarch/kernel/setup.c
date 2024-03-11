68e139ee34296a9804f3666db11a481c477f0458
Tiezhu Yang <yangtiezhu@loongson.cn>
2024-03-11 22:23:47 +0800

    LoongArch: Add ORC stack unwinder support
    Mainline: cb8a2ef0848ca80d67d6d56e2df757cfdf6b3355
    From: v6.9-rc1
    Severity: Moderate
    
    task: 217982
    
    The kernel CONFIG_UNWINDER_ORC option enables the ORC unwinder, which is
    similar in concept to a DWARF unwinder. The difference is that the format
    of the ORC data is much simpler than DWARF, which in turn allows the ORC
    unwinder to be much simpler and faster.
    
    The ORC data consists of unwind tables which are generated by objtool.
    After analyzing all the code paths of a .o file, it determines information
    about the stack state at each instruction address in the file and outputs
    that information to the .orc_unwind and .orc_unwind_ip sections.
    
    The per-object ORC sections are combined at link time and are sorted and
    post-processed at boot time. The unwinder uses the resulting data to
    correlate instruction addresses with their stack states at run time.
    
    Most of the logic are similar with x86, in order to get ra info before ra
    is saved into stack, add ra_reg and ra_offset into orc_entry. At the same
    time, modify some arch-specific code to silence the objtool warnings.
    
    Co-developed-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Co-developed-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    
    (backported from commit cb8a2ef0848ca80d67d6d56e2df757cfdf6b3355)
    
    K2CI-Arch: Loongarch
    Signed-off-by: George Guo <guodongtai@kylinos.cn>
    Change-Id: I9e5855575131d927415f98ec4a5a1d90a12f6d40
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/110846
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>

--- arch/loongarch/kernel/setup.c
+++ arch/loongarch/kernel/setup.c
@@ -49,6 +49,7 @@
 #include <asm/setup.h>
 #include <asm/time.h>
 #include "legacy_boot.h"
+#include <asm/unwind.h>
 
 #define SMBIOS_BIOSSIZE_OFFSET		0x09
 #define SMBIOS_BIOSEXTERN_OFFSET	0x13
@@ -666,6 +667,7 @@ static void __init prefill_possible_map(void)
 void __init setup_arch(char **cmdline_p)
 {
 	cpu_probe();
+	unwind_init();
 
 	init_environ();
 	efi_init();
