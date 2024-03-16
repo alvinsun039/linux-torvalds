29967700b5ba796c8a11d6d6ca4397e8053962a1
Yosry Ahmed <yosryahmed@google.com>
2024-03-16 01:58:03 +0000

    mm: memcg: add NULL check to obj_cgroup_put()
    Mainline: 91b71e78b8e478e1a9af36b15ff1d38810572866
    From: v6.10-rc1
    Severity: Low
    
    9 out of 16 callers perform a NULL check before calling obj_cgroup_put().
    Move the NULL check in the function, similar to mem_cgroup_put().  The
    unlikely() NULL check in current_objcg_update() was left alone to avoid
    dropping the unlikey() annotation as this a fast path.
    
    Link: https://lkml.kernel.org/r/20240316015803.2777252-1-yosryahmed@google.com
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 91b71e78b8e478e1a9af36b15ff1d38810572866)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I7c9cfbee2879c50373bfd39a20734870c4663901
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/88432
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/bpf/memalloc.c
+++ kernel/bpf/memalloc.c
@@ -754,8 +754,7 @@ void bpf_mem_alloc_destroy(struct bpf_mem_alloc *ma)
 			rcu_in_progress += atomic_read(&c->call_rcu_ttrace_in_progress);
 			rcu_in_progress += atomic_read(&c->call_rcu_in_progress);
 		}
-		if (ma->objcg)
-			obj_cgroup_put(ma->objcg);
+		obj_cgroup_put(ma->objcg);
 		destroy_mem_alloc(ma, rcu_in_progress);
 	}
 	if (ma->caches) {
@@ -771,8 +770,7 @@ void bpf_mem_alloc_destroy(struct bpf_mem_alloc *ma)
 				rcu_in_progress += atomic_read(&c->call_rcu_in_progress);
 			}
 		}
-		if (ma->objcg)
-			obj_cgroup_put(ma->objcg);
+		obj_cgroup_put(ma->objcg);
 		destroy_mem_alloc(ma, rcu_in_progress);
 	}
 }
