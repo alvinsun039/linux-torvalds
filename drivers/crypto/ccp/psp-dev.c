3a142ede43f400f90baf56b9b8e2353c6918aef5
chench <chench@hygon.cn>
2024-03-18 11:55:07 +0800

    HYGON: crypto: ccp: Add another mailbox interrupt support for PSP sending command to X86
    Mainline: KYLIN-only
    Severity: Important
    
    task: 216398
    
    The existing kernel supports only interrupt for the mailbox interface
    for X86 sending commands to PSP and PSP to ack, e.g. the SEV commands.
    However, some PSP-based security modules in Hygon CPU, such as TPCM
    and TDM(Trusted Dynamic Measuring), needs sending
    commands/notifications proactively to X86 core via interrupt and a 2nd
    mailbox interface. Similar to the existing one, the 2nd mailbox
    consists of a 32-bits command register and two 32-bits data registers.
    
    The PSP interrupt handling needs to add this interrupt support;
    besides, in order to support user defined command handler, a callback
    registration function is also provided. Up to 16 command callbacks is
    supported, which are indexed by command IDs. Currently, command ID 0
    is assigned to TPCM and 1 to TDM, while others are reserved.
    
    Currently, Hygon PSP does not support bootloader info reg, remove
    the value of bootloader_info_reg.
    
    Signed-off-by: chench <chench@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: shitao <shitao@kylinos.cn>
    Change-Id: Iee6741593e27d0753bcd52320500abecc86670cb
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/112026
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/crypto/ccp/psp-dev.c
+++ drivers/crypto/ccp/psp-dev.c
@@ -17,6 +17,8 @@
 #include "platform-access.h"
 #include "dbc.h"
 
+#include "hygon/psp-dev.h"
+
 struct psp_device *psp_master;
 
 static struct psp_device *psp_alloc_struct(struct sp_device *sp)
@@ -187,7 +189,11 @@ int psp_dev_init(struct sp_device *sp)
 	iowrite32(-1, psp->io_regs + psp->vdata->intsts_reg);
 
 	/* Request an irq */
-	ret = sp_request_psp_irq(psp->sp, psp_irq_handler, psp->name, psp);
+	if (is_vendor_hygon()) {
+		ret = sp_request_hygon_psp_irq(psp->sp, psp_irq_handler, psp->name, psp);
+	} else {
+		ret = sp_request_psp_irq(psp->sp, psp_irq_handler, psp->name, psp);
+	}
 	if (ret) {
 		dev_err(dev, "psp: unable to allocate an IRQ\n");
 		goto e_err;
