c0c7d95be6ff8cd23770d5abef80303342c5416a
Ai Chao <aichao@kylinos.cn>
2024-03-19 14:42:43 +0800

    platform/x86: huawei-wmi: use sysfs_emit() instead of sprintf()
    Mainline: 415c33d20a2d985fa9be34bccca2a780c72b14cc
    From: v6.10-rc1
    Severity: Moderate
    
    task: #218078
    
    Follow the advice in Documentation/filesystems/sysfs.rst:
    show() should only use sysfs_emit() or sysfs_emit_at() when formatting
    the value to be returned to user space.
    
    Signed-off-by: Ai Chao <aichao@kylinos.cn>
    Link: https://lore.kernel.org/r/20240319064243.297320-1-aichao@kylinos.cn
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: luoqing <luoqing@kylinos.cn>
    Change-Id: I0430c02254032e94170f140e8905f3f5667f5b65
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/122276
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/platform/x86/huawei-wmi.c
+++ drivers/platform/x86/huawei-wmi.c
@@ -380,7 +380,7 @@ static ssize_t charge_control_start_threshold_show(struct device *dev,
 	if (err)
 		return err;
 
-	return sprintf(buf, "%d\n", start);
+	return sysfs_emit(buf, "%d\n", start);
 }
 
 static ssize_t charge_control_end_threshold_show(struct device *dev,
@@ -393,7 +393,7 @@ static ssize_t charge_control_end_threshold_show(struct device *dev,
 	if (err)
 		return err;
 
-	return sprintf(buf, "%d\n", end);
+	return sysfs_emit(buf, "%d\n", end);
 }
 
 static ssize_t charge_control_thresholds_show(struct device *dev,
@@ -406,7 +406,7 @@ static ssize_t charge_control_thresholds_show(struct device *dev,
 	if (err)
 		return err;
 
-	return sprintf(buf, "%d %d\n", start, end);
+	return sysfs_emit(buf, "%d %d\n", start, end);
 }
 
 static ssize_t charge_control_start_threshold_store(struct device *dev,
@@ -563,7 +563,7 @@ static ssize_t fn_lock_state_show(struct device *dev,
 	if (err)
 		return err;
 
-	return sprintf(buf, "%d\n", on);
+	return sysfs_emit(buf, "%d\n", on);
 }
 
 static ssize_t fn_lock_state_store(struct device *dev,
