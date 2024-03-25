adfb6fa61b762c3746e063a36a19633b6031e58f
Thomas Zimmermann <tzimmermann@suse.de>
2024-03-25 21:06:57 +0100

    drm/ast: Implement polling for VGA and SIL164 connectors
    Mainline: 225a8d0bd93eb87fe49947069075260031bad8af
    From: v6.10-rc1
    Severity: Moderate
    
    Task: #490895
    
    Implement polling for VGA and SIL164 connectors. Set the flag
    DRM_CONNECTOR_POLL_DISCONNECT for each to detect the removal of the
    monitor cable. Implement struct drm_connector_helper_funcs.detect_ctx
    for each type of connector by testing for EDID data.
    
    The helper drm_connector_helper_detect_ctx() implements .detect_ctx()
    on top of the connector's DDC channel. The function can be used by
    other drivers as companion to drm_connector_helper_get_modes().
    
    v6:
    - change helper name to drm_connector_helper_detec_from_ddc()
      (Maxime, Sui)
    v5:
    - share implementation in drm_connector_helper_detect_ctx() (Maxime)
    - test for DDC presence with drm_probe_ddc() (Maxime, Jani)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Acked-by: Sui Jingfeng <sui.jingfeng@linux.dev>
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240325200855.21150-13-tzimmermann@suse.de
    
    K2CI-Arch: All
    Change-Id: I8ca14b7da960a993170fc851078f44e5896ce5cc
    Signed-off-by: Lu Yao <yaolu@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/84949
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/gpu/drm/ast/ast_mode.c
+++ drivers/gpu/drm/ast/ast_mode.c
@@ -1348,6 +1348,7 @@ static int ast_crtc_init(struct drm_device *dev)
 
 static const struct drm_connector_helper_funcs ast_vga_connector_helper_funcs = {
 	.get_modes = drm_connector_helper_get_modes,
+	.detect_ctx = drm_connector_helper_detect_from_ddc,
 };
 
 static const struct drm_connector_funcs ast_vga_connector_funcs = {
@@ -1381,7 +1382,7 @@ static int ast_vga_connector_init(struct drm_device *dev, struct drm_connector *
 	connector->interlace_allowed = 0;
 	connector->doublescan_allowed = 0;
 
-	connector->polled = DRM_CONNECTOR_POLL_CONNECT;
+	connector->polled = DRM_CONNECTOR_POLL_CONNECT | DRM_CONNECTOR_POLL_DISCONNECT;
 
 	return 0;
 }
@@ -1416,6 +1417,7 @@ static int ast_vga_output_init(struct ast_device *ast)
 
 static const struct drm_connector_helper_funcs ast_sil164_connector_helper_funcs = {
 	.get_modes = drm_connector_helper_get_modes,
+	.detect_ctx = drm_connector_helper_detect_from_ddc,
 };
 
 static const struct drm_connector_funcs ast_sil164_connector_funcs = {
@@ -1449,7 +1451,7 @@ static int ast_sil164_connector_init(struct drm_device *dev, struct drm_connecto
 	connector->interlace_allowed = 0;
 	connector->doublescan_allowed = 0;
 
-	connector->polled = DRM_CONNECTOR_POLL_CONNECT;
+	connector->polled = DRM_CONNECTOR_POLL_CONNECT | DRM_CONNECTOR_POLL_DISCONNECT;
 
 	return 0;
 }
