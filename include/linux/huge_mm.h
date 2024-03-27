e89c98f1cdc2d6b1d90c008070da3fa47df71610
Peter Xu <peterx@redhat.com>
2024-03-27 11:23:22 -0400

    mm: make HPAGE_PXD_* macros even if !THP
    Mainline: b979db1611a63b207dbc47706de989ac113ea898
    From: v6.10-rc1
    Severity: Moderate
    
    task: 216398
    
    These macros can be helpful when we plan to merge hugetlb code into
    generic code.  Move them out and define them as long as
    PGTABLE_HAS_HUGE_LEAVES is selected, because there are systems that only
    define HUGETLB_PAGE not THP.
    
    One note here is HPAGE_PMD_SHIFT must be defined even if PMD_SHIFT is not
    defined (e.g.  !CONFIG_MMU case); it (or in other forms, like
    HPAGE_PMD_NR) is already used in lots of common codes without ifdef
    guards.  Use the old trick to let complations work.
    
    Here we only need to differenciate HPAGE_PXD_SHIFT definitions.  All the
    rest macros will be defined based on it.  When at it, move HPAGE_PMD_NR /
    HPAGE_PMD_ORDER over together.
    
    Link: https://lkml.kernel.org/r/20240327152332.950956-4-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Tested-by: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Andrew Jones <andrew.jones@linux.dev>
    Cc: Aneesh Kumar K.V (IBM) <aneesh.kumar@kernel.org>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: James Houghton <jthoughton@google.com>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Kirill A. Shutemov <kirill@shutemov.name>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: "Mike Rapoport (IBM)" <rppt@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit b979db1611a63b207dbc47706de989ac113ea898)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I7acfbab5bf05cf650cc4ee86e30e8f5ac130fd71
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/129172
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -66,9 +66,6 @@ ssize_t single_hugepage_flag_show(struct kobject *kobj,
 extern struct kobj_attribute shmem_enabled_attr;
 extern struct kobj_attribute thpsize_shmem_enabled_attr;
 
-#define HPAGE_PMD_ORDER (HPAGE_PMD_SHIFT-PAGE_SHIFT)
-#define HPAGE_PMD_NR (1<<HPAGE_PMD_ORDER)
-
 /*
  * Mask of all large folio orders supported for anonymous THP; all orders up to
  * and including PMD_ORDER, except order-0 (which is not "huge") and order-1
@@ -93,14 +90,25 @@ extern struct kobj_attribute thpsize_shmem_enabled_attr;
 #define thp_vma_allowable_order(vma, vm_flags, tva_flags, order) \
 	(!!thp_vma_allowable_orders(vma, vm_flags, tva_flags, BIT(order)))
 
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+#ifdef CONFIG_PGTABLE_HAS_HUGE_LEAVES
 #define HPAGE_PMD_SHIFT PMD_SHIFT
-#define HPAGE_PMD_SIZE	((1UL) << HPAGE_PMD_SHIFT)
-#define HPAGE_PMD_MASK	(~(HPAGE_PMD_SIZE - 1))
-
 #define HPAGE_PUD_SHIFT PUD_SHIFT
-#define HPAGE_PUD_SIZE	((1UL) << HPAGE_PUD_SHIFT)
+#else
+#define HPAGE_PMD_SHIFT ({ BUILD_BUG(); 0; })
+#define HPAGE_PUD_SHIFT ({ BUILD_BUG(); 0; })
+#endif
+
+#define HPAGE_PMD_ORDER (HPAGE_PMD_SHIFT-PAGE_SHIFT)
+#define HPAGE_PMD_NR (1<<HPAGE_PMD_ORDER)
+#define HPAGE_PMD_MASK	(~(HPAGE_PMD_SIZE - 1))
+#define HPAGE_PMD_SIZE	((1UL) << HPAGE_PMD_SHIFT)
+
+#define HPAGE_PUD_ORDER (HPAGE_PUD_SHIFT-PAGE_SHIFT)
+#define HPAGE_PUD_NR (1<<HPAGE_PUD_ORDER)
 #define HPAGE_PUD_MASK	(~(HPAGE_PUD_SIZE - 1))
+#define HPAGE_PUD_SIZE	((1UL) << HPAGE_PUD_SHIFT)
+
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
 
 extern unsigned long transparent_hugepage_flags;
 extern unsigned long huge_anon_orders_always;
@@ -428,13 +436,6 @@ bool unmap_huge_pmd_locked(struct vm_area_struct *vma, unsigned long addr,
 			   pmd_t *pmdp, struct folio *folio);
 
 #else /* CONFIG_TRANSPARENT_HUGEPAGE */
-#define HPAGE_PMD_SHIFT ({ BUILD_BUG(); 0; })
-#define HPAGE_PMD_MASK ({ BUILD_BUG(); 0; })
-#define HPAGE_PMD_SIZE ({ BUILD_BUG(); 0; })
-
-#define HPAGE_PUD_SHIFT ({ BUILD_BUG(); 0; })
-#define HPAGE_PUD_MASK ({ BUILD_BUG(); 0; })
-#define HPAGE_PUD_SIZE ({ BUILD_BUG(); 0; })
 
 static inline bool folio_test_pmd_mappable(struct folio *folio)
 {
