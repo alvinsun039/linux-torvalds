a15cea33950681b20ee391549bf3992078593b19
Jackie Liu <liuyun01@kylinos.cn>
2024-03-29 14:37:24 +0800

    KYLIN: machine_t: provide is_xxx for cpu type
    Mainline: KYLIN-only
    Severity: Low
    
    Sometimes, we need to make a certain distinction between the CPU, so
    that some quirk settings can be performed, especially the Phytium
    machine. It has many special settings, and these settings cannot be
    avoided by pci-qurik, nor can it be through the CPU bugs.
    
    This patch provides a intuitive judgment of the CPU type function to
    make a CPU type check, but I do not recommend that you use this
    method for quirk processing and try to set special code through standard
    methods.
    
    K2CI-Arch: All
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Iaecbfcf4e9598b514ac0c2113249163c59163526
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/99670
    Tested-by: k2ci <kernel-bot@kylinos.cn>

---
+++ include/asm-generic/machine_t.h
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/*
+ * Machine type interface of KYLINOS.
+ *
+ * Authors: Jackie Liu <liuyun01@kylinos.cn>
+ * Copyright (C) 2018-2024 KylinSoft Corporation.
+ */
+
+#ifndef __MACHINE_TYPE_H_
+#define __MACHINE_TYPE_H_
+
+static inline bool is_ft1500a(void)
+{
+	return false;
+}
+
+static inline bool is_ft2000a4(void)
+{
+	return false;
+}
+
+static inline bool is_ft2000ahk(void)
+{
+	return false;
+}
+
+static inline bool is_ft2000plus(void)
+{
+	return false;
+}
+
+static inline bool is_ft2000_pc(void)
+{
+	return false;
+}
+
+static inline bool is_ft2500(void)
+{
+	return false;
+}
+
+static inline bool is_ftd2000(void)
+{
+	return false;
+}
+
+static inline bool is_fts5000(void)
+{
+	return false;
+}
+
+static inline bool is_ft_all(void)
+{
+	return false;
+}
+
+static inline bool is_kunpeng920(void)
+{
+	return false;
+}
+
+static inline bool is_hisi(void)
+{
+	return false;
+}
+
+#endif // __MACHINE_TYPE_H_
