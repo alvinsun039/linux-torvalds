4d5d8f4d8d28d025f0f138b9953c2336bc869496
Pablo Neira Ayuso <pablo@netfilter.org>
2024-04-10 18:50:45 +0200

    netfilter: nft_set_pipapo: walk over current view on netlink dump
    Mainline: 29b359cf6d95fd60730533f7f10464e95bd17c73
    From: v6.9-rc5
    Severity: Moderate
    CVE: CVE-2024-27017
    
    task: 217621
    
    The generation mask can be updated while netlink dump is in progress.
    The pipapo set backend walk iterator cannot rely on it to infer what
    view of the datastructure is to be used. Add notation to specify if user
    wants to read/update the set.
    
    Based on patch from Florian Westphal.
    
    Fixes: 2b84e215f874 ("netfilter: nft_set_pipapo: .walk does not deal with generations")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    (backported from commit 29b359cf6d95fd60730533f7f10464e95bd17c73)
    
    K2CI-Arch: All
    Signed-off-by: yicong <yicong@kylinos.cn>
    Change-Id: I55f51bce0017620537edebd1a434cb9fe64586a9
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/128766
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -626,6 +626,7 @@ static void nft_map_deactivate(const struct nft_ctx *ctx, struct nft_set *set)
 {
 	struct nft_set_iter iter = {
 		.genmask	= nft_genmask_next(ctx->net),
+		.type		= NFT_ITER_UPDATE,
 		.fn		= nft_mapelem_deactivate,
 	};
 
@@ -5386,6 +5387,7 @@ int nf_tables_bind_set(const struct nft_ctx *ctx, struct nft_set *set,
 		}
 
 		iter.genmask	= nft_genmask_next(ctx->net);
+		iter.type	= NFT_ITER_UPDATE;
 		iter.skip 	= 0;
 		iter.count	= 0;
 		iter.err	= 0;
@@ -5459,6 +5461,7 @@ static void nft_map_activate(const struct nft_ctx *ctx, struct nft_set *set)
 {
 	struct nft_set_iter iter = {
 		.genmask	= nft_genmask_next(ctx->net),
+		.type		= NFT_ITER_UPDATE,
 		.fn		= nft_mapelem_activate,
 	};
 
@@ -5835,6 +5838,7 @@ static int nf_tables_dump_set(struct sk_buff *skb, struct netlink_callback *cb)
 	args.skb		= skb;
 	args.reset		= reset;
 	args.iter.genmask	= nft_genmask_cur(net);
+	args.iter.type		= NFT_ITER_READ;
 	args.iter.skip		= cb->args[0];
 	args.iter.count		= 0;
 	args.iter.err		= 0;
@@ -7230,6 +7234,7 @@ static int nft_set_flush(struct nft_ctx *ctx, struct nft_set *set, u8 genmask)
 {
 	struct nft_set_iter iter = {
 		.genmask	= genmask,
+		.type		= NFT_ITER_UPDATE,
 		.fn		= nft_setelem_flush,
 	};
 
