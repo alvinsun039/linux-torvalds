99231836a548e45aced1b9d034f0104f6a18461d
Eric Dumazet <edumazet@google.com>
2024-04-11 08:25:29 +0000

    tcp: small optimization when TCP_TW_SYN is processed
    Mainline: d13b05962369bade0fb8e2d55af56492f866cd36
    From: v6.10-rc1
    Severity: Low
    
    task: 507929
    
    When TCP_TW_SYN is processed, we perform a lookup to find
    a listener and jump back in tcp_v6_rcv() and tcp_v4_rcv()
    
    Paolo suggested that we do not have to check if the
    found socket is a TIME_WAIT or NEW_SYN_RECV one.
    
    Suggested-by: Paolo Abeni <pabeni@redhat.com>
    Link: https://lore.kernel.org/netdev/68085c8a84538cacaac991415e4ccc72f45e76c2.camel@redhat.com/
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Neal Cardwell <ncardwell@google.com>
    Link: https://lore.kernel.org/r/20240411082530.907113-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    (backported from commit d13b05962369bade0fb8e2d55af56492f866cd36)
    
    Signed-off-by: Wang Yan <wangyan01@kylinos.cn>
    Change-Id: Idbe0cf1f8cbd9c4bd35fda87f93abdcb84569e88
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/94184
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1784,7 +1784,6 @@ lookup:
 	if (!sk)
 		goto no_tcp_socket;
 
-process:
 	if (sk->sk_state == TCP_TIME_WAIT)
 		goto do_time_wait;
 
@@ -1859,6 +1858,7 @@ process:
 		}
 	}
 
+process:
 	if (static_branch_unlikely(&ip6_min_hopcount)) {
 		/* min_hopcount can be changed concurrently from do_ipv6_setsockopt() */
 		if (unlikely(hdr->hop_limit < READ_ONCE(tcp_inet6_sk(sk)->min_hopcount))) {
