7714b126f31c176aee18d7edc1dc72e84b779d7d
Zack Rusin <zack.rusin@broadcom.com>
2024-04-11 22:55:08 -0400

    drm/vmwgfx: Implement virtual crc generation
    Mainline: 7b0062036c3b71b4a69e244ecf0502c06c4cf5f0
    From: v6.10-rc1
    Severity: Moderate
    
    task: 454530
    
    crc checksums are used to validate the output. Normally they're part
    of the actual display hardware but on virtual stack there's nothing
    to automatically generate them.
    
    Implement crc generation for the vmwgfx stack. This works only on
    screen targets, where it's possibly to easily make sure that the
    guest side contents of the surface matches the host sides output.
    
    Just like the vblank support, crc generation can only be enabled via:
    guestinfo.vmwgfx.vkms_enable = "TRUE"
    option in the vmx file.
    
    Makes IGT's kms_pipe_crc_basic pass and allows a huge number of other
    IGT tests which require CRC generation of the output to actually run
    on vmwgfx. Makes it possible to actually validate a lof of the kms and
    drm functionality with vmwgfx.
    
    Signed-off-by: Zack Rusin <zack.rusin@broadcom.com>
    Acked-by: Martin Krastev <martin.krastev@broadcom.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240412025511.78553-3-zack.rusin@broadcom.com
    
    (backported from commit 7b0062036c3b71b4a69e244ecf0502c06c4cf5f0)
    
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>
    Change-Id: I7ad80b5ff90e13ab200a227130537b3bd57fee38
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/53468
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/gpu/drm/vmwgfx/vmwgfx_drv.h
+++ drivers/gpu/drm/vmwgfx/vmwgfx_drv.h
@@ -643,6 +643,7 @@ struct vmw_private {
 	uint32 *devcaps;
 
 	bool vkms_enabled;
+	struct workqueue_struct *crc_workq;
 
 	/*
 	 * mksGuestStat instance-descriptor and pid arrays
@@ -838,6 +839,7 @@ void vmw_resource_mob_attach(struct vmw_resource *res);
 void vmw_resource_mob_detach(struct vmw_resource *res);
 void vmw_resource_dirty_update(struct vmw_resource *res, pgoff_t start,
 			       pgoff_t end);
+int vmw_resource_clean(struct vmw_resource *res);
 int vmw_resources_clean(struct vmw_bo *vbo, pgoff_t start,
 			pgoff_t end, pgoff_t *num_prefault);
 
