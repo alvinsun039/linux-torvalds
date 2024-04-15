c35530aeb102dae1653fa72f837f00dea6c6c8b3
Johannes Berg <johannes.berg@intel.com>
2024-04-15 11:27:11 +0300

    wifi: mac80211: mlme: handle cross-link CSA
    Mainline: 7ef8f6821d16623a3f6ea279542f9f47ece9ff1e
    From: v6.11-rc1
    Severity: Moderate
    
    task: 496179
    
    Backport note:
    Conflict in:
    net/mac80211/mlme.c
    1. This patch change sdata_info to link_info.
    2. Fix conflict in after rebase.
    
    Origin message:
    If we see a channel switch announcement on one link for
    another, handle that case and start the CSA. The driver
    can react to this in whatever way it needs. The stack
    will have the ability to track it via the RNR/MLE in the
    reporting link's beacon if it sees it for inactive links
    and adjust everything accordingly.
    
    Note that currently the timings for the CSA aren't set,
    the values are only used by the Intel drivers, and they
    don't need this for newer devices that support MLO, so
    I've left it out for now.
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240415112355.4d34b6a31be7.Ie8453979f5805873a8411c99346bcc3810cd6476@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    K2CI-Arch: All
    Change-Id: I4348a9e6c26c40c24d566a6adc02acc862387931
    Signed-off-by: Yi Cong <yicong@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/92073
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- net/mac80211/driver-ops.h
+++ net/mac80211/driver-ops.h
@@ -1153,6 +1153,9 @@ drv_pre_channel_switch(struct ieee80211_sub_if_data *sdata,
 	if (!check_sdata_in_driver(sdata))
 		return -EIO;
 
+	if (!ieee80211_vif_link_active(&sdata->vif, ch_switch->link_id))
+		return 0;
+
 	trace_drv_pre_channel_switch(local, sdata, ch_switch);
 	if (local->ops->pre_channel_switch)
 		ret = local->ops->pre_channel_switch(&local->hw, &sdata->vif,
@@ -1174,6 +1177,9 @@ drv_post_channel_switch(struct ieee80211_link_data *link)
 	if (!check_sdata_in_driver(sdata))
 		return -EIO;
 
+	if (!ieee80211_vif_link_active(&sdata->vif, link->link_id))
+		return 0;
+
 	trace_drv_post_channel_switch(local, sdata);
 	if (local->ops->post_channel_switch)
 		ret = local->ops->post_channel_switch(&local->hw, &sdata->vif,
@@ -1194,6 +1200,9 @@ drv_abort_channel_switch(struct ieee80211_link_data *link)
 	if (!check_sdata_in_driver(sdata))
 		return;
 
+	if (!ieee80211_vif_link_active(&sdata->vif, link->link_id))
+		return;
+
 	trace_drv_abort_channel_switch(local, sdata);
 
 	if (local->ops->abort_channel_switch)
@@ -1211,6 +1220,9 @@ drv_channel_switch_rx_beacon(struct ieee80211_sub_if_data *sdata,
 	lockdep_assert_wiphy(local->hw.wiphy);
 
 	if (!check_sdata_in_driver(sdata))
+		return;
+
+	if (!ieee80211_vif_link_active(&sdata->vif, ch_switch->link_id))
 		return;
 
 	trace_drv_channel_switch_rx_beacon(local, sdata, ch_switch);
