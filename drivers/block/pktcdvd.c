3feff5ebd5cb0e1135c8e1aba40c27e5d1a04a32
Al Viro <viro@zeniv.linux.org.uk>
2024-04-17 00:28:03 -0400

    pktcdvd: sort set_blocksize() calls out
    Mainline: 3a52c03d1ece8f480d6a6c35d92f7c1c6215d2a6
    From: v6.10-rc1
    Severity: Moderate
    
    task: 507937
    
    1) it doesn't make any sense to have ->open() call set_blocksize() on the
    device being opened - the caller will override that anyway.
    
    2) setting block size on underlying device, OTOH, ought to be done when
    we are opening it exclusive - i.e. as part of pkt_open_dev().  Having
    it done at setup time doesn't guarantee us anything about the state
    at the time we start talking to it.  Worse, if you happen to have
    the underlying device containing e.g. ext2 with 4Kb blocks that
    is currently mounted r/o, that set_blocksize() will confuse the hell
    out of filesystem.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    (backported from commit 3a52c03d1ece8f480d6a6c35d92f7c1c6215d2a6)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: If9e0e17711ce665bd1fb4757b20ca99049c91169
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/122425
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/block/pktcdvd.c
+++ drivers/block/pktcdvd.c
@@ -2214,6 +2214,7 @@ static int pkt_open_dev(struct pktcdvd_device *pd, bool write)
 		}
 		dev_info(ddev, "%lukB available on disc\n", lba << 1);
 	}
+	set_blocksize(file_bdev(bdev_file), CD_FRAMESIZE);
 
 	return 0;
 
@@ -2277,11 +2278,6 @@ static int pkt_open(struct gendisk *disk, blk_mode_t mode)
 		ret = pkt_open_dev(pd, mode & BLK_OPEN_WRITE);
 		if (ret)
 			goto out_dec;
-		/*
-		 * needed here as well, since ext2 (among others) may change
-		 * the blocksize at mount time
-		 */
-		set_blocksize(disk->part0, CD_FRAMESIZE);
 	}
 	mutex_unlock(&ctl_mutex);
 	mutex_unlock(&pktcdvd_mutex);
@@ -2534,7 +2530,6 @@ static int pkt_new_dev(struct pktcdvd_device *pd, dev_t dev)
 	__module_get(THIS_MODULE);
 
 	pd->bdev_file = bdev_file;
-	set_blocksize(file_bdev(bdev_file), CD_FRAMESIZE);
 
 	pkt_init_queue(pd);
 
