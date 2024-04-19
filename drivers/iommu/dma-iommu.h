f0353b8e547d09d0762659c6e1514836fb1d9819
Robin Murphy <robin.murphy@arm.com>
2024-04-19 17:54:45 +0100

    iommu/dma: Centralise iommu_setup_dma_ops()
    Mainline: b67483b3c44eaef2f771fa4c712e13f452675a67
    From: v6.10-rc1
    Severity: Low
    
    Task: 567507
    
    It's somewhat hard to see, but arm64's arch_setup_dma_ops() should only
    ever call iommu_setup_dma_ops() after a successful iommu_probe_device(),
    which means there should be no harm in achieving the same order of
    operations by running it off the back of iommu_probe_device() itself.
    This then puts it in line with the x86 and s390 .probe_finalize bodges,
    letting us pull it all into the main flow properly. As a bonus this lets
    us fold in and de-scope the PCI workaround setup as well.
    
    At this point we can also then pull the call up inside the group mutex,
    and avoid having to think about whether iommu_group_store_type() could
    theoretically race and free the domain if iommu_setup_dma_ops() ran just
    *before* iommu_device_use_default_domain() claims it... Furthermore we
    replace one .probe_finalize call completely, since the only remaining
    implementations are now one which only needs to run once for the initial
    boot-time probe, and two which themselves render that path unreachable.
    
    This leaves us a big step closer to realistically being able to unpick
    the variety of different things that iommu_setup_dma_ops() has been
    muddling together, and further streamline iommu-dma into core API flows
    in future.
    
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com> # For Intel IOMMU
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Tested-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/bebea331c1d688b34d9862eefd5ede47503961b8.1713523152.git.robin.murphy@arm.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    Conflicts:
    	drivers/iommu/intel/iommu.c
    	keep intel_iommu_probe_finalize for zhaoxin_kh40000 support.
    
    K2CI-Arch: All
    Change-Id: I7e3e5aeefe0eb09baea18ae6fdf1bc2b2a9618d2
    Signed-off-by: Yufeng Wang <wangyufeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/162455
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/iommu/dma-iommu.h
+++ drivers/iommu/dma-iommu.h
@@ -9,6 +9,8 @@
 
 #ifdef CONFIG_IOMMU_DMA
 
+void iommu_setup_dma_ops(struct device *dev);
+
 int iommu_get_dma_cookie(struct iommu_domain *domain);
 void iommu_put_dma_cookie(struct iommu_domain *domain);
 
@@ -17,14 +19,14 @@ int iommu_dma_init_fq(struct iommu_domain *domain);
 void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list);
 
 extern bool iommu_dma_forcedac;
-static inline void iommu_dma_set_pci_32bit_workaround(struct device *dev)
-{
-	dev->iommu->pci_32bit_workaround = !iommu_dma_forcedac;
-}
 
 int iova_reserve_domain_addr(struct iommu_domain *domain, dma_addr_t start, dma_addr_t end);
 
 #else /* CONFIG_IOMMU_DMA */
+
+static inline void iommu_setup_dma_ops(struct device *dev)
+{
+}
 
 static inline int iommu_dma_init_fq(struct iommu_domain *domain)
 {
@@ -41,10 +43,6 @@ static inline void iommu_put_dma_cookie(struct iommu_domain *domain)
 }
 
 static inline void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list)
-{
-}
-
-static inline void iommu_dma_set_pci_32bit_workaround(struct device *dev)
 {
 }
 
