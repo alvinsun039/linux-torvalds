347bd5d905f074bf9593a4258a8d6d5e0c94d618
Shakeel Butt <shakeel.butt@linux.dev>
2024-04-20 16:25:05 -0700

    memcg: simple cleanup of stats update functions
    Mainline: 91882c1617c15eb2a4e996b0822e0b9cbd854dea
    From: v6.10-rc1
    Severity: Moderate
    
    task: 466594
    
    mod_memcg_lruvec_state() is never called from outside of memcontrol.c and
    with always irq disabled.  So, replace it with the irq disabled version
    and add an assert that irq is disabled in the caller.
    
    Similarly mod_objcg_state() is not called from outside of memcontrol.c, so
    simply make it static and change it's name to __mod_objcg_state().
    
    Link: https://lkml.kernel.org/r/20240420232505.2768428-1-shakeel.butt@linux.dev
    Signed-off-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: T.J. Mercier <tjmercier@google.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 91882c1617c15eb2a4e996b0822e0b9cbd854dea)
    
    K2CI-Arch: All
    Change-Id: I57eef4d223b8123879a66f6b517401e72468acd0
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/67422
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -971,8 +971,6 @@ unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 void mem_cgroup_flush_stats(struct mem_cgroup *memcg);
 void mem_cgroup_flush_stats_ratelimited(struct mem_cgroup *memcg);
 
-void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
-			      int val);
 void __mod_lruvec_kmem_state(void *p, enum node_stat_item idx, int val);
 
 static inline void mod_lruvec_kmem_state(void *p, enum node_stat_item idx,
@@ -982,16 +980,6 @@ static inline void mod_lruvec_kmem_state(void *p, enum node_stat_item idx,
 
 	local_irq_save(flags);
 	__mod_lruvec_kmem_state(p, idx, val);
-	local_irq_restore(flags);
-}
-
-static inline void mod_memcg_lruvec_state(struct lruvec *lruvec,
-					  enum node_stat_item idx, int val)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	__mod_memcg_lruvec_state(lruvec, idx, val);
 	local_irq_restore(flags);
 }
 
@@ -1443,11 +1431,6 @@ static inline void mem_cgroup_flush_stats(struct mem_cgroup *memcg)
 }
 
 static inline void mem_cgroup_flush_stats_ratelimited(struct mem_cgroup *memcg)
-{
-}
-
-static inline void __mod_memcg_lruvec_state(struct lruvec *lruvec,
-					    enum node_stat_item idx, int val)
 {
 }
 
