922471260845633176bcf73845cd2b45457d6b6e
Su Hui <suhui@nfschina.com>
2024-04-24 10:27:25 +0800

    octeontx2-af: fix the double free in rvu_npc_freemem()
    Mainline: 6e965eba43e9724f3e603d7b7cc83e53b23d155e
    From: v6.9-rc6
    Severity: Moderate
    
    task: 435042
    
    Clang static checker(scan-build) warning：
    drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c:line 2184, column 2
    Attempt to free released memory.
    
    npc_mcam_rsrcs_deinit() has released 'mcam->counters.bmap'. Deleted this
    redundant kfree() to fix this double free problem.
    
    Fixes: dd7842878633 ("octeontx2-af: Add new devlink param to configure maximum usable NIX block LFs")
    Signed-off-by: Su Hui <suhui@nfschina.com>
    Reviewed-by: Geetha sowjanya <gakula@marvell.com>
    Reviewed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Hariprasad Kelam <hkelam@marvell.com>
    Link: https://lore.kernel.org/r/20240424022724.144587-1-suhui@nfschina.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    (backported from commit 6e965eba43e9724f3e603d7b7cc83e53b23d155e)
    
    Change-Id: I833bb9c8d0d9da02ab4aa96fa711628b72ba06f2
    Signed-off-by: zhaochenguang <zhaochenguang@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/83050
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
@@ -2193,7 +2193,6 @@ void rvu_npc_freemem(struct rvu *rvu)
 
 	kfree(pkind->rsrc.bmap);
 	npc_mcam_rsrcs_deinit(rvu);
-	kfree(mcam->counters.bmap);
 	if (rvu->kpu_prfl_addr)
 		iounmap(rvu->kpu_prfl_addr);
 	else
