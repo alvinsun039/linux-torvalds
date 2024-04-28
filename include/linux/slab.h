5248aeb9c2d8ef198d7e863e1edf706e320a71b8
Dan Carpenter <dan.carpenter@linaro.org>
2024-04-28 17:26:44 +0300

    mm/slab: make __free(kfree) accept error pointers
    Mainline: cd7eb8f83fcf258f71e293f7fc52a70be8ed0128
    From: v6.9
    Severity: Moderate
    
    task: 564651
    
    Currently, if an automatically freed allocation is an error pointer that
    will lead to a crash.  An example of this is in wm831x_gpio_dbg_show().
    
       171	char *label __free(kfree) = gpiochip_dup_line_label(chip, i);
       172	if (IS_ERR(label)) {
       173		dev_err(wm831x->dev, "Failed to duplicate label\n");
       174		continue;
       175  }
    
    The auto clean up function should check for error pointers as well,
    otherwise we're going to keep hitting issues like this.
    
    Fixes: 54da6a092431 ("locking: Introduce __cleanup() based infrastructure")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    
    (backported from commit cd7eb8f83fcf258f71e293f7fc52a70be8ed0128)
    
    Change-Id: Ib5faea76e7ba9eab21cdadb238d55e08522d1aab
    Signed-off-by: Wang Yan <wangyan01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/152616
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -379,7 +379,7 @@ void kfree(const void *objp);
 void kfree_sensitive(const void *objp);
 size_t __ksize(const void *objp);
 
-DEFINE_FREE(kfree, void *, if (_T) kfree(_T))
+DEFINE_FREE(kfree, void *, if (!IS_ERR_OR_NULL(_T)) kfree(_T))
 
 /**
  * ksize - Report actual allocation size of associated object
@@ -940,7 +940,7 @@ void *kvrealloc_noprof(const void *p, size_t size, gfp_t flags)
 #define kvrealloc(...)				alloc_hooks(kvrealloc_noprof(__VA_ARGS__))
 
 extern void kvfree(const void *addr);
-DEFINE_FREE(kvfree, void *, if (_T) kvfree(_T))
+DEFINE_FREE(kvfree, void *, if (!IS_ERR_OR_NULL(_T)) kvfree(_T))
 
 extern void kvfree_sensitive(const void *addr, size_t len);
 
