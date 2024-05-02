99716996d748646bb5aa6d48ffdf087442e296c6
Puranjay Mohan <puranjay12@gmail.com>
2024-05-02 15:18:53 +0000

    arm64, bpf: add internal-only MOV instruction to resolve per-CPU addrs
    Mainline: 7a4c32222b0e14349a6311e72bf6ebd3e1d1064b
    From: v6.10-rc1
    Severity: Low
    
    task: 512029
    
    Support an instruction for resolving absolute addresses of per-CPU
    data from their per-CPU offsets. This instruction is internal-only and
    users are not allowed to use them directly. They will only be used for
    internal inlining optimizations for now between BPF verifier and BPF
    JITs.
    
    Since commit 7158627686f0 ("arm64: percpu: implement optimised pcpu
    access using tpidr_el1"), the per-cpu offset for the CPU is stored in
    the tpidr_el1/2 register of that CPU.
    
    To support this BPF instruction in the ARM64 JIT, the following ARM64
    instructions are emitted:
    
    mov dst, src		// Move src to dst, if src != dst
    mrs tmp, tpidr_el1/2	// Move per-cpu offset of the current cpu in tmp.
    add dst, dst, tmp	// Add the per cpu offset to the dst.
    
    To measure the performance improvement provided by this change, the
    benchmark in [1] was used:
    
    Before:
    glob-arr-inc   :   23.597 ± 0.012M/s
    arr-inc        :   23.173 ± 0.019M/s
    hash-inc       :   12.186 ± 0.028M/s
    
    After:
    glob-arr-inc   :   23.819 ± 0.034M/s
    arr-inc        :   23.285 ± 0.017M/s
    hash-inc       :   12.419 ± 0.011M/s
    
    [1] https://github.com/anakryiko/linux/commit/8dec900975ef
    
    Signed-off-by: Puranjay Mohan <puranjay12@gmail.com>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/r/20240502151854.9810-4-puranjay@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
     Conflicts:
           arch/arm64/include/asm/insn.h
           arch/arm64/lib/insn.c
           arch/arm64/net/bpf_jit_comp.c
    Change-Id: I6351d03d0636712193c276834092f27c6798b159
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/98149
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -1018,6 +1018,15 @@ static int build_insn(const struct bpf_insn *insn, struct jit_ctx *ctx,
 			emit(A64_ORR(1, tmp, dst, tmp), ctx);
 			emit(A64_MOV(1, dst, tmp), ctx);
 			break;
+		} else if (insn_is_mov_percpu_addr(insn)) {
+			if (dst != src)
+				emit(A64_MOV(1, dst, src), ctx);
+			if (cpus_have_cap(ARM64_HAS_VIRT_HOST_EXTN))
+				emit(A64_MRS_TPIDR_EL2(tmp), ctx);
+			else
+				emit(A64_MRS_TPIDR_EL1(tmp), ctx);
+			emit(A64_ADD(1, dst, dst, tmp), ctx);
+			break;
 		}
 		switch (insn->off) {
 		case 0:
@@ -2595,6 +2604,11 @@ out:
 }
 
 bool bpf_jit_supports_arena(void)
+{
+	return true;
+}
+
+bool bpf_jit_supports_percpu_insn(void)
 {
 	return true;
 }
