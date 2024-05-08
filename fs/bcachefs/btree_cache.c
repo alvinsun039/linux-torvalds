a95d17bb02cb8351a86d8d9c789e51ee3d4b18f6
Kent Overstreet <kent.overstreet@linux.dev>
2024-05-08 01:42:10 -0400

    bcachefs: for_each_bset() declares loop iter
    Mainline: b6fb4269e707bb410e202fb160c8d7a188ef60f8
    From: v6.10-rc1
    Severity: Moderate
    
    task: 205710
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    (backported from commit b6fb4269e707bb410e202fb160c8d7a188ef60f8)
    
    K2CI-Arch: All
    Signed-off-by: Youling Tang <tangyouling@kylinos.cn>
    Change-Id: Icc85062c327b6361f9f505b49dd2b44cc3727680
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/120239
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- fs/bcachefs/btree_cache.c
+++ fs/bcachefs/btree_cache.c
@@ -882,7 +882,6 @@ static struct btree *__bch2_btree_node_get(struct btree_trans *trans, struct btr
 	struct bch_fs *c = trans->c;
 	struct btree_cache *bc = &c->btree_cache;
 	struct btree *b;
-	struct bset_tree *t;
 	bool need_relock = false;
 	int ret;
 
@@ -1002,7 +1001,6 @@ struct btree *bch2_btree_node_get(struct btree_trans *trans, struct btree_path *
 {
 	struct bch_fs *c = trans->c;
 	struct btree *b;
-	struct bset_tree *t;
 	int ret;
 
 	EBUG_ON(level >= BTREE_MAX_DEPTH);
@@ -1079,7 +1077,6 @@ struct btree *bch2_btree_node_get_noiter(struct btree_trans *trans,
 	struct bch_fs *c = trans->c;
 	struct btree_cache *bc = &c->btree_cache;
 	struct btree *b;
-	struct bset_tree *t;
 	int ret;
 
 	EBUG_ON(level >= BTREE_MAX_DEPTH);
