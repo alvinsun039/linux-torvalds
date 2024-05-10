78b864fe09d17031f885dc651a3fe0f45ce64a37
Ming Lei <ming.lei@redhat.com>
2024-05-10 11:50:28 +0800

    io_uring: support to inject result for NOP
    Mainline: deb1e496a83557896fe0cca0b8af01c2a97c0dc6
    From: v6.10-rc1
    Severity: Moderate
    
    task: 526909
    
    Support to inject result for NOP so that we can inject failure from
    userspace. It is very helpful for covering failure handling code in
    io_uring core change.
    
    With nop flags, it becomes possible to add more test features on NOP in
    future.
    
    Suggested-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20240510035031.78874-3-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit deb1e496a83557896fe0cca0b8af01c2a97c0dc6)
    
    K2CI-Arch: All
    Change-Id: Icd3e5033bb8de8b1f91c72fb0f079ebb82f1435f
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151367
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -71,6 +71,7 @@ struct io_uring_sqe {
 		__u32		uring_cmd_flags;
 		__u32		waitid_flags;
 		__u32		futex_flags;
+		__u32		nop_flags;
 	};
 	__u64	user_data;	/* data to be passed back at completion time */
 	/* pack this to avoid bogus arm OABI complaints */
@@ -397,6 +398,13 @@ enum {
 #define IORING_MSG_RING_CQE_SKIP	(1U << 0)
 /* Pass through the flags from sqe->file_index to cqe->flags */
 #define IORING_MSG_RING_FLAGS_PASS	(1U << 1)
+
+/*
+ * IORING_OP_NOP flags (sqe->nop_flags)
+ *
+ * IORING_NOP_INJECT_RESULT	Inject result from sqe->result
+ */
+#define IORING_NOP_INJECT_RESULT	(1U << 0)
 
 /*
  * IO completion data structure (Completion Queue Entry)
