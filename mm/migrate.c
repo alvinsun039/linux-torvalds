18f44cefff0ce1c524d902d5c074b8616c0177f0
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-05-24 13:28:39 +0800

    mm: migrate: simplify __buffer_migrate_folio()
    Mainline: 01878f10f8e01e6ca1040ccc19b76e10ff7678ad
    From: v6.11-rc1
    Severity: Moderate
    
    task: 584739
    
    Patch series "mm: cleanup MIGRATE_SYNC_NO_COPY mode".
    
    Commit 2916ecc0f9d4 ("mm/migrate: new migrate mode MIGRATE_SYNC_NO_COPY")
    introduce a new MIGRATE_SYNC_NO_COPY mode to allow to offload the copy to
    a device DMA engine, which is only used __migrate_device_pages() to decide
    whether or not copy the old page, and the MIGRATE_SYNC_NO_COPY mode only
    used in hmm, a easy way is just to call the folio_migrate_mapping() and
    folio_migrate_flags(), which help to remove the MIGRATE_SYNC_NO_COPY mode.
    
    This patch (of 5):
    
    Use filemap_migrate_folio() helper to simplify __buffer_migrate_folio().
    
    Link: https://lkml.kernel.org/r/20240524052843.182275-1-wangkefeng.wang@huawei.com
    Link: https://lkml.kernel.org/r/20240524052843.182275-2-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Reviewed-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Benjamin LaHaise <bcrl@kvack.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: Jiaqi Yan <jiaqiyan@google.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Naoya Horiguchi <nao.horiguchi@gmail.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 01878f10f8e01e6ca1040ccc19b76e10ff7678ad)
    
    Change-Id: If7cde9dc912c4dddebf716f620466328b8955269
    Signed-off-by: liuye <liuye@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/171306
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- mm/migrate.c
+++ mm/migrate.c
@@ -807,11 +807,9 @@ recheck_buffers:
 		}
 	}
 
-	rc = folio_migrate_mapping(mapping, dst, src, 0);
+	rc = filemap_migrate_folio(mapping, dst, src, mode);
 	if (rc != MIGRATEPAGE_SUCCESS)
 		goto unlock_buffers;
-
-	folio_attach_private(dst, folio_detach_private(src));
 
 	bh = head;
 	do {
@@ -819,12 +817,6 @@ recheck_buffers:
 		bh = bh->b_this_page;
 	} while (bh != head);
 
-	if (mode != MIGRATE_SYNC_NO_COPY)
-		folio_migrate_copy(dst, src);
-	else
-		folio_migrate_flags(dst, src);
-
-	rc = MIGRATEPAGE_SUCCESS;
 unlock_buffers:
 	if (check_refs)
 		spin_unlock(&mapping->private_lock);
