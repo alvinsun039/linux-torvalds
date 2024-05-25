da74306551b3a3870e3c51d24efdca3a213f009a
Perry Yuan <perry.yuan@amd.com>
2024-05-25 23:08:43 +0800

    x86/cpufeatures: Add AMD FAST CPPC feature flag
    Mainline: c7107750b2ffaa4950faa53f84f9c9aa2caac89a
    From: v6.11-rc1
    Severity: Moderate
    
    task: 564671
    
    Some AMD Zen 4 processors support a new feature FAST CPPC which
    allows for a faster CPPC loop due to internal architectural
    enhancements. The goal of this faster loop is higher performance
    at the same power consumption.
    
    Reference:
    See the page 99 of PPR for AMD Family 19h Model 61h rev.B1, docID 56713
    
    Signed-off-by: Perry Yuan <perry.yuan@amd.com>
    Signed-off-by: Xiaojian Du <Xiaojian.Du@amd.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    
    (backported from commit c7107750b2ffaa4950faa53f84f9c9aa2caac89a)
    
    Conflicts:
    	arch/x86/include/asm/cpufeatures.h
    
    K2CI-Arch: Amd64
    Change-Id: I4402bcd8d8a82fa73388c1d6ade07fa4656ddf35
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/152323
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -497,7 +497,7 @@
 #define X86_FEATURE_BHI_CTRL		(21*32+ 2) /* "" BHI_DIS_S HW control available */
 #define X86_FEATURE_CLEAR_BHB_HW	(21*32+ 3) /* "" BHI_DIS_S HW control enabled */
 #define X86_FEATURE_CLEAR_BHB_LOOP_ON_VMEXIT (21*32+ 4) /* "" Clear branch history at vmexit using SW loop */
-#define X86_FEATURE_INDIRECT_THUNK_ITS	(21*32 + 5) /* "" Use thunk for indirect branches in lower half of cacheline */
+#define X86_FEATURE_INDIRECT_THUNK_ITS	(21*32 + 10) /* "" Use thunk for indirect branches in lower half of cacheline */
 
 #define X86_FEATURE_TSA_SQ_NO		(21*32+11) /* "" AMD CPU not vulnerable to TSA-SQ */
 #define X86_FEATURE_TSA_L1_NO		(21*32+12) /* "" AMD CPU not vulnerable to TSA-L1 */
@@ -511,6 +511,7 @@
 #define X86_FEATURE_SM3			(23*32 + 1) /* SM3 instructions */
 #define X86_FEATURE_SM4			(23*32 + 2) /* SM4 instructions */
 #define X86_FEATURE_APX			(21*32+ 9) /* Advanced Performance Extensions */
+#define X86_FEATURE_FAST_CPPC		(21*32 + 5) /* "" AMD Fast CPPC */
 
 /*
  * BUG word(s)
