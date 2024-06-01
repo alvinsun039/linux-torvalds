b9a807d29c545bf26d1c92dda043c3d3a0dc7b1b
Al Viro <viro@zeniv.linux.org.uk>
2024-06-01 00:48:50 -0400

    do_mq_notify(): switch to CLASS(fd)
    Mainline: 54dac3dacc86e388e0cd3934cf2a0b6fc7a06323
    From: v6.13-rc1
    Severity: Low
    
    task: 536731
    
    The only failure exit before fdget() is a return, the only thing done
    after fdput() is transposable with it.
    
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    (backported from commit 54dac3dacc86e388e0cd3934cf2a0b6fc7a06323)
    
    Change-Id: If1236244689ddcc4f6c60981ca1cae4cebbf36b6
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/122482
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -1316,7 +1316,6 @@ SYSCALL_DEFINE5(mq_timedreceive, mqd_t, mqdes, char __user *, u_msg_ptr,
 static int do_mq_notify(mqd_t mqdes, const struct sigevent *notification)
 {
 	int ret;
-	struct fd f;
 	struct sock *sock;
 	struct inode *inode;
 	struct mqueue_inode_info *info;
@@ -1369,8 +1368,8 @@ retry:
 		}
 	}
 
-	f = fdget(mqdes);
-	if (!fd_file(f)) {
+	CLASS(fd, f)(mqdes);
+	if (fd_empty(f)) {
 		ret = -EBADF;
 		goto out;
 	}
@@ -1378,7 +1377,7 @@ retry:
 	inode = file_inode(fd_file(f));
 	if (unlikely(fd_file(f)->f_op != &mqueue_file_operations)) {
 		ret = -EBADF;
-		goto out_fput;
+		goto out;
 	}
 	info = MQUEUE_I(inode);
 
@@ -1416,8 +1415,6 @@ retry:
 		inode->i_atime = inode_set_ctime_current(inode);
 	}
 	spin_unlock(&info->lock);
-out_fput:
-	fdput(f);
 out:
 	if (sock)
 		netlink_detachskb(sock, nc);
