23102191b4d101fd29ab17e10c1f0363d9cc525b
Steven Rostedt (Google) <rostedt@goodmis.org>
2024-06-03 15:07:17 -0400

    function_graph: Add pid tracing back to function graph tracer
    Mainline: df3ec5da6a1e7f6e142680d7c5266d3af187170b
    From: v6.11-rc1
    Severity: Low
    
    Now that the function_graph has a main callback that handles the function
    graph subops tracing, it no longer honors the pid filtering of ftrace. Add
    back this logic in the function_graph code to update the gops callback for
    the entry function to test if it should trace the current task or not.
    
    Link: https://lore.kernel.org/linux-trace-kernel/20240603190822.991720703@goodmis.org
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Cc: Florent Revest <revest@chromium.org>
    Cc: Martin KaFai Lau <martin.lau@linux.dev>
    Cc: bpf <bpf@vger.kernel.org>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Alan Maguire <alan.maguire@oracle.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Guo Ren <guoren@kernel.org>
    Reviewed-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    
    (backported from commit df3ec5da6a1e7f6e142680d7c5266d3af187170b)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I01980c96d1f842b2c961fe107c771e77da78e664
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/134803
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -1042,6 +1042,7 @@ typedef int (*trace_func_graph_ent_t)(struct ftrace_graph_ent *,
 				      struct fgraph_ops *); /* entry */
 
 extern int ftrace_graph_entry_stub(struct ftrace_graph_ent *trace, struct fgraph_ops *gops);
+bool ftrace_pids_enabled(struct ftrace_ops *ops);
 
 #ifdef CONFIG_FUNCTION_GRAPH_TRACER
 
@@ -1050,6 +1051,7 @@ struct fgraph_ops {
 	trace_func_graph_ret_t		retfunc;
 	struct ftrace_ops		ops; /* for the hash lists */
 	void				*private;
+	trace_func_graph_ent_t		saved_func;
 	int				idx;
 };
 
