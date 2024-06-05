dc543618cf056691c934caf1c0f5f99351e78377
Steven Rostedt (Google) <rostedt@goodmis.org>
2024-06-05 16:26:50 -0400

    function_graph: Do not update pid func if CONFIG_DYNAMIC_FTRACE not enabled
    Mainline: 4057fd2cddaea0d6f6c5f8af4a455741f5632259
    From: v6.11-rc1
    Severity: Low
    
    The ftrace subops is only defined if CONFIG_DYNAMIC_FTRACE is enabled. If
    it is not, function tracing is extremely limited, and the subops in the
    ftrace_ops structure is not defined (and will fail to compile). If
    DYNAMIC_FTRACE is not enabled, then function graph filtering will not
    work (as it shouldn't).
    
    Link: https://lore.kernel.org/linux-trace-kernel/20240605202709.096020676@goodmis.org
    
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Fixes: df3ec5da6a1e7 ("function_graph: Add pid tracing back to function graph tracer")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202406051855.9VIYXbTB-lkp@intel.com/
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    
    (backported from commit 4057fd2cddaea0d6f6c5f8af4a455741f5632259)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I71615a00b45b21ffe1a4eca4910f2134483bc323
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/134869
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/trace/fgraph.c
+++ kernel/trace/fgraph.c
@@ -1188,6 +1188,7 @@ void fgraph_update_pid_func(void)
 	if (!(graph_ops.flags & FTRACE_OPS_FL_INITIALIZED))
 		return;
 
+#ifdef CONFIG_DYNAMIC_FTRACE
 	list_for_each_entry(op, &graph_ops.subop_list, list) {
 		if (op->flags & FTRACE_OPS_FL_PID) {
 			gops = container_of(op, struct fgraph_ops, ops);
@@ -1197,6 +1198,7 @@ void fgraph_update_pid_func(void)
 				static_call_update(fgraph_func, gops->entryfunc);
 		}
 	}
+#endif
 }
 
 /* Allocate a return stack for each task */
