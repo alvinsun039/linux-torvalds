8c644d11bc885ce1461246c3717d9e8276dbaf1b
Yang Feng <yangfeng@kylinos.cn>
2024-06-05 10:28:13 +0800

    KYLIN: net: mark 8192fu module support by partner
    Mainline: KYLIN-only
    Severity: Low
    
    task: 222937
    
    This driver is not directly supported by Kylin Software, we mark it
    where from.
    
    Signed-off-by: Yang Feng <yangfeng@kylinos.cn>
    Change-Id: I2e459fd2996d1860feaf9d8883b4b68d8b64a050
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/115813
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- drivers/net/wireless/realtek/rtl8192FU/os_dep/linux/usb_intf.c
+++ drivers/net/wireless/realtek/rtl8192FU/os_dep/linux/usb_intf.c
@@ -1314,6 +1314,7 @@ static int rtw_drv_init(struct usb_interface *pusb_intf, const struct usb_device
 	int i;
 #endif
 
+	mark_partner_supported("8192FU", "Realtek Semiconductor Corp.", THIS_MODULE);
 	/* RTW_INFO("+rtw_drv_init\n"); */
 
 	/* step 0. */
