5eea19a282c6bf546a88dac1cdc97322c9f3e4a3
Yang Feng <yangfeng@kylinos.cn>
2024-06-05 10:25:30 +0800

    KYLIN: net: mark 88x2cu module support by partner
    Mainline: KYLIN-only
    Severity: Low
    
    task: 222939
    
    This driver is not directly supported by Kylin Software, we mark it
    where from.
    
    Signed-off-by: Yang Feng <yangfeng@kylinos.cn>
    Change-Id: I8da64b9d76502a6c344ea4cc5acafff0d00eff5f
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/115812
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- drivers/net/wireless/realtek/rtl88x2CU/os_dep/linux/usb_intf.c
+++ drivers/net/wireless/realtek/rtl88x2CU/os_dep/linux/usb_intf.c
@@ -1315,6 +1315,7 @@ static int rtw_drv_init(struct usb_interface *pusb_intf, const struct usb_device
 	int i;
 #endif
 
+	mark_partner_supported("88x2CU", "Realtek Semiconductor Corp.", THIS_MODULE);
 	/* RTW_INFO("+rtw_drv_init\n"); */
 
 	/* step 0. */
