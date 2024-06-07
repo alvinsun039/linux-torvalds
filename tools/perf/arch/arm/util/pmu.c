e83573cdadcc92294e88519b06199ec17b8bd223
Ian Rogers <irogers@google.com>
2024-06-06 23:53:43 -0700

    perf arm: Workaround ARM PMUs cpu maps having offline cpus
    Mainline: 5518063fcb2e022411c1112b1b9b069e68380bbb
    From: v6.11-rc1
    Severity: Moderate
    
    task: 435539
    
    When PMUs have a cpu map in the 'cpus' or 'cpumask' file, perf will
    try to open events on those CPUs. ARM doesn't remove offline CPUs
    meaning taking a CPU offline will cause perf commands to fail unless a
    CPU map is passed on the command line.
    
    More context in:
    https://lore.kernel.org/lkml/20240603092812.46616-1-yangyicong@huawei.com/
    
    Reported-by: Yicong Yang <yangyicong@huawei.com>
    Closes: https://lore.kernel.org/lkml/20240603092812.46616-2-yangyicong@huawei.com/
    Signed-off-by: Ian Rogers <irogers@google.com>
    Tested-by: Yicong Yang <yangyicong@hisilicon.com>
    Tested-by: Leo Yan <leo.yan@arm.com>
    Cc: James Clark <james.clark@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Mike Leach <mike.leach@linaro.org>
    Cc: Leo Yan <leo.yan@linux.dev>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: coresight@lists.linaro.org
    Cc: John Garry <john.g.garry@oracle.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Link: https://lore.kernel.org/r/20240607065343.695369-1-irogers@google.com
    
    (backported from commit 5518063fcb2e022411c1112b1b9b069e68380bbb)
    
    K2CI-Arch: All
    Signed-off-by: zhouyuhang <zhouyuhang@kylinos.cn>
    Change-Id: I6a604d934b5a616640a33b43496657ec2f8f5abc
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/158795
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- tools/perf/arch/arm/util/pmu.c
+++ tools/perf/arch/arm/util/pmu.c
@@ -11,11 +11,14 @@
 
 #include "arm-spe.h"
 #include "hisi-ptt.h"
+#include "../../../util/cpumap.h"
 #include "../../../util/pmu.h"
 #include "../../../util/cs-etm.h"
 
-void perf_pmu__arch_init(struct perf_pmu *pmu __maybe_unused)
+void perf_pmu__arch_init(struct perf_pmu *pmu)
 {
+	struct perf_cpu_map *intersect;
+
 #ifdef HAVE_AUXTRACE_SUPPORT
 	if (!strcmp(pmu->name, CORESIGHT_ETM_PMU_NAME)) {
 		/* add ETM default config here */
@@ -28,6 +31,9 @@ void perf_pmu__arch_init(struct perf_pmu *pmu __maybe_unused)
 		pmu->selectable = true;
 #endif
 	}
-
 #endif
+	/* Workaround some ARM PMU's failing to correctly set CPU maps for online processors. */
+	intersect = perf_cpu_map__intersect(cpu_map__online(), pmu->cpus);
+	perf_cpu_map__put(pmu->cpus);
+	pmu->cpus = intersect;
 }
