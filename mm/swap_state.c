ad0d638ed96a6f6fd26f76c5d632eaadfba78528
Yosry Ahmed <yosryahmed@google.com>
2024-06-07 04:55:15 +0000

    mm: swap: remove 'synchronous' argument to swap_read_folio()
    Mainline: b2d1f38b524121130befa3a9b37dca790cfa9ab9
    From: v6.11-rc1
    Severity: Moderate
    
    bug: 384799
    
    Commit [1] introduced IO polling support duding swapin to reduce swap read
    latency for block devices that can be polled.  However later commit [2]
    removed polling support.  Commit [3] removed the remnants of polling
    support from read_swap_cache_async() and __read_swap_cache_async().
    However, it left behind some remnants in swap_read_folio(), the
    'synchronous' argument.
    
    swap_read_folio() reads the folio synchronously if synchronous=true or if
    SWP_SYNCHRONOUS_IO is set in swap_info_struct.  The only caller that
    passes synchronous=true is in do_swap_page() in the SWP_SYNCHRONOUS_IO
    case.
    
    Hence, the argument is redundant, it is only set to true when the swap
    read would have been synchronous anyway. Remove it.
    
    [1] Commit 23955622ff8d ("swap: add block io poll in swapin path")
    [2] Commit 9650b453a3d4 ("block: ignore RWF_HIPRI hint for sync dio")
    [3] Commit b243dcbf2f13 ("swap: remove remnants of polling from read_swap_cache_async")
    
    Link: https://lkml.kernel.org/r/20240607045515.1836558-1-yosryahmed@google.com
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    K2CI-Arch: All
    Change-Id: I2d9f2a423fc2dbed46f52cf60cd22400513fb962
    Signed-off-by: Lincheng Yang <yanglincheng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/102429
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -564,7 +564,7 @@ struct folio *read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 	mpol_cond_put(mpol);
 
 	if (page_allocated)
-		swap_read_folio(folio, false, plug);
+		swap_read_folio(folio, plug);
 	return folio;
 }
 
@@ -681,7 +681,7 @@ struct folio *swap_cluster_readahead(swp_entry_t entry, gfp_t gfp_mask,
 		if (!folio)
 			continue;
 		if (page_allocated) {
-			swap_read_folio(folio, false, &splug);
+			swap_read_folio(folio, &splug);
 			if (offset != entry_offset) {
 				folio_set_readahead(folio);
 				count_vm_event(SWAP_RA);
@@ -698,7 +698,7 @@ skip:
 					&page_allocated, false);
 	if (unlikely(page_allocated)) {
 		zswap_folio_swapin(folio);
-		swap_read_folio(folio, false, NULL);
+		swap_read_folio(folio, NULL);
 	}
 	return folio;
 }
@@ -855,7 +855,7 @@ static struct folio *swap_vma_readahead(swp_entry_t targ_entry, gfp_t gfp_mask,
 		if (!folio)
 			continue;
 		if (page_allocated) {
-			swap_read_folio(folio, false, &splug);
+			swap_read_folio(folio, &splug);
 			if (i != ra_info.offset) {
 				folio_set_readahead(folio);
 				count_vm_event(SWAP_RA);
@@ -874,7 +874,7 @@ skip:
 					&page_allocated, false);
 	if (unlikely(page_allocated)) {
 		zswap_folio_swapin(folio);
-		swap_read_folio(folio, false, NULL);
+		swap_read_folio(folio, NULL);
 	}
 	return folio;
 }
