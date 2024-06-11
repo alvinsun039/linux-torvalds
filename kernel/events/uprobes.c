17925ff35118ee2a17cee5d70537ce9862d36137
Jiri Olsa <jolsa@kernel.org>
2024-06-12 08:44:28 +0900

    uprobe: Add uretprobe syscall to speed up return probe
    Mainline: ff474a78cef5cb5f32be52fe25b78441327a2e7c
    From: v6.11-rc1
    Severity: Low
    
    task: 507941
    
    Adding uretprobe syscall instead of trap to speed up return probe.
    
    At the moment the uretprobe setup/path is:
    
      - install entry uprobe
    
      - when the uprobe is hit, it overwrites probed function's return address
        on stack with address of the trampoline that contains breakpoint
        instruction
    
      - the breakpoint trap code handles the uretprobe consumers execution and
        jumps back to original return address
    
    This patch replaces the above trampoline's breakpoint instruction with new
    ureprobe syscall call. This syscall does exactly the same job as the trap
    with some more extra work:
    
      - syscall trampoline must save original value for rax/r11/rcx registers
        on stack - rax is set to syscall number and r11/rcx are changed and
        used by syscall instruction
    
      - the syscall code reads the original values of those registers and
        restore those values in task's pt_regs area
    
      - only caller from trampoline exposed in '[uprobes]' is allowed,
        the process will receive SIGILL signal otherwise
    
    Even with some extra work, using the uretprobes syscall shows speed
    improvement (compared to using standard breakpoint):
    
      On Intel (11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz)
    
      current:
        uretprobe-nop  :    1.498 ± 0.000M/s
        uretprobe-push :    1.448 ± 0.001M/s
        uretprobe-ret  :    0.816 ± 0.001M/s
    
      with the fix:
        uretprobe-nop  :    1.969 ± 0.002M/s  < 31% speed up
        uretprobe-push :    1.910 ± 0.000M/s  < 31% speed up
        uretprobe-ret  :    0.934 ± 0.000M/s  < 14% speed up
    
      On Amd (AMD Ryzen 7 5700U)
    
      current:
        uretprobe-nop  :    0.778 ± 0.001M/s
        uretprobe-push :    0.744 ± 0.001M/s
        uretprobe-ret  :    0.540 ± 0.001M/s
    
      with the fix:
        uretprobe-nop  :    0.860 ± 0.001M/s  < 10% speed up
        uretprobe-push :    0.818 ± 0.001M/s  < 10% speed up
        uretprobe-ret  :    0.578 ± 0.000M/s  <  7% speed up
    
    The performance test spawns a thread that runs loop which triggers
    uprobe with attached bpf program that increments the counter that
    gets printed in results above.
    
    The uprobe (and uretprobe) kind is determined by which instruction
    is being patched with breakpoint instruction. That's also important
    for uretprobes, because uprobe is installed for each uretprobe.
    
    The performance test is part of bpf selftests:
      tools/testing/selftests/bpf/run_bench_uprobes.sh
    
    Note at the moment uretprobe syscall is supported only for native
    64-bit process, compat process still uses standard breakpoint.
    
    Note that when shadow stack is enabled the uretprobe syscall returns
    via iret, which is slower than return via sysret, but won't cause the
    shadow stack violation.
    
    Link: https://lore.kernel.org/all/20240611112158.40795-4-jolsa@kernel.org/
    
    Suggested-by: Andrii Nakryiko <andrii@kernel.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    
    (backported from commit ff474a78cef5cb5f32be52fe25b78441327a2e7c)
    
    Change-Id: I362d4ac504d414e7a96600c217e631bcaea1c458
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/112428
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -1494,11 +1494,20 @@ static int xol_add_vma(struct mm_struct *mm, struct xol_area *area)
 	return ret;
 }
 
+void * __weak arch_uprobe_trampoline(unsigned long *psize)
+{
+	static uprobe_opcode_t insn = UPROBE_SWBP_INSN;
+
+	*psize = UPROBE_SWBP_INSN_SIZE;
+	return &insn;
+}
+
 static struct xol_area *__create_xol_area(unsigned long vaddr)
 {
 	struct mm_struct *mm = current->mm;
-	uprobe_opcode_t insn = UPROBE_SWBP_INSN;
+	unsigned long insns_size;
 	struct xol_area *area;
+	void *insns;
 
 	area = kzalloc(sizeof(*area), GFP_KERNEL);
 	if (unlikely(!area))
@@ -1521,7 +1530,8 @@ static struct xol_area *__create_xol_area(unsigned long vaddr)
 	/* Reserve the 1st slot for get_trampoline_vaddr() */
 	set_bit(0, area->bitmap);
 	atomic_set(&area->slot_count, 1);
-	arch_uprobe_copy_ixol(area->pages[0], 0, &insn, UPROBE_SWBP_INSN_SIZE);
+	insns = arch_uprobe_trampoline(&insns_size);
+	arch_uprobe_copy_ixol(area->pages[0], 0, insns, insns_size);
 
 	if (!xol_add_vma(mm, area))
 		return area;
@@ -1846,7 +1856,7 @@ void uprobe_copy_process(struct task_struct *t, unsigned long flags)
  *
  * Returns -1 in case the xol_area is not allocated.
  */
-static unsigned long get_trampoline_vaddr(void)
+unsigned long uprobe_get_trampoline_vaddr(void)
 {
 	struct xol_area *area;
 	unsigned long trampoline_vaddr = -1;
@@ -1897,7 +1907,7 @@ static void prepare_uretprobe(struct uprobe *uprobe, struct pt_regs *regs)
 	if (!ri)
 		return;
 
-	trampoline_vaddr = get_trampoline_vaddr();
+	trampoline_vaddr = uprobe_get_trampoline_vaddr();
 	orig_ret_vaddr = arch_uretprobe_hijack_return_addr(trampoline_vaddr, regs);
 	if (orig_ret_vaddr == -1)
 		goto fail;
@@ -2144,7 +2154,7 @@ static struct return_instance *find_next_ret_chain(struct return_instance *ri)
 	return ri;
 }
 
-static void handle_trampoline(struct pt_regs *regs)
+void uprobe_handle_trampoline(struct pt_regs *regs)
 {
 	struct uprobe_task *utask;
 	struct return_instance *ri, *next;
@@ -2208,8 +2218,8 @@ static void handle_swbp(struct pt_regs *regs)
 	int is_swbp;
 
 	bp_vaddr = uprobe_get_swbp_addr(regs);
-	if (bp_vaddr == get_trampoline_vaddr())
-		return handle_trampoline(regs);
+	if (bp_vaddr == uprobe_get_trampoline_vaddr())
+		return uprobe_handle_trampoline(regs);
 
 	uprobe = find_active_uprobe(bp_vaddr, &is_swbp);
 	if (!uprobe) {
