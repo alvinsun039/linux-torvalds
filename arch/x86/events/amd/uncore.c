alvinsun039/linux-torvalds

commit cd8e20b233da316d7e54be77ac74ef874a429c6b

HYGON: perf/x86/uncore: Add L3 PMU support for Hygon family 18h model 6h
Mainline: KYLIN-only
Severity: Important

task: 205979
task: 216501

Adjust the L3 PMU slicemask and threadmask for Hygon family 18h
model 6h processor.

K2CI-Arch: Amd64
Link: https://gitee.com/openeuler/kernel/issues/I8U3HZ
Signed-off-by: Pu Wen <puwen@hygon.cn>
Signed-off-by: lifangxu <lifangxu@kylinos.cn>
Change-Id: I70d506f8686990e64ddb17b5cd18e1e5abf079cc
Reviewed-on: http://gerrit.kylin.com/c/klinux/+/109488
Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
Code-Style: k2ci <kernel-bot@kylinos.cn>
Tested-by: k2ci <kernel-bot@kylinos.cn>

arch/x86/events/amd/uncore.c
@@ -248,6 +248,13 @@ amd_f17h_uncore_is_visible(struct kobject *kobj, struct attribute *attr, int i)
 }
 
 static umode_t
+hygon_f18h_m6h_uncore_is_visible(struct kobject *kobj, struct attribute *attr, int i)
+{
+	return boot_cpu_data.x86 == 0x18 && boot_cpu_data.x86_model == 0x6 ?
+	       attr->mode : 0;
+}
+
+static umode_t
 amd_f19h_uncore_is_visible(struct kobject *kobj, struct attribute *attr, int i)
 {
 	return boot_cpu_data.x86 >= 0x19 ? attr->mode : 0;
@@ -298,6 +305,8 @@ DEFINE_UNCORE_FORMAT_ATTR(enallslices,	enallslices,	"config:46");		   /* F19h L3
 DEFINE_UNCORE_FORMAT_ATTR(enallcores,	enallcores,	"config:47");		   /* F19h L3 */
 DEFINE_UNCORE_FORMAT_ATTR(sliceid,	sliceid,	"config:48-50");	   /* F19h L3 */
 DEFINE_UNCORE_FORMAT_ATTR(rdwrmask,	rdwrmask,	"config:8-9");		   /* PerfMonV2 UMC */
+DEFINE_UNCORE_FORMAT_ATTR(slicemask4,	slicemask,	"config:28-31");	   /* F18h L3 */
+DEFINE_UNCORE_FORMAT_ATTR(threadmask32,	threadmask,	"config:32-63");	   /* F18h L3 */
 
 /* Common DF and NB attributes */
 static struct attribute *amd_uncore_df_format_attr[] = {
@@ -327,6 +336,12 @@ static struct attribute *amd_f17h_uncore_l3_format_attr[] = {
 	NULL,
 };
 
+/* F18h M06h unique L3 attributes */
+static struct attribute *hygon_f18h_m6h_uncore_l3_format_attr[] = {
+	&format_attr_slicemask4.attr,	/* slicemask */
+	NULL,
+};
+
 /* F19h unique L3 attributes */
 static struct attribute *amd_f19h_uncore_l3_format_attr[] = {
 	&format_attr_coreid.attr,	/* coreid */
@@ -350,6 +365,12 @@ static struct attribute_group amd_f17h_uncore_l3_format_group = {
 	.name = "format",
 	.attrs = amd_f17h_uncore_l3_format_attr,
 	.is_visible = amd_f17h_uncore_is_visible,
+};
+
+static struct attribute_group hygon_f18h_m6h_uncore_l3_format_group = {
+	.name = "format",
+	.attrs = hygon_f18h_m6h_uncore_l3_format_attr,
+	.is_visible = hygon_f18h_m6h_uncore_is_visible,
 };
 
 static struct attribute_group amd_f19h_uncore_l3_format_group = {
@@ -378,6 +399,11 @@ static const struct attribute_group *amd_uncore_l3_attr_groups[] = {
 static const struct attribute_group *amd_uncore_l3_attr_update[] = {
 	&amd_f17h_uncore_l3_format_group,
 	&amd_f19h_uncore_l3_format_group,
+	NULL,
+};
+
+static const struct attribute_group *hygon_uncore_l3_attr_update[] = {
+	&hygon_f18h_m6h_uncore_l3_format_group,
 	NULL,
 };
 
@@ -742,9 +768,19 @@ static int amd_uncore_l3_event_init(struct perf_event *event)
 			 AMD64_L3_EN_ALL_CORES | AMD64_L3_EN_ALL_SLICES |
 			 AMD64_L3_COREID_MASK);
 
-	if (boot_cpu_data.x86 <= 0x18)
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_AMD &&
+	    boot_cpu_data.x86 <= 0x18)
 		mask = ((config & AMD64_L3_SLICE_MASK) ? : AMD64_L3_SLICE_MASK) |
 		       ((config & AMD64_L3_THREAD_MASK) ? : AMD64_L3_THREAD_MASK);
+	else if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON &&
+		 boot_cpu_data.x86 == 0x18) {
+		if (boot_cpu_data.x86_model == 0x6)
+			return ((config & HYGON_L3_SLICE_MASK) ? : HYGON_L3_SLICE_MASK) |
+			       ((config & HYGON_L3_THREAD_MASK) ? : HYGON_L3_THREAD_MASK);
+		else
+			return ((config & AMD64_L3_SLICE_MASK) ? : AMD64_L3_SLICE_MASK) |
+			       ((config & AMD64_L3_THREAD_MASK) ? : AMD64_L3_THREAD_MASK);
+	}
 
 	/*
 	 * If the user doesn't specify a ThreadMask, they're not trying to
@@ -836,6 +872,18 @@ int amd_uncore_l3_ctx_init(struct amd_uncore *uncore, unsigned int cpu)
 		.capabilities	= PERF_PMU_CAP_NO_EXCLUDE | PERF_PMU_CAP_NO_INTERRUPT,
 		.module		= THIS_MODULE,
 	};
+
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON &&
+	    boot_cpu_data.x86 == 0x18) {
+		*l3_attr++ = &format_attr_event8.attr;
+		*l3_attr++ = &format_attr_umask8.attr;
+		if (boot_cpu_data.x86_model == 0x6) {
+			*l3_attr++ = &format_attr_threadmask32.attr;
+			pmu->pmu.attr_update = hygon_uncore_l3_attr_update;
+		} else {
+			*l3_attr++ = &format_attr_threadmask8.attr;
+		}
+	}
 
 	if (perf_pmu_register(&pmu->pmu, pmu->pmu.name, -1)) {
 		free_percpu(pmu->ctx);

arch/x86/include/asm/perf_event.h
@@ -61,6 +61,14 @@
 #define INTEL_ARCH_EVENT_MASK	\
 	(ARCH_PERFMON_EVENTSEL_UMASK | ARCH_PERFMON_EVENTSEL_EVENT)
 
+#define HYGON_L3_SLICE_SHIFT				28
+#define HYGON_L3_SLICE_MASK				\
+	(0xFULL << HYGON_L3_SLICE_SHIFT)
+
+#define HYGON_L3_THREAD_SHIFT				32
+#define HYGON_L3_THREAD_MASK				\
+	(0xFFFFFFFFULL << HYGON_L3_THREAD_SHIFT)
+
 #define AMD64_L3_SLICE_SHIFT				48
 #define AMD64_L3_SLICE_MASK				\
 	(0xFULL << AMD64_L3_SLICE_SHIFT)

commit 919b1d0a3edf9eebd3477efe6d2e0fffa683faa8

HYGON: perf/x86/uncore: Create L3 event strictly to the CPU vendor
Mainline: KYLIN-only
Severity: Important

task: 224996

In function amd_uncore_l3_ctx_init(), there is already a family check
existed before the part Hygon added in commit 26559114c45e
("perf/x86/uncore: Add L3 PMU support for Hygon family 18h model 6h"),
and it will take effect for Hygon which is not expected, otherwise the
following process will produce error messages as follow:
 "amd uncore: 4 HYGON amd df counters detected"
 "sysfs: cannot create duplicate filename '/devices/amd 13/format/event'"

So restrict the family check to the specific CPU vendor.

Link: https://gitee.com/openeuler/kernel/issues/I9OYEP
Fixes: cd8e20b233 ("HYGON: perf/x86/uncore: Add L3 PMU support for Hygon family 18h model 6h")
Signed-off-by: Pu Wen <puwen@hygon.cn>
Signed-off-by: tanze <tanze@kylinos.cn>
Change-Id: I3581e35f12a24a4e5605ebd187e5551d6e10b8f6
Reviewed-on: http://gerrit.kylin.com/c/klinux/+/117724
Code-Style: k2ci <kernel-bot@kylinos.cn>
Tested-by: k2ci <kernel-bot@kylinos.cn>
Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

arch/x86/events/amd/uncore.c
@@ -846,7 +846,8 @@ int amd_uncore_l3_ctx_init(struct amd_uncore *uncore, unsigned int cpu)
 	pmu->rdpmc_base = RDPMC_BASE_LLC;
 	pmu->group = amd_uncore_ctx_gid(uncore, cpu);
 
-	if (boot_cpu_data.x86 >= 0x17) {
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_AMD &&
+	    boot_cpu_data.x86 >= 0x17) {
 		*l3_attr++ = &format_attr_event8.attr;
 		*l3_attr++ = &format_attr_umask8.attr;
 		*l3_attr++ = boot_cpu_data.x86 >= 0x19 ?