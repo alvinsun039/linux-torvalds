f3554316435e548994f63fa38835b74338e00599
Jackie Liu <liuyun01@kylinos.cn>
2024-06-18 17:02:32 +0800

    KYLIN: wifi/realtek: mark rtl8852CU/rtl8852BU/rtl8821CU/rtl8188EU module support by partner
    Mainline: KYLIN-only
    Severity: Low
    
    task: 222942
    
    This driver is not directly supported by Kylin Software, we mark it
    where from.
    
    K2CI-Arch: All
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I1a8e5f2319ce07042dcaa1b16439a0459dde0d28
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/118879
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/net/wireless/realtek/rtl8188EU/os_dep/linux/usb_intf.c
+++ drivers/net/wireless/realtek/rtl8188EU/os_dep/linux/usb_intf.c
@@ -1315,6 +1315,7 @@ static int rtw_drv_init(struct usb_interface *pusb_intf, const struct usb_device
 #endif
 
 	/* RTW_INFO("+rtw_drv_init\n"); */
+	mark_partner_supported(NULL, "Realtek Semiconductor Corp.", THIS_MODULE);
 
 	/* step 0. */
 	process_spec_devid(pdid);
