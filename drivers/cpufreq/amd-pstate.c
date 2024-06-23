360265e80f0fb0fd544597ab8d248c7e62f8a3f3
Mario Limonciello <mario.limonciello@amd.com>
2024-06-23 15:09:18 -0500

    cpufreq: amd-pstate: Don't create attributes when registration fails
    Mainline: bc76f57574741cc8e7d04c53f199d8722ce09413
    From: v6.11-rc1
    Severity: Moderate
    
    task: 564671
    
    If driver registration fails then immediately return the failure
    instead of continuing to register attributes.
    
    This fixes issues of falling back from amd-pstate to other drivers
    when cpufreq init has failed for any reason.
    
    Reported-by: alex.s.cochran@proton.me
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Perry Yuan <Perry.Yuan@amd.com>
    Link: https://lore.kernel.org/r/20240623200918.52104-1-mario.limonciello@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    (backported from commit bc76f57574741cc8e7d04c53f199d8722ce09413)
    
    K2CI-Arch: Amd64
    Change-Id: I76907daade113ff20f3d5ab3980dc1d55b8a008d
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/152334
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -1888,8 +1888,10 @@ static int __init amd_pstate_init(void)
 	}
 
 	ret = cpufreq_register_driver(current_pstate_driver);
-	if (ret)
+	if (ret) {
 		pr_err("failed to register with return %d\n", ret);
+		goto disable_driver;
+	}
 
 	dev_root = bus_get_dev_root(&cpu_subsys);
 	if (dev_root) {
@@ -1905,6 +1907,8 @@ static int __init amd_pstate_init(void)
 
 global_attr_free:
 	cpufreq_unregister_driver(current_pstate_driver);
+disable_driver:
+	amd_pstate_enable(false);
 	return ret;
 }
 device_initcall(amd_pstate_init);
