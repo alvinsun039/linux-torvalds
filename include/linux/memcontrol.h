bb963fc2c4eeda542899aa6d6aec03eb5e4d62db
Roman Gushchin <roman.gushchin@linux.dev>
2024-06-28 21:03:17 +0000

    mm: memcg: put struct task_struct::in_user_fault under CONFIG_MEMCG_V1
    Mainline: 1419ff984aad4c08d121793f71a520b432ead88a
    From: v6.11-rc1
    Severity: Moderate
    
    task: 447145
    
    The struct task_struct's in_user_fault member is not used by the cgroup
    v2's memory controller, so it can be put under the CONFIG_MEMCG_V1 config
    option.  To do so, mem_cgroup_enter_user_fault() and
    mem_cgroup_exit_user_fault() are moved under the CONFIG_MEMCG_V1 option as
    well.
    
    Link: https://lkml.kernel.org/r/20240628210317.272856-10-roman.gushchin@linux.dev
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 1419ff984aad4c08d121793f71a520b432ead88a)
    
    Change-Id: I6e736ed0366b57cc7d6876c90e0e5654d3144e38
    Signed-off-by: liqiang01 <liqiang01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56743
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -947,18 +947,6 @@ void mem_cgroup_print_oom_context(struct mem_cgroup *memcg,
 
 void mem_cgroup_print_oom_meminfo(struct mem_cgroup *memcg);
 
-static inline void mem_cgroup_enter_user_fault(void)
-{
-	WARN_ON(current->in_user_fault);
-	current->in_user_fault = 1;
-}
-
-static inline void mem_cgroup_exit_user_fault(void)
-{
-	WARN_ON(!current->in_user_fault);
-	current->in_user_fault = 0;
-}
-
 struct mem_cgroup *mem_cgroup_get_oom_group(struct task_struct *victim,
 					    struct mem_cgroup *oom_domain);
 void mem_cgroup_print_oom_group(struct mem_cgroup *memcg);
@@ -1420,14 +1408,6 @@ mem_cgroup_print_oom_meminfo(struct mem_cgroup *memcg)
 }
 
 static inline void mem_cgroup_handle_over_high(gfp_t gfp_mask)
-{
-}
-
-static inline void mem_cgroup_enter_user_fault(void)
-{
-}
-
-static inline void mem_cgroup_exit_user_fault(void)
 {
 }
 
@@ -1916,6 +1896,18 @@ static inline void mem_cgroup_unlock_pages(void)
 	rcu_read_unlock();
 }
 
+static inline void mem_cgroup_enter_user_fault(void)
+{
+	WARN_ON(current->in_user_fault);
+	current->in_user_fault = 1;
+}
+
+static inline void mem_cgroup_exit_user_fault(void)
+{
+	WARN_ON(!current->in_user_fault);
+	current->in_user_fault = 0;
+}
+
 #else /* CONFIG_MEMCG_V1 */
 static inline
 unsigned long memcg1_soft_limit_reclaim(pg_data_t *pgdat, int order,
@@ -1953,6 +1945,14 @@ static inline bool task_in_memcg_oom(struct task_struct *p)
 static inline bool mem_cgroup_oom_synchronize(bool wait)
 {
 	return false;
+}
+
+static inline void mem_cgroup_enter_user_fault(void)
+{
+}
+
+static inline void mem_cgroup_exit_user_fault(void)
+{
 }
 
 #endif /* CONFIG_MEMCG_V1 */
