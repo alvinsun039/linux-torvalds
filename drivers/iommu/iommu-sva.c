9e94f83ce68f2ed4808a9a65717f2be24d7d3155
Lu Baolu <baolu.lu@linux.intel.com>
2024-07-02 14:34:35 +0800

    iommu: Introduce domain attachment handle
    Mainline: 14678219cf4093e897ab353fd78eab7994d1be7d
    From: v6.11-rc1
    Severity: Important
    
    task: 588865
    
    Currently, when attaching a domain to a device or its PASID, domain is
    stored within the iommu group. It could be retrieved for use during the
    window between attachment and detachment.
    
    With new features introduced, there's a need to store more information
    than just a domain pointer. This information essentially represents the
    association between a domain and a device. For example, the SVA code
    already has a custom struct iommu_sva which represents a bond between
    sva domain and a PASID of a device. Looking forward, the IOMMUFD needs
    a place to store the iommufd_device pointer in the core, so that the
    device object ID could be quickly retrieved in the critical fault handling
    path.
    
    Introduce domain attachment handle that explicitly represents the
    attachment relationship between a domain and a device or its PASID.
    
    Co-developed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Link: https://lore.kernel.org/r/20240702063444.105814-2-baolu.lu@linux.intel.com
    Signed-off-by: Will Deacon <will@kernel.org>
    
    (backported from commit 14678219cf4093e897ab353fd78eab7994d1be7d)
    
    Cc: kernel-stable-team #2603+
    Change-Id: I1a2466e348665a69db2bcb5fa28f7d8c5f6ef682
    Signed-off-by: jianghaoran <jianghaoran@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/171840
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/iommu/iommu-sva.c
+++ drivers/iommu/iommu-sva.c
@@ -102,7 +102,9 @@ struct iommu_sva *iommu_sva_bind_device(struct device *dev, struct mm_struct *mm
 
 	/* Search for an existing domain. */
 	list_for_each_entry(domain, &mm->iommu_mm->sva_domains, next) {
-		ret = iommu_attach_device_pasid(domain, dev, iommu_mm->pasid);
+		handle->handle.domain = domain;
+		ret = iommu_attach_device_pasid(domain, dev, iommu_mm->pasid,
+						&handle->handle);
 		if (!ret) {
 			domain->users++;
 			goto out;
@@ -116,7 +118,9 @@ struct iommu_sva *iommu_sva_bind_device(struct device *dev, struct mm_struct *mm
 		goto out_free_handle;
 	}
 
-	ret = iommu_attach_device_pasid(domain, dev, iommu_mm->pasid);
+	handle->handle.domain = domain;
+	ret = iommu_attach_device_pasid(domain, dev, iommu_mm->pasid,
+					&handle->handle);
 	if (ret)
 		goto out_free_domain;
 	domain->users = 1;
@@ -127,7 +131,6 @@ out:
 	list_add(&handle->handle_item, &mm->iommu_mm->sva_handles);
 	mutex_unlock(&iommu_sva_lock);
 	handle->dev = dev;
-	handle->domain = domain;
 	return handle;
 
 out_free_domain:
@@ -150,7 +153,7 @@ EXPORT_SYMBOL_GPL(iommu_sva_bind_device);
  */
 void iommu_sva_unbind_device(struct iommu_sva *handle)
 {
-	struct iommu_domain *domain = handle->domain;
+	struct iommu_domain *domain = handle->handle.domain;
 	struct iommu_mm_data *iommu_mm = domain->mm->iommu_mm;
 	struct device *dev = handle->dev;
 
@@ -173,7 +176,7 @@ EXPORT_SYMBOL_GPL(iommu_sva_unbind_device);
 
 u32 iommu_sva_get_pasid(struct iommu_sva *handle)
 {
-	struct iommu_domain *domain = handle->domain;
+	struct iommu_domain *domain = handle->handle.domain;
 
 	return mm_get_enqcmd_pasid(domain->mm);
 }
