135ff292e2e3cf06c283c9ec517f67721a6a9b57
Muchun Song <songmuchun@bytedance.com>
2024-07-18 17:18:21 +0800

    mm: kmem: remove mem_cgroup_from_obj()
    Mainline: 4fd568faf6e7e21f206cd66dd4fca9a72e7d922c
    From: v6.12-rc1
    Severity: Low
    
    task: 553143
    
    There is no user of mem_cgroup_from_obj(), remove it.
    
    Link: https://lkml.kernel.org/r/20240718091821.44740-1-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 4fd568faf6e7e21f206cd66dd4fca9a72e7d922c)
    
    Cc: kernel-stable-team #2503
    Change-Id: I564d3e63bfe66e1bcfd89c580fd046e2b888f009
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/140944
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1709,7 +1709,6 @@ static inline int memcg_kmem_id(struct mem_cgroup *memcg)
 	return memcg ? memcg->kmemcg_id : -1;
 }
 
-struct mem_cgroup *mem_cgroup_from_obj(void *p);
 struct mem_cgroup *mem_cgroup_from_slab_obj(void *p);
 
 static inline void count_objcg_event(struct obj_cgroup *objcg,
@@ -1772,11 +1771,6 @@ static inline bool memcg_kmem_online(void)
 static inline int memcg_kmem_id(struct mem_cgroup *memcg)
 {
 	return -1;
-}
-
-static inline struct mem_cgroup *mem_cgroup_from_obj(void *p)
-{
-	return NULL;
 }
 
 static inline struct mem_cgroup *mem_cgroup_from_slab_obj(void *p)
