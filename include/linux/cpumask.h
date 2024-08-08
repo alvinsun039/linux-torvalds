f41b99cc8eba212e2ed12f83e619929671eb965c
Gavin Shan <gshan@redhat.com>
2024-08-08 14:08:08 +1000

    cpumask: Fix crash on updating CPU enabled mask
    Mainline: 5819e464a17587e6830cfab05f3e91a9a8753a41
    From: v6.11-rc3
    Severity: Low
    
    task: 551019
    
    The CPU enabled mask instead of the CPU possible mask should be used
    by set_cpu_enabled(). Otherwise, we run into crash due to write to
    the read-only CPU possible mask when vCPU is hot added on ARM64.
    
      (qemu) device_add host-arm-cpu,id=cpu1,socket-id=1
      Unable to handle kernel write to read-only memory at virtual address ffff800080fa7190
        :
      Call trace:
        register_cpu+0x1a4/0x2e8
        arch_register_cpu+0x84/0xd8
        acpi_processor_add+0x480/0x5b0
        acpi_bus_attach+0x1c4/0x300
        acpi_dev_for_one_check+0x3c/0x50
        device_for_each_child+0x68/0xc8
        acpi_dev_for_each_child+0x48/0x80
        acpi_bus_attach+0x84/0x300
        acpi_bus_scan+0x74/0x220
        acpi_scan_rescan_bus+0x54/0x88
        acpi_device_hotplug+0x208/0x478
        acpi_hotplug_work_fn+0x2c/0x50
        process_one_work+0x15c/0x3c0
        worker_thread+0x2ec/0x400
        kthread+0x120/0x130
        ret_from_fork+0x10/0x20
    
    Fix it by passing the CPU enabled mask instead of the CPU possible
    mask to set_cpu_enabled().
    
    Fixes: 51c4767503d5 ("Merge tag 'bitmap-6.11-rc1' of https://github.com:/norov/linux")
    Signed-off-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    
    Change-Id: Idd1df73d0cb17b64255e53b39860d258a900d318
    Signed-off-by: Yufeng Wang <wangyufeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/154659
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -1049,16 +1049,8 @@ static inline void reset_cpu_possible_mask(void)
 #define assign_cpu(cpu, mask, val)	\
 	assign_bit(cpumask_check(cpu), cpumask_bits(mask), (val))
 
-static inline void
-set_cpu_enabled(unsigned int cpu, bool can_be_onlined)
-{
-	if (can_be_onlined)
-		cpumask_set_cpu(cpu, &__cpu_enabled_mask);
-	else
-		cpumask_clear_cpu(cpu, &__cpu_enabled_mask);
-}
-
 #define set_cpu_possible(cpu, possible)	assign_cpu((cpu), &__cpu_possible_mask, (possible))
+#define set_cpu_enabled(cpu, enabled)	assign_cpu((cpu), &__cpu_enabled_mask, (enabled))
 #define set_cpu_present(cpu, present)	assign_cpu((cpu), &__cpu_present_mask, (present))
 #define set_cpu_active(cpu, active)	assign_cpu((cpu), &__cpu_active_mask, (active))
 #define set_cpu_dying(cpu, dying)	assign_cpu((cpu), &__cpu_dying_mask, (dying))
