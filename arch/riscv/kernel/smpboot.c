4ba6e874f6d55caf865a38ceba8fa07fa87c6cb5
Samuel Holland <samuel.holland@sifive.com>
2024-08-14 01:10:56 -0700

    riscv: Call riscv_user_isa_enable() only on the boot hart
    Mainline: 368546ebe7e74cb6e18f17768533ab7077392a8c
    From: v6.13-rc1
    Severity: Low
    
    task: 551019
    
    Now that the [ms]envcfg CSR value is maintained per thread, not per
    hart, riscv_user_isa_enable() only needs to be called once during boot,
    to set the value for the init task. This also allows it to be marked as
    __init.
    
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Reviewed-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Reviewed-by: Charlie Jenkins <charlie@rivosinc.com>
    Link: https://lore.kernel.org/r/20240814081126.956287-4-samuel.holland@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    Change-Id: I67c8872e3a02048e35c3bf92b2e1a74e87549002
    Signed-off-by: Yufeng Wang <wangyufeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/150656
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- arch/riscv/kernel/smpboot.c
+++ arch/riscv/kernel/smpboot.c
@@ -259,8 +259,6 @@ asmlinkage __visible void smp_callin(void)
 	check_unaligned_access(curr_cpuid);
 	set_cpu_online(curr_cpuid, 1);
 
-	riscv_user_isa_enable();
-
 	/*
 	 * Remote TLB flushes are ignored while the CPU is offline, so emit
 	 * a local TLB flush right now just in case.
