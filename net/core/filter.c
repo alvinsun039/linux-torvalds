fdf09ef051671cb24c4983089742e8446a83a3f9
Ido Schimmel <idosch@nvidia.com>
2024-08-21 15:52:40 +0300

    bpf: Unmask upper DSCP bits in bpf_fib_lookup() helper
    Mainline: ef434fae72287a9cd0f7b85ad1701b998cce800b
    From: v6.12-rc1
    Severity: Low
    
    The helper performs a FIB lookup according to the parameters in the
    'params' argument, one of which is 'tos'. According to the test in
    test_tc_neigh_fib.c, it seems that BPF programs are expected to
    initialize the 'tos' field to the full 8 bit DS field from the IPv4
    header.
    
    Unmask the upper DSCP bits before invoking the IPv4 FIB lookup APIs so
    that in the future the lookup could be performed according to the full
    DSCP value.
    
    No functional changes intended since the upper DSCP bits are masked when
    comparing against the TOS selectors in FIB rules and routes.
    
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: Guillaume Nault <gnault@redhat.com>
    Acked-by: Florian Westphal <fw@strlen.de>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://patch.msgid.link/20240821125251.1571445-2-idosch@nvidia.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    (backported from commit ef434fae72287a9cd0f7b85ad1701b998cce800b)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Iab6d9ae2b507182942c70bd97f4717079c20e71b
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56186
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- net/core/filter.c
+++ net/core/filter.c
@@ -83,6 +83,7 @@
 #include <net/netfilter/nf_conntrack_bpf.h>
 #include <linux/un.h>
 #include <net/xdp_sock_drv.h>
+#include <net/inet_dscp.h>
 
 static const struct bpf_func_proto *
 bpf_sk_base_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog);
@@ -5906,7 +5907,7 @@ static int bpf_ipv4_fib_lookup(struct net *net, struct bpf_fib_lookup *params,
 		fl4.flowi4_iif = params->ifindex;
 		fl4.flowi4_oif = 0;
 	}
-	fl4.flowi4_tos = params->tos & IPTOS_RT_MASK;
+	fl4.flowi4_tos = params->tos & INET_DSCP_MASK;
 	fl4.flowi4_scope = RT_SCOPE_UNIVERSE;
 	fl4.flowi4_flags = 0;
 
