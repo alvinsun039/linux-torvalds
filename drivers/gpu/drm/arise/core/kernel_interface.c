75dca0c4678c236ef87a0d9cb3174b068595f524
brightyi <brightyi@glenfly.com>
2024-08-27 17:51:17 +0800

    GLENFLY: drm/arise: fix i386 config build error
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 350917
    
    fix i386 config build error
    
    Reported-by: k2ci <kernel-bot@kylinos.cn>
    Signed-off-by: brightyi <brightyi@glenfly.com>
    Signed-off-by: jindongyang <jindongyang@kylinos.cn>
    Change-Id: I1209dbecc68d194f10e53afc0f0213e01381d537
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/131460
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- drivers/gpu/drm/arise/core/kernel_interface.c
+++ drivers/gpu/drm/arise/core/kernel_interface.c
@@ -1311,7 +1311,7 @@ static void krnl_task_timeout_update(void* data, unsigned long long *value, int 
     if (update)
         adapter->hw_hang_fast_timeout_ns = *value * 1000000;
     else
-        *value = adapter->hw_hang_fast_timeout_ns / 1000000;
+        *value = gf_do_div(adapter->hw_hang_fast_timeout_ns, 1000000);
 }
 
 static core_interface_t gfe3k_gpu_core = {
