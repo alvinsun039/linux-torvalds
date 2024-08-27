1b157a75e524a98d87cc34a8dafea7af154e0c39
Marc Zyngier <maz@kernel.org>
2024-08-27 16:25:07 +0100

    KVM: arm64: Move GICv3 trap configuration to kvm_calculate_traps()
    Mainline: d2137ba8d8fe56cd2470c82b98e494cbcababd76
    From: v6.12-rc1
    Severity: Low
    
    Follow the pattern introduced with vcpu_set_hcr(), and introduce
    vcpu_set_ich_hcr(), which configures the GICv3 traps at the same
    point.
    
    This will allow future changes to introduce trap configuration on
    a per-VM basis.
    
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20240827152517.3909653-2-maz@kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    (backported from commit d2137ba8d8fe56cd2470c82b98e494cbcababd76)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Iaebcff8eb3de25ede66b292e87ca524fd3a71915
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/167815
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -4202,6 +4202,7 @@ void kvm_calculate_traps(struct kvm_vcpu *vcpu)
 
 	mutex_lock(&kvm->arch.config_lock);
 	vcpu_set_hcr(vcpu);
+	vcpu_set_ich_hcr(vcpu);
 
 	if (cpus_have_final_cap(ARM64_HAS_HCX)) {
 		vcpu->arch.hcrx_el2 = HCRX_GUEST_FLAGS;
