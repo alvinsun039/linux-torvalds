649cb7eff46fbb39be25fd28da05794dedcacab9
Ingo Molnar <mingo@kernel.org>
2024-08-29 21:37:26 +0800

    sched/fair: Rename SG_OVERLOAD to SG_OVERLOADED
    Mainline: 7bda10ba7f453729f210264dd07d38989fb858d9
    From: v6.10-rc1
    Severity: Low
    
    Task: #340825
    
    Follow the rename of the root_domain::overloaded flag.
    
    Note that this also matches the SG_OVERUTILIZED flag better.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Shrikanth Hegde <sshegde@linux.ibm.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/ZgVHq65XKsOZpfgK@gmail.com
    
    K2CI-Arch: All
    Signed-off-by: Huang Bing <huangbing@kylinos.cn>
    Change-Id: I0815a1109d1e868bfbbf85bb4ab225b18b6f440c
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/136761
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9848,7 +9848,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->sum_nr_running += nr_running;
 
 		if (nr_running > 1)
-			*sg_status |= SG_OVERLOAD;
+			*sg_status |= SG_OVERLOADED;
 
 		if (cpu_overutilized(i))
 			*sg_status |= SG_OVERUTILIZED;
@@ -9873,7 +9873,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			/* Check for a misfit task on the cpu */
 			if (sgs->group_misfit_task_load < rq->misfit_task_load) {
 				sgs->group_misfit_task_load = rq->misfit_task_load;
-				*sg_status |= SG_OVERLOAD;
+				*sg_status |= SG_OVERLOADED;
 			}
 		} else if ((env->idle != CPU_NOT_IDLE) &&
 			   sched_reduced_capacity(rq, env->sd)) {
@@ -10558,7 +10558,7 @@ next_group:
 
 	if (!env->sd->parent) {
 		/* update overload indicator if we are at root domain */
-		set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOAD);
+		set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
 
 		/* Update over-utilization (tipping point, U >= 0) indicator */
 		set_rd_overutilized_status(env->dst_rq->rd,
