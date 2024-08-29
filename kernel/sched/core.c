5570b670475d9affffc802e1cb1236ea261baece
Shrikanth Hegde <sshegde@linux.ibm.com>
2024-08-29 19:16:07 +0800

    sched/core: Simplify code by removing duplicate #ifdefs
    Mainline: 8cec3dd9e5930c82c6bd0af3fdb3a36bcd428310
    From: v6.9-rc1
    Severity: Low
    
    Task: #340825
    
    There's a few cases of nested #ifdefs in the scheduler code
    that can be simplified:
    
      #ifdef DEFINE_A
      ...code block...
        #ifdef DEFINE_A       <-- This is a duplicate.
        ...code block...
        #endif
      #else
        #ifndef DEFINE_A     <-- This is also duplicate.
        ...code block...
        #endif
      #endif
    
    More details about the script and methods used to find these code
    patterns can be found at:
    
      https://lore.kernel.org/all/20240118080326.13137-1-sshegde@linux.ibm.com/
    
    No change in functionality intended.
    
    [ mingo: Clarified the changelog. ]
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20240216061433.535522-1-sshegde@linux.ibm.com
    
    K2CI-Arch: All
    Signed-off-by: Huang Bing <huangbing@kylinos.cn>
    Change-Id: I57b51ddb94edc00bbdf8d1e4478ddea52f5def13
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/136666
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1788,7 +1788,6 @@ static void cpu_util_update_eff(struct cgroup_subsys_state *css);
 #endif
 
 #ifdef CONFIG_SYSCTL
-#ifdef CONFIG_UCLAMP_TASK
 #ifdef CONFIG_UCLAMP_TASK_GROUP
 static void uclamp_update_root_tg(void)
 {
@@ -1893,7 +1892,6 @@ undo:
 	sysctl_sched_uclamp_util_min_rt_default = old_min_rt;
 	return result;
 }
-#endif
 #endif
 
 static int uclamp_validate(struct task_struct *p,
@@ -2061,7 +2059,7 @@ static void __init init_uclamp(void)
 	}
 }
 
-#else /* CONFIG_UCLAMP_TASK */
+#else /* !CONFIG_UCLAMP_TASK */
 static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p) { }
 static inline void uclamp_rq_dec(struct rq *rq, struct task_struct *p) { }
 static inline int uclamp_validate(struct task_struct *p,
