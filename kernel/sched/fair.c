e9f24351fb02808aeb9d60e63780198fee3d9bfd
Ingo Molnar <mingo@kernel.org>
2024-08-30 11:07:14 +0800

    sched/balancing: Simplify the sg_status bitmask and use separate ->overloaded and ->overutilized flags
    Mainline: 4475cd8bfd9bcb898953fcadb2f51b3432eb68a1
    From: v6.10-rc1
    Severity: Low
    
    Task: #340825
    
    SG_OVERLOADED and SG_OVERUTILIZED flags plus the sg_status bitmask are an
    unnecessary complication that only make the code harder to read and slower.
    
    We only ever set them separately:
    
     thule:~/tip> git grep SG_OVER kernel/sched/
     kernel/sched/fair.c:            set_rd_overutilized_status(rq->rd, SG_OVERUTILIZED);
     kernel/sched/fair.c:                    *sg_status |= SG_OVERLOADED;
     kernel/sched/fair.c:                    *sg_status |= SG_OVERUTILIZED;
     kernel/sched/fair.c:                            *sg_status |= SG_OVERLOADED;
     kernel/sched/fair.c:            set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
     kernel/sched/fair.c:                                       sg_status & SG_OVERUTILIZED);
     kernel/sched/fair.c:    } else if (sg_status & SG_OVERUTILIZED) {
     kernel/sched/fair.c:            set_rd_overutilized_status(env->dst_rq->rd, SG_OVERUTILIZED);
     kernel/sched/sched.h:#define SG_OVERLOADED              0x1 /* More than one runnable task on a CPU. */
     kernel/sched/sched.h:#define SG_OVERUTILIZED            0x2 /* One or more CPUs are over-utilized. */
     kernel/sched/sched.h:           set_rd_overloaded(rq->rd, SG_OVERLOADED);
    
    And use them separately, which results in suboptimal code:
    
                    /* update overload indicator if we are at root domain */
                    set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
    
                    /* Update over-utilization (tipping point, U >= 0) indicator */
                    set_rd_overutilized_status(env->dst_rq->rd,
    
    Introduce separate sg_overloaded and sg_overutilized flags in update_sd_lb_stats()
    and its lower level functions, and change all of them to 'bool'.
    
    Remove the now unused SG_OVERLOADED and SG_OVERUTILIZED flags.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Tested-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/ZgVPhODZ8/nbsqbP@gmail.com
    
    K2CI-Arch: All
    Signed-off-by: Huang Bing <huangbing@kylinos.cn>
    Change-Id: I66355e032cf3946b7b8d5abf0f4b7380c916cd55
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/136766
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6580,19 +6580,18 @@ static inline bool cpu_overutilized(int cpu)
 /*
  * overutilized value make sense only if EAS is enabled
  */
-static inline int is_rd_overutilized(struct root_domain *rd)
+static inline bool is_rd_overutilized(struct root_domain *rd)
 {
 	return !sched_energy_enabled() || READ_ONCE(rd->overutilized);
 }
 
-static inline void set_rd_overutilized(struct root_domain *rd,
-					      unsigned int status)
+static inline void set_rd_overutilized(struct root_domain *rd, bool flag)
 {
 	if (!sched_energy_enabled())
 		return;
 
-	WRITE_ONCE(rd->overutilized, status);
-	trace_sched_overutilized_tp(rd, !!status);
+	WRITE_ONCE(rd->overutilized, flag);
+	trace_sched_overutilized_tp(rd, flag);
 }
 
 static inline void check_update_overutilized_status(struct rq *rq)
@@ -6603,7 +6602,7 @@ static inline void check_update_overutilized_status(struct rq *rq)
 	 */
 
 	if (!is_rd_overutilized(rq->rd) && cpu_overutilized(rq->cpu))
-		set_rd_overutilized(rq->rd, SG_OVERUTILIZED);
+		set_rd_overutilized(rq->rd, 1);
 }
 #else
 static inline void check_update_overutilized_status(struct rq *rq) { }
@@ -9827,13 +9826,15 @@ sched_reduced_capacity(struct rq *rq, struct sched_domain *sd)
  * @sds: Load-balancing data with statistics of the local group.
  * @group: sched_group whose statistics are to be updated.
  * @sgs: variable to hold the statistics for this group.
- * @sg_status: Holds flag indicating the status of the sched_group
+ * @sg_overloaded: sched_group is overloaded
+ * @sg_overutilized: sched_group is overutilized
  */
 static inline void update_sg_lb_stats(struct lb_env *env,
 				      struct sd_lb_stats *sds,
 				      struct sched_group *group,
 				      struct sg_lb_stats *sgs,
-				      int *sg_status)
+				      bool *sg_overloaded,
+				      bool *sg_overutilized)
 {
 	int i, nr_running, local_group;
 
@@ -9854,10 +9855,10 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->sum_nr_running += nr_running;
 
 		if (nr_running > 1)
-			*sg_status |= SG_OVERLOADED;
+			*sg_overloaded = 1;
 
 		if (cpu_overutilized(i))
-			*sg_status |= SG_OVERUTILIZED;
+			*sg_overutilized = 1;
 
 #ifdef CONFIG_NUMA_BALANCING
 		sgs->nr_numa_running += rq->nr_numa_running;
@@ -9879,7 +9880,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			/* Check for a misfit task on the cpu */
 			if (sgs->group_misfit_task_load < rq->misfit_task_load) {
 				sgs->group_misfit_task_load = rq->misfit_task_load;
-				*sg_status |= SG_OVERLOADED;
+				*sg_overloaded = 1;
 			}
 		} else if ((env->idle != CPU_NOT_IDLE) &&
 			   sched_reduced_capacity(rq, env->sd)) {
@@ -10514,7 +10515,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 	struct sg_lb_stats *local = &sds->local_stat;
 	struct sg_lb_stats tmp_sgs;
 	unsigned long sum_util = 0;
-	int sg_status = 0;
+	bool sg_overloaded = 0, sg_overutilized = 0;
 
 	do {
 		struct sg_lb_stats *sgs = &tmp_sgs;
@@ -10530,7 +10531,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 				update_group_capacity(env->sd, env->dst_cpu);
 		}
 
-		update_sg_lb_stats(env, sds, sg, sgs, &sg_status);
+		update_sg_lb_stats(env, sds, sg, sgs, &sg_overloaded, &sg_overutilized);
 
 		if (local_group)
 			goto next_group;
@@ -10564,13 +10565,12 @@ next_group:
 
 	if (!env->sd->parent) {
 		/* update overload indicator if we are at root domain */
-		set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
+		set_rd_overloaded(env->dst_rq->rd, sg_overloaded);
 
 		/* Update over-utilization (tipping point, U >= 0) indicator */
-		set_rd_overutilized(env->dst_rq->rd,
-					   sg_status & SG_OVERUTILIZED);
-	} else if (sg_status & SG_OVERUTILIZED) {
-		set_rd_overutilized(env->dst_rq->rd, SG_OVERUTILIZED);
+		set_rd_overutilized(env->dst_rq->rd, sg_overloaded);
+	} else if (sg_overutilized) {
+		set_rd_overutilized(env->dst_rq->rd, sg_overutilized);
 	}
 
 	update_idle_cpu_scan(env, sum_util);
