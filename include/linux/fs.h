8a62cd6da70771be93290e520f4130baa46e0692
Christian Brauner <brauner@kernel.org>
2024-08-30 15:05:01 +0200

    fs: remove f_version
    Mainline: 11068e0b64cbb540b96e577fcca0926242ecaf58
    From: v6.12-rc1
    Severity: Moderate
    
    bug: 354726
    
    Now that detecting concurrent seeks is done by the filesystems that
    require it we can remove f_version and free up 8 bytes for future
    extensions.
    
    Link: https://lore.kernel.org/r/20240830-vfs-file-f_version-v1-20-6d3e4816aa7b@kernel.org
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit 11068e0b64cbb540b96e577fcca0926242ecaf58)
    
    Signed-off-by: Zhou Yuhang <zhouyuhang@kylinos.cn>
    Change-Id: Ib0619db3d391a296609dfd06d88d4912b399d31c
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/71352
    Reviewed-by: 李强 <liqiang01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1013,7 +1013,6 @@ static inline int ra_has_index(struct file_ra_state *ra, pgoff_t index)
  * @f_pos_lock: lock protecting file position
  * @f_pipe: specific to pipes
  * @f_pos: file position
- * @f_version: file version
  * @f_security: LSM security context of this file
  * @f_wb_err: writeback error
  * @f_sb_err: per sb writeback errors
@@ -1044,8 +1043,6 @@ struct file {
 		u64			f_pipe;
 	};
 	loff_t				f_pos;
-	u64				f_version;
-	/* --- cacheline 2 boundary (128 bytes) --- */
 #ifdef CONFIG_SECURITY
 	void				*f_security;
 #endif
