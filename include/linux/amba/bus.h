a1302fa56fda66ae211d083221fffddbab26d0b7
Kunwu Chan <chentao@kylinos.cn>
2024-09-02 07:39:20 +0100

    ARM: 9415/1: amba: Add dev_is_amba() function and export it for modules
    Mainline: ad8d1e323dd37f91d0c973e2a74c7b9054219adc
    From: v6.13-rc1
    Severity: Moderate
    
    task: 436744
    
    Add dev_is_amba() function to determine
    whether the device is a AMBA device.
    
    Suggested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Kunwu Chan <chentao@kylinos.cn>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    
    (backported from commit ad8d1e323dd37f91d0c973e2a74c7b9054219adc)
    
    Signed-off-by: lihaofeng <lihaofeng@kylinos.cn>
    Change-Id: Iff7800d4da229f41da081496879d59d9d38c3e44
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/159355
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>

--- include/linux/amba/bus.h
+++ include/linux/amba/bus.h
@@ -115,6 +115,7 @@ extern const struct bus_type amba_bustype;
 #ifdef CONFIG_ARM_AMBA
 int amba_driver_register(struct amba_driver *);
 void amba_driver_unregister(struct amba_driver *);
+bool dev_is_amba(const struct device *dev);
 #else
 static inline int amba_driver_register(struct amba_driver *drv)
 {
@@ -122,6 +123,10 @@ static inline int amba_driver_register(struct amba_driver *drv)
 }
 static inline void amba_driver_unregister(struct amba_driver *drv)
 {
+}
+static inline bool dev_is_amba(const struct device *dev)
+{
+	return false;
 }
 #endif
 
