899e8babdfed238fdb0e58fec82e12c0f23861ae
Christian Brauner <brauner@kernel.org>
2024-09-05 09:57:00 +0200

    io_uring: port to struct kmem_cache_args
    Mainline: a6711d1cd4e291f334ae900065e18f585732acfa
    From: v6.12-rc1
    Severity: Moderate
    
    task: 444220
    
    Port req_cachep to struct kmem_cache_args.
    
    K2CI-Arch: All
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Jens Axboe <axboe@kernel.dk>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Can Peng <pengcan@kylinos.cn>
    Change-Id: I809fe07db665bc152c4926022afac40461ea16ea
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/60313
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -4736,6 +4736,11 @@ out_fput:
 
 static int __init io_uring_init(void)
 {
+	struct kmem_cache_args kmem_args = {
+		.useroffset = offsetof(struct io_kiocb, cmd.data),
+		.usersize = sizeof_field(struct io_kiocb, cmd.data),
+	};
+
 #define __BUILD_BUG_VERIFY_OFFSET_SIZE(stype, eoffset, esize, ename) do { \
 	BUILD_BUG_ON(offsetof(stype, ename) != eoffset); \
 	BUILD_BUG_ON(sizeof_field(stype, ename) != esize); \
@@ -4817,12 +4822,9 @@ static int __init io_uring_init(void)
 	 * range, and HARDENED_USERCOPY will complain if we haven't
 	 * correctly annotated this range.
 	 */
-	req_cachep = kmem_cache_create_usercopy("io_kiocb",
-				sizeof(struct io_kiocb), 0,
-				SLAB_HWCACHE_ALIGN | SLAB_PANIC |
-				SLAB_ACCOUNT | SLAB_TYPESAFE_BY_RCU,
-				offsetof(struct io_kiocb, cmd.data),
-				sizeof_field(struct io_kiocb, cmd.data), NULL);
+	req_cachep = kmem_cache_create("io_kiocb", sizeof(struct io_kiocb), &kmem_args,
+				SLAB_HWCACHE_ALIGN | SLAB_PANIC | SLAB_ACCOUNT |
+				SLAB_TYPESAFE_BY_RCU);
 
 	iou_wq = alloc_workqueue("iou_exit", WQ_UNBOUND, 64);
 
