4739b4a924b92217fd1ea33ca0d916560ae2218c
Oleg Nesterov <oleg@redhat.com>
2024-09-11 15:14:07 +0200

    uprobes: introduce the global struct vm_special_mapping xol_mapping
    Mainline: 6d27a31ef195951c9b03098edfdf986549a213b7
    From: v6.12-rc1
    Severity: Low
    
    task: 507951
    
    Currently each xol_area has its own instance of vm_special_mapping, this
    is suboptimal and ugly.  Kill xol_area->xol_mapping and add a single
    global instance of vm_special_mapping, the ->fault() method can use
    area->pages rather than xol_mapping->pages.
    
    As a side effect this fixes the problem introduced by the recent commit
    223febc6e557 ("mm: add optional close() to struct vm_special_mapping"), if
    special_mapping_close() is called from the __mmput() paths, it will use
    vma->vm_private_data = &area->xol_mapping freed by uprobe_clear_state().
    
    Link: https://lkml.kernel.org/r/20240911131407.GB3448@redhat.com
    Fixes: 223febc6e557 ("mm: add optional close() to struct vm_special_mapping")
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Reported-by: Sven Schnelle <svens@linux.ibm.com>
    Closes: https://lore.kernel.org/all/yt9dy149vprr.fsf@linux.ibm.com/
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andrii Nakryiko <andrii@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Conflicts: kernel/events/uprobes.c
    Change-Id: Icebbe448225320b6890512f77ab39c37765c79a1
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/113636
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -103,7 +103,6 @@ struct xol_area {
 	atomic_t 			slot_count;	/* number of in-use slots */
 	unsigned long 			*bitmap;	/* 0 = free slot */
 
-	struct vm_special_mapping	xol_mapping;
 	struct page 			*pages[2];
 	/*
 	 * We keep the vma's vm_start rather than a pointer to the vma
@@ -1483,6 +1482,21 @@ void uprobe_munmap(struct vm_area_struct *vma, unsigned long start, unsigned lon
 		set_bit(MMF_RECALC_UPROBES, &vma->vm_mm->flags);
 }
 
+static vm_fault_t xol_fault(const struct vm_special_mapping *sm,
+			    struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+	struct xol_area *area = vma->vm_mm->uprobes_state.xol_area;
+
+	vmf->page = area->pages[0];
+	get_page(vmf->page);
+	return 0;
+}
+
+static const struct vm_special_mapping xol_mapping = {
+	.name = "[uprobes]",
+	.fault = xol_fault,
+};
+
 /* Slot allocation for XOL */
 static int xol_add_vma(struct mm_struct *mm, struct xol_area *area)
 {
@@ -1509,7 +1523,7 @@ static int xol_add_vma(struct mm_struct *mm, struct xol_area *area)
 
 	vma = _install_special_mapping(mm, area->vaddr, PAGE_SIZE,
 				VM_EXEC|VM_MAYEXEC|VM_DONTCOPY|VM_IO,
-				&area->xol_mapping);
+				&xol_mapping);
 	if (IS_ERR(vma)) {
 		ret = PTR_ERR(vma);
 		goto fail;
@@ -1548,8 +1562,6 @@ static struct xol_area *__create_xol_area(unsigned long vaddr)
 	if (!area->bitmap)
 		goto free_area;
 
-	area->xol_mapping.name = "[uprobes]";
-	area->xol_mapping.pages = area->pages;
 	area->pages[0] = alloc_page(GFP_HIGHUSER | __GFP_ZERO);
 	if (!area->pages[0])
 		goto free_bitmap;
