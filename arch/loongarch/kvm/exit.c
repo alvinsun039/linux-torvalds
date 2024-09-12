e1b88262e2cec8dd92f10267a6bdb541093cbdb2
Bibo Mao <maobibo@loongson.cn>
2024-09-12 20:53:40 +0800

    LoongArch: KVM: Enable paravirt feature control from VMM
    commit cdc118f802410525cca872e0861a14d76d12c574 upstream.
    
    Export kernel paravirt features to user space, so that VMM can control
    each single paravirt feature. By default paravirt features will be the
    same with kvm supported features if VMM does not set it.
    
    Also a new feature KVM_FEATURE_VIRT_EXTIOI is added which can be set
    from user space. This feature indicates that the virt EIOINTC can route
    interrupts to 256 vCPUs, rather than 4 vCPUs like with real HW.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Reviewed-by: Juxin Gao <gaojuxin@loongson.cn>
    Signed-off-by: Kanglong Wang <wangkanglong@loongson.cn>

--- arch/loongarch/kvm/exit.c
+++ arch/loongarch/kvm/exit.c
@@ -50,9 +50,7 @@ static int kvm_emu_cpucfg(struct kvm_vcpu *vcpu, larch_inst inst)
 		vcpu->arch.gprs[rd] = *(unsigned int *)KVM_SIGNATURE;
 		break;
 	case CPUCFG_KVM_FEATURE:
-		ret = KVM_FEATURE_IPI;
-		if (kvm_pvtime_supported())
-			ret |= KVM_FEATURE_STEAL_TIME;
+		ret = vcpu->kvm->arch.pv_features & LOONGARCH_PV_FEAT_MASK;
 		vcpu->arch.gprs[rd] = ret;
 		break;
 	default:
@@ -827,19 +825,21 @@ static int kvm_send_pv_ipi(struct kvm_vcpu *vcpu)
  */
 static void kvm_handle_service(struct kvm_vcpu *vcpu)
 {
+	long ret = KVM_HCALL_INVALID_CODE;
 	unsigned long func = kvm_read_reg(vcpu, LOONGARCH_GPR_A0);
-	long ret;
 
 	switch (func) {
 	case KVM_HCALL_FUNC_IPI:
-		kvm_send_pv_ipi(vcpu);
-		ret = KVM_HCALL_SUCCESS;
+		if (kvm_guest_has_pv_feature(vcpu, KVM_FEATURE_IPI)) {
+			kvm_send_pv_ipi(vcpu);
+			ret = KVM_HCALL_SUCCESS;
+		}
 		break;
 	case KVM_HCALL_FUNC_NOTIFY:
-		ret = kvm_save_notify(vcpu);
+		if (kvm_guest_has_pv_feature(vcpu, KVM_FEATURE_STEAL_TIME))
+                       ret = kvm_save_notify(vcpu);		
 		break;
 	default:
-		ret = KVM_HCALL_INVALID_CODE;
 		break;
 	}
 
