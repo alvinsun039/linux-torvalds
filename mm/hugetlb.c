a8458d575b6f49046c790c5db0371cdc37d35424
Vishal Moola (Oracle) <vishal.moola@gmail.com>
2024-09-14 12:41:19 -0700

    mm/hugetlb.c: fix UAF of vma in hugetlb fault pathway
    Mainline: 98b74bb4d7e96b4da5ef3126511febe55b76b807
    From: v6.12-rc1
    Severity: Important
    CVE: CVE-2024-47676
    
    task: 376433
    
    Syzbot reports a UAF in hugetlb_fault().  This happens because
    vmf_anon_prepare() could drop the per-VMA lock and allow the current VMA
    to be freed before hugetlb_vma_unlock_read() is called.
    
    We can fix this by using a modified version of vmf_anon_prepare() that
    doesn't release the VMA lock on failure, and then release it ourselves
    after hugetlb_vma_unlock_read().
    
    Link: https://lkml.kernel.org/r/20240914194243.245-2-vishal.moola@gmail.com
    Fixes: 9acad7ba3e25 ("hugetlb: use vmf_anon_prepare() instead of anon_vma_prepare()")
    Reported-by: syzbot+2dab93857ee95f2eeb08@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/linux-mm/00000000000067c20b06219fbc26@google.com/
    Signed-off-by: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 98b74bb4d7e96b4da5ef3126511febe55b76b807)
    
    Signed-off-by: wangxuewen <wangxuewen@kylinos.cn>
    Change-Id: I39b4317465e5d1b4fd77929f82a2d1e71623478e
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/141958
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5759,7 +5759,7 @@ retry_avoidcopy:
 	 * When the original hugepage is shared one, it does not have
 	 * anon_vma prepared.
 	 */
-	ret = vmf_anon_prepare(vmf);
+	ret = __vmf_anon_prepare(vmf);
 	if (unlikely(ret))
 		goto out_release_all;
 
@@ -5961,7 +5961,7 @@ static vm_fault_t hugetlb_no_page(struct mm_struct *mm,
 		}
 
 		if (!(vma->vm_flags & VM_MAYSHARE)) {
-			ret = vmf_anon_prepare(vmf);
+			ret = __vmf_anon_prepare(vmf);
 			if (unlikely(ret))
 				goto out;
 		}
@@ -6090,6 +6090,14 @@ static vm_fault_t hugetlb_no_page(struct mm_struct *mm,
 	folio_unlock(folio);
 out:
 	hugetlb_vma_unlock_read(vma);
+
+	/*
+	 * We must check to release the per-VMA lock. __vmf_anon_prepare() is
+	 * the only way ret can be set to VM_FAULT_RETRY.
+	 */
+	if (unlikely(ret & VM_FAULT_RETRY))
+		vma_end_read(vma);
+
 	mutex_unlock(&hugetlb_fault_mutex_table[hash]);
 	return ret;
 
@@ -6314,6 +6322,14 @@ out_ptl:
 	}
 out_mutex:
 	hugetlb_vma_unlock_read(vma);
+
+	/*
+	 * We must check to release the per-VMA lock. __vmf_anon_prepare() in
+	 * hugetlb_wp() is the only way ret can be set to VM_FAULT_RETRY.
+	 */
+	if (unlikely(ret & VM_FAULT_RETRY))
+		vma_end_read(vma);
+
 	mutex_unlock(&hugetlb_fault_mutex_table[hash]);
 	/*
 	 * Generally it's safe to hold refcount during waiting page lock. But
