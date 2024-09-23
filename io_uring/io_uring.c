7003f3a533083083da4cae9f1c25753b3b509142
Guixin Liu <kanie@linux.alibaba.com>
2024-09-23 18:05:12 +0800

    io_uring: fix memory leak when cache init fail
    Mainline: 3a87e264290d71ec86a210ab3e8d23b715ad266d
    From: v6.12-rc2
    Severity: Moderate
    
    task: 526909
    
    Exit the percpu ref when cache init fails to free the data memory with
    in struct percpu_ref.
    
    Fixes: 206aefde4f88 ("io_uring: reduce/pack size of io_ring_ctx")
    Signed-off-by: Guixin Liu <kanie@linux.alibaba.com>
    Reviewed-by: Gabriel Krisman Bertazi <krisman@suse.de>
    Link: https://lore.kernel.org/r/20240923100512.64638-1-kanie@linux.alibaba.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit 3a87e264290d71ec86a210ab3e8d23b715ad266d)
    
    K2CI-Arch: All
    Change-Id: Idf4f9c860e9c75fdbf2b57d18d22d6f7989f83b9
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151445
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -330,7 +330,7 @@ static __cold struct io_ring_ctx *io_ring_ctx_alloc(struct io_uring_params *p)
 			    sizeof(struct io_kiocb));
 	ret |= io_futex_cache_init(ctx);
 	if (ret)
-		goto err;
+		goto free_ref;
 	init_completion(&ctx->ref_comp);
 	xa_init_flags(&ctx->personalities, XA_FLAGS_ALLOC1);
 	mutex_init(&ctx->uring_lock);
@@ -356,6 +356,9 @@ static __cold struct io_ring_ctx *io_ring_ctx_alloc(struct io_uring_params *p)
 	INIT_WQ_LIST(&ctx->submit_state.compl_reqs);
 	INIT_HLIST_HEAD(&ctx->cancelable_uring_cmd);
 	return ctx;
+
+free_ref:
+	percpu_ref_exit(&ctx->refs);
 err:
 	io_alloc_cache_free(&ctx->rsrc_node_cache, kfree);
 	io_alloc_cache_free(&ctx->apoll_cache, kfree);
