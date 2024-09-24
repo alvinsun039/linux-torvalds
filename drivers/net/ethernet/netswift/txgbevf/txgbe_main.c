52cfd4dc9a087ff3591a1c0b6886566187e41240
duanqiangwen <duanqiangwen@net-swift.com>
2024-09-24 16:17:16 +0800

    NETSWIFT: txgbevf: fix: tx csum by soft except tcp udp sctp
    Mainline: KYLIN-only
    Severity: Low
    
    Task: #552421
    
    reference: txgbe commit e88ebcb3ae461d52630ba9bc1af7f006ad493796
    
    when l4 is not tcp, udp, or sctp, compute csum by software.
    
    Signed-off-by: duanqiangwen <duanqiangwen@net-swift.com>
    
    K2CI-Arch: All
    Signed-off-by: luoqing <luoqing@kylinos.cn>
    Change-Id: I84430cf708ece83fdbbca5d76a7699cd5987c96d

--- drivers/net/ethernet/netswift/txgbevf/txgbe_main.c
+++ drivers/net/ethernet/netswift/txgbevf/txgbe_main.c
@@ -4736,6 +4736,7 @@ void txgbe_tx_csum(struct txgbe_ring *tx_ring,
 	//type_tucmd = TXGBE_TXD_PKTTYPE(dptype.ptype);
 
 	if (skb->ip_summed != CHECKSUM_PARTIAL) {
+csum_failed:
 		if (!(first->tx_flags & TXGBE_TX_FLAGS_VLAN) &&
 		    !(first->tx_flags & TXGBE_TX_FLAGS_CC))
 			return;
@@ -4863,7 +4864,8 @@ void txgbe_tx_csum(struct txgbe_ring *tx_ring,
 					TXGBE_TXD_L4LEN_SHIFT;
 			break;
 		default:
-			break;
+			skb_checksum_help(skb);
+			goto csum_failed;
 		}
 
 		/* update TX checksum flag */
