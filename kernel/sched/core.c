6e148308a5d2c2b9a3430acb8b4c4e018f644f7e
Andrea Parri <parri.andrea@gmail.com>
2024-09-27 11:33:52 +0800

    membarrier: Create Documentation/scheduler/membarrier.rst
    Mainline: a14d11a0f5f4105e0df96811dfa81dc5f79fecba
    From: v6.9-rc1
    Severity: Low
    
    Task: #340825
    
    To gather the architecture requirements of the "private/global
    expedited" membarrier commands.  The file will be expanded to
    integrate further information about the membarrier syscall (as
    needed/desired in the future).  While at it, amend some related
    inline comments in the membarrier codebase.
    
    Suggested-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Andrea Parri <parri.andrea@gmail.com>
    Reviewed-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Link: https://lore.kernel.org/r/20240131144936.29190-3-parri.andrea@gmail.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    K2CI-Arch: All
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Change-Id: Id7ee7225018a99ba485166d81acc706870b0eee7
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/136663
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6648,7 +6648,9 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	 *     if (signal_pending_state())	    if (p->state & @state)
 	 *
 	 * Also, the membarrier system call requires a full memory barrier
-	 * after coming from user-space, before storing to rq->curr.
+	 * after coming from user-space, before storing to rq->curr; this
+	 * barrier matches a full barrier in the proximity of the membarrier
+	 * system call exit.
 	 */
 	rq_lock(rq, &rf);
 	smp_mb__after_spinlock();
@@ -6726,6 +6728,9 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 		 *   architectures where spin_unlock is a full barrier,
 		 * - switch_to() for arm64 (weakly-ordered, spin_unlock
 		 *   is a RELEASE barrier),
+		 *
+		 * The barrier matches a full barrier in the proximity of
+		 * the membarrier system call entry.
 		 */
 		++*switch_count;
 
