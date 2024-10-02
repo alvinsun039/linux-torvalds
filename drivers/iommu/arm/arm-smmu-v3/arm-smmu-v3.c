9e872afe0cfeeb316581725c8922753785642767
Daniel Mentz <danielmentz@google.com>
2024-10-01 18:53:57 -0700

    iommu/arm-smmu-v3: Fix last_sid_idx calculation for sid_bits==32
    Mainline: f63237f54cf18448728201a65e6c82018e807cd9
    From: v6.12-rc4
    Severity: Important
    
    task: 588865
    
    The function arm_smmu_init_strtab_2lvl uses the expression
    
    ((1 << smmu->sid_bits) - 1)
    
    to calculate the largest StreamID value. However, this fails for the
    maximum allowed value of SMMU_IDR1.SIDSIZE which is 32. The C standard
    states:
    
    "If the value of the right operand is negative or is greater than or
    equal to the width of the promoted left operand, the behavior is
    undefined."
    
    With smmu->sid_bits being 32, the prerequisites for undefined behavior
    are met.  We observed that the value of (1 << 32) is 1 and not 0 as we
    initially expected.
    
    Similar bit shift operations in arm_smmu_init_strtab_linear seem to not
    be affected, because it appears to be unlikely for an SMMU to have
    SMMU_IDR1.SIDSIZE set to 32 but then not support 2-level Stream tables
    
    This issue was found by Ryan Huang <tzukui@google.com> on our team.
    
    Fixes: ce410410f1a7 ("iommu/arm-smmu-v3: Add arm_smmu_strtab_l1/2_idx()")
    Signed-off-by: Daniel Mentz <danielmentz@google.com>
    Link: https://lore.kernel.org/r/20241002015357.1766934-1-danielmentz@google.com
    Signed-off-by: Will Deacon <will@kernel.org>
    
    (backported from commit f63237f54cf18448728201a65e6c82018e807cd9)
    
    K2CI-Arch: All
    Cc: kernel-stable-team #2603+
    Change-Id: I5dedbe066f27949af063b2fb6ce78846e8f9c2eb
    Signed-off-by: jianghaoran <jianghaoran@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/171874
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3860,7 +3860,7 @@ static int arm_smmu_init_strtab_2lvl(struct arm_smmu_device *smmu)
 	u32 l1size;
 	struct arm_smmu_strtab_cfg *cfg = &smmu->strtab_cfg;
 	unsigned int last_sid_idx =
-		arm_smmu_strtab_l1_idx((1 << smmu->sid_bits) - 1);
+		arm_smmu_strtab_l1_idx((1ULL << smmu->sid_bits) - 1);
 #ifdef CONFIG_SMMU_BYPASS_DEV
 	int ret;
 #endif
