0724505f393a2391824649e5d21d77133cda2d24
Jackie Liu <liuyun01@kylinos.cn>
2024-10-08 11:27:29 +0800

    KYLIN: Revert "OPENEULER: net: stmmac: dwmac-loongson: Add ref and ptp clocks for Loongson"
    Mainline: KYLIN-only
    Severity: Low
    
    task: 366821
    
    This reverts commit e6d5b5d026fb1c55388a9809da73831b739e2ad7.
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I801748dd31fe1a8468e98cba831f0327fc78dc81
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/137601
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-loongson.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-loongson.c
@@ -108,9 +108,6 @@ static void loongson_default_data(struct pci_dev *pdev,
 	/* Disable RX queues routing by default */
 	plat->rx_queues_cfg[0].pkt_route = 0x0;
 
-	plat->clk_ref_rate = 125000000;
-	plat->clk_ptp_rate = 125000000;
-
 	/* Default to phy auto-detection */
 	plat->phy_addr = -1;
 
