0cb84b1ac502a108169eab573f923d6bf95593a8
Namhyung Kim <namhyung@kernel.org>
2024-10-10 16:25:04 -0700

    mm/bpf: Add bpf_get_kmem_cache() kfunc
    Mainline: a992d7a3979120fbd7c13435d27b3da8d9ed095a
    From: v6.13-rc1
    Severity: Low
    
    task: 501509
    
    The bpf_get_kmem_cache() is to get a slab cache information from a
    virtual address like virt_to_cache().  If the address is a pointer
    to a slab object, it'd return a valid kmem_cache pointer, otherwise
    NULL is returned.
    
    It doesn't grab a reference count of the kmem_cache so the caller is
    responsible to manage the access.  The returned point is marked as
    PTR_UNTRUSTED.
    
    The intended use case for now is to symbolize locks in slab objects
    from the lock contention tracepoints.
    
    Suggested-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Roman Gushchin <roman.gushchin@linux.dev> (mm/*)
    Acked-by: Vlastimil Babka <vbabka@suse.cz> #mm/slab
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Link: https://lore.kernel.org/r/20241010232505.1339892-3-namhyung@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Change-Id: Ib65956712ecc6e5ff1a69caf7c1db1514247726d
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/94476
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -3111,6 +3111,7 @@ BTF_ID_FLAGS(func, bpf_preempt_enable)
 BTF_ID_FLAGS(func, bpf_iter_bits_new, KF_ITER_NEW)
 BTF_ID_FLAGS(func, bpf_iter_bits_next, KF_ITER_NEXT | KF_RET_NULL)
 BTF_ID_FLAGS(func, bpf_iter_bits_destroy, KF_ITER_DESTROY)
+BTF_ID_FLAGS(func, bpf_get_kmem_cache)
 BTF_SET8_END(common_btf_ids)
 
 static const struct btf_kfunc_id_set common_kfunc_set = {
