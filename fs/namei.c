ded64abb98dbbfca6566092850d4a19ff8b12b03
Al Viro <viro@zeniv.linux.org.uk>
2024-10-19 20:32:39 -0400

    teach filename_lookup() to treat NULL filename as ""
    Mainline: 5b313bcb6e3597dacd893ae9545fd087df46db45
    From: v6.13-rc1
    Severity: Low
    
    task: 536731
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    (backported from commit 5b313bcb6e3597dacd893ae9545fd087df46db45)
    
    Change-Id: I55bb0da372c8839d3194be197c0f248b87f36a82
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/122501
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- fs/namei.c
+++ fs/namei.c
@@ -595,6 +595,7 @@ struct nameidata {
 		unsigned seq;
 	} *stack, internal[EMBEDDED_LEVELS];
 	struct filename	*name;
+	const char *pathname;
 	struct nameidata *saved;
 	unsigned	root_seq;
 	int		dfd;
@@ -613,6 +614,7 @@ static void __set_nameidata(struct nameidata *p, int dfd, struct filename *name)
 	p->depth = 0;
 	p->dfd = dfd;
 	p->name = name;
+	p->pathname = likely(name) ? name->name : "";
 	p->path.mnt = NULL;
 	p->path.dentry = NULL;
 	p->total_link_count = old ? old->total_link_count : 0;
@@ -2363,7 +2365,7 @@ OK:
 static const char *path_init(struct nameidata *nd, unsigned flags)
 {
 	int error;
-	const char *s = nd->name->name;
+	const char *s = nd->pathname;
 
 	/* LOOKUP_CACHED requires RCU, ask caller to retry */
 	if ((flags & (LOOKUP_RCU | LOOKUP_CACHED)) == LOOKUP_CACHED)
