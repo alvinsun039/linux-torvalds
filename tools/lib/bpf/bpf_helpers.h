3d3e5109822bed1d06bf59dc3af5e90334c8044b
Kui-Feng Lee <thinker.li@gmail.com>
2024-10-23 16:47:54 -0700

    libbpf: define __uptr.
    Mainline: 7aa12b8d9f24e9623effa12a3fc330de056d572e
    From: v6.13-rc1
    Severity: Low
    
    task: 501509
    
    Make __uptr available to BPF programs to enable them to define uptrs.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Kui-Feng Lee <thinker.li@gmail.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://lore.kernel.org/r/20241023234759.860539-8-martin.lau@linux.dev
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Change-Id: If07790a63afad760f5de2d1a592f0fee399281b0
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/94772
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- tools/lib/bpf/bpf_helpers.h
+++ tools/lib/bpf/bpf_helpers.h
@@ -183,6 +183,7 @@ enum libbpf_tristate {
 #define __kptr_untrusted __attribute__((btf_type_tag("kptr_untrusted")))
 #define __kptr __attribute__((btf_type_tag("kptr")))
 #define __percpu_kptr __attribute__((btf_type_tag("percpu_kptr")))
+#define __uptr __attribute__((btf_type_tag("uptr")))
 
 #define bpf_ksym_exists(sym) ({									\
 	_Static_assert(!__builtin_constant_p(!!sym), #sym " should be marked as __weak");	\
