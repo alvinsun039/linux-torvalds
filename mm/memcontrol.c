e80564edb4c3bebbaa56b1bc8ca5bec6f5eb34ba
Shakeel Butt <shakeel.butt@linux.dev>
2024-10-24 18:23:03 -0700

    memcg-v1: remove memcg move locking code
    Mainline: a29c0e4b2e867f4e362a6740c430bfdc2efdd1d9
    From: v6.13-rc1
    Severity: Moderate
    
    task: 447145
    
    The memcg v1's charge move feature has been deprecated.  All the places
    using the memcg move lock, have stopped using it as they don't need the
    protection any more.  Let's proceed to remove all the locking code related
    to charge moving.
    
    Link: https://lkml.kernel.org/r/20241025012304.2473312-7-shakeel.butt@linux.dev
    Change-Id: I93274cc82a17679b0f634c4146f689227b9ec643
    Signed-off-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: liqiang01 <liqiang01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56970
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 彭灿 <pengcan@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 黄锦慧 <huangjinhui@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1158,7 +1158,6 @@ void lruvec_memcg_debug(struct lruvec *lruvec, struct folio *folio)
  * These functions are safe to use under any of the following conditions:
  * - folio locked
  * - folio_test_lru false
- * - folio_memcg_lock()
  * - folio frozen (refcount of 0)
  *
  * Return: The lruvec this folio is on with its lock held.
@@ -1180,7 +1179,6 @@ struct lruvec *folio_lruvec_lock(struct folio *folio)
  * These functions are safe to use under any of the following conditions:
  * - folio locked
  * - folio_test_lru false
- * - folio_memcg_lock()
  * - folio frozen (refcount of 0)
  *
  * Return: The lruvec this folio is on with its lock held and interrupts
@@ -1204,7 +1202,6 @@ struct lruvec *folio_lruvec_lock_irq(struct folio *folio)
  * These functions are safe to use under any of the following conditions:
  * - folio locked
  * - folio_test_lru false
- * - folio_memcg_lock()
  * - folio frozen (refcount of 0)
  *
  * Return: The lruvec this folio is on with its lock held and interrupts
@@ -2317,9 +2314,7 @@ static void commit_charge(struct folio *folio, struct mem_cgroup *memcg)
 	 *
 	 * - the page lock
 	 * - LRU isolation
-	 * - folio_memcg_lock()
 	 * - exclusive reference
-	 * - mem_cgroup_trylock_pages()
 	 */
 	folio->memcg_data = (unsigned long)memcg;
 }
