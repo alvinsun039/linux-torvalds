e7b816cca40247ca6463dc38ea9f4e92aedbcc1b
Shakeel Butt <shakeel.butt@linux.dev>
2024-10-24 18:23:01 -0700

    memcg-v1: no need for memcg locking for writeback tracking
    Mainline: 568bcf4148493a3cf544f88df4e81e862b69f5e9
    From: v6.13-rc1
    Severity: Moderate
    
    task: 447145
    
    During the era of memcg charge migration, the kernel has to be make
    sure that the writeback stat updates do not race with the charge
    migration.  Otherwise it might update the writeback stats of the wrong
    memcg.  Now with the memcg charge migration gone, there is no more race
    for writeback stat updates and the previous locking can be removed.
    
    Link: https://lkml.kernel.org/r/20241025012304.2473312-5-shakeel.butt@linux.dev
    Signed-off-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 568bcf4148493a3cf544f88df4e81e862b69f5e9)
    
    Change-Id: I0180644f8a00245da71033e60e5cf29f38c9ab96
    Signed-off-by: liqiang01 <liqiang01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56968
    Reviewed-by: 彭灿 <pengcan@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 黄锦慧 <huangjinhui@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -2981,7 +2981,6 @@ bool __folio_end_writeback(struct folio *folio)
 	struct address_space *mapping = folio_mapping(folio);
 	bool ret;
 
-	folio_memcg_lock(folio);
 	if (mapping && mapping_use_writeback_tags(mapping)) {
 		struct inode *inode = mapping->host;
 		struct backing_dev_info *bdi = inode_to_bdi(inode);
@@ -3012,7 +3011,6 @@ bool __folio_end_writeback(struct folio *folio)
 	lruvec_stat_mod_folio(folio, NR_WRITEBACK, -nr);
 	zone_stat_mod_folio(folio, NR_ZONE_WRITE_PENDING, -nr);
 	node_stat_mod_folio(folio, NR_WRITTEN, nr);
-	folio_memcg_unlock(folio);
 
 	return ret;
 }
@@ -3025,7 +3023,6 @@ void __folio_start_writeback(struct folio *folio, bool keep_write)
 
 	VM_BUG_ON_FOLIO(folio_test_writeback(folio), folio);
 
-	folio_memcg_lock(folio);
 	if (mapping && mapping_use_writeback_tags(mapping)) {
 		XA_STATE(xas, &mapping->i_pages, folio_index(folio));
 		struct inode *inode = mapping->host;
@@ -3066,7 +3063,6 @@ void __folio_start_writeback(struct folio *folio, bool keep_write)
 
 	lruvec_stat_mod_folio(folio, NR_WRITEBACK, nr);
 	zone_stat_mod_folio(folio, NR_ZONE_WRITE_PENDING, nr);
-	folio_memcg_unlock(folio);
 
 	access_ret = arch_make_folio_accessible(folio);
 	/*
