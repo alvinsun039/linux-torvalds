dce0d57027f9c776ef892514019d2e54233893f5
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2024-11-01 18:46:27 +0000

    mm: remove unnecessary page_table_lock on stack expansion
    Mainline: ad2bc8812fc17c8536d5e37aa0754463b76b66a4
    From: v6.13-rc1
    Severity: Moderate
    
    bug: 384799
    
    Ever since commit 8d7071af8907 ("mm: always expand the stack with the mmap
    write lock held") we have been expanding the stack with the mmap write
    lock held.
    
    This is true in all code paths:
    
    get_arg_page()
      -> expand_downwards()
    setup_arg_pages()
      -> expand_stack_locked()
        -> expand_downwards() / expand_upwards()
    lock_mm_and_find_vma()
      -> expand_stack_locked()
        -> expand_downwards() / expand_upwards()
    create_elf_tables()
      -> find_extend_vma_locked()
        -> expand_stack_locked()
    expand_stack()
      -> vma_expand_down()
        -> expand_downwards()
    expand_stack()
      -> vma_expand_up()
        -> expand_upwards()
    
    Each of which acquire the mmap write lock before doing so.  Despite this,
    we maintain code that acquires a page table lock in the expand_upwards()
    and expand_downwards() code, stating that we hold a shared mmap lock and
    thus this is necessary.
    
    It is not, we do not have to worry about concurrent VMA expansions so we
    can simply drop this, and update comments accordingly.
    
    We do not even need be concerned with racing page faults, as
    vma_start_write() is invoked in both cases.
    
    Link: https://lkml.kernel.org/r/20241101184627.131391-1-lorenzo.stoakes@oracle.com
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Reviewed-by: Jann Horn <jannh@google.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    K2CI-Arch: All
    Change-Id: I237a924f656de497fade05f0639f72edfa5e5441
    Signed-off-by: Lincheng Yang <yanglincheng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/89491
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2022,6 +2022,8 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 	if (!(vma->vm_flags & VM_GROWSUP))
 		return -EFAULT;
 
+	mmap_assert_write_locked(mm);
+
 	/* Guard against exceeding limits of the address space. */
 	address &= PAGE_MASK;
 	if (address >= (TASK_SIZE & PAGE_MASK))
@@ -2057,11 +2059,7 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 
 	/* Lock the VMA before expanding to prevent concurrent page faults */
 	vma_start_write(vma);
-	/*
-	 * vma->vm_start/vm_end cannot change under us because the caller
-	 * is required to hold the mmap_lock in read mode.  We need the
-	 * anon_vma lock to serialize against concurrent expand_stacks.
-	 */
+	/* We update the anon VMA tree. */
 	anon_vma_lock_write(vma->anon_vma);
 
 	/* Somebody else might have raced and expanded it already */
@@ -2075,16 +2073,6 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 		if (vma->vm_pgoff + (size >> PAGE_SHIFT) >= vma->vm_pgoff) {
 			error = acct_stack_growth(vma, size, grow);
 			if (!error) {
-				/*
-				 * We only hold a shared mmap_lock lock here, so
-				 * we need to protect against concurrent vma
-				 * expansions.  anon_vma_lock_write() doesn't
-				 * help here, as we don't guarantee that all
-				 * growable vmas in a mm share the same root
-				 * anon vma.  So, we reuse mm->page_table_lock
-				 * to guard against concurrent vma expansions.
-				 */
-				spin_lock(&mm->page_table_lock);
 				if (vma->vm_flags & VM_LOCKED)
 					mm->locked_vm += grow;
 				vm_stat_account(mm, vma->vm_flags, grow);
@@ -2093,7 +2081,6 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 				/* Overwrite old entry in mtree. */
 				mas_store_prealloc(&mas, vma);
 				anon_vma_interval_tree_post_update_vma(vma);
-				spin_unlock(&mm->page_table_lock);
 
 				perf_event_mmap(vma);
 			}
@@ -2119,6 +2106,8 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 
 	if (!(vma->vm_flags & VM_GROWSDOWN))
 		return -EFAULT;
+
+	mmap_assert_write_locked(mm);
 
 	address &= PAGE_MASK;
 	if (address < mmap_min_addr || address < FIRST_USER_ADDRESS)
@@ -2149,11 +2138,7 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 
 	/* Lock the VMA before expanding to prevent concurrent page faults */
 	vma_start_write(vma);
-	/*
-	 * vma->vm_start/vm_end cannot change under us because the caller
-	 * is required to hold the mmap_lock in read mode.  We need the
-	 * anon_vma lock to serialize against concurrent expand_stacks.
-	 */
+	/* We update the anon VMA tree. */
 	anon_vma_lock_write(vma->anon_vma);
 
 	/* Somebody else might have raced and expanded it already */
@@ -2167,16 +2152,6 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 		if (grow <= vma->vm_pgoff) {
 			error = acct_stack_growth(vma, size, grow);
 			if (!error) {
-				/*
-				 * We only hold a shared mmap_lock lock here, so
-				 * we need to protect against concurrent vma
-				 * expansions.  anon_vma_lock_write() doesn't
-				 * help here, as we don't guarantee that all
-				 * growable vmas in a mm share the same root
-				 * anon vma.  So, we reuse mm->page_table_lock
-				 * to guard against concurrent vma expansions.
-				 */
-				spin_lock(&mm->page_table_lock);
 				if (vma->vm_flags & VM_LOCKED)
 					mm->locked_vm += grow;
 				vm_stat_account(mm, vma->vm_flags, grow);
@@ -2186,7 +2161,6 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 				/* Overwrite old entry in mtree. */
 				mas_store_prealloc(&mas, vma);
 				anon_vma_interval_tree_post_update_vma(vma);
-				spin_unlock(&mm->page_table_lock);
 
 				perf_event_mmap(vma);
 			}
