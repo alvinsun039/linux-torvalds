f9f858e182380d144e429325a97157015f16d07b
Mateusz Guzik <mjguzik@gmail.com>
2024-11-08 15:19:54 +0800

    mm: batch unlink_file_vma calls in free_pgd_range
    Mainline: 3577dbb192419e37b6f54aced8777b6c81cd03d4
    From: v6.11-rc1
    Severity: Low
    
    Task: 400173
    
    Execs of dynamically linked binaries at 20-ish cores are bottlenecked on
    the i_mmap_rwsem semaphore, while the biggest singular contributor is
    free_pgd_range inducing the lock acquire back-to-back for all consecutive
    mappings of a given file.
    
    Tracing the count of said acquires while building the kernel shows:
    [1, 2)     799579 |@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|
    [2, 3)          0 |                                                    |
    [3, 4)       3009 |                                                    |
    [4, 5)       3009 |                                                    |
    [5, 6)     326442 |@@@@@@@@@@@@@@@@@@@@@                               |
    
    So in particular there were 326442 opportunities to coalesce 5 acquires
    into 1.
    
    Doing so increases execs per second by 4% (~50k to ~52k) when running
    the benchmark linked below.
    
    The lock remains the main bottleneck, I have not looked at other spots
    yet.
    
    Bench can be found here:
    http://apollo.backplane.com/DFlyMisc/doexec.c
    
    $ cc -O2 -o shared-doexec doexec.c
    $ ./shared-doexec $(nproc)
    
    Note this particular test makes sure binaries are separate, but the
    loader is shared.
    
    Stats collected on the patched kernel (+ "noinline") with:
    bpftrace -e 'kprobe:unlink_file_vma_batch_process
    { @ = lhist(((struct unlink_vma_file_batch *)arg0)->count, 0, 8, 1); }'
    
    K2CI-Arch: All
    Link: https://lkml.kernel.org/r/20240521234321.359501-1-mjguzik@gmail.com
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Cc: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Conflicts: mm/internal.h
    [Context conflict]
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Signed-off-by: liqiang <liqiang01@kylinos.cn>
    Change-Id: I2c82c30f1ae0c9b0a0af130b133d385f6daccb0a
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/158278
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- mm/internal.h
+++ mm/internal.h
@@ -1535,4 +1535,14 @@ static inline void shrinker_debugfs_remove(struct dentry *debugfs_entry,
 }
 #endif /* CONFIG_SHRINKER_DEBUG */
 
+
+struct unlink_vma_file_batch {
+	int count;
+	struct vm_area_struct *vmas[8];
+};
+
+void unlink_file_vma_batch_init(struct unlink_vma_file_batch *);
+void unlink_file_vma_batch_add(struct unlink_vma_file_batch *, struct vm_area_struct *);
+void unlink_file_vma_batch_final(struct unlink_vma_file_batch *);
+
 #endif	/* __MM_INTERNAL_H */
