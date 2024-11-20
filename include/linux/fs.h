2a87870bfba208fb92505de70cb2e01df49346fe
Mateusz Guzik <mjguzik@gmail.com>
2024-11-20 12:20:34 +0100

    vfs: support caching symlink lengths in inodes
    Mainline: ea382199071931d19aac5f688b543e07360e2b64
    From: v6.14-rc1
    Severity: Important
    
    task: 530755
    
    When utilized it dodges strlen() in vfs_readlink(), giving about 1.5%
    speed up when issuing readlink on /initrd.img on ext4.
    
    Filesystems opt in by calling inode_set_cached_link() when creating an
    inode.
    
    The size is stored in a new union utilizing the same space as i_devices,
    thus avoiding growing the struct or taking up any more space.
    
    Churn-wise the current readlink_copy() helper is patched to accept the
    size instead of calculating it.
    
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>
    Link: https://lore.kernel.org/r/20241120112037.822078-2-mjguzik@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit ea382199071931d19aac5f688b543e07360e2b64)
    
    Change-Id: Icfa5dcf29c0b1a8b61b70bf43063807ab23ae03e
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117842
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -620,6 +620,7 @@ is_uncached_acl(struct posix_acl *acl)
 #define IOP_NOFOLLOW	0x0004
 #define IOP_XATTR	0x0008
 #define IOP_DEFAULT_READLINK	0x0010
+#define IOP_CACHED_LINK	0x0040
 
 /*
  * Keep mostly read-only and often accessed (especially for
@@ -712,7 +713,10 @@ struct inode {
 	};
 	struct file_lock_context	*i_flctx;
 	struct address_space	i_data;
-	struct list_head	i_devices;
+	union {
+		struct list_head	i_devices;
+		int			i_linklen;
+	};
 	union {
 		struct pipe_inode_info	*i_pipe;
 		struct cdev		*i_cdev;
@@ -737,6 +741,13 @@ struct inode {
 
 	void			*i_private; /* fs or device private pointer */
 } __randomize_layout;
+
+static inline void inode_set_cached_link(struct inode *inode, char *link, int linklen)
+{
+	inode->i_link = link;
+	inode->i_linklen = linklen;
+	inode->i_opflags |= IOP_CACHED_LINK;
+}
 
 /*
  * Get bit address from inode->i_state to use with wait_var_event()
@@ -3219,7 +3230,7 @@ extern const struct file_operations generic_ro_fops;
 
 #define special_file(m) (S_ISCHR(m)||S_ISBLK(m)||S_ISFIFO(m)||S_ISSOCK(m))
 
-extern int readlink_copy(char __user *, int, const char *);
+extern int readlink_copy(char __user *, int, const char *, int);
 extern int page_readlink(struct dentry *, char __user *, int);
 extern const char *page_get_link_raw(struct dentry *, struct inode *,
 				     struct delayed_call *);
