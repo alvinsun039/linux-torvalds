fe053234ffc574196ba75ea0df24d182c4faa929
Vincent Guittot <vincent.guittot@linaro.org>
2024-12-02 18:46:06 +0100

    sched/fair: Fix variable declaration position
    Mainline: 0429489e092851f066b08deed9ce0f3910515383
    From: v6.14-rc1
    Severity: Moderate
    
    task: 507947
    
    Move variable declaration at the beginning of the function
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20241202174606.4074512-12-vincent.guittot@linaro.org
    
    (backported from commit 0429489e092851f066b08deed9ce0f3910515383)
    
    Signed-off-by: Haofeng Li <lihaofeng@kylinos.cn>
    Change-Id: Iac7b93046d02bc35e084f68a433daeb59a832fd7
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/110862
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5480,6 +5480,7 @@ static bool
 dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
 	bool sleep = flags & DEQUEUE_SLEEP;
+	int action = UPDATE_TG;
 
 	update_curr(cfs_rq);
 	clear_buddies(cfs_rq, se);
@@ -5505,7 +5506,6 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 		}
 	}
 
-	int action = UPDATE_TG;
 	if (entity_is_task(se) && task_on_rq_migrating(task_of(se)))
 		action |= DO_DETACH;
 
@@ -5613,6 +5613,8 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags);
 static struct sched_entity *
 pick_next_entity(struct rq *rq, struct cfs_rq *cfs_rq)
 {
+	struct sched_entity *se;
+
 	/*
 	 * Enabling NEXT_BUDDY will affect latency but not fairness.
 	 */
@@ -5623,7 +5625,7 @@ pick_next_entity(struct rq *rq, struct cfs_rq *cfs_rq)
 		return cfs_rq->next;
 	}
 
-	struct sched_entity *se = pick_eevdf(cfs_rq);
+	se = pick_eevdf(cfs_rq);
 	if (se->sched_delayed) {
 		dequeue_entities(rq, se, DEQUEUE_SLEEP | DEQUEUE_DELAYED);
 		/*
