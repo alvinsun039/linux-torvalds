21482a455bcb944a5100ae8a502d710a3eb279fa
juxiahe <juxiahe@net-swift.com>
2024-12-06 16:19:49 +0800

    NETSWIFT: txgbevf: update version to 2.0.1
    Mainline: KYLIN-only
    Severity: Low
    
    Task: #552421
    
    K2CI-Arch: All
    Signed-off-by: luoqing <luoqing@kylinos.cn>
    Change-Id: Ib022fccc75ea03fbaf18f1450b543d38c2fd153e

--- drivers/net/ethernet/netswift/txgbevf/txgbe_main.c
+++ drivers/net/ethernet/netswift/txgbevf/txgbe_main.c
@@ -59,15 +59,15 @@
 #endif /* HAVE_XDP_SUPPORT */
 /*====================*/
 #if defined(TXGBE_SUPPORT_KYLIN)
-#define DRV_VERSION     __stringify(2.0.0-kylin)
+#define DRV_VERSION     __stringify(2.0.1-kylin)
 #elif defined(CONFIG_EULER_KERNEL)
-#define DRV_VERSION     __stringify(2.0.0-kylin)
+#define DRV_VERSION     __stringify(2.0.1-kylin)
 #elif defined(CONFIG_UOS_KERNEL)
-#define DRV_VERSION     __stringify(2.0.0-kylin)
+#define DRV_VERSION     __stringify(2.0.1-kylin)
 #elif defined(TXGBE_SUPPORT_DEEPIN_SW)
-#define DRV_VERSION     __stringify(2.0.0-kylin)
+#define DRV_VERSION     __stringify(2.0.1-kylin)
 #else
-#define DRV_VERSION     __stringify(2.0.0-kylin)
+#define DRV_VERSION     __stringify(2.0.1-kylin)
 #endif
 /*====================*/
 
