bc848da2f52955e1fad4e1390803dfa66b23746c
Mario Limonciello <mario.limonciello@amd.com>
2024-12-05 21:18:59 -0600

    ACPI: platform_profile: Add device pointer into platform profile handler
    Mainline: 6f5e63ddc333dae371be6f8a8f70a82043697a4c
    From: v6.14-rc1
    Severity: Low
    
    In order to let platform profile handlers manage platform profile
    for their driver the core code will need a pointer to the device.
    
    Add this to the structure and use it in the trivial driver cases.
    
    Reviewed-by: Armin Wolf <W_Armin@gmx.de>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Reviewed-by: Maximilian Luz <luzmaximilian@gmail.com>
    Reviewed-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://lore.kernel.org/r/20241206031918.1537-4-mario.limonciello@amd.com
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    
    (backported from commit 6f5e63ddc333dae371be6f8a8f70a82043697a4c)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I2acbcf21eaecc21c3e3f1456ad4def7ea462bc05
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/69309
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -1581,7 +1581,7 @@ static inline void omen_unregister_powersource_event_handler(void)
 	unregister_acpi_notifier(&platform_power_source_nb);
 }
 
-static int thermal_profile_setup(void)
+static int thermal_profile_setup(struct platform_device *device)
 {
 	int err, tp;
 
@@ -1641,6 +1641,7 @@ static int thermal_profile_setup(void)
 	}
 
 	platform_profile_handler.name = "hp-wmi";
+	platform_profile_handler.dev = &device->dev;
 	set_bit(PLATFORM_PROFILE_BALANCED, platform_profile_handler.choices);
 	set_bit(PLATFORM_PROFILE_PERFORMANCE, platform_profile_handler.choices);
 
@@ -1680,7 +1681,7 @@ static int __init hp_wmi_bios_setup(struct platform_device *device)
 	if (err < 0)
 		return err;
 
-	thermal_profile_setup();
+	thermal_profile_setup(device);
 
 	return 0;
 }
