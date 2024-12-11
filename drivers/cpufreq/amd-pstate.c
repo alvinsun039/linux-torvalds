4a85ef59b7fded576995d0b5f912d32d8d0d32cf
Mario Limonciello <mario.limonciello@amd.com>
2024-12-11 14:57:57 -0600

    cpufreq/amd-pstate: Show a warning when a CPU fails to setup
    Mainline: a9ba0fd452d82ca0da170eb6291aac01075a17d5
    From: v6.15-rc1
    Severity: Moderate
    
    task: 564669
    
    I came across a system that MSR_AMD_CPPC_CAP1 for some CPUs isn't
    populated.  This is an unexpected behavior that is most likely a
    BIOS bug. In the event it happens I'd like users to report bugs
    to properly root cause and get this fixed.
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    (backported from commit a9ba0fd452d82ca0da170eb6291aac01075a17d5)
    
    K2CI-Arch: Amd64
    Change-Id: I01b59e0e7703540df8416d5a446c7d7b37e940af
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/153994
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -988,6 +988,7 @@ static int amd_pstate_cpu_init(struct cpufreq_policy *policy)
 free_cpudata2:
 	freq_qos_remove_request(&cpudata->req[0]);
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
@@ -1474,6 +1475,7 @@ static int amd_pstate_epp_cpu_init(struct cpufreq_policy *policy)
 	return 0;
 
 free_cpudata1:
+	pr_warn("Failed to initialize CPU %d: %d\n", policy->cpu, ret);
 	kfree(cpudata);
 	return ret;
 }
