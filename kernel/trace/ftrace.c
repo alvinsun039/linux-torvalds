5eb42ea433fb0e797108922b6fd7547df8bed149
Steven Rostedt <rostedt@goodmis.org>
2024-12-23 13:46:21 -0500

    ftrace: Remove unneeded goto jumps
    Mainline: 77e53cb2fcf2c5e81f06f4a41783d8752cf9d038
    From: v6.14-rc1
    Severity: Low
    
    task: 507949
    
    There are some goto jumps to exit a program to just return a value. The
    code after the label doesn't free anything nor does it do any unlocks. It
    simply returns the variable that was set before the jump.
    
    Remove these unneeded goto jumps.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lore.kernel.org/20241223184941.544855549@goodmis.org
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    
    (backported from commit 77e53cb2fcf2c5e81f06f4a41783d8752cf9d038)
    
    Signed-off-by: pengyu <pengyu@kylinos.cn>
    Change-Id: I7a92fd2792469b034ed40b0110246e89d4ffb437
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/107916
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -1637,14 +1637,12 @@ unsigned long ftrace_location(unsigned long ip)
 	loc = ftrace_location_range(ip, ip);
 	if (!loc) {
 		if (!kallsyms_lookup_size_offset(ip, &size, &offset))
-			goto out;
+			return 0;
 
 		/* map sym+0 to __fentry__ */
 		if (!offset)
 			loc = ftrace_location_range(ip, ip + size - 1);
 	}
-
-out:
 	return loc;
 }
 
@@ -2036,7 +2034,7 @@ rollback:
 			continue;
 
 		if (rec == end)
-			goto err_out;
+			return -EBUSY;
 
 		in_old = !!ftrace_lookup_ip(old_hash, rec->ip);
 		in_new = !!ftrace_lookup_ip(new_hash, rec->ip);
@@ -2049,7 +2047,6 @@ rollback:
 			rec->flags |= FTRACE_FL_IPMODIFY;
 	} while_for_each_ftrace_rec();
 
-err_out:
 	return -EBUSY;
 }
 
@@ -5696,12 +5693,10 @@ ftrace_regex_write(struct file *file, const char __user *ubuf,
 					   parser->idx, enable);
 		trace_parser_clear(parser);
 		if (ret < 0)
-			goto out;
+			return ret;
 	}
 
-	ret = read;
- out:
-	return ret;
+	return read;
 }
 
 ssize_t
