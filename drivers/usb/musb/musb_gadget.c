b36eb8fdc92af5e51bee2d34732738abf139eea0
lsm <1355280398@qq.com>
2024-12-30 17:06:44 +0000

    rename drivers/usb/musb/musb_ep_select(musb->mregs, req->epnum); to drivers/usb/musb/musb_gadget.c. 修改
    Signed-off-by: lsm <1355280398@qq.com>

--- drivers/usb/musb/musb_gadget.c
+++ drivers/usb/musb/musb_gadget.c
@@ -1170,6 +1170,9 @@ struct free_record {
  */
 void musb_ep_restart(struct musb *musb, struct musb_request *req)
 {
+	u16 csr;
+	void __iomem *epio = req->ep->hw_ep->regs;
+
 	trace_musb_req_start(req);
 	musb_ep_select(musb->mregs, req->epnum);
 	if (req->tx) {
