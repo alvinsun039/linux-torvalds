b3fd82a4793a4f62fa1cf1bad58f1abd4428b823
Bui Quang Minh <minhquangbui99@gmail.com>
2025-01-03 22:04:11 +0700

    io_uring/rsrc: simplify the bvec iter count calculation
    Mainline: 2a51c327d4a4a2eb62d67f4ea13a17efd0f25c5c
    From: v6.14-rc1
    Severity: Moderate
    
    task: 526909
    
    As we don't use iov_iter_advance() but our own logic in io_import_fixed(),
    we can remove the logic that over-sets the iter's count to len + offset
    then adjusts it later to len. This helps to make the code cleaner.
    
    Signed-off-by: Bui Quang Minh <minhquangbui99@gmail.com>
    Link: https://lore.kernel.org/r/20250103150412.12549-1-minhquangbui99@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit 2a51c327d4a4a2eb62d67f4ea13a17efd0f25c5c)
    
    K2CI-Arch: All
    Change-Id: I97265c0060cffca4873c10735e867cefb97023ec
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151577
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- io_uring/rsrc.c
+++ io_uring/rsrc.c
@@ -1107,7 +1107,7 @@ int io_import_fixed(int ddir, struct iov_iter *iter,
 	 * and advance us to the beginning.
 	 */
 	offset = buf_addr - imu->ubuf;
-	iov_iter_bvec(iter, ddir, imu->bvec, imu->nr_bvecs, offset + len);
+	iov_iter_bvec(iter, ddir, imu->bvec, imu->nr_bvecs, len);
 
 	if (offset) {
 		/*
@@ -1129,7 +1129,6 @@ int io_import_fixed(int ddir, struct iov_iter *iter,
 		const struct bio_vec *bvec = imu->bvec;
 
 		if (offset < bvec->bv_len) {
-			iter->count -= offset;
 			iter->iov_offset = offset;
 		} else {
 			unsigned long seg_skip;
@@ -1140,7 +1139,6 @@ int io_import_fixed(int ddir, struct iov_iter *iter,
 
 			iter->bvec += seg_skip;
 			iter->nr_segs -= seg_skip;
-			iter->count -= bvec->bv_len + offset;
 			iter->iov_offset = offset & ((1UL << imu->folio_shift) - 1);
 		}
 	}
