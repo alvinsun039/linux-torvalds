39a3610528ab98a62be3f0b8bf236ccf2cadf4e1
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2025-01-10 13:09:11 +0100

    Revert "x86, crash: wrap crash dumping code into crash related ifdefs"
    Mainline: a4eeb2176d89fdf2785851521577b94b31690a60
    From: v6.9-rc1
    Severity: Moderate
    
    task: 421187
    
    This reverts commit e5b1574a8ca28c40cf53eda43f6c3b016ed41e27 which is
    commit a4eeb2176d89fdf2785851521577b94b31690a60 upstream.
    
    When this change is backported to the 6.6.y tree, it can cause build
    errors on some configurations when KEXEC is not enabled, so revert it
    for now.
    
    Reported-by: Ignat Korchagin <ignat@cloudflare.com>
    Link: https://lore.kernel.org/r/3DB3A6D3-0D3A-4682-B4FA-407B2D3263B2@cloudflare.com
    Reported-by: Lars Wendler <wendler.lars@web.de>
    Link: https://lore.kernel.org/r/20250110103328.0e3906a8@chagall.paradoxon.rec
    Reported-by: Chris Clayton <chris2553@googlemail.com>
    Link: https://lore.kernel.org/r/10c7be00-b1f8-4389-801b-fb2d0b22468d@googlemail.com
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Dexuan Cui <decui@microsoft.com>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Hari Bathini <hbathini@linux.ibm.com>
    Cc: Klara Modin <klarasmodin@gmail.com>
    Cc: Michael Kelley <mhklinux@outlook.com>
    Cc: Michael Kelley <mhklinux@outlook.com>
    Cc: Naman Jain <namjain@linux.microsoft.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Pingfan Liu <piliu@redhat.com>
    Cc: Sasha Levin <sashal@kernel.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Wei Liu <wei.liu@kernel.org>
    Cc: Yang Li <yang.lee@linux.alibaba.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (backported from commit b34e805539dabbebfa6030842f4a0ba14de8f813)
    
    K2CI-Arch: None
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Change-Id: I6c63c3e15ab80ae8c84e182e890e97ad855910a3
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/153369
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -478,7 +478,7 @@ static void __init arch_reserve_crashkernel(void)
 	bool high = false;
 	int ret;
 
-	if (!IS_ENABLED(CONFIG_CRASH_RESERVE))
+	if (!IS_ENABLED(CONFIG_KEXEC_CORE))
 		return;
 
 	ret = parse_crashkernel(cmdline, memblock_phys_mem_size(),
