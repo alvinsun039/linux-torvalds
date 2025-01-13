68c14857c74267c6f7a00c8c20dfe1b25539f303
Hao Ge <gehao@kylinos.cn>
2025-01-13 11:28:58 +0800

    selftests/mm/cow: modify the incorrect checking parameters
    Mainline: bf069012df19cf80b460a03c92bfe6320dc268b0
    From: v6.14-rc1
    Severity: Moderate
    
    task: 436744
    
    In run_with_memfd_hugetlb(), some error handle have passed incorrect
    parameters.  It should be "smem", but it was mistakenly written as "mem".
    
    Let's fix it.
    
    [gehao@kylinos.cn: fix other errant sites, per Anshuman]
      Link: https://lkml.kernel.org/r/20250113050908.93638-1-hao.ge@linux.dev
    Link: https://lkml.kernel.org/r/20250113032858.63670-1-hao.ge@linux.dev
    Fixes: f8664f3c4a08 ("selftests/vm: cow: basic COW tests for non-anonymous pages")
    Signed-off-by: Hao Ge <gehao@kylinos.cn>
    Cc: SeongJae Park <sj@kernel.org>
    Cc: Shuah Khan (Samsung OSG) <shuah@kernel.org>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit bf069012df19cf80b460a03c92bfe6320dc268b0)
    
    Signed-off-by: lihaofeng <lihaofeng@kylinos.cn>
    Change-Id: Ifd8a0c0f4485733b5e35bb2ea88459fcf915ac68
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/159400
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- tools/testing/selftests/mm/cow.c
+++ tools/testing/selftests/mm/cow.c
@@ -1446,7 +1446,7 @@ static void run_with_zeropage(non_anon_test_fn fn, const char *desc)
 	}
 
 	smem = mmap(NULL, pagesize, PROT_READ, MAP_PRIVATE | MAP_ANON, -1, 0);
-	if (mem == MAP_FAILED) {
+	if (smem == MAP_FAILED) {
 		ksft_test_result_fail("mmap() failed\n");
 		goto munmap;
 	}
@@ -1547,7 +1547,7 @@ static void run_with_memfd(non_anon_test_fn fn, const char *desc)
 		goto close;
 	}
 	smem = mmap(NULL, pagesize, PROT_READ, MAP_SHARED, fd, 0);
-	if (mem == MAP_FAILED) {
+	if (smem == MAP_FAILED) {
 		ksft_test_result_fail("mmap() failed\n");
 		goto munmap;
 	}
@@ -1598,7 +1598,7 @@ static void run_with_tmpfile(non_anon_test_fn fn, const char *desc)
 		goto close;
 	}
 	smem = mmap(NULL, pagesize, PROT_READ, MAP_SHARED, fd, 0);
-	if (mem == MAP_FAILED) {
+	if (smem == MAP_FAILED) {
 		ksft_test_result_fail("mmap() failed\n");
 		goto munmap;
 	}
@@ -1648,7 +1648,7 @@ static void run_with_memfd_hugetlb(non_anon_test_fn fn, const char *desc,
 		goto close;
 	}
 	smem = mmap(NULL, hugetlbsize, PROT_READ, MAP_SHARED, fd, 0);
-	if (mem == MAP_FAILED) {
+	if (smem == MAP_FAILED) {
 		ksft_test_result_fail("mmap() failed\n");
 		goto munmap;
 	}
@@ -1660,7 +1660,7 @@ static void run_with_memfd_hugetlb(non_anon_test_fn fn, const char *desc,
 	fn(mem, smem, hugetlbsize);
 munmap:
 	munmap(mem, hugetlbsize);
-	if (mem != MAP_FAILED)
+	if (smem != MAP_FAILED)
 		munmap(smem, hugetlbsize);
 close:
 	close(fd);
