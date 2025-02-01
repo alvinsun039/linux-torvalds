10e6c55c75aa0da14e5958cf06131e973e4a292d
Suren Baghdasaryan <surenb@google.com>
2025-02-01 15:18:00 -0800

    mm: avoid extra mem_alloc_profiling_enabled() checks
    Mainline: 51ff4d7486f0c0b4110a6da4af805b179dd7b11e
    From: v6.15-rc1
    Severity: Important
    
    Refactor code to avoid extra mem_alloc_profiling_enabled() checks inside
    pgalloc_tag_get() function which is often called after that check was
    already done.
    
    Link: https://lkml.kernel.org/r/20250201231803.2661189-1-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: David Wang <00107082@163.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Kent Overstreet <kent.overstreet@linux.dev>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Sourav Panda <souravpanda@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Yu Zhao <yuzhao@google.com>
    Cc: Zhenhua Huang <quic_zhenhuah@quicinc.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 51ff4d7486f0c0b4110a6da4af805b179dd7b11e)
    
    Cc: kernel-stable-team #2503+
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I574384ea41abb08994cb0a221f3da65121dbe028
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/85905
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4252,7 +4252,7 @@ static inline void pgalloc_tag_split(struct folio *folio, int old_order, int new
 	if (!mem_alloc_profiling_enabled())
 		return;
 
-	tag = pgalloc_tag_get(&folio->page);
+	tag = __pgalloc_tag_get(&folio->page);
 	if (!tag)
 		return;
 
@@ -4275,10 +4275,10 @@ static inline void pgalloc_tag_swap(struct folio *new, struct folio *old)
 	if (!mem_alloc_profiling_enabled())
 		return;
 
-	tag_old = pgalloc_tag_get(&old->page);
+	tag_old = __pgalloc_tag_get(&old->page);
 	if (!tag_old)
 		return;
-	tag_new = pgalloc_tag_get(&new->page);
+	tag_new = __pgalloc_tag_get(&new->page);
 	if (!tag_new)
 		return;
 
