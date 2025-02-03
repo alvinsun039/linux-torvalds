76c57a5a8b46c49cdd84dede27d1cbef5f66910a
Amir Goldstein <amir73il@gmail.com>
2025-02-03 23:32:04 +0100

    fsnotify: disable notification by default for all pseudo files
    Mainline: 2a42754b3104d78a2bc7a2ad8844427411c76ca6
    From: v6.14-rc2
    Severity: Important
    
    task: 530755
    
    Most pseudo files are not applicable for fsnotify events at all,
    let alone to the new pre-content events.
    
    Disable notifications to all files allocated with alloc_file_pseudo()
    and enable legacy inotify events for the specific cases of pipe and
    socket, which have known users of inotify events.
    
    Pre-content events are also kept disabled for sockets and pipes.
    
    Fixes: 20bf82a898b6 ("mm: don't allow huge faults for files with pre content watches")
    Reported-by: Alex Williamson <alex.williamson@redhat.com>
    Closes: https://lore.kernel.org/linux-fsdevel/20250131121703.1e4d00a7.alex.williamson@redhat.com/
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/linux-fsdevel/CAHk-=wi2pThSVY=zhO=ZKxViBj5QCRX-=AS2+rVknQgJnHXDFg@mail.gmail.com/
    Tested-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Link: https://lore.kernel.org/r/20250203223205.861346-3-amir73il@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit 2a42754b3104d78a2bc7a2ad8844427411c76ca6)
    
    Change-Id: Ieb67b1a9680be4915e792ffd1baff0372598e993
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/120856
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/file_table.c
+++ fs/file_table.c
@@ -376,7 +376,13 @@ struct file *alloc_file_pseudo(struct inode *inode, struct vfsmount *mnt,
 	if (IS_ERR(file)) {
 		ihold(inode);
 		path_put(&path);
+		return file;
 	}
+	/*
+	 * Disable all fsnotify events for pseudo files by default.
+	 * They may be enabled by caller with file_set_fsnotify_mode().
+	 */
+	file_set_fsnotify_mode(file, FMODE_NONOTIFY);
 	return file;
 }
 EXPORT_SYMBOL(alloc_file_pseudo);
@@ -401,6 +407,11 @@ struct file *alloc_file_pseudo_noaccount(struct inode *inode,
 		return file;
 	}
 	file_init_path(file, &path, fops);
+	/*
+	 * Disable all fsnotify events for pseudo files by default.
+	 * They may be enabled by caller with file_set_fsnotify_mode().
+	 */
+	file_set_fsnotify_mode(file, FMODE_NONOTIFY);
 	return file;
 }
 EXPORT_SYMBOL_GPL(alloc_file_pseudo_noaccount);
