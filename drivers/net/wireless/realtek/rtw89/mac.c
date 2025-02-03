7b0c050c28475cf5ad3fffdc6ee85d51683afd31
Ping-Ke Shih <pkshih@realtek.com>
2025-02-03 15:29:08 +0800

    wifi: rtw89: call power_on ahead before selecting firmware
    Driver selects firmware by hardware version, which normally can be read
    from registers before selecting firmware. However, certain chips such as
    RTL8851B, it needs to read hardware version from efuse while doing
    power_on, but do power_on after selecting firmware in current flow.
    
    To resolve this flow problem, move power_on out from
    rtw89_mac_partial_init(), and call rtw89_mac_pwr_on() separately at
    proper places to have expected flow.
    
    Change-Id: Ifdbde59fcf27bdc75d864603a7f5f8fb0f000ad4
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20250203072911.47313-2-pkshih@realtek.com

--- drivers/net/wireless/realtek/rtw89/mac.c
+++ drivers/net/wireless/realtek/rtw89/mac.c
@@ -1514,6 +1514,21 @@ static int rtw89_mac_power_switch(struct rtw89_dev *rtwdev, bool on)
 #undef PWR_ACT
 }
 
+int rtw89_mac_pwr_on(struct rtw89_dev *rtwdev)
+{
+	int ret;
+
+	ret = rtw89_mac_power_switch(rtwdev, true);
+	if (ret) {
+		rtw89_mac_power_switch(rtwdev, false);
+		ret = rtw89_mac_power_switch(rtwdev, true);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 void rtw89_mac_pwr_off(struct rtw89_dev *rtwdev)
 {
 	rtw89_mac_power_switch(rtwdev, false);
@@ -4015,14 +4030,6 @@ int rtw89_mac_partial_init(struct rtw89_dev *rtwdev, bool include_bb)
 {
 	int ret;
 
-	ret = rtw89_mac_power_switch(rtwdev, true);
-	if (ret) {
-		rtw89_mac_power_switch(rtwdev, false);
-		ret = rtw89_mac_power_switch(rtwdev, true);
-		if (ret)
-			return ret;
-	}
-
 	rtw89_mac_ctrl_hci_dma_trx(rtwdev, true);
 
 	if (include_bb) {
@@ -4055,6 +4062,10 @@ int rtw89_mac_init(struct rtw89_dev *rtwdev)
 	bool include_bb = !!chip->bbmcu_nr;
 	int ret;
 
+	ret = rtw89_mac_pwr_on(rtwdev);
+	if (ret)
+		return ret;
+
 	ret = rtw89_mac_partial_init(rtwdev, include_bb);
 	if (ret)
 		goto fail;
@@ -4086,7 +4097,7 @@ int rtw89_mac_init(struct rtw89_dev *rtwdev)
 
 	return ret;
 fail:
-	rtw89_mac_power_switch(rtwdev, false);
+	rtw89_mac_pwr_off(rtwdev);
 
 	return ret;
 }
