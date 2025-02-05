f65e8e1d0e292a1687d4f9291049dc6595aff126
Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
2025-02-05 11:25:17 +0000

    cpufreq/amd-pstate: Convert all perf values to u8
    Mainline: 555bbe67a622b297405e246d1868dbda3284bde8
    From: v6.15-rc1
    Severity: Moderate
    
    task: 564669
    
    All perf values are always within 0-255 range, hence convert their
    datatype to u8 everywhere.
    
    Signed-off-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Link: https://lore.kernel.org/r/20250205112523.201101-7-dhananjay.ugwekar@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    
    (backported from commit 555bbe67a622b297405e246d1868dbda3284bde8)
    
    K2CI-Arch: Amd64
    Change-Id: Ib5bf36cfa4f06fa082a0c06bc910d2cdc1a95939
    Signed-off-by: lifangxu <lifangxu@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/153131
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/cpufreq/amd-pstate.h
+++ drivers/cpufreq/amd-pstate.h
@@ -69,13 +69,13 @@ struct amd_cpudata {
 	struct	freq_qos_request req[2];
 	u64	cppc_req_cached;
 
-	u32	highest_perf;
-	u32	nominal_perf;
-	u32	lowest_nonlinear_perf;
-	u32	lowest_perf;
-	u32     prefcore_ranking;
-	u32     min_limit_perf;
-	u32     max_limit_perf;
+	u8	highest_perf;
+	u8	nominal_perf;
+	u8	lowest_nonlinear_perf;
+	u8	lowest_perf;
+	u8	prefcore_ranking;
+	u8	min_limit_perf;
+	u8	max_limit_perf;
 	u32     min_limit_freq;
 	u32     max_limit_freq;
 
@@ -92,10 +92,10 @@ struct amd_cpudata {
 	bool	hw_prefcore;
 
 	/* EPP feature related attributes*/
-	s16	epp_cached;
+	u8	epp_cached;
 	u32	policy;
 	bool	suspended;
-	s16	epp_default;
+	u8	epp_default;
 };
 
 /*
