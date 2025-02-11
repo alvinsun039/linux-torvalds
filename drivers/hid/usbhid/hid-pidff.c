7455b45f8e189b5deb0f229bcd9a58ce3f624ab5
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2025-02-11 15:35:08 +0100

    HID: pidff: Make sure to fetch pool before checking SIMULTANEOUS_MAX
    Mainline: 1f650dcec32d22deb1d6db12300a2b98483099a9
    From: v6.15-rc1
    Severity: Important
    CVE: CVE-2025-37942
    
    task: 466581
    
    As noted by Anssi some 20 years ago, pool report is sometimes messed up.
    This worked fine on many devices but casued oops on VRS DirectForce PRO.
    
    Here, we're making sure pool report is refetched before trying to access
    any of it's fields. While loop was replaced with a for loop + exit
    conditions were moved aroud to decrease the possibility of creating an
    infinite loop scenario.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    
    (backported from commit 1f650dcec32d22deb1d6db12300a2b98483099a9)
    
    Cc: kernel-stable-team #SP0+
    Signed-off-by: Pei Xiao <xiaopei01@kylinos.cn>
    Change-Id: I7071468825a95cab5158912072ecca3044bb47fa
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/69244
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/hid/usbhid/hid-pidff.c
+++ drivers/hid/usbhid/hid-pidff.c
@@ -1231,20 +1231,17 @@ static void pidff_reset(struct pidff_device *pidff)
 	hid_hw_request(hid, pidff->reports[PID_POOL], HID_REQ_GET_REPORT);
 	hid_hw_wait(hid);
 
-	if (pidff->pool[PID_SIMULTANEOUS_MAX].value) {
-		while (pidff->pool[PID_SIMULTANEOUS_MAX].value[0] < 2) {
-			if (i++ > 20) {
-				hid_warn(pidff->hid,
-					 "device reports %d simultaneous effects\n",
-					 pidff->pool[PID_SIMULTANEOUS_MAX].value[0]);
-				break;
-			}
-			hid_dbg(pidff->hid, "pid_pool requested again\n");
-			hid_hw_request(hid, pidff->reports[PID_POOL],
-					  HID_REQ_GET_REPORT);
-			hid_hw_wait(hid);
-		}
+	/* Repeat if PID_SIMULTANEOUS_MAX < 2 to make sure it's correct */
+	for(i = 0; i < 20; i++) {
+		hid_hw_request(hid, pidff->reports[PID_POOL], HID_REQ_GET_REPORT);
+		hid_hw_wait(hid);
+		if (!pidff->pool[PID_SIMULTANEOUS_MAX].value)
+			return;
+		if (pidff->pool[PID_SIMULTANEOUS_MAX].value[0] >= 2)
+			return;
 	}
+	hid_warn(hid, "device reports %d simultaneous effects\n",
+		 pidff->pool[PID_SIMULTANEOUS_MAX].value[0]);
 }
 
 /*
