257a14bbbcb640b98a8a4c3bfde820656c4c844e
Ge Yang <yangge1116@126.com>
2025-02-11 10:37:03 +0800

    KVM: SEV: Use long-term pin when registering encrypted memory regions
    Mainline: 7e066cb9b71a22c3e5ef233de63ff14525baf6f0
    From: v6.15-rc1
    Severity: Moderate
    
    task: 542911
    
    When registering an encrypted memory region for SEV-MEM/SEV-ES guests,
    pin the pages with FOLL_TERM so that the pages are migrated out of
    MIGRATE_CMA/ZONE_MOVABLE.  Failure to do so violates the CMA/MOVABLE
    mechanisms and can result in fragmentation due to unmovable pages, e.g.
    can make CMA allocations fail.
    
    Signed-off-by: Ge Yang <yangge1116@126.com>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Link: https://lore.kernel.org/r/1739241423-14326-1-git-send-email-yangge1116@126.com
    [sean: massage changelog, make @flags an unsigned int]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    Conflicts:
    	arch/x86/kvm/svm/csv.c
    	arch/x86/kvm/svm/csv.h
    	modify for support HYGON.
    
    Change-Id: I1df0945859d306ca1e6350a691adc6d1506c26e4
    Signed-off-by: Zeng Chi <zengchi@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/152466
    Reviewed-by: 张浩 <zhanghao1@kylinos.cn>
    Reviewed-by: 王宇锋 <wangyufeng@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- arch/x86/kvm/svm/csv.c
+++ arch/x86/kvm/svm/csv.c
@@ -78,7 +78,7 @@ int csv_vm_attestation(struct kvm *kvm, unsigned long gpa, unsigned long len)
 	}
 
 	guest_uaddr = gfn_to_hva(kvm, gpa_to_gfn(gpa));
-	pages = hygon_kvm_hooks.sev_pin_memory(kvm, guest_uaddr, len, &n, 1);
+	pages = hygon_kvm_hooks.sev_pin_memory(kvm, guest_uaddr, len, &n, FOLL_WRITE);
 	if (IS_ERR(pages))
 		return PTR_ERR(pages);
 
@@ -403,7 +403,7 @@ csv_receive_update_data_to_ringbuf(struct kvm *kvm,
 
 	/* Pin guest memory */
 	guest_page = hygon_kvm_hooks.sev_pin_memory(kvm, params.guest_uaddr & PAGE_MASK,
-						    PAGE_SIZE, &n, 1);
+						    PAGE_SIZE, &n, FOLL_WRITE);
 	if (IS_ERR(guest_page)) {
 		ret = PTR_ERR(guest_page);
 		goto e_free;
