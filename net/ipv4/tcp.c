e45758b45491f1cace2e3d41097230f5355f1362
Jason Xing <kerneljasonxing@gmail.com>
2025-02-20 15:29:37 +0800

    bpf: Add BPF_SOCK_OPS_TSTAMP_ACK_CB callback
    Mainline: b3b81e6b009dd8f85cd3b9c65eb492249c2649a8
    From: v6.15-rc1
    Severity: Low
    
    task: 501509
    
    Support the ACK case for bpf timestamping.
    
    Add a new sock_ops callback, BPF_SOCK_OPS_TSTAMP_ACK_CB. This
    callback will occur at the same timestamping point as the user
    space's SCM_TSTAMP_ACK. The BPF program can use it to get the
    same SCM_TSTAMP_ACK timestamp without modifying the user-space
    application.
    
    This patch extends txstamp_ack to two bits: 1 stands for
    SO_TIMESTAMPING mode, 2 bpf extension.
    
    Signed-off-by: Jason Xing <kerneljasonxing@gmail.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://patch.msgid.link/20250220072940.99994-10-kerneljasonxing@gmail.com
    
    Change-Id: If712249ca05ccfa6efce010722a31a386d7849cb
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/96293
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -476,7 +476,7 @@ static void tcp_tx_timestamp(struct sock *sk, u16 tsflags)
 
 		sock_tx_timestamp(sk, tsflags, &shinfo->tx_flags);
 		if (tsflags & SOF_TIMESTAMPING_TX_ACK)
-			tcb->txstamp_ack = 1;
+			tcb->txstamp_ack |= TSTAMP_ACK_SK;
 		if (tsflags & SOF_TIMESTAMPING_TX_RECORD_MASK)
 			shinfo->tskey = TCP_SKB_CB(skb)->seq + skb->len - 1;
 	}
