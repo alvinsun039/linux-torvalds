2ab274c3fb44e9fa401526472a6ba84b280418be
jianghaoran <jianghaoran@kylinos.cn>
2025-02-20 10:05:30 +0800

    Revert "HYGON: KVM: force pat to write-back"
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 430128
    
    This reverts commit fc08eb50bec1ccabc07ed0e57048271dc09486ca.
    
    Force pat conflict with
    set_guest_pat_wb parameter for non-passthrough application scenarios
    so revert it.
    
    Signed-off-by: jianghaoran <jianghaoran@kylinos.cn>
    Change-Id: I6bc9b6160be43690312c137440739adf5e6d131f
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/157877
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Reviewed-by: 张浩 <zhanghao1@kylinos.cn>
    Reviewed-by: 李唯实 <liweishi@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -3088,19 +3088,7 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 		if (ret)
 			break;
 
-		if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
-			/* Unlike Intel, AMD takes the guest's CR0.CD into count.
-			*
-			* AMD does not have IPAT. To emulate it for the case of guests
-			* with no assigned devices,just set everything to WB. If guests
-			* have assigned devices,however,we cannot force WB for RAM
-			* pages only,so use the guest PAT directly.
-			*/
-			svm->vmcb01.ptr->save.g_pat = 0x0606060606060606;
-		} else {
-			svm->vmcb01.ptr->save.g_pat = data;
-		}
-
+		svm->vmcb01.ptr->save.g_pat = data;
 		if (is_guest_mode(vcpu))
 			nested_vmcb02_compute_g_pat(svm);
 		if (npt_enabled && set_guest_pat_wb) {
