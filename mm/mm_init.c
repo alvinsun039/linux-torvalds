c525017a0ff98c8b740ea20ed26fc8d5cb410579
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-02-25 10:30:17 +0200

    mm/mm_init: rename init_reserved_page to init_deferred_page
    Mainline: b4f65dbdf87812056c224fd8d2c66318b2140ab5
    From: v6.15-rc1
    Severity: Low
    
    When CONFIG_DEFERRED_STRUCT_PAGE_INIT is enabled, init_reserved_page()
    function performs initialization of a struct page that would have been
    deferred normally.
    
    Rename it to init_deferred_page() to better reflect what the function does.
    
    Link: https://lkml.kernel.org/r/20250225083017.567649-3-rppt@kernel.org
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Wei Yang <richard.weiyang@gmail.com>
    Cc: Frank van der Linden <fvdl@google.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Changyuan Lyu <changyuanl@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit b4f65dbdf87812056c224fd8d2c66318b2140ab5)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ida5ac333e9ef299378c98a993ba696f971681492
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/74072
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -704,7 +704,7 @@ defer_init(int nid, unsigned long pfn, unsigned long end_pfn)
 	return false;
 }
 
-static void __meminit init_reserved_page(unsigned long pfn, int nid)
+static void __meminit init_deferred_page(unsigned long pfn, int nid)
 {
 	pg_data_t *pgdat;
 	int zid;
@@ -735,7 +735,7 @@ static inline bool defer_init(int nid, unsigned long pfn, unsigned long end_pfn)
 	return false;
 }
 
-static inline void init_reserved_page(unsigned long pfn, int nid)
+static inline void init_deferred_page(unsigned long pfn, int nid)
 {
 }
 #endif /* CONFIG_DEFERRED_STRUCT_PAGE_INIT */
@@ -756,7 +756,7 @@ void __meminit reserve_bootmem_region(phys_addr_t start,
 		if (pfn_valid(start_pfn)) {
 			struct page *page = pfn_to_page(start_pfn);
 
-			init_reserved_page(start_pfn, nid);
+			init_deferred_page(start_pfn, nid);
 
 			/* Avoid false-positive PageTail() */
 			INIT_LIST_HEAD(&page->lru);
