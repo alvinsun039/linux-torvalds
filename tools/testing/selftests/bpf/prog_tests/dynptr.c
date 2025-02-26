a584b738ae3c10e7bd45489168dca0e116cc653f
Mykyta Yatsenko <yatsenko@meta.com>
2025-02-26 18:32:01 +0000

    selftests/bpf: Add tests for bpf_dynptr_copy
    Mainline: 064e9aacfd70a7805eeb0fc9ec6e97247ea8bc4c
    From: v6.15-rc1
    Severity: Low
    
    task: 501509
    
    Add XDP setup type for dynptr tests, enabling testing for
    non-contiguous buffer.
    Add 2 tests:
     - test_dynptr_copy - verify correctness for the fast (contiguous
     buffer) code path.
     - test_dynptr_copy_xdp - verifies code paths that handle
     non-contiguous buffer.
    
    Signed-off-by: Mykyta Yatsenko <yatsenko@meta.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20250226183201.332713-4-mykyta.yatsenko5@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Conflicts: tools/testing/selftests/bpf/prog_tests/dynptr.c
           tools/testing/selftests/bpf/progs/dynptr_success.c
    Change-Id: Ief5942da1f8f68e30247091524a17324a33d9f5a
    Signed-off-by: Feng Yang <yangfeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/96300
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- tools/testing/selftests/bpf/prog_tests/dynptr.c
+++ tools/testing/selftests/bpf/prog_tests/dynptr.c
@@ -9,6 +9,7 @@
 enum test_setup_type {
 	SETUP_SYSCALL_SLEEP,
 	SETUP_SKB_PROG,
+	SETUP_XDP_PROG,
 };
 
 static struct {
@@ -17,6 +18,8 @@ static struct {
 } success_tests[] = {
 	{"test_read_write", SETUP_SYSCALL_SLEEP},
 	{"test_dynptr_data", SETUP_SYSCALL_SLEEP},
+	{"test_dynptr_copy", SETUP_SYSCALL_SLEEP},
+	{"test_dynptr_copy_xdp", SETUP_XDP_PROG},
 	{"test_ringbuf", SETUP_SYSCALL_SLEEP},
 	{"test_skb_readonly", SETUP_SKB_PROG},
 	{"test_dynptr_skb_data", SETUP_SKB_PROG},
@@ -81,6 +84,24 @@ static void verify_success(const char *prog_name, enum test_setup_type setup_typ
 			goto cleanup;
 
 		err = bpf_prog_test_run_opts(prog_fd, &topts);
+
+		if (!ASSERT_OK(err, "test_run"))
+			goto cleanup;
+
+		break;
+	}
+	case SETUP_XDP_PROG:
+	{
+		char data[5000];
+		int err, prog_fd;
+		LIBBPF_OPTS(bpf_test_run_opts, opts,
+			    .data_in = &data,
+			    .data_size_in = sizeof(data),
+			    .repeat = 1,
+		);
+
+		prog_fd = bpf_program__fd(prog);
+		err = bpf_prog_test_run_opts(prog_fd, &opts);
 
 		if (!ASSERT_OK(err, "test_run"))
 			goto cleanup;
