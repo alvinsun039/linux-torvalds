33b4abb806c4ea6ee55336f5a3f782584d8d3cc8
Eric Dumazet <edumazet@google.com>
2025-03-01 20:14:23 +0000

    tcp: remove READ_ONCE(req->ts_recent)
    Mainline: 5282de17621f80a1e2c6c26229975182ecade5b9
    From: v6.15-rc1
    Severity: Moderate
    
    task: 554291
    
    After commit 8d52da23b6c6 ("tcp: Defer ts_recent changes
    until req is owned"), req->ts_recent is not changed anymore.
    
    It is set once in tcp_openreq_init(), bpf_sk_assign_tcp_reqsk()
    or cookie_tcp_reqsk_alloc() before the req can be seen by other
    cpus/threads.
    
    This completes the revert of eba20811f326 ("tcp: annotate
    data-races around tcp_rsk(req)->ts_recent").
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Wang Hai <wanghai38@huawei.com>
    Reviewed-by: Jason Xing <kerneljasonxing@gmail.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://patch.msgid.link/20250301201424.2046477-6-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Change-Id: I59292f5ed979de8f4a1a9832d9e55cb647cddb0d
    Signed-off-by: Yun Lu <luyun@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/159366
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1272,7 +1272,7 @@ static void tcp_v6_reqsk_send_ack(const struct sock *sk, struct sk_buff *skb,
 			tcp_rsk(req)->rcv_nxt,
 			req->rsk_rcv_wnd >> inet_rsk(req)->rcv_wscale,
 			tcp_rsk_tsval(tcp_rsk(req)),
-			READ_ONCE(req->ts_recent), sk->sk_bound_dev_if,
+			req->ts_recent, sk->sk_bound_dev_if,
 			&key, ipv6_get_dsfield(ipv6_hdr(skb)), 0,
 			READ_ONCE(sk->sk_priority),
 			READ_ONCE(tcp_rsk(req)->txhash));
