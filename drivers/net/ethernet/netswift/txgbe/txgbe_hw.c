2d71473d8646a328432fbe98642ab2783d6a55a5
zhongxiangji <zhongxiangji@net-swift.com>
2025-03-02 11:24:30 +0800

    NETSWIFT: txgbe: FIX: fix bug that ping failed sometimes
    Mainline: KYLIN-only
    Severity: Low
    
    Task: #552421
    
    After the mac reset, does not wait for the mac reset is complete.
    As a result, the value of TXGBE_MAC_MISC_CTL is 0. wr32m interface
    is used, and mac_dp_sw_rst_n bit is 0.
    
    At this time, if the firmware reads the mac layer register,
    the mac card will die, and the packet cannot be sent.
    
    so add msleep after mac reset.
    
    Signed-off-by: zhongxiangji <zhongxiangji@net-swift.com>
    
    K2CI-Arch: All
    Signed-off-by: luoqing <luoqing@kylinos.cn>
    Change-Id: Ic58d5b4ea766813c1928efaf1c3be8679819be6a

--- drivers/net/ethernet/netswift/txgbe/txgbe_hw.c
+++ drivers/net/ethernet/netswift/txgbe/txgbe_hw.c
@@ -6016,6 +6016,8 @@ int txgbe_reconfig_mac(struct txgbe_hw *hw)
 	else if (hw->bus.lan_id == 1)
 		wr32(hw, TXGBE_MIS_RST, TXGBE_MIS_RST_LAN1_MAC_RST);
 
+	/* wait for mac rst complete */
+	msleep(100);
 	wr32m(hw, TXGBE_MAC_MISC_CTL, TXGBE_MAC_MISC_LINK_STS_MOD,
 				TXGBE_LINK_BOTH_PCS_MAC);
 
