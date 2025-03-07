372645fafbcb9f0b28c56a577e788de644d84f16
K Prateek Nayak <kprateek.nayak@amd.com>
2025-03-07 05:29:18 +0000

    fs/pipe: Use pipe_buf() helper to retrieve pipe buffer
    Mainline: ba0822021c3c5aa8029c16078c7091d35b4979bc
    From: v6.15-rc1
    Severity: Low
    
    Use pipe_buf() helper to retrieve the pipe buffer throughout the file
    replacing the open-coded the logic.
    
    Suggested-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lore.kernel.org/r/20250307052919.34542-4-kprateek.nayak@amd.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit ba0822021c3c5aa8029c16078c7091d35b4979bc)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I95088f73927dab98e755e4e1c3aedcbda8dff147
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/54310
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- fs/pipe.c
+++ fs/pipe.c
@@ -274,7 +274,6 @@ anon_pipe_read(struct kiocb *iocb, struct iov_iter *to)
 		/* Read ->head with a barrier vs post_one_notification() */
 		unsigned int head = smp_load_acquire(&pipe->head);
 		unsigned int tail = pipe->tail;
-		unsigned int mask = pipe->ring_size - 1;
 
 #ifdef CONFIG_WATCH_QUEUE
 		if (pipe->note_loss) {
@@ -301,7 +300,7 @@ anon_pipe_read(struct kiocb *iocb, struct iov_iter *to)
 #endif
 
 		if (!pipe_empty(head, tail)) {
-			struct pipe_buffer *buf = &pipe->bufs[tail & mask];
+			struct pipe_buffer *buf = pipe_buf(pipe, tail);
 			size_t chars = buf->len;
 			size_t written;
 			int error;
@@ -457,8 +456,7 @@ anon_pipe_write(struct kiocb *iocb, struct iov_iter *from)
 	was_empty = pipe_empty(head, pipe->tail);
 	chars = total_len & (PAGE_SIZE-1);
 	if (chars && !was_empty) {
-		unsigned int mask = pipe->ring_size - 1;
-		struct pipe_buffer *buf = &pipe->bufs[(head - 1) & mask];
+		struct pipe_buffer *buf = pipe_buf(pipe, head - 1);
 		int offset = buf->offset + buf->len;
 
 		if ((buf->flags & PIPE_BUF_FLAG_CAN_MERGE) &&
@@ -489,7 +487,6 @@ anon_pipe_write(struct kiocb *iocb, struct iov_iter *from)
 
 		head = pipe->head;
 		if (!pipe_full(head, pipe->tail, pipe->max_usage)) {
-			unsigned int mask = pipe->ring_size - 1;
 			struct pipe_buffer *buf;
 			struct page *page = pipe->tmp_page;
 			int copied;
@@ -513,7 +510,7 @@ anon_pipe_write(struct kiocb *iocb, struct iov_iter *from)
 			pipe->head = head + 1;
 			pipe->tmp_page = NULL;
 			/* Insert it into the buffer array */
-			buf = &pipe->bufs[head & mask];
+			buf = pipe_buf(pipe, head);
 			buf->page = page;
 			buf->ops = &anon_pipe_buf_ops;
 			buf->offset = 0;
