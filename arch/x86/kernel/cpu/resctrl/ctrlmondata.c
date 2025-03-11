e8a3c50c6e86305ea1fd9ac3df450a6a60311601
James Morse <james.morse@arm.com>
2025-03-11 18:36:52 +0000

    x86/resctrl: Add max_bw to struct resctrl_membw
    Mainline: 634ebb98b929443ea1a5502c93f26d01aedbd5c8
    From: v6.15-rc1
    Severity: Moderate
    
    task: 527377
    
    __rdt_get_mem_config_amd() and __get_mem_config_intel() both use the
    default_ctrl property as a maximum value. This is because the MBA schema works
    differently between these platforms. Doing this complicates determining
    whether the default_ctrl property belongs to the arch code, or can be derived
    from the schema format.
    
    Deriving the maximum or default value from the schema format would avoid the
    architecture code having to tell resctrl such obvious things as the maximum
    percentage is 100, and the maximum bitmap is all ones.
    
    Maximum bandwidth is always going to vary per platform. Add max_bw as
    a special case. This is currently used for the maximum MBA percentage on Intel
    platforms, but can be removed from the architecture code if 'percentage'
    becomes a schema format resctrl supports directly.
    
    This value isn't needed for other schema formats.
    
    This will allow the default_ctrl to be generated from the schema properties
    when it is needed.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
    Reviewed-by: Babu Moger <babu.moger@amd.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Peter Newman <peternewman@google.com>
    Tested-by: Amit Singh Tomar <amitsinght@marvell.com> # arm64
    Tested-by: Shanker Donthineni <sdonthineni@nvidia.com> # arm64
    Tested-by: Babu Moger <babu.moger@amd.com>
    Link: https://lore.kernel.org/r/20250311183715.16445-8-james.morse@arm.com
    
    (backported from commit 634ebb98b929443ea1a5502c93f26d01aedbd5c8)
    
    K2CI-Arch: All
    Signed-off-by: tanze <tanze@kylinos.cn>
    Change-Id: Iec757d9c3ec1443566ec5600f27e1ca4d99338ee
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/161595
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- arch/x86/kernel/cpu/resctrl/ctrlmondata.c
+++ arch/x86/kernel/cpu/resctrl/ctrlmondata.c
@@ -57,10 +57,9 @@ static bool bw_validate(char *buf, unsigned long *data, struct rdt_resource *r)
 		return false;
 	}
 
-	if ((bw < r->membw.min_bw || bw > r->default_ctrl) &&
-	    !is_mba_sc(r)) {
-		rdt_last_cmd_printf("MB value %ld out of range [%d,%d]\n", bw,
-				    r->membw.min_bw, r->default_ctrl);
+	if (bw < r->membw.min_bw || bw > r->membw.max_bw) {
+		rdt_last_cmd_printf("MB value %ld out of range [%d,%d]\n",
+				bw, r->membw.min_bw, r->membw.max_bw);
 		return false;
 	}
 
