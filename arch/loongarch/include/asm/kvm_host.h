27faea6c85fff4b87ff1bcc6111dbf76e93656e3
cuitao <cuitao@kylinos.cn>
2025-03-21 16:21:56 +0800

    KYLIN: Revert "LOONGSON: LoongArch: KVM: Add PMU support"
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 399013
    
    This solution originated from the Euler Community and has
    some shortcomings. It was completed earlier in the upstream
    community. Now that the upstream community has developed a
    better solution, we will switch to the upstream solution.
    
    This reverts commit 5db9a4f9132ecf986850aff34d0fce1b974575de.
    
    Signed-off-by: cuitao <cuitao@kylinos.cn>
    Change-Id: I0a18f8301a84099b1e4cb5f5e9f83c17d99f54a1
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/163685
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Reviewed-by: 张国鹏 <zhangguopeng@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- arch/loongarch/include/asm/kvm_host.h
+++ arch/loongarch/include/asm/kvm_host.h
@@ -33,7 +33,6 @@
 #define KVM_HALT_POLL_NS_DEFAULT	500000
 #define KVM_REQ_TLB_FLUSH_GPA		KVM_ARCH_REQ(0)
 #define KVM_REQ_STEAL_UPDATE		KVM_ARCH_REQ(1)
-#define KVM_REQ_PMU                    KVM_ARCH_REQ(2)
 
 #define KVM_LOONGARCH_IRQ_NUM_MASK      0xffff
 
@@ -72,13 +71,9 @@ struct kvm_arch_memory_slot {
 	unsigned long flags;
 };
 
-#define HOST_MAX_PMNUM			16
 struct kvm_context {
 	unsigned long vpid_cache;
 	struct kvm_vcpu *last_vcpu;
-	/* Host PMU CSR */
-	u64 perf_ctrl[HOST_MAX_PMNUM];
-	u64 perf_cntr[HOST_MAX_PMNUM];
 };
 
 struct kvm_world_switch {
@@ -201,9 +196,6 @@ struct kvm_vcpu_arch {
 	/* CSR state */
 	struct loongarch_csrs *csr;
 
-	/* Guest max PMU CSR id */
-	int max_pmu_csrid;
-
 	/* GPR used as IO source/target */
 	u32 io_gpr;
 
@@ -276,16 +268,6 @@ static inline bool kvm_guest_has_lasx(struct kvm_vcpu_arch *arch)
 static inline bool kvm_guest_has_lbt(struct kvm_vcpu_arch *arch)
 {
 	return arch->cpucfg[2] & (CPUCFG2_X86BT | CPUCFG2_ARMBT | CPUCFG2_MIPSBT);
-}
-
-static inline bool kvm_guest_has_pmu(struct kvm_vcpu_arch *arch)
-{
-	return arch->cpucfg[6] & CPUCFG6_PMP;
-}
-
-static inline int kvm_get_pmu_num(struct kvm_vcpu_arch *arch)
-{
-	return (arch->cpucfg[6] & CPUCFG6_PMNUM) >> CPUCFG6_PMNUM_SHIFT;
 }
 
 /* Debug: dump vcpu state */
