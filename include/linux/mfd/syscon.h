6e65283089abea27af3c5a4ded5327f45ad4bf94
Guomin Chen <Guomin.Chen@cixtech.com>
2025-03-26 06:34:42 +0000

    syscon: Add ACPI support for syscon
    Add ACPI support for syscon subsystem.
    
    Signed-off-by: Lihua Liu <Lihua.Liu@cixtech.com>
    Signed-off-by: Guomin Chen <Guomin.Chen@cixtech.com>

--- include/linux/mfd/syscon.h
+++ include/linux/mfd/syscon.h
@@ -15,6 +15,7 @@
 #include <linux/errno.h>
 
 struct device_node;
+struct device;
 
 #ifdef CONFIG_MFD_SYSCON
 extern struct regmap *device_node_to_regmap(struct device_node *np);
@@ -30,6 +31,9 @@ extern struct regmap *syscon_regmap_lookup_by_phandle_args(
 					unsigned int *out_args);
 extern struct regmap *syscon_regmap_lookup_by_phandle_optional(
 					struct device_node *np,
+					const char *property);
+extern struct regmap *device_syscon_regmap_lookup_by_property(
+					struct device *dev,
 					const char *property);
 #else
 static inline struct regmap *device_node_to_regmap(struct device_node *np)
@@ -68,6 +72,13 @@ static inline struct regmap *syscon_regmap_lookup_by_phandle_optional(
 					const char *property)
 {
 	return NULL;
+}
+
+extern struct regmap
+*device_syscon_regmap_lookup_by_property(struct device *dev,
+					 const char *property)
+{
+	return ERR_PTR(-EOPNOTSUPP);
 }
 
 #endif
