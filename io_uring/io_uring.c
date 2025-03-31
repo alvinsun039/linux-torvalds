6149063229c0df50968ead5bc3b38dbec0494cbc
Pavel Begunkov <asml.silence@gmail.com>
2025-03-31 08:54:00 +0100

    io_uring: add req flag invariant build assertion
    Mainline: 697b2876ac037545ba2761e2ffe9a5c2af6424e6
    From: v6.15-rc1
    Severity: Low
    
    We're caching some of file related request flags in a tricky way, put
    a build check to make sure flags don't get reshuffled.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/9877577b83c25dd78224a8274f799187e7ec7639.1743407551.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    (backported from commit 697b2876ac037545ba2761e2ffe9a5c2af6424e6)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I275f1d06d5d3b61c4ab8eb2a5e7c5c3e5575f35f
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/122048
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -1740,6 +1740,8 @@ unsigned int io_file_get_flags(struct file *file)
 {
 	unsigned int res = 0;
 
+	BUILD_BUG_ON(REQ_F_ISREG_BIT != REQ_F_SUPPORT_NOWAIT_BIT + 1);
+
 	if (S_ISREG(file_inode(file)->i_mode))
 		res |= REQ_F_ISREG;
 	if ((file->f_flags & O_NONBLOCK) || (file->f_mode & FMODE_NOWAIT))
