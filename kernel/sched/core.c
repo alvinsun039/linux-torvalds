484589a134284a85b90615dfea84c6594d49cb02
Zhang Qiao <zhangqiao22@huawei.com>
2025-04-02 15:11:52 +0800

    sched: Initialize the vruntime of a new task when it is first enqueued
    Mainline: c40dd90ac045fa1fdf6acc5bf9109a2315e6c92c
    From: v6.12-rc1
    Severity: Low
    
    Task: #340825
    
    When creating a new task, we initialize vruntime of the newly task at
    sched_cgroup_fork(). However, the timing of executing this action is too
    early and may not be accurate.
    
    Because it uses current CPU to init the vruntime, but the new task
    actually runs on the cpu which be assigned at wake_up_new_task().
    
    To optimize this case, we pass ENQUEUE_INITIAL flag to activate_task()
    in wake_up_new_task(), in this way, when place_entity is called in
    enqueue_entity(), the vruntime of the new task will be initialized.
    
    In addition, place_entity() in task_fork_fair() was introduced for two
    reasons:
    1. Previously, the __enqueue_entity() was in task_new_fair(),
    in order to provide vruntime for enqueueing the newly task, the
    vruntime assignment equation "se->vruntime = cfs_rq->min_vruntime" was
    introduced by commit e9acbff6484d ("sched: introduce se->vruntime").
    This is the initial state of place_entity().
    
    2. commit 4d78e7b656aa ("sched: new task placement for vruntime") added
    child_runs_first task placement feature which based on vruntime, this
    also requires the new task's vruntime value.
    
    After removing the child_runs_first and enqueue_entity() from
    task_fork_fair(), this place_entity() no longer makes sense, so remove
    it also.
    
    Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20240627133359.1370598-1-zhangqiao22@huawei.com
    
    K2CI-Arch: All
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Change-Id: I77a8fa3e406fb366878df60d31b3b51840c9c18c
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/166234
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4756,7 +4756,7 @@ void wake_up_new_task(struct task_struct *p)
 	update_rq_clock(rq);
 	post_init_entity_util_avg(p);
 
-	activate_task(rq, p, ENQUEUE_NOCLOCK);
+	activate_task(rq, p, ENQUEUE_NOCLOCK | ENQUEUE_INITIAL);
 	trace_sched_wakeup_new(p);
 	wakeup_preempt(rq, p, wake_flags);
 #ifdef CONFIG_SMP
