5a42887c19c36f0309bc2ff7c9dd71c1aecae62f
Qais Yousef <qyousef@layalina.io>
2025-04-02 16:57:17 +0800

    sched/rt: Clean up usage of rt_task()
    Mainline: 130fd056dd82b02db9a661c013071af35309be1a
    From: v6.12-rc1
    Severity: Low
    
    Task: #340825
    
    rt_task() checks if a task has RT priority. But depends on your
    dictionary, this could mean it belongs to RT class, or is a 'realtime'
    task, which includes RT and DL classes.
    
    Since this has caused some confusion already on discussion [1], it
    seemed a clean up is due.
    
    I define the usage of rt_task() to be tasks that belong to RT class.
    Make sure that it returns true only for RT class and audit the users and
    replace the ones required the old behavior with the new realtime_task()
    which returns true for RT and DL classes. Introduce similar
    realtime_prio() to create similar distinction to rt_prio() and update
    the users that required the old behavior to use the new function.
    
    Move MAX_DL_PRIO to prio.h so it can be used in the new definitions.
    
    Document the functions to make it more obvious what is the difference
    between them. PI-boosted tasks is a factor that must be taken into
    account when choosing which function to use.
    
    Rename task_is_realtime() to realtime_task_policy() as the old name is
    confusing against the new realtime_task().
    
    No functional changes were intended.
    
    [1] https://lore.kernel.org/lkml/20240506100509.GL40213@noisy.programming.kicks-ass.net/
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Reviewed-by: "Steven Rostedt (Google)" <rostedt@goodmis.org>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/20240610192018.1567075-2-qyousef@layalina.io
    
    K2CI-Arch: All
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Change-Id: Ia77607303b1c9da3476c4427d7c2c1da25ab350f
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/166247
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2641,7 +2641,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 			error = current->timer_slack_ns;
 		break;
 	case PR_SET_TIMERSLACK:
-		if (task_is_realtime(current))
+		if (realtime_task_policy(current))
 			break;
 		if (arg2 <= 0)
 			current->timer_slack_ns =
--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -2058,7 +2058,7 @@ static void __hrtimer_init_sleeper(struct hrtimer_sleeper *sl,
 	 * expiry.
 	 */
 	if (IS_ENABLED(CONFIG_PREEMPT_RT)) {
-		if (task_is_realtime(current) && !(mode & HRTIMER_MODE_SOFT))
+		if (realtime_task_policy(current) && !(mode & HRTIMER_MODE_SOFT))
 			mode |= HRTIMER_MODE_HARD;
 	}
 
