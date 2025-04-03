37cc64069a063b3276c4d6b37b320d18b8a22b14
Peter Zijlstra <peterz@infradead.org>
2025-04-03 09:51:38 +0800

    sched/eevdf: Fixup PELT vs DELAYED_DEQUEUE
    Mainline: fc1892becd5672f52329a75c73117b60ac7841b7
    From: v6.12-rc1
    Severity: Low
    
    Task: #340825
    
    Note that tasks that are kept on the runqueue to burn off negative
    lag, are not in fact runnable anymore, they'll get dequeued the moment
    they get picked.
    
    As such, don't count this time towards runnable.
    
    Thanks to Valentin for spotting I had this backwards initially.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105030.514088302@infradead.org
    
    K2CI-Arch: All
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Change-Id: Id128dcc557489f105d7ee749e511418ce7e7f71c
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/166265
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -914,6 +914,9 @@ static inline void se_update_runnable(struct sched_entity *se)
 
 static inline long se_runnable(struct sched_entity *se)
 {
+	if (se->sched_delayed)
+		return false;
+
 	if (entity_is_task(se))
 		return !!se->on_rq;
 	else
@@ -928,6 +931,9 @@ static inline void se_update_runnable(struct sched_entity *se) { }
 
 static inline long se_runnable(struct sched_entity *se)
 {
+	if (se->sched_delayed)
+		return false;
+
 	return !!se->on_rq;
 }
 
