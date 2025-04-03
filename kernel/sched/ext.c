d310748b0a6485f957d5dec22a1749e12cd58b04
Peter Zijlstra <peterz@infradead.org>
2025-04-03 09:24:35 +0800

    sched: Fix delayed_dequeue vs switched_from_fair()
    Mainline: 98442f0ccd828ac42e89281a815e9e7a97533822
    From: v6.12-rc4
    Severity: Low
    
    Task: #340825
    
    Commit 2e0199df252a ("sched/fair: Prepare exit/cleanup paths for delayed_dequeue")
    and its follow up fixes try to deal with a rather unfortunate
    situation where is task is enqueued in a new class, even though it
    shouldn't have been. Mostly because the existing ->switched_to/from()
    hooks are in the wrong place for this case.
    
    This all led to Paul being able to trigger failures at something like
    once per 10k CPU hours of RCU torture.
    
    For now, do the ugly thing and move the code to the right place by
    ignoring the switch hooks.
    
    Note: Clean up the whole sched_class::switch*_{to,from}() thing.
    
    Fixes: 2e0199df252a ("sched/fair: Prepare exit/cleanup paths for delayed_dequeue")
    Reported-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20241003185037.GA5594@noisy.programming.kicks-ass.net
    
    K2CI-Arch: All
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Change-Id: I327c2322f5550488db4ebd56ca9ff5a7953b188c
    Reviewed-on: http://gerrit.kylin.com/c/klinux/+/166273
    Reviewed-by: Jackie Liu <liuyun01@kylinos.cn>
    Code-Style: k2ci <kernel-bot@kylinos.cn>
    Tested-by: k2ci <kernel-bot@kylinos.cn>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -4487,7 +4487,7 @@ static void scx_ops_disable_workfn(struct kthread_work *work)
 
 		sched_deq_and_put_task(p, DEQUEUE_SAVE | DEQUEUE_MOVE, &ctx);
 
-		__setscheduler_prio(p, p->prio);
+		p->sched_class = __setscheduler_class(p, p->prio);
 		check_class_changing(task_rq(p), p, old_class);
 
 		sched_enq_and_set_task(&ctx);
@@ -5198,7 +5198,7 @@ static int scx_ops_enable(struct sched_ext_ops *ops)
 		sched_deq_and_put_task(p, DEQUEUE_SAVE | DEQUEUE_MOVE, &ctx);
 
 		p->scx.slice = SCX_SLICE_DFL;
-		__setscheduler_prio(p, p->prio);
+		p->sched_class = __setscheduler_class(p, p->prio);
 		check_class_changing(task_rq(p), p, old_class);
 
 		sched_enq_and_set_task(&ctx);
