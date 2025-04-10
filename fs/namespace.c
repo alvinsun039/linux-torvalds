6c7b705add0f4354a01252530abd198527e38d71
Jan Stancek <jstancek@redhat.com>
2025-04-10 17:05:42 +0200

    fs: use namespace_{lock,unlock} in dissolve_on_fput()
    Mainline: 47a742fd977a7a8c39fea890712e9bfdf76f98f1
    From: v6.15-rc3
    Severity: Important
    
    task: 530755
    
    In commit b73ec10a4587 ("fs: add fastpath for dissolve_on_fput()"),
    the namespace_{lock,unlock} has been replaced with scoped_guard
    using the namespace_sem. This however now also skips processing of
    'unmounted' list in namespace_unlock(), and mount is not (immediately)
    cleaned up.
    
    For example, this causes LTP move_mount02 fail:
        ...
        move_mount02.c:80: TPASS: invalid-from-fd: move_mount() failed as expected: EBADF (9)
        move_mount02.c:80: TPASS: invalid-from-path: move_mount() failed as expected: ENOENT (2)
        move_mount02.c:80: TPASS: invalid-to-fd: move_mount() failed as expected: EBADF (9)
        move_mount02.c:80: TPASS: invalid-to-path: move_mount() failed as expected: ENOENT (2)
        move_mount02.c:80: TPASS: invalid-flags: move_mount() failed as expected: EINVAL (22)
        tst_test.c:1833: TINFO: === Testing on ext3 ===
        tst_test.c:1170: TINFO: Formatting /dev/loop0 with ext3 opts='' extra opts=''
        mke2fs 1.47.2 (1-Jan-2025)
        /dev/loop0 is apparently in use by the system; will not make a filesystem here!
        tst_test.c:1170: TBROK: mkfs.ext3 failed with exit code 1
    
    The test makes number of move_mount() calls but these are all designed to fail
    with specific errno. Even after test, 'losetup -d' can't detach loop device.
    
    Define a new guard for dissolve_on_fput, that will use namespace_{lock,unlock}.
    
    Fixes: b73ec10a4587 ("fs: add fastpath for dissolve_on_fput()")
    Signed-off-by: Jan Stancek <jstancek@redhat.com>
    Link: https://lore.kernel.org/cad2f042b886bf0ced3d8e3aff120ec5e0125d61.1744297468.git.jstancek@redhat.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    (backported from commit 47a742fd977a7a8c39fea890712e9bfdf76f98f1)
    
    Change-Id: Icfa5b5b58e73b9ff7051e0d266790eaac77fe526
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117899
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1822,6 +1822,8 @@ static inline void namespace_lock(void)
 	down_write(&namespace_sem);
 }
 
+DEFINE_GUARD(namespace_lock, struct rw_semaphore *, namespace_lock(), namespace_unlock())
+
 enum umount_tree_flags {
 	UMOUNT_SYNC = 1,
 	UMOUNT_PROPAGATE = 2,
@@ -2370,7 +2372,7 @@ void dissolve_on_fput(struct vfsmount *mnt)
 			return;
 	}
 
-	scoped_guard(rwsem_write, &namespace_sem) {
+	scoped_guard(namespace_lock, &namespace_sem) {
 		ns = m->mnt_ns;
 		if (!must_dissolve(ns))
 			return;
