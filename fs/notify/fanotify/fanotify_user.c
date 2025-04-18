2daf90fce0daac73678960af314c8b5b3155e212
Amir Goldstein <amir73il@gmail.com>
2025-04-18 21:39:02 +0200

    fanotify: fix flush of mntns marks
    Mainline: c73c67026fe65d6677260dfd15dd968b709dc237
    From: v6.15-rc5
    Severity: Important
    
    task: 530755
    
    fanotify_mark(fd, FAN_MARK_FLUSH | FAN_MARK_MNTNS, ...) incorrectly
    ends up causing removal inode marks.
    
    Fixes: 0f46d81f2bce ("fanotify: notify on mount attach and detach")
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20250418193903.2607617-2-amir73il@gmail.com
    
    (backported from commit c73c67026fe65d6677260dfd15dd968b709dc237)
    
    Change-Id: I46e6eb3df8afb9bf2aabb08bb6e9b658c0f4117b
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/120865
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- fs/notify/fanotify/fanotify_user.c
+++ fs/notify/fanotify/fanotify_user.c
@@ -1973,12 +1973,7 @@ static int do_fanotify_mark(int fanotify_fd, unsigned int flags, __u64 mask,
 
 	if (mark_cmd == FAN_MARK_FLUSH) {
 		ret = 0;
-		if (mark_type == FAN_MARK_MOUNT)
-			fsnotify_clear_vfsmount_marks_by_group(group);
-		else if (mark_type == FAN_MARK_FILESYSTEM)
-			fsnotify_clear_sb_marks_by_group(group);
-		else
-			fsnotify_clear_inode_marks_by_group(group);
+		fsnotify_clear_marks_by_group(group, obj_type);
 		goto fput_and_out;
 	}
 
