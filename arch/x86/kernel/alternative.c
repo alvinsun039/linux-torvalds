e03c12ab6a792b90e88e90ba036c60a946322834
Peter Zijlstra <peterz@infradead.org>
2025-04-23 09:57:31 +0200

    x86/its: FineIBT-paranoid vs ITS
    Mainline: e52c1dc7455d32c8a55f9949d300e5e87d011fa6
    From: v6.15-rc5-12-ge52c1dc7455d
    Severity: Moderate
    
    task: 464978
    
    commit e52c1dc7455d32c8a55f9949d300e5e87d011fa6 upstream.
    
    FineIBT-paranoid was using the retpoline bytes for the paranoid check,
    disabling retpolines, because all parts that have IBT also have eIBRS
    and thus don't need no stinking retpolines.
    
    Except... ITS needs the retpolines for indirect calls must not be in
    the first half of a cacheline :-/
    
    So what was the paranoid call sequence:
    
      <fineibt_paranoid_start>:
       0:   41 ba 78 56 34 12       mov    $0x12345678, %r10d
       6:   45 3b 53 f7             cmp    -0x9(%r11), %r10d
       a:   4d 8d 5b <f0>           lea    -0x10(%r11), %r11
       e:   75 fd                   jne    d <fineibt_paranoid_start+0xd>
      10:   41 ff d3                call   *%r11
      13:   90                      nop
    
    Now becomes:
    
      <fineibt_paranoid_start>:
       0:   41 ba 78 56 34 12       mov    $0x12345678, %r10d
       6:   45 3b 53 f7             cmp    -0x9(%r11), %r10d
       a:   4d 8d 5b f0             lea    -0x10(%r11), %r11
       e:   2e e8 XX XX XX XX	cs call __x86_indirect_paranoid_thunk_r11
    
      Where the paranoid_thunk looks like:
    
       1d:  <ea>                    (bad)
       __x86_indirect_paranoid_thunk_r11:
       1e:  75 fd                   jne 1d
       __x86_indirect_its_thunk_r11:
       20:  41 ff eb                jmp *%r11
       23:  cc                      int3
    
    [ dhansen: remove initialization to false ]
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    [ Just a portion of the original commit, in order to fix a build issue
      in stable kernels due to backports ]
    Tested-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Link: https://lore.kernel.org/r/20250514113952.GB16434@noisy.programming.kicks-ass.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (backported from commit 772934d9062a0f7297ad4e5bffbd904208655660)
    
    K2CI-Arch: Amd64|Arm64
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Change-Id: Ia042c04f65b98c2a65b87afbc396f0df175955bb
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/64513
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -129,6 +129,13 @@ const unsigned char * const x86_nops[ASM_NOP_MAX+1] =
 
 #ifdef CONFIG_MITIGATION_ITS
 
+u8 *its_static_thunk(int reg)
+{
+	u8 *thunk = __x86_indirect_its_thunk_array[reg];
+
+	return thunk;
+}
+
 #ifdef CONFIG_MODULES
 static struct module *its_mod;
 static void *its_page;
