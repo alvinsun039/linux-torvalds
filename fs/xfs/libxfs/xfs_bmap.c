f185cfd90bd67849e6014c890b0133cc5f0fbc02
Shida Zhang <zhangshida@kylinos.cn>
2025-04-24 16:45:47 +0800

    KYLIN: xfs: add infrastructure to support AF allocation algorithm
    Mainline: KYLIN-only
    Severity: Important
    
    task: 225344
    
    Add a function to search through all the AFs in a alloction.
    
    Add two members in *args* to trace the current AF.
    
    And properly initialize these members so as to keeping the
    behavior exacly the same with the original code logic.
    
    And for those bmbt alloction, we can slightly break the roles
    imposed by AF, since it's allocating one block at a time.
    
    Remember our goal to propose the concept of AF is to slow down
    the over-speed extending of fragmented pieces. And the speed of
    alloc one block at a time will do no harm to our goal.
    
    So just initialize it in a way like this alloctions are in a
    AF ranging from [0, ag_count).
    
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>
    Change-Id: Iedfa930ee67df2d87c1d02ebd7ee6de795faf61a
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/56052
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Reviewed-by: 池志凌 <chizhiling@kylinos.cn>

--- fs/xfs/libxfs/xfs_bmap.c
+++ fs/xfs/libxfs/xfs_bmap.c
@@ -654,6 +654,8 @@ xfs_bmap_extents_to_btree(
 	args.minlen = args.maxlen = args.prod = 1;
 	args.wasdel = wasdel;
 	*logflagsp = 0;
+	args.curr_af = 0;
+	args.next_af = mp->m_sb.sb_agcount;
 	error = xfs_alloc_vextent_start_ag(&args,
 				XFS_INO_TO_FSB(mp, ip->i_ino));
 	if (error)
@@ -803,6 +805,8 @@ xfs_bmap_local_to_extents(
 	 */
 	args.total = total;
 	args.minlen = args.maxlen = args.prod = 1;
+	args.curr_af = 0;
+	args.next_af = args.mp->m_sb.sb_agcount;
 	error = xfs_alloc_vextent_start_ag(&args,
 			XFS_INO_TO_FSB(args.mp, ip->i_ino));
 	if (error)
@@ -3541,6 +3545,8 @@ xfs_bmap_btalloc_low_space(
 
 	if (args->minlen > ap->minlen) {
 		args->minlen = ap->minlen;
+		args->curr_af = 0;
+		args->next_af = args->mp->m_sb.sb_agcount;
 		error = xfs_alloc_vextent_start_ag(args, ap->blkno);
 		if (error || args->fsbno != NULLFSBLOCK)
 			return error;
@@ -3646,6 +3652,32 @@ xfs_bmap_btalloc_best_length(
 	return xfs_bmap_btalloc_low_space(ap, args);
 }
 
+static int __maybe_unused
+xfs_bmap_btalloc_best_length_iterate_afs(
+	struct xfs_bmalloca     *ap,
+	struct xfs_alloc_arg    *args,
+	int                     stripe_align)
+{
+	struct xfs_mount        *mp = ap->ip->i_mount;
+	int                     error;
+	unsigned int i;
+
+	args->curr_af = 0;
+
+	for (i = 0; args->curr_af < mp->m_sb.sb_agcount; i++) {
+		args->next_af = mp->m_sb.sb_agcount - mp->m_af[i];
+		error = xfs_bmap_btalloc_best_length(ap, args, stripe_align);
+		if (error || args->fsbno != NULLFSBLOCK)
+			break;
+
+		args->curr_af = args->next_af;
+		/* Exit LOWMODE when going to the next AF. */
+		ap->tp->t_flags &= ~XFS_TRANS_LOWMODE;
+	}
+
+	return error;
+}
+
 static int
 xfs_bmap_btalloc(
 	struct xfs_bmalloca	*ap)
@@ -3663,6 +3695,8 @@ xfs_bmap_btalloc(
 		.alignment	= 1,
 		.minalignslop	= 0,
 		.postallocs	= 1,
+		.curr_af	= 0,
+		.next_af	= mp->m_sb.sb_agcount,
 	};
 	xfs_fileoff_t		orig_offset;
 	xfs_extlen_t		orig_length;
