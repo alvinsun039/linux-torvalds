42c255ce2e914835d97cf5c6f01456f447f26fa5
Al Viro <viro@zeniv.linux.org.uk>
2025-04-25 12:55:39 -0400

    do_move_mount(): take dropping the old mountpoint into attach_recursive_mnt()
    Mainline: 761de25854424aa23fd1d7b2bef5c7d184690926
    From: v6.17-rc1
    Severity: Important
    
    task: 530755
    
    ... and fold it with unhash_mnt() there - there's no need to retain a reference
    to old_mp beyond that point, since by then all mountpoints we were going to add
    are either explicitly pinned by get_mountpoint() or have stuff already added
    to them.
    
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    (backported from commit 761de25854424aa23fd1d7b2bef5c7d184690926)
    
    Change-Id: Ib211f27551074066ead9ef65059cf5ef8e2e8b7f
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117928
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2686,7 +2686,7 @@ static int attach_recursive_mnt(struct mount *source_mnt,
 	}
 
 	if (moving) {
-		unhash_mnt(source_mnt);
+		umount_mnt(source_mnt);
 		mnt_notify_add(source_mnt);
 	} else {
 		if (source_mnt->mnt_ns) {
@@ -3604,7 +3604,7 @@ static int do_move_mount(struct path *old_path,
 	struct mount *p;
 	struct mount *old;
 	struct mount *parent;
-	struct mountpoint *mp, *old_mp;
+	struct mountpoint *mp;
 	int err;
 	bool attached, beneath = flags & MNT_TREE_BENEATH;
 
@@ -3616,7 +3616,6 @@ static int do_move_mount(struct path *old_path,
 	p = real_mount(new_path->mnt);
 	parent = old->mnt_parent;
 	attached = mnt_has_parent(old);
-	old_mp = old->mnt_mp;
 	ns = old->mnt_ns;
 
 	err = -EINVAL;
@@ -3690,8 +3689,6 @@ static int do_move_mount(struct path *old_path,
 	/* if the mount is moved, it should no longer be expire
 	 * automatically */
 	list_del_init(&old->mnt_expire);
-	if (attached)
-		put_mountpoint(old_mp);
 out:
 	unlock_mount(mp);
 	if (!err) {
