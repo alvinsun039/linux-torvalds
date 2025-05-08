3355d1618ca7048e73318424108cdd8b12bc3ab1
Al Viro <viro@zeniv.linux.org.uk>
2025-05-08 00:09:30 -0400

    do_move_mount(): get rid of 'attached' flag
    Mainline: ee1ee33ccc1ba0620a77833b2a3e320588701217
    From: v6.17-rc1
    Severity: Important
    
    task: 530755
    
    'attached' serves as a proxy for "source is a subtree of our namespace
    and not the entirety of anon namespace"; finish massaging it away.
    
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    (backported from commit ee1ee33ccc1ba0620a77833b2a3e320588701217)
    
    Change-Id: I6218606950cfa54832a8f5669dff062a63368ea0
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117929
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- fs/namespace.c
+++ fs/namespace.c
@@ -3606,7 +3606,7 @@ static int do_move_mount(struct path *old_path,
 	struct mount *parent;
 	struct mountpoint *mp;
 	int err;
-	bool attached, beneath = flags & MNT_TREE_BENEATH;
+	bool beneath = flags & MNT_TREE_BENEATH;
 
 	mp = do_lock_mount(new_path, beneath);
 	if (IS_ERR(mp))
@@ -3615,7 +3615,6 @@ static int do_move_mount(struct path *old_path,
 	old = real_mount(old_path->mnt);
 	p = real_mount(new_path->mnt);
 	parent = old->mnt_parent;
-	attached = mnt_has_parent(old);
 	ns = old->mnt_ns;
 
 	err = -EINVAL;
@@ -3627,6 +3626,9 @@ static int do_move_mount(struct path *old_path,
 			goto out;
 		/* ... and the target should be in our namespace */
 		if (!check_mnt(p))
+			goto out;
+		/* parent of the source should not be shared */
+		if (IS_MNT_SHARED(parent))
 			goto out;
 	} else {
 		/*
@@ -3654,11 +3656,6 @@ static int do_move_mount(struct path *old_path,
 
 	if (d_is_dir(new_path->dentry) !=
 	    d_is_dir(old_path->dentry))
-		goto out;
-	/*
-	 * Don't move a mount residing in a shared parent.
-	 */
-	if (attached && IS_MNT_SHARED(parent))
 		goto out;
 
 	if (beneath) {
@@ -3692,7 +3689,7 @@ static int do_move_mount(struct path *old_path,
 out:
 	unlock_mount(mp);
 	if (!err) {
-		if (attached) {
+		if (!is_anon_ns(ns)) {
 			mntput_no_expire(parent);
 		} else {
 			/* Make sure we notice when we leak mounts. */
