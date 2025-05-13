c947ac9b822ed929824dc3e96e6bd08f5b0ea342
Eric Dumazet <edumazet@google.com>
2025-05-13 19:39:12 +0000

    tcp: add receive queue awareness in tcp_rcv_space_adjust()
    Mainline: ea33537d82921e71f852ea2ed985acc562125efe
    From: v6.16-rc1
    Severity: Low
    
    If the application can not drain fast enough a TCP socket queue,
    tcp_rcv_space_adjust() can overestimate tp->rcvq_space.space.
    
    Then sk->sk_rcvbuf can grow and hit tcp_rmem[2] for no good reason.
    
    Fix this by taking into acount the number of available bytes.
    
    Keeping sk->sk_rcvbuf at the right size allows better cache efficiency.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Wei Wang <weiwan@google.com>
    Link: https://patch.msgid.link/20250513193919.1089692-5-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    (backported from commit ea33537d82921e71f852ea2ed985acc562125efe)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: If932b3c4949549659bc6b703ae664e6c386d8b4d
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/85601
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -781,8 +781,7 @@ static void tcp_rcvbuf_grow(struct sock *sk)
 void tcp_rcv_space_adjust(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
-	u32 copied;
-	int time;
+	int time, inq, copied;
 
 	trace_tcp_rcv_space_adjust(sk);
 
@@ -793,6 +792,9 @@ void tcp_rcv_space_adjust(struct sock *sk)
 
 	/* Number of bytes copied to user in last RTT */
 	copied = tp->copied_seq - tp->rcvq_space.seq;
+	/* Number of bytes in receive queue. */
+	inq = tp->rcv_nxt - tp->copied_seq;
+	copied -= inq;
 	if (copied <= tp->rcvq_space.space)
 		goto new_measure;
 
