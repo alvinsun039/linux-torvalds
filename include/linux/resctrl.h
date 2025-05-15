8f12eade5122017cb60f39b8b262143d750c4266
James Morse <james.morse@arm.com>
2025-05-15 16:58:39 +0000

    x86/resctrl: Drop __init/__exit on assorted symbols
    Mainline: bc740420d7ae7878696d5ad9f3dbcb11e2599b2e
    From: v6.16-rc1
    Severity: Moderate
    
    task: 527377
    
    Because ARM's MPAM controls are probed using MMIO, resctrl can't be
    initialised until enough CPUs are online to have determined the system-wide
    supported num_closid. Arm64 also supports 'late onlined secondaries', where
    only a subset of CPUs are online during boot.
    
    These two combine to mean the MPAM driver may not be able to initialise
    resctrl until user-space has brought 'enough' CPUs online.
    
    To allow MPAM to initialise resctrl after __init text has been free'd, remove
    all the __init markings from resctrl.
    
    The existing __exit markings cause these functions to be removed by the linker
    as it has never been possible to build resctrl as a module. MPAM has an error
    interrupt which causes the driver to reset and disable itself. Remove the
    __exit markings to allow the MPAM driver to tear down resctrl when an error
    occurs.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
    Tested-by: Fenghua Yu <fenghuay@nvidia.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Peter Newman <peternewman@google.com>
    Tested-by: Amit Singh Tomar <amitsinght@marvell.com> # arm64
    Tested-by: Shanker Donthineni <sdonthineni@nvidia.com> # arm64
    Tested-by: Babu Moger <babu.moger@amd.com>
    Tested-by: Tony Luck <tony.luck@intel.com>
    Link: https://lore.kernel.org/20250515165855.31452-10-james.morse@arm.com
    
    (backported from commit bc740420d7ae7878696d5ad9f3dbcb11e2599b2e)
    
    K2CI-Arch: All
    Signed-off-by: tanze <tanze@kylinos.cn>
    Change-Id: I3fd643af46fe585d8b98c8b81b0bd1be1aadf823
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/161628
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -312,7 +312,7 @@ u32 resctrl_arch_get_num_closid(struct rdt_resource *r);
 u32 resctrl_arch_system_num_rmid_idx(void);
 int resctrl_arch_update_domains(struct rdt_resource *r, u32 closid);
 
-__init bool resctrl_arch_is_evt_configurable(enum resctrl_event_id evt);
+bool resctrl_arch_is_evt_configurable(enum resctrl_event_id evt);
 
 /*
  * Update the ctrl_val and apply this config right now.
@@ -429,7 +429,7 @@ void resctrl_arch_reset_all_ctrls(struct rdt_resource *r);
 extern unsigned int resctrl_rmid_realloc_threshold;
 extern unsigned int resctrl_rmid_realloc_limit;
 
-int __init resctrl_init(void);
-void __exit resctrl_exit(void);
+int resctrl_init(void);
+void resctrl_exit(void);
 
 #endif /* _RESCTRL_H */
