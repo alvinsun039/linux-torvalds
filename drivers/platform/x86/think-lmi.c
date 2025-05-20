844ecd9a18fa7184ac77f6860e58f3f9124e8143
Mark Pearson <mpearson-lenovo@squebb.ca>
2025-05-19 20:50:18 -0400

    platform/x86: think-lmi: Fix attribute name usage for non-compliant items
    Mainline: 8508427a6e21c1ef01ae4c9f4e2675fc99deb949
    From: v6.15
    Severity: Low
    
    A few, quite rare, WMI attributes have names that are not compatible with
    filenames, e.g. "Intel VT for Directed I/O (VT-d)".
    For these cases the '/' gets replaced with '\' for display, but doesn't
    get switched again when doing the WMI access.
    
    Fix this by keeping the original attribute name and using that for sending
    commands to the BIOS
    
    Fixes: a40cd7ef22fb ("platform/x86: think-lmi: Add WMI interface support on Lenovo platforms")
    Signed-off-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Link: https://lore.kernel.org/r/20250520005027.3840705-1-mpearson-lenovo@squebb.ca
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    
    (backported from commit 8508427a6e21c1ef01ae4c9f4e2675fc99deb949)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Id23fc318fa7d29a59508ee533ab4603125e2e139
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/69334
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/platform/x86/think-lmi.c
+++ drivers/platform/x86/think-lmi.c
@@ -1008,8 +1008,8 @@ static ssize_t current_value_store(struct kobject *kobj,
 			ret = -EINVAL;
 			goto out;
 		}
-		set_str = kasprintf(GFP_KERNEL, "%s,%s,%s", setting->display_name,
-					new_setting, tlmi_priv.pwd_admin->signature);
+		set_str = kasprintf(GFP_KERNEL, "%s,%s,%s", setting->name,
+				    new_setting, tlmi_priv.pwd_admin->signature);
 		if (!set_str) {
 			ret = -ENOMEM;
 			goto out;
@@ -1039,7 +1039,7 @@ static ssize_t current_value_store(struct kobject *kobj,
 				goto out;
 		}
 
-		set_str = kasprintf(GFP_KERNEL, "%s,%s;", setting->display_name,
+		set_str = kasprintf(GFP_KERNEL, "%s,%s;", setting->name,
 				    new_setting);
 		if (!set_str) {
 			ret = -ENOMEM;
@@ -1067,11 +1067,11 @@ static ssize_t current_value_store(struct kobject *kobj,
 		}
 
 		if (auth_str)
-			set_str = kasprintf(GFP_KERNEL, "%s,%s,%s", setting->display_name,
-					new_setting, auth_str);
+			set_str = kasprintf(GFP_KERNEL, "%s,%s,%s", setting->name,
+					    new_setting, auth_str);
 		else
-			set_str = kasprintf(GFP_KERNEL, "%s,%s;", setting->display_name,
-					new_setting);
+			set_str = kasprintf(GFP_KERNEL, "%s,%s;", setting->name,
+					    new_setting);
 		if (!set_str) {
 			ret = -ENOMEM;
 			goto out;
@@ -1584,9 +1584,6 @@ static int tlmi_analyze(void)
 			continue;
 		}
 
-		/* It is not allowed to have '/' for file name. Convert it into '\'. */
-		strreplace(item, '/', '\\');
-
 		/* Remove the value part */
 		p = strchrnul(item, ',');
 		*p = '\0';
@@ -1599,11 +1596,16 @@ static int tlmi_analyze(void)
 			goto fail_clear_attr;
 		}
 		setting->index = i;
+
+		strscpy(setting->name, item);
+		/* It is not allowed to have '/' for file name. Convert it into '\'. */
+		strreplace(item, '/', '\\');
 		strscpy(setting->display_name, item);
+
 		/* If BIOS selections supported, load those */
 		if (tlmi_priv.can_get_bios_selections) {
-			ret = tlmi_get_bios_selections(setting->display_name,
-					&setting->possible_values);
+			ret = tlmi_get_bios_selections(setting->name,
+						       &setting->possible_values);
 			if (ret || !setting->possible_values)
 				pr_info("Error retrieving possible values for %d : %s\n",
 						i, setting->display_name);
