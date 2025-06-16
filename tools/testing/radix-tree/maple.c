f68966786251d0de6399efe8a161eb9a321fd6e2
Liam R. Howlett <howlett@gmail.com>
2025-06-16 14:45:19 -0400

    testing/radix-tree/maple: increase readers and reduce delay for faster machines
    Mainline: f687fd5af8bf0358259278319f5bc18fc8c547cb
    From: v6.16-rc5-79-gf687fd5af8bf
    Severity: Low
    
    Faster machines may not see the initial or updated value in the race
    condition.  Reduce the delay so that faster machines are less likely to
    fail testing of the race conditions.
    
    Link: https://lkml.kernel.org/r/20250616184521.3382795-2-Liam.Howlett@oracle.com
    Signed-off-by: Liam R. Howlett <howlett@gmail.com>
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Hailong Liu <hailong.liu@oppo.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Peng Zhang <zhangpeng.00@bytedance.com>
    Cc: Sidhartha Kumar <sidhartha.kumar@oracle.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit f687fd5af8bf0358259278319f5bc18fc8c547cb)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I5521046035da25b209df03071cb9b1fbc80f3481
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/94821
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- tools/testing/radix-tree/maple.c
+++ tools/testing/radix-tree/maple.c
@@ -35039,7 +35039,7 @@ void run_check_rcu_slowread(struct maple_tree *mt, struct rcu_test_struct *vals)
 
 	int i;
 	void *(*function)(void *);
-	pthread_t readers[20];
+	pthread_t readers[30];
 	unsigned int index = vals->index;
 
 	mt_set_in_rcu(mt);
@@ -35057,14 +35057,14 @@ void run_check_rcu_slowread(struct maple_tree *mt, struct rcu_test_struct *vals)
 		}
 	}
 
-	usleep(5); /* small yield to ensure all threads are at least started. */
+	usleep(3); /* small yield to ensure all threads are at least started. */
 
 	while (index <= vals->last) {
 		mtree_store(mt, index,
 			    (index % 2 ? vals->entry2 : vals->entry3),
 			    GFP_KERNEL);
 		index++;
-		usleep(5);
+		usleep(2);
 	}
 
 	while (i--)
@@ -35075,6 +35075,7 @@ void run_check_rcu_slowread(struct maple_tree *mt, struct rcu_test_struct *vals)
 	MT_BUG_ON(mt, !vals->seen_entry3);
 	MT_BUG_ON(mt, !vals->seen_both);
 }
+
 static noinline void __init check_rcu_simulated(struct maple_tree *mt)
 {
 	unsigned long i, nr_entries = 1000;
