7ea316edcec2813c60be5bdbb9e3704f4891c1f2
Eric Dumazet <edumazet@google.com>
2025-06-30 12:19:31 +0000

    ipv4: adopt dst_dev, skb_dst_dev and skb_dst_dev_net[_rcu]
    Mainline: a74fc62eec155ca5a6da8ff3856f3dc87fe24558
    From: v6.17-rc1
    Severity: Moderate
    
    task: 554291
    
    Use the new helpers as a first step to deal with
    potential dst->dev races.
    
    	Conflicts:
    	net/ipv4/ip_output.c
    	net/ipv4/route.c
    	(Due to commit 1dbf1d590d10 and commit caedcc5b6df1 have
    	 backported before.)
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20250630121934.3399505-8-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Change-Id: If32e836ad9be6e9741aaad07a14c26df8cc5d58e
    Signed-off-by: Yun Lu <luyun@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/150933
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -416,7 +416,7 @@ static struct neighbour *ipv4_neigh_lookup(const struct dst_entry *dst,
 					   const void *daddr)
 {
 	const struct rtable *rt = container_of(dst, struct rtable, dst);
-	struct net_device *dev = dst->dev;
+	struct net_device *dev = dst_dev(dst);
 	struct neighbour *n;
 
 	rcu_read_lock();
@@ -443,7 +443,7 @@ static struct neighbour *ipv4_neigh_lookup(const struct dst_entry *dst,
 static void ipv4_confirm_neigh(const struct dst_entry *dst, const void *daddr)
 {
 	const struct rtable *rt = container_of(dst, struct rtable, dst);
-	struct net_device *dev = dst->dev;
+	struct net_device *dev = dst_dev(dst);
 	const __be32 *pkey = daddr;
 
 	if (rt->rt_gw_family == AF_INET) {
