8180d2707a33f39137bbfaeca2afc7c88732cf46
wenglianfa <wenglianfa@huawei.com>
2025-07-26 15:53:45 +0800

    RDMA/hns: Fix querying wrong SCC context for DIP algorithm
    Mainline: 085a1b42e52750769a3fa29d4da6c05ab56f18f8
    From: v6.17-rc3
    Severity: Low
    
    When using DIP algorithm, all QPs establishing connections with
    the same destination IP share the same SCC, which is indexed by
    dip_idx, but dip_idx isn't necessarily equal to qpn. Therefore,
    dip_idx should be used to query SCC context instead of qpn.
    
    Fixes: 124a9fbe43aa ("RDMA/hns: Append SCC context to the raw dump of QPC")
    Signed-off-by: wenglianfa <wenglianfa@huawei.com>
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>
    Link: https://patch.msgid.link/20250726075345.846957-1-huangjunxian6@hisilicon.com
    Reviewed-by: Zhu Yanjun <yanjun.zhu@linux.dev>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    
    (backported from commit 085a1b42e52750769a3fa29d4da6c05ab56f18f8)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ic557761bd5d948c03bb706ad19a5359d5a94aac7
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/120106
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.c
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.c
@@ -5737,7 +5737,7 @@ out:
 	return ret;
 }
 
-static int hns_roce_v2_query_sccc(struct hns_roce_dev *hr_dev, u32 qpn,
+static int hns_roce_v2_query_sccc(struct hns_roce_dev *hr_dev, u32 sccn,
 				  void *buffer)
 {
 	struct hns_roce_v2_scc_context *context;
@@ -5749,7 +5749,7 @@ static int hns_roce_v2_query_sccc(struct hns_roce_dev *hr_dev, u32 qpn,
 		return PTR_ERR(mailbox);
 
 	ret = hns_roce_cmd_mbox(hr_dev, 0, mailbox->dma, HNS_ROCE_CMD_QUERY_SCCC,
-				qpn);
+				sccn);
 	if (ret)
 		goto out;
 
