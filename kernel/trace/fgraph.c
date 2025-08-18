0902883fd1681658f734a7151733194d3f602945
Ye Weihua <yeweihua4@huawei.com>
2025-08-18 07:33:32 +0000

    trace/fgraph: Fix the warning caused by missing unregister notifier
    Mainline: edede7a6dcd7435395cf757d053974aaab6ab1c2
    From: v6.17-rc3
    Severity: Low
    
    task: 524071
    
    This warning was triggered during testing on v6.16:
    
    notifier callback ftrace_suspend_notifier_call already registered
    WARNING: CPU: 2 PID: 86 at kernel/notifier.c:23 notifier_chain_register+0x44/0xb0
    ...
    Call Trace:
     <TASK>
     blocking_notifier_chain_register+0x34/0x60
     register_ftrace_graph+0x330/0x410
     ftrace_profile_write+0x1e9/0x340
     vfs_write+0xf8/0x420
     ? filp_flush+0x8a/0xa0
     ? filp_close+0x1f/0x30
     ? do_dup2+0xaf/0x160
     ksys_write+0x65/0xe0
     do_syscall_64+0xa4/0x260
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    When writing to the function_profile_enabled interface, the notifier was
    not unregistered after start_graph_tracing failed, causing a warning the
    next time function_profile_enabled was written.
    
    Fixed by adding unregister_pm_notifier in the exception path.
    
    Link: https://lore.kernel.org/20250818073332.3890629-1-yeweihua4@huawei.com
    Fixes: 4a2b8dda3f870 ("tracing/function-graph-tracer: fix a regression while suspend to disk")
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Ye Weihua <yeweihua4@huawei.com>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    
    (backported from commit edede7a6dcd7435395cf757d053974aaab6ab1c2)
    
    Change-Id: I4af38b65c0ea86d4e22eb2ed277ec7edd4ddecd9
    Signed-off-by: Wang Yan <wangyan01@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/117368
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/trace/fgraph.c
+++ kernel/trace/fgraph.c
@@ -1360,6 +1360,7 @@ error:
 		ftrace_graph_active--;
 		gops->saved_func = NULL;
 		fgraph_lru_release_index(i);
+		unregister_pm_notifier(&ftrace_suspend_notifier);
 	}
 out:
 	mutex_unlock(&ftrace_lock);
