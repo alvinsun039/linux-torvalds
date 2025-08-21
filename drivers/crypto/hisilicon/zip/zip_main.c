7051d46077cf235452a53e2528ccf6c54fc248db
Chenghai Huang <huangchenghai2@huawei.com>
2025-08-21 09:38:04 +0800

    crypto: hisilicon/zip - remove unnecessary validation for high-performance mode configurations
    Mainline: d4e081510471e79171c4e0a11f6cb608e49bc082
    From: v6.18-rc1
    Severity: Moderate
    
    task: 542185
    
    [ Upstream commit d4e081510471e79171c4e0a11f6cb608e49bc082 ]
    
    When configuring the high-performance mode register, there is no
    need to verify whether the register has been successfully
    enabled, as there is no possibility of a write failure for this
    register.
    
    Fixes: a9864bae1806 ("crypto: hisilicon/zip - add zip comp high perf mode configuration")
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    (backported from commit 94b09b0ac0b8f55c50ef9c9b98bf60b8901d506b)
    
    K2CI-Arch: None
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Change-Id: I2d6ffeb52250a9ee4255d965fa2278930baae80e
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/128789
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/crypto/hisilicon/zip/zip_main.c
+++ drivers/crypto/hisilicon/zip/zip_main.c
@@ -447,10 +447,9 @@ bool hisi_zip_alg_support(struct hisi_qm *qm, u32 alg)
 	return false;
 }
 
-static int hisi_zip_set_high_perf(struct hisi_qm *qm)
+static void hisi_zip_set_high_perf(struct hisi_qm *qm)
 {
 	u32 val;
-	int ret;
 
 	val = readl_relaxed(qm->io_base + HZIP_HIGH_PERF_OFFSET);
 	if (perf_mode == HZIP_HIGH_COMP_PERF)
@@ -460,13 +459,6 @@ static int hisi_zip_set_high_perf(struct hisi_qm *qm)
 
 	/* Set perf mode */
 	writel(val, qm->io_base + HZIP_HIGH_PERF_OFFSET);
-	ret = readl_relaxed_poll_timeout(qm->io_base + HZIP_HIGH_PERF_OFFSET,
-					 val, val == perf_mode, HZIP_DELAY_1_US,
-					 HZIP_POLL_TIMEOUT_US);
-	if (ret)
-		pci_err(qm->pdev, "failed to set perf mode\n");
-
-	return ret;
 }
 
 static void hisi_zip_open_sva_prefetch(struct hisi_qm *qm)
@@ -1250,9 +1242,7 @@ static int hisi_zip_pf_probe_init(struct hisi_zip *hisi_zip)
 	if (ret)
 		return ret;
 
-	ret = hisi_zip_set_high_perf(qm);
-	if (ret)
-		return ret;
+	hisi_zip_set_high_perf(qm);
 
 	hisi_zip_open_sva_prefetch(qm);
 	hisi_zip_debug_regs_clear(qm);
