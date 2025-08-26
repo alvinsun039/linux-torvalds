d7dec88b28890407b7e91125712bdf768ec92472
Baolin Wang <baolin.wang@linux.alibaba.com>
2025-08-26 17:35:55 +0800

    mm: shmem: drop the unnecessary folio_nr_pages()
    Mainline: 6d11dec130ad16f2b08ef88fb7f3d37dc99d74d6
    From: v6.18-rc1
    Severity: Low
    
    Task: #547325
    
    We've got the number of pages in the folio earlier, thus remove the redundant
    folio_nr_pages() call.
    
    Link: https://lkml.kernel.org/r/67c80182ebd949e3894908e01e224697c143aabb.1756200587.git.baolin.wang@linux.alibaba.com
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit 6d11dec130ad16f2b08ef88fb7f3d37dc99d74d6)
    
    Change-Id: Id7ccb942c27f7d7da1001e38e2947034338f9354
    Signed-off-by: Qingshuang Fu <fuqingshuang@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/136290
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2407,7 +2407,6 @@ static int shmem_swapin_folio(struct inode *inode, pgoff_t index,
 		goto failed;
 	}
 	folio_wait_writeback(folio);
-	nr_pages = folio_nr_pages(folio);
 
 	/*
 	 * Some architectures may have to restore extra metadata to the
