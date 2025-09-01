0b217e5bad2d04602d1a236c2723b8673f0fe90c
Gang Yan <yangang@kylinos.cn>
2025-09-01 14:29:16 +0800

    KYLIN: Revert "selftests: mptcp: join: disable get and dump addr checks"
    Mainline: KYLIN-only
    Severity: Low
    
    task: 525537
    
    This reverts commit c2efc463bec9889a03e3900c630c6e4abc631162.
    
    The commit aims to fix the CI test in upstream when some related
    features are not backported. The output of mptcp_join.sh is:
    
    '''
    ...
    115 userspace pm add & remove address
    ...
            dump addrs signal                   [SKIP]
            get id 10 addr                      [SKIP]
            get id 20 addr                      [SKIP]
            dump addrs after rm_sf 10           [SKIP]
            dump addrs after rm_addr 20         [SKIP]
    ...
    116 userspace pm create destroy subflow
    ...
            dump addrs subflow                  [SKIP]
            get id 20 addr                      [SKIP]
            dump addrs after rm_sf 20           [SKIP]
    ...
    117 userspace pm create id 0 subflow
            dump addrs id 0 subflow             [SKIP]
    ...
    '''
    
    But in KylinOS, these features are backported before and it should be tested in
    selftests. So revert this commit, and the 'SKIP' should replaced by
    'OK'.
    
    K2CI-Arch: All
    Cc: kernel-stable-team #2503
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Change-Id: Iac5ec67d0f732109b1e2d80ec59944a84d09977b
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/109582
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_join.sh
+++ tools/testing/selftests/net/mptcp/mptcp_join.sh
@@ -3441,7 +3441,7 @@ userspace_pm_chk_dump_addr()
 
 	print_check "dump addrs ${check}"
 
-	if false && mptcp_lib_kallsyms_has "mptcp_userspace_pm_dump_addr$"; then
+	if mptcp_lib_kallsyms_has "mptcp_userspace_pm_dump_addr$"; then
 		check_output "userspace_pm_dump ${ns}" "${exp}"
 	else
 		print_skip
@@ -3456,7 +3456,7 @@ userspace_pm_chk_get_addr()
 
 	print_check "get id ${id} addr"
 
-	if false && mptcp_lib_kallsyms_has "mptcp_userspace_pm_get_addr$"; then
+	if mptcp_lib_kallsyms_has "mptcp_userspace_pm_get_addr$"; then
 		check_output "userspace_pm_get_addr ${ns} ${id}" "${exp}"
 	else
 		print_skip
