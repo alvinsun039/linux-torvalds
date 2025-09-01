7cf6fd523555c0a255259820a0c185e6868b31f0
Jackie Liu <liuyun01@kylinos.cn>
2025-09-01 14:11:08 +0800

    KYLIN: Revert "OPENEULER: ACPI: APEI: send SIGBUS to current task if synchronous memory error not recovered"
    Mainline: KYLIN-only
    Severity: Low
    
    task: 525135
    
    This reverts commit becb19ccd7c6f74b259d0fdf178145a9748ab828.
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ibafa35c89bc3297aca99b3a85893d7af08eab7a2
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/109532
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>

--- drivers/acpi/apei/ghes.c
+++ drivers/acpi/apei/ghes.c
@@ -721,15 +721,6 @@ static bool ghes_do_proc(struct ghes *ghes,
 		}
 	}
 
-	/*
-	 * If no memory failure work is queued for abnormal synchronous
-	 * errors, do a force kill.
-	 */
-	if (sync && !queued) {
-		pr_err("Sending SIGBUS to current task due to memory error not recovered");
-		force_sig(SIGBUS);
-	}
-
 	return queued;
 }
 
