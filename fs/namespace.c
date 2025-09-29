dbe66444f6ef6ad70bc7b62753e62d01d0d6fc22
Christian Brauner <brauner@kernel.org>
2025-09-29 11:41:16 +0200

    mount: handle NULL values in mnt_ns_release()
    Mainline: 6c7ca6a02f8f9549a438a08a23c6327580ecf3d6
    From: v6.18-rc1
    Severity: Moderate
    CVE: CVE-2025-40195
    
    task: 554283
    
    When calling in listmount() mnt_ns_release() may be passed a NULL
    pointer. Handle that case gracefully.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (backported from commit 6c7ca6a02f8f9549a438a08a23c6327580ecf3d6)
    
    K2CI-Arch: All
    Cc: kernel-stable-team #2503 #KLAD
    Change-Id: Ib266472002c995b7a25d70c4c1392b9244fbbd6f
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/142304
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- fs/namespace.c
+++ fs/namespace.c
@@ -177,7 +177,7 @@ static void mnt_ns_release(struct mnt_namespace *ns)
 	lockdep_assert_not_held(&mnt_ns_tree_lock.lock);
 
 	/* keep alive for {list,stat}mount() */
-	if (refcount_dec_and_test(&ns->passive)) {
+	if (ns && refcount_dec_and_test(&ns->passive)) {
 		fsnotify_mntns_delete(ns);
 		put_user_ns(ns->user_ns);
 		kfree(ns);
