4d65db13157c69a0d221b3e0af38aeef05782e4a
Xueqin Luo <luoxueqin@kylinos.cn>
2025-10-21 19:37:26 +0800

    PM: hibernate: dynamically allocate crc->unc_len/unc for configurable threads
    Mainline: e114e2eb7e85b5cc737ff5286cfe68a58caffeba
    Severity: Moderate
    From: 6.19-rc1
    
    task: 550695
    
    Convert crc->unc_len and crc->unc from fixed-size arrays to dynamically
    allocated arrays, sized according to the actual number of threads selected
    at runtime. This removes the fixed limit imposed by CMP_THREADS.
    
    Signed-off-by: Xueqin Luo <luoxueqin@kylinos.cn>
    Link: https://patch.msgid.link/b5db63bb95729482d2649b12d3a11cb7547b7fcc.1761046167.git.luoxueqin@kylinos.cn
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Change-Id: I42f5c8d4bf353d7e42871f7dc14e867bda5852b8
    Signed-off-by: Xueqin Luo <luoxueqin@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/139902
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/power/swap.c
+++ kernel/power/swap.c
@@ -588,9 +588,47 @@ struct crc_data {
 	wait_queue_head_t go;                     /* start crc update */
 	wait_queue_head_t done;                   /* crc update done */
 	u32 *crc32;                               /* points to handle's crc32 */
-	size_t *unc_len[LZO_THREADS];             /* uncompressed lengths */
-	unsigned char *unc[LZO_THREADS];          /* uncompressed data */
+	size_t **unc_len;						  /* uncompressed lengths */
+	unsigned char **unc;					  /* uncompressed data */
 };
+
+static struct crc_data *alloc_crc_data(int nr_threads)
+{
+	struct crc_data *crc;
+
+	crc = kzalloc(sizeof(*crc), GFP_KERNEL);
+	if (!crc)
+		return NULL;
+
+	crc->unc = kcalloc(nr_threads, sizeof(*crc->unc), GFP_KERNEL);
+	if (!crc->unc)
+		goto err_free_crc;
+
+	crc->unc_len = kcalloc(nr_threads, sizeof(*crc->unc_len), GFP_KERNEL);
+	if (!crc->unc_len)
+		goto err_free_unc;
+
+	return crc;
+
+err_free_unc:
+	kfree(crc->unc);
+err_free_crc:
+	kfree(crc);
+	return NULL;
+}
+
+static void free_crc_data(struct crc_data *crc)
+{
+	if (!crc)
+		return;
+
+	if (crc->thr)
+		kthread_stop(crc->thr);
+
+	kfree(crc->unc_len);
+	kfree(crc->unc);
+	kfree(crc);
+}
 
 /*
  * CRC32 update function that runs in its own thread.
@@ -710,7 +748,7 @@ static int save_image_lzo(struct swap_map_handle *handle,
 		goto out_clean;
 	}
 
-	crc = kzalloc(sizeof(*crc), GFP_KERNEL);
+	crc = alloc_crc_data(nr_threads);
 	if (!crc) {
 		pr_err("Failed to allocate crc\n");
 		ret = -ENOMEM;
@@ -859,11 +897,7 @@ out_finish:
 	swsusp_show_speed(start, stop, nr_to_write, "Wrote");
 out_clean:
 	hib_finish_batch(&hb);
-	if (crc) {
-		if (crc->thr)
-			kthread_stop(crc->thr);
-		kfree(crc);
-	}
+	free_crc_data(crc);
 	if (data) {
 		for (thr = 0; thr < nr_threads; thr++)
 			if (data[thr].thr)
@@ -1200,7 +1234,7 @@ static int load_image_lzo(struct swap_map_handle *handle,
 		goto out_clean;
 	}
 
-	crc = kzalloc(sizeof(*crc), GFP_KERNEL);
+	crc = alloc_crc_data(nr_threads);
 	if (!crc) {
 		pr_err("Failed to allocate crc\n");
 		ret = -ENOMEM;
@@ -1453,11 +1487,7 @@ out_clean:
 	hib_finish_batch(&hb);
 	for (i = 0; i < ring_size; i++)
 		free_page((unsigned long)page[i]);
-	if (crc) {
-		if (crc->thr)
-			kthread_stop(crc->thr);
-		kfree(crc);
-	}
+	free_crc_data(crc);
 	if (data) {
 		for (thr = 0; thr < nr_threads; thr++)
 			if (data[thr].thr)
