19d7cf55b3a0e7a37c4577808f098a355ee37953
Yang Xiuwei <yangxiuwei@kylinos.cn>
2025-11-06 09:14:41 +0800

    KYLIN: Revert "io_uring/net: commit partial buffers on retry"
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 526909
    
    This reverts commit 4ef926f69e9ec1df82921593a0f0072f40fb05c3.
    
    Change-Id: I32ccebd964b05b252166512b0fef4e37f2c38860
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151263
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- io_uring/net.c
+++ io_uring/net.c
@@ -366,13 +366,6 @@ static int io_setup_async_addr(struct io_kiocb *req,
 	return -EAGAIN;
 }
 
-static void io_net_kbuf_recyle(struct io_kiocb *req)
-{
-	req->flags |= REQ_F_PARTIAL_IO;
-	if (req->flags & REQ_F_BUFFER_RING)
-		io_kbuf_recycle_ring(req);
-}
-
 int io_sendmsg_prep_async(struct io_kiocb *req)
 {
 	int ret;
@@ -465,7 +458,7 @@ int io_sendmsg(struct io_kiocb *req, unsigned int issue_flags)
 			kmsg->msg.msg_controllen = 0;
 			kmsg->msg.msg_control = NULL;
 			sr->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return io_setup_async_msg(req, kmsg, issue_flags);
 		}
 		if (ret == -ERESTARTSYS)
@@ -544,7 +537,7 @@ int io_send(struct io_kiocb *req, unsigned int issue_flags)
 			sr->len -= ret;
 			sr->buf += ret;
 			sr->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return io_setup_async_addr(req, &__address, issue_flags);
 		}
 		if (ret == -ERESTARTSYS)
@@ -916,7 +909,7 @@ retry_multishot:
 		}
 		if (ret > 0 && io_net_retry(sock, flags)) {
 			sr->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return io_setup_async_msg(req, kmsg, issue_flags);
 		}
 		if (ret == -ERESTARTSYS)
@@ -1017,7 +1010,7 @@ retry_multishot:
 			sr->len -= ret;
 			sr->buf += ret;
 			sr->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return -EAGAIN;
 		}
 		if (ret == -ERESTARTSYS)
@@ -1261,7 +1254,7 @@ int io_send_zc(struct io_kiocb *req, unsigned int issue_flags)
 			zc->len -= ret;
 			zc->buf += ret;
 			zc->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return io_setup_async_addr(req, &__address, issue_flags);
 		}
 		if (ret == -ERESTARTSYS)
@@ -1332,7 +1325,7 @@ int io_sendmsg_zc(struct io_kiocb *req, unsigned int issue_flags)
 
 		if (ret > 0 && io_net_retry(sock, flags)) {
 			sr->done_io += ret;
-			io_net_kbuf_recyle(req);
+			req->flags |= REQ_F_PARTIAL_IO;
 			return io_setup_async_msg(req, kmsg, issue_flags);
 		}
 		if (ret == -ERESTARTSYS)
