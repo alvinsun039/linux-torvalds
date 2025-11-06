8f4f895e7595bc60c310cbf05d299454c1e55714
Yang Xiuwei <yangxiuwei@kylinos.cn>
2025-11-06 09:48:46 +0800

    KYLIN: Revert "io_uring/rw: commit provided buffer state on async"
    Mainline: KYLIN-only
    Severity: Moderate
    
    task: 526909
    
    This reverts commit 3ab1e59cd9d69446fee6ad0a9e21e8c858b42c7f.
    
    Change-Id: Ia71b5fa1ec185c550a448323c5dea1abed836d7c
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/151264
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- io_uring/rw.c
+++ io_uring/rw.c
@@ -854,8 +854,6 @@ static int __io_read(struct io_kiocb *req, unsigned int issue_flags)
 			goto done;
 		ret = 0;
 	} else if (ret == -EIOCBQUEUED) {
-		req->flags |= REQ_F_PARTIAL_IO;
-		io_kbuf_recycle(req, issue_flags);
 		if (iovec)
 			kfree(iovec);
 		return IOU_ISSUE_SKIP_COMPLETE;
@@ -878,9 +876,6 @@ static int __io_read(struct io_kiocb *req, unsigned int issue_flags)
 		ret = ret > 0 ? ret : ret2;
 		goto done;
 	}
-
-	req->flags |= REQ_F_PARTIAL_IO;
-	io_kbuf_recycle(req, issue_flags);
 
 	io = req->async_data;
 	s = &io->s;
@@ -1090,11 +1085,6 @@ int io_write(struct io_kiocb *req, unsigned int issue_flags)
 		ret2 = loop_rw_iter(WRITE, rw, &s->iter);
 	else
 		ret2 = -EINVAL;
-
-	if (ret2 == -EIOCBQUEUED) {
-		req->flags |= REQ_F_PARTIAL_IO;
-		io_kbuf_recycle(req, issue_flags);
-	}
 
 	if (req->flags & REQ_F_REISSUE) {
 		req->flags &= ~REQ_F_REISSUE;
