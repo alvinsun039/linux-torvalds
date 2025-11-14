136d666d8e995284c6e01160a583a05f3b9c2c1a
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-11-14 13:59:55 -0500

    kho: always expose output FDT in debugfs
    Mainline: e268689a528288d5629ee017630186327403cc51
    From: v6.18-rc5-143-ge268689a5282
    Severity: Low
    
    Currently, the output FDT is added to debugfs only when KHO is finalized
    and removed when aborted.
    
    There is no need to hide the FDT based on the state.  Always expose it
    starting from initialization.  This aids the transition toward removing
    the explicit abort functionality and converting KHO to be fully stateless.
    
    Link: https://lkml.kernel.org/r/20251114190002.3311679-7-pasha.tatashin@soleen.com
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Pratyush Yadav <pratyush@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Coiby Xu <coxu@redhat.com>
    Cc: Dave Vasilevsky <dave@vasilevsky.ca>
    Cc: Eric Biggers <ebiggers@google.com>
    Cc: Kees Cook <kees@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit e268689a528288d5629ee017630186327403cc51)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: I2cf98f47a77146b70f3bd51b014fda9b8706ed76
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/155112
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -1230,8 +1230,6 @@ int kho_abort(void)
 	__kho_abort();
 	kho_out.finalized = false;
 
-	kho_debugfs_fdt_remove(&kho_out.dbg, kho_out.fdt);
-
 	return 0;
 }
 
@@ -1301,9 +1299,6 @@ int kho_finalize(void)
 		return ret;
 
 	kho_out.finalized = true;
-
-	WARN_ON_ONCE(kho_debugfs_fdt_add(&kho_out.dbg, "fdt",
-					 kho_out.fdt, true));
 
 	return 0;
 }
@@ -1400,6 +1395,9 @@ static __init int kho_init(void)
 		     pfn += pageblock_nr_pages)
 			init_cma_reserved_pageblock(pfn_to_page(pfn));
 	}
+
+	WARN_ON_ONCE(kho_debugfs_fdt_add(&kho_out.dbg, "fdt",
+					 kho_out.fdt, true));
 
 	return 0;
 
