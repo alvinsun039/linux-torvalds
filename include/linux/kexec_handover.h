085f70b97d92bbde2f975b51d88ea95263bcfb24
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-11-14 14:00:01 -0500

    kho: allow memory preservation state updates after finalization
    Mainline: de51999e687c70a41997124b43291f84324c7924
    From: v6.18-rc5-149-gde51999e687c
    Severity: Low
    
    Currently, kho_preserve_* and kho_unpreserve_* return -EBUSY if KHO is
    finalized.  This enforces a rigid "freeze" on the KHO memory state.
    
    With the introduction of re-entrant finalization, this restriction is no
    longer necessary.  Users should be allowed to modify the preservation set
    (e.g., adding new pages or freeing old ones) even after an initial
    finalization.
    
    The intended workflow for updates is now:
    1. Modify state (preserve/unpreserve).
    2. Call kho_finalize() again to refresh the serialized metadata.
    
    Remove the kho_out.finalized checks to enable this dynamic behavior.
    
    This also allows to convert kho_unpreserve_* functions to void, as they do
    not return any error anymore.
    
    Link: https://lkml.kernel.org/r/20251114190002.3311679-13-pasha.tatashin@soleen.com
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Pratyush Yadav <pratyush@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Coiby Xu <coxu@redhat.com>
    Cc: Dave Vasilevsky <dave@vasilevsky.ca>
    Cc: Eric Biggers <ebiggers@google.com>
    Cc: Kees Cook <kees@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (backported from commit de51999e687c70a41997124b43291f84324c7924)
    
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
    Change-Id: Ia2ce2d59e539c5ab6522d8deee1d31ec2da2d0ab
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/155118
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>

--- include/linux/kexec_handover.h
+++ include/linux/kexec_handover.h
@@ -43,11 +43,11 @@ struct kho_vmalloc {
 bool kho_is_enabled(void);
 
 int kho_preserve_folio(struct folio *folio);
-int kho_unpreserve_folio(struct folio *folio);
+void kho_unpreserve_folio(struct folio *folio);
 int kho_preserve_pages(struct page *page, unsigned int nr_pages);
-int kho_unpreserve_pages(struct page *page, unsigned int nr_pages);
+void kho_unpreserve_pages(struct page *page, unsigned int nr_pages);
 int kho_preserve_vmalloc(void *ptr, struct kho_vmalloc *preservation);
-int kho_unpreserve_vmalloc(struct kho_vmalloc *preservation);
+void kho_unpreserve_vmalloc(struct kho_vmalloc *preservation);
 void *kho_alloc_preserve(size_t size);
 void kho_unpreserve_free(void *mem);
 void kho_restore_free(void *mem);
@@ -73,20 +73,14 @@ static inline int kho_preserve_folio(struct folio *folio)
 	return -EOPNOTSUPP;
 }
 
-static inline int kho_unpreserve_folio(struct folio *folio)
-{
-	return -EOPNOTSUPP;
-}
+static inline void kho_unpreserve_folio(struct folio *folio) { }
 
 static inline int kho_preserve_pages(struct page *page, unsigned int nr_pages)
 {
 	return -EOPNOTSUPP;
 }
 
-static inline int kho_unpreserve_pages(struct page *page, unsigned int nr_pages)
-{
-	return -EOPNOTSUPP;
-}
+static inline void kho_unpreserve_pages(struct page *page, unsigned int nr_pages) { }
 
 static inline int kho_preserve_vmalloc(void *ptr,
 				       struct kho_vmalloc *preservation)
@@ -94,10 +88,7 @@ static inline int kho_preserve_vmalloc(void *ptr,
 	return -EOPNOTSUPP;
 }
 
-static inline int kho_unpreserve_vmalloc(struct kho_vmalloc *preservation)
-{
-	return -EOPNOTSUPP;
-}
+static inline void kho_unpreserve_vmalloc(struct kho_vmalloc *preservation) { }
 
 static inline void *kho_alloc_preserve(size_t size)
 {
