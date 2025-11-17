410af843b969b9fecb8c2f5c7ca9bfe573028e5e
Paolo Bonzini <pbonzini@redhat.com>
2025-11-17 11:23:49 +0800

    KVM: guest_memfd: Add hook for initializing memory
    Mainline: 3bb2531e20bff99f9cd8719ee7aea8bd82687173
    From: v6.11-rc1
    Severity: Low
    
    task: 451663
    
    guest_memfd pages are generally expected to be in some arch-defined
    initial state prior to using them for guest memory. For SEV-SNP this
    initial state is 'private', or 'guest-owned', and requires additional
    operations to move these pages into a 'private' state by updating the
    corresponding entries the RMP table.
    
    Allow for an arch-defined hook to handle updates of this sort, and go
    ahead and implement one for x86 so KVM implementations like AMD SVM can
    register a kvm_x86_ops callback to handle these updates for SEV-SNP
    guests.
    
    The preparation callback is always called when allocating/grabbing
    folios via gmem, and it is up to the architecture to keep track of
    whether or not the pages are already in the expected state (e.g. the RMP
    table in the case of SEV-SNP).
    
    In some cases, it is necessary to defer the preparation of the pages to
    handle things like in-place encryption of initial guest memory payloads
    before marking these pages as 'private'/'guest-owned'.  Add an argument
    (always true for now) to kvm_gmem_get_folio() that allows for the
    preparation callback to be bypassed.  To detect possible issues in
    the way userspace initializes memory, it is only possible to add an
    unprepared page if it is not already included in the filemap.
    
    Link: https://lore.kernel.org/lkml/ZLqVdvsF11Ddo7Dq@google.com/
    Co-developed-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Message-Id: <20231230172351.574091-5-michael.roth@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    K2CI-Arch: All
    Signed-off-by: 张浩 <zhanghao1@kylinos.cn>
    Change-Id: I68e7f102956fb00e6bb244d6b24dae661ffe07f4
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/142573
    Reviewed-by: 曾驰 <zengchi@kylinos.cn>
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>
    Code-Style: kernel-bot <kernel-bot@kylinos.cn>
    Tested-by: kernel-bot <kernel-bot@kylinos.cn>
    Reviewed-by: 江锋 <jiangfeng@kylinos.cn>
    Reviewed-by: 李唯实 <liweishi@kylinos.cn>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1802,6 +1802,7 @@ struct kvm_x86_ops {
 	int (*control_post_system_reset)(struct kvm *kvm);
 
 	gva_t (*get_untagged_addr)(struct kvm_vcpu *vcpu, gva_t gva, unsigned int flags);
+	int (*gmem_prepare)(struct kvm *kvm, kvm_pfn_t pfn, gfn_t gfn, int max_order);
 };
 
 struct kvm_x86_nested_ops {
