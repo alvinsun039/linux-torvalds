d81ecdaec29d07ff0ed099e714a7c9094e78e1b0
Marc Kleine-Budde <mkl@pengutronix.de>
2025-12-23 21:21:39 +0100

    can: gs_usb: gs_usb_receive_bulk_callback(): fix URB memory leak
    Mainline: 7352e1d5932a0e777e39fa4b619801191f57e603
    From: v6.19-rc6
    Severity: Moderate
    
    task: 594541
    
    commit 7352e1d5932a0e777e39fa4b619801191f57e603 upstream.
    
    In gs_can_open(), the URBs for USB-in transfers are allocated, added to the
    parent->rx_submitted anchor and submitted. In the complete callback
    gs_usb_receive_bulk_callback(), the URB is processed and resubmitted. In
    gs_can_close() the URBs are freed by calling
    usb_kill_anchored_urbs(parent->rx_submitted).
    
    However, this does not take into account that the USB framework unanchors
    the URB before the complete function is called. This means that once an
    in-URB has been completed, it is no longer anchored and is ultimately not
    released in gs_can_close().
    
    Fix the memory leak by anchoring the URB in the
    gs_usb_receive_bulk_callback() to the parent->rx_submitted anchor.
    
    Fixes: d08e973a77d1 ("can: gs_usb: Added support for the GS_USB CAN devices")
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20260105-gs_usb-fix-memory-leak-v2-1-cc6ed6438034@pengutronix.de
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (backported from commit f905bcfa971edb89e398c98957838d8c6381c0c7)
    
    K2CI-Arch: None
    Signed-off-by: k2ci <kernel-bot@kylinos.cn>
    Change-Id: If2e137f8252a9a75bcf9e75780e05463a5175e06
    Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/174489
    Reviewed-by: 刘云 <liuyun01@kylinos.cn>

--- drivers/net/can/usb/gs_usb.c
+++ drivers/net/can/usb/gs_usb.c
@@ -687,6 +687,8 @@ resubmit_urb:
 			  hf, dev->parent->hf_size_rx,
 			  gs_usb_receive_bulk_callback, parent);
 
+	usb_anchor_urb(urb, &parent->rx_submitted);
+
 	rc = usb_submit_urb(urb, GFP_ATOMIC);
 
 	/* USB failure take down all interfaces */
