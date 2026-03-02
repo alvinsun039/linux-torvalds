ekuiter/torte-linux

ata: libata-core: Disable LPM on Silicon Motion MD619{H,G}XCLDE3TC
According to a user report, the Silicon Motion MD619HXCLDE3TC SSD and
the Silicon Motion MD619GXCLDE3TC SSD have problems with LPM.

Reported-by: Yihang Li <liyihang9@h-partners.com>
Closes: https://lore.kernel.org/linux-ide/20251121073502.3388239-1-liyihang9@h-partners.com/
Signed-off-by: Niklas Cassel <cassel@kernel.org>

drivers/ata/libata-core.c
@@ -4209,6 +4209,10 @@ static const struct ata_dev_quirks_entry __ata_dev_quirks[] = {
 	/* Apacer models with LPM issues */
 	{ "Apacer AS340*",		NULL,	ATA_QUIRK_NOLPM },
 
+	/* Silicon Motion models with LPM issues */
+	{ "MD619HXCLDE3TC",		"TCVAID", ATA_QUIRK_NOLPM },
+	{ "MD619GXCLDE3TC",		"TCV35D", ATA_QUIRK_NOLPM },
+
 	/* These specific Samsung models/firmware-revs do not handle LPM well */
 	{ "SAMSUNG MZMPC128HBFU-000MV", "CXM14M1Q", ATA_QUIRK_NOLPM },
 	{ "SAMSUNG SSD PM830 mSATA *",  "CXM13D1Q", ATA_QUIRK_NOLPM },

alvinsun039/linux-torvalds

Mainline: 6ce0dd9f54ea9773c0aedfaab7b858fc68a848ba
From: v6.19-rc1
Severity: Important
(backported from commit 6ce0dd9f54ea9773c0aedfaab7b858fc68a848ba)
Cc: kernel-stable-team #2503+
Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>
Change-Id: Ic12f9380611fe666e16bf1a58b13f32069bcc9f8
Reviewed-on: https://code.kylinos.cn:443/c/institute/kernel/klinux/+/172288
Tested-by: kernel-bot <kernel-bot@kylinos.cn>

drivers/ata/libata-core.c
@@ -4153,6 +4153,10 @@ static const struct ata_blacklist_entry ata_device_blacklist [] = {
 						ATA_HORKAGE_ZERO_AFTER_TRIM |
 						ATA_HORKAGE_NOLPM },
 
+	/* Silicon Motion models with LPM issues */
+	{ "MD619HXCLDE3TC",		"TCVAID", ATA_HORKAGE_NOLPM },
+	{ "MD619GXCLDE3TC",		"TCV35D", ATA_HORKAGE_NOLPM },
+
 	/* These specific Samsung models/firmware-revs do not handle LPM well */
 	{ "SAMSUNG MZMPC128HBFU-000MV", "CXM14M1Q", ATA_HORKAGE_NOLPM },
 	{ "SAMSUNG SSD PM830 mSATA *",  "CXM13D1Q", ATA_HORKAGE_NOLPM },