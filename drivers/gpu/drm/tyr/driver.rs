eaaac60e560dfe1cf42ceb7ceb417c8e739d09e3
Deborah Brouwer <deborah.brouwer@collabora.com>
2026-03-18 15:04:07 -0700

    fix_imports

--- drivers/gpu/drm/tyr/driver.rs
+++ drivers/gpu/drm/tyr/driver.rs
@@ -16,7 +16,11 @@ use kernel::{
         DmaMask, //
     },
     drm,
-    drm::ioctl,
+    drm::{
+        driver::Registration,
+        ioctl,
+        UnregisteredDevice, //
+    },
     io::{
         poll,
         Io, //
@@ -152,8 +156,8 @@ impl platform::Driver for TyrPlatformDriverData {
                 gpu_info,
         });
 
-        let tdev = drm::UnregisteredDevice::<TyrDrmDriver>::new(pdev.as_ref(), data)?;
-        let tdev = drm::driver::Registration::new_foreign_owned(tdev, pdev.as_ref(), 0)?;
+        let tdev = UnregisteredDevice::<TyrDrmDriver>::new(pdev.as_ref(), data)?;
+        let tdev = Registration::new_foreign_owned(tdev, pdev.as_ref(), 0)?;
 
         let driver = TyrPlatformDriverData {
             _device: tdev.into(),
